bd2414081a63b5b96d2ea4b7c8e7634bff2364d2
jjgomezcadenas <jjgomezcadenas@gmail.com>
2016-12-20 17:01:53 +0100

    Remove slow marks from db tests
    data base tests are no longer slow

--- tests/test_db.py
+++ tests/test_db.py
@@ -6,19 +6,19 @@ import os
 
 from pytest import fixture, mark
 
-@mark.slow
+
 def test_numberOfPMTs():
     """Check that we retrieve the correct number of PMTs."""
     pmts = DB.DataPMT()
     assert pmts.shape[0] == 12
 
-@mark.slow
+
 def test_numberOfSiPMs():
     """Check that we retrieve the correct number of SiPMs."""
     sipms = DB.DataSiPM()
     assert sipms.shape[0] == 1792
 
-@mark.slow
+
 def test_SiPMNoise():
     """Check we have noise for all SiPMs and energy of each bin."""
     noise, energy, baseline = DB.SiPMNoise()
@@ -26,7 +26,7 @@ def test_SiPMNoise():
     assert noise.shape[0] == 1792
     assert noise.shape[1] == energy.shape[0]
 
-@mark.slow
+
 def test_DetectorGeometry():
     """Check Detector Geometry."""
     geo = DB.DetectorGeo()
