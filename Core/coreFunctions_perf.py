961357271d17c1d04669a2fb431f3871316fac45
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2016-12-21 21:02:49 +0100

    Cleaning up after JJ's terrorist attack
    jacg thinks that the meaningful parts of the origial commit messages
    were:
    
    ===================================================================
    
    Tests, performance and fixes
    
    1. from future import division does not compile in MAC (jacg doubts that
       this is true)
    2. added a faster (python) rebin function
    3. added test for rebin functions
    4. removed subtract_baseline (obsolete and unreliable)
    5. added a performance test (coreFunctions_perf.py)
    
    Commented out "true PMT and SiPM" tables
    
    “True PMT and SiPM” are not being used currently. Commented out to
    boost performance.
    
    ====================================================================
    
    On top of that, jacg
    
    1. Restorted python 3.5 to travis.
    
    2. Fixed the spelling of the future division import which was causing
       the compilation failure.
    
    3. Fixed some problems with float-int type mismatch in the Cython code.
    
    4. Fixed some issues with truedivison in Python3.
    
    5. Fixed a relative import in the Cython version.

---
+++ Core/coreFunctions_perf.py
@@ -0,0 +1,31 @@
+
+"""
+Performance of some coreFunctions
+"""
+
+from time import time
+import numpy as np
+import wfmFunctions as wfm
+import ICython.Core.peakFunctions as cpf
+
+
+t = np.arange(1.,100., 0.1, dtype=np.double)
+e = np.exp(-t/t**2)
+
+t0 = time()
+T, E = wfm.rebin_wf(t, e, stride=2)
+t1 = time()
+dt = t1 - t0
+print("rebin_wf (wfmFunctions) run in {} s".format(dt))
+
+t0 = time()
+T, E = wfm.rebin_waveform(t, e, stride=2)
+t1 = time()
+dt = t1 - t0
+print("rebin_waveform (wfmFunctions) run in {} s".format(dt))
+
+t0 = time()
+T, E = cpf.rebin_waveform(t, e, stride=2)
+t1 = time()
+dt = t1 - t0
+print("cython rebin_waveform (ICython/Core/peakFunctions) run in {} s".format(dt))
