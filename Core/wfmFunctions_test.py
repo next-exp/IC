40bc8eb99769762907e2dc645b5a825f6d27fc3a
jjgomezcadenas <jjgomezcadenas@gmail.com>
2016-12-22 02:39:13 +0100

    adding a small test line

--- Core/wfmFunctions_test.py
+++ Core/wfmFunctions_test.py
@@ -36,4 +36,5 @@ def test_rebin_wf2(t):
     e = np.exp(-t/t**2)
 
     T, E   = wfm.rebin_waveform(t, e, stride=10)
+    np.testing.assert_allclose(T, T2, rtol=1e-5, atol=1e-5)
     np.testing.assert_allclose(np.sum(e), np.sum(E), rtol=1e-5, atol=1e-5)
