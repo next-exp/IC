508864f504a0fc47bc542623e50cbcb05a3ba33b
jjgomezcadenas <jjgomezcadenas@gmail.com>
2016-12-22 01:37:39 +0100

    Tests, performance and fixes
    1. from future import division does not compile in MAC
    2. added a faster (python) rebin function
    3. added test for rebin functions
    4. removed subtract_baseline (obsolete and unreliable)
    5. added a performance test (coreFunctions_perf.py)

--- Core/wfmFunctions_test.py
+++ Core/wfmFunctions_test.py
@@ -1,9 +1,39 @@
-from hypothesis import given
-from hypothesis.extra.numpy import arrays, np
 
-from .wfmFunctions import subtract_baseline
+from hypothesis import given, assume
+from hypothesis.strategies import lists, integers, floats
+from hypothesis.extra.numpy import arrays
 
-@given(arrays(float, (10,20)))
-def test_subtract_baseline(waveforms):
-    subtract_baseline(waveforms)
-    assert 'Did not crash'
+import wfmFunctions as wfm
+import ICython.Core.peakFunctions as cpf
+import numpy as np
+
+
+def ndarrays_of_shape(shape, lo=-1000.0, hi=1000.0):
+    return arrays('float64', shape=shape, elements=floats(min_value=lo,
+                   max_value=hi))
+
+
+def test_rebin_wf():
+    # First, consider a simple test function
+
+    t = np.arange(1.,100., 0.1)
+    e = np.exp(-t/t**2)
+
+    T, E   = wfm.rebin_waveform(t, e, stride=10)
+    T2, E2 = wfm.rebin_wf(t, e, stride=10)
+    T3, E3 = cpf.rebin_waveform(t, e, stride=10)
+    np.testing.assert_allclose(T, T2, rtol=1e-5, atol=0)
+    np.testing.assert_allclose(T, T3, rtol=1e-5, atol=0)
+    np.testing.assert_allclose(E, E2, rtol=1e-5, atol=0)
+    np.testing.assert_allclose(E, E3, rtol=1e-5, atol=0)
+    np.testing.assert_allclose(np.sum(e), np.sum(E), rtol=1e-5, atol=0)
+
+
+@given(t = ndarrays_of_shape(shape=(1), lo=-1000.0, hi=1000.0))
+def test_rebin_wf2(t):
+    # First, consider a simple test function
+
+    e = np.exp(-t/t**2)
+
+    T, E   = wfm.rebin_waveform(t, e, stride=10)
+    np.testing.assert_allclose(np.sum(e), np.sum(E), rtol=1e-5, atol=1e-5)
