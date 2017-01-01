54db898cf2935ac93fba265d10d03b758c58ce9a
“jjgomezcadenas” <jjgomezcadenas@gmail.com>
2017-01-01 14:42:16 -0800

    New version of DIOMIRA (diomira_ms.py)
    This commit includes a new version of DIOMIRA (diomira_ms.py),
    organized as a state-machine, and optimized to minimize storage and
    increase speed. The most importat differences with respect to the
    initial version of the city (DIOMIRA.py) are:
    
    1. diomira_ms simulates the response of the PMT plane, generating RWF
    and BLR waveforms (same as DIOMIRA.py), but saves the BLR functions
    without baseline shift (BLR functions are only used for MC study
    purposes and are to be compared with fully corrected waveforms).
    
    2. diomira_ms does not propagate to the RWF file it writes neither
    the sensor tables (since they are in the DB), nor the MCTrk
    tables (which are not used in practice).
    
    3. No rebinning or saving of the "true waveform", thus saving storage
    and computation time (for comparison studies BLR is enough).
    
    In addition, a number of small changes are introduced in aux modules
    tblFunctions.py and wfmFunctions.py
    
    Finally, two new files are staged, together with the main
    script. These are:
    
    a) diomira_ms.conf in the Config directory, which configures the
    script to run for 10 events, using an input file (assumed to be in the
    tests directory)
    
    Add extensive testing to DIOMIRA
    
    Module diomira_test.py adds extensive testing to DIOMIRA city. The
    tests require reading and writting files (electrons_40keV**) which are
    also committed.
    
    The tests require the use of shell commands inside the script,
    implemented with the os.system module.
    
    DIOMIRA and auxiliary functions are now python-3 compliant
    
    Changes have been made in diomira_ms.py as well as in several
    auxiliary functions to port code to python3. Most of the changes are
    related with map, range etc. returning iterators rather than lists.
    
    Remove RWF file from repository
    
    Only MCRD (input to DIOMIRA) file is necessary for tests. RWF file is
    produced as test ouput and does not need to be in the repository (in
    fact it cannot be, since the file itself changes depending on the
    running conditions, thus resulting in conflicts)

---
+++ invisible_cities/Cities/diomira_ms.py
@@ -0,0 +1,361 @@
+from __future__ import print_function
+import sys
+from glob import glob
+from time import time
+
+import numpy as np
+import tables as tb
+
+import invisible_cities.Core.tblFunctions as tbl
+from invisible_cities.Core.Configure import configure, print_configuration
+from invisible_cities.Core.Nh5 import FEE
+import invisible_cities.Core.mplFunctions as mpl
+import invisible_cities.Core.wfmFunctions as wfm
+from invisible_cities.Core.RandomSampling import NoiseSampler as SiPMsNoiseSampler
+from invisible_cities.Database import loadDB
+import invisible_cities.Sierpe.FEE as FE
+
+
+class Diomira:
+    """
+    The city of DIOMIRA simulates the response of the energy and
+    traking plane sensors.
+
+    """
+    def __init__(self):
+        """
+        -1. Inits the machine
+        -2. Loads the data base to access calibration data
+        -3. Sets all switches to default value
+        """
+
+        DataPMT = loadDB.DataPMT(0)  # argument = 0 implies MC
+        DataSiPM = loadDB.DataSiPM(0)
+
+        self.adc_to_pes = abs(DataPMT.adc_to_pes.values).astype(np.double)
+        self.sipm_adc_to_pes = DataSiPM.adc_to_pes.values.astype(np.double)
+        self.coeff_c = DataPMT.coeff_c.values.astype(np.double)
+        self.coeff_blr = DataPMT.coeff_blr.values.astype(np.double)
+        self.noise_rms = DataPMT.noise_rms.values.astype(np.double)
+
+        self.set_input_files_    = False
+        self.set_output_file_    = False
+        self.set_sipm_noise_cut_ = False
+
+        self.plot_raw = False
+        self.plot_blr = False
+        self.plot_rwf = False
+
+        self.nprint = 1000000
+        self.signal_start=0
+        self.signal_end=1200
+        self.ymax = 200
+
+
+    def simulate_sipm_response(self, event, sipmrd,
+                               sipms_noise_sampler):
+        """Add noise with the NoiseSampler class and return
+        the noisy waveform (in pes)."""
+
+        return sipmrd[event] + sipms_noise_sampler.Sample()
+
+
+    def simulate_pmt_response(self, event, pmtrd):
+        """ Full simulation of the energy plane response
+        Input:
+         1) extensible array pmtrd
+         2) event_number
+
+        returns:
+        array of raw waveforms (RWF) obtained by convoluting pmtrd with the PMT
+        front end electronics (LPF, HPF filters)
+        array of BLR waveforms (only decimation)
+        """
+
+        spe = FE.SPE()  # spe
+        # FEE, with noise PMT
+        fee = FE.FEE(noise_FEEPMB_rms=FE.NOISE_I, noise_DAQ_rms=FE.NOISE_DAQ)
+        NPMT = pmtrd.shape[1]
+        RWF = []
+        BLRX = []
+
+        for pmt in range(NPMT):
+            # normalize calibration constants from DB to MC value
+            cc = self.adc_to_pes[pmt] / FE.ADC_TO_PES
+            # signal_i in current units
+            signal_i = FE.spe_pulse_from_vector(spe, pmtrd[event, pmt])
+            # Decimate (DAQ decimation)
+            signal_d = FE.daq_decimator(FE.f_mc, FE.f_sample, signal_i)
+            # Effect of FEE and transform to adc counts
+            signal_fee = FE.signal_v_fee(fee, signal_d, pmt) * FE.v_to_adc()
+            # add noise daq
+            signal_daq = cc * FE.noise_adc(fee, signal_fee)
+            # signal blr is just pure MC decimated by adc in adc counts
+            signal_blr = cc * FE.signal_v_lpf(fee, signal_d)*FE.v_to_adc()
+            # raw waveform stored with negative sign and offset
+            RWF.append(FE.OFFSET - signal_daq)
+            # blr waveform stored with positive sign and no offset
+            BLRX.append(signal_blr)
+        return np.array(RWF), np.array(BLRX)
+
+    def set_plot(self, plot_raw=False, plot_blr=False,
+                 plot_rwf=False, w_size=800):
+        """Decide what to plot."""
+
+        self.plot_raw     = plot_raw
+        self.plot_blr     = plot_blr
+        self.plot_rwf     = plot_rwf
+        self.w_size       = w_size
+
+
+    def set_print(self, nprint=10):
+        """Print frequency."""
+        self.nprint = nprint
+
+
+    def set_input_files(self, input_files):
+        """Set the input files."""
+        self.input_files = input_files
+        self.set_input_files_ = True
+
+
+    def set_output_file(self, output_file, compression='ZLIB4'):
+        """Set the output file."""
+
+        filename = output_file
+        self.compression = compression
+        self.h5out = tb.open_file(filename, "w",
+                          filters=tbl.filters(compression))
+
+        self.set_output_file_ = True
+
+
+    def set_sipm_noise_cut(self, noise_cut=0.995):
+        self.sipm_noise_cut = noise_cut
+        self.set_sipm_noise_cut_ = True
+
+
+    def store_FEE_table(self):
+        """Store the parameters of the EP FEE simulation."""
+
+        row = self.fee_table.row
+        row["OFFSET"] = FE.OFFSET
+        row["CEILING"] = FE.CEILING
+        row["PMT_GAIN"] = FE.PMT_GAIN
+        row["FEE_GAIN"] = FE.FEE_GAIN
+        row["R1"] = FE.R1
+        row["C1"] = FE.C1
+        row["C2"] = FE.C2
+        row["ZIN"] = FE.Zin
+        row["DAQ_GAIN"] = FE.DAQ_GAIN
+        row["NBITS"] = FE.NBITS
+        row["LSB"] = FE.LSB
+        row["NOISE_I"] = FE.NOISE_I
+        row["NOISE_DAQ"] = FE.NOISE_DAQ
+        row["t_sample"] = FE.t_sample
+        row["f_sample"] = FE.f_sample
+        row["f_mc"] = FE.f_mc
+        row["f_LPF1"] = FE.f_LPF1
+        row["f_LPF2"] = FE.f_LPF2
+        row["coeff_c"] = self.coeff_c
+        row["coeff_blr"] = self.coeff_blr
+        row["adc_to_pes"] = self.adc_to_pes
+        row["pmt_noise_rms"] = self.noise_rms
+        row.append()
+        self.fee_table.flush()
+
+
+    def run(self, nmax):
+        """
+        Run the machine
+        nmax is the max number of events to run
+        """
+
+        n_events_tot = 0
+
+        if self.set_input_files_    == False:
+            raise IOError('must set input files before running')
+        if len(self.input_files)    == 0:
+            raise IOError('input file list is empty')
+        if self.set_output_file_    == False:
+            raise IOError('must set output file before running')
+        if self.set_sipm_noise_cut_ == False:
+            raise IOError('must set sipm_noise_cut before running')
+
+
+        print("""
+                 DIOMIRA will run a max of {} events
+                 Input Files = {}
+                 Output File = {}
+                          """.format(nmax,
+                                     self.input_files,
+                                     self.h5out))
+
+        # loop over input files
+        first=False
+        for ffile in self.input_files:
+
+            print("Opening file {}".format(ffile))
+            filename = ffile
+
+            try:
+                with tb.open_file(filename, "r+") as h5in:
+                    pmtrd  = h5in.root.pmtrd
+                    sipmrd = h5in.root.sipmrd
+
+                    NEVT, NPMT, PMTWL = pmtrd.shape
+                    PMTWL_FEE = int(PMTWL/FE.t_sample)
+                    NEVENTS_DST, NSIPM, SIPMWL = sipmrd.shape
+
+                    print("Events in file = {}".format(NEVENTS_DST))
+
+                    if first == False:
+                        # print configuration, create vectors
+                        # init SiPM noiser, store FEE table
+
+                        print_configuration({"# PMT": NPMT, "PMT WL": PMTWL,
+                                             "PMT WL (FEE)": PMTWL_FEE,
+                                             "# SiPM": NSIPM,
+                                             "SIPM WL": SIPMWL})
+                        # RD group
+                        RD = self.h5out.create_group(self.h5out.root, "RD")
+
+                        # MC group
+                        MC = self.h5out.create_group(self.h5out.root, "MC")
+                        # create a table to store Energy plane FEE
+                        self.fee_table = self.h5out.\
+                             create_table(MC, "FEE", FEE,
+                                          "EP-FEE parameters",
+                                          tbl.filters("NOCOMPR"))
+
+                        # create vectors
+                        self.pmtrwf = self.h5out.\
+                            create_earray(RD,
+                                          "pmtrwf",
+                                          atom=tb.Int16Atom(),
+                                          shape=(0, NPMT, PMTWL_FEE),
+                                          expectedrows=nmax,
+                                          filters=tbl.filters(self.compression))
+
+                        self.pmtblr = self.h5out.\
+                            create_earray(RD,
+                                          "pmtblr",
+                                          atom=tb.Int16Atom(),
+                                          shape=(0, NPMT, PMTWL_FEE),
+                                          expectedrows=nmax,
+                                          filters=tbl.filters(self.compression))
+
+                        self.sipmrwf = self.h5out.\
+                            create_earray(RD,
+                                          "sipmrwf",
+                                          atom=tb.Int16Atom(),
+                                          shape=(0, NSIPM, SIPMWL),
+                                          expectedrows=nmax,
+                                          filters=tbl.filters(self.compression))
+
+                        # Create instance of the noise sampler
+                        self.noise_sampler = SiPMsNoiseSampler(SIPMWL, True)
+                        self.sipms_thresholds = self.\
+                            sipm_noise_cut * self.sipm_adc_to_pes
+
+                        self.store_FEE_table()
+
+                        first = True
+
+                    # loop over events in the file. Break when nmax is reached
+                    for evt in range(NEVT):
+                        # simulate PMT and SiPM response
+
+                        dataPMT, blrPMT = self.simulate_pmt_response(evt,
+                                                                     pmtrd)
+                        self.pmtrwf.\
+                            append(dataPMT.astype(int).reshape(1, NPMT,
+                                                               PMTWL_FEE))
+                        self.pmtblr.\
+                            append(blrPMT.astype(int).reshape(1, NPMT,
+                                                              PMTWL_FEE))
+                        # in PES
+                        dataSiPM = self.\
+                        simulate_sipm_response(evt,
+                                               sipmrd,
+                                               self.noise_sampler)
+                        # in adc counts
+                        dataSiPM = wfm.to_adc(dataSiPM, self.sipm_adc_to_pes)
+                        dataSiPM = wfm.noise_suppression(dataSiPM,
+                                                         self.sipms_thresholds)
+
+                        self.sipmrwf.\
+                            append(dataSiPM.astype(int).reshape(1, NSIPM,
+                                                                SIPMWL))
+
+
+                        if self.plot_raw:
+                            print('plot raw')
+                            mpl.plot_waveforms(pmtrd[evt], maxlen=0, zoom=True,
+                                                window_size=self.w_size*40)
+                            plt.show()
+                            raw_input('->')
+
+                        if self.plot_rwf:
+                            print('plot rwf')
+                            mpl.plot_waveforms(dataPMT, maxlen=0, zoom=True,
+                                                window_size=self.w_size)
+                            plt.show()
+                            raw_input('->')
+
+                        if self.plot_blr:
+                            print('plot blr')
+                            mpl.plot_waveforms(blrPMT, maxlen=0, zoom=True,
+                                                window_size=self.w_size)
+                            plt.show()
+                            raw_input('->')
+
+                        n_events_tot +=1
+                        if n_events_tot%self.nprint == 0:
+                            print('event in file = {}, total = {}'.\
+                              format(evt, n_events_tot))
+
+                        if n_events_tot >= nmax and nmax > -1:
+                            print('reached maximum number of events (={})'.\
+                                  format(nmax))
+                            break
+
+
+            except:
+                print('error')
+                raise
+
+
+        self.pmtrwf.flush()
+        self.sipmrwf.flush()
+        self.pmtblr.flush()
+        self.h5out.close()
+
+        return n_events_tot
+
+
+def DIOMIRA(argv=sys.argv):
+    """DIOMIRA DRIVER"""
+
+    CFP = configure(argv)
+    fpp = Diomira()
+    files_in = glob(CFP['FILE_IN'])
+    files_in.sort()
+    fpp.set_input_files(files_in)
+    fpp.set_output_file(CFP['FILE_OUT'],
+                        compression=CFP['COMPRESSION'])
+    fpp.set_print(nprint=CFP['NPRINT'])
+
+    fpp.set_sipm_noise_cut(noise_cut=CFP["NOISE_CUT"])
+
+    nevts = CFP['NEVENTS'] if not CFP['RUN_ALL'] else -1
+    t0 = time()
+    nevt = fpp.run(nmax=nevts)
+    t1 = time()
+    dt = t1 - t0
+
+    print("run {} evts in {} s, time/event = {}".format(nevt, dt, dt/nevt))
+
+
+if __name__ == "__main__":
+    DIOMIRA(sys.argv)
