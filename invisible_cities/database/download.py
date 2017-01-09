b06884102c1aa716b25cee21d2e4f92377435fe8
Jacek Generowicz <github@my-post-office.net>
2017-01-09 20:15:23 +0100

    Swapped ICDIR and ICTDIR
    The T in ICTDIR is supposed to stand for TOP

--- invisible_cities/database/download.py
+++ invisible_cities/database/download.py
@@ -7,7 +7,7 @@ from base64 import b64decode as dec
 
 
 def loadDB():
-    dbfile = os.environ['ICTDIR'] + '/database/localdb.sqlite3'
+    dbfile = os.environ['ICDIR'] + '/database/localdb.sqlite3'
     try:
         os.remove(dbfile)
     except:
