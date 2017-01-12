e4abeecd1c1fe36e6083269b9a4f1435c761204c
Jacek Generowicz <github@my-post-office.net>
2017-01-12 18:25:34 +0100

    Make DB test and implementation names consistent
    loadDB_test.py -> load_db_test.py to match load_db.py

---
+++ invisible_cities/database/load_db_test.py
@@ -0,0 +1,39 @@
+import tables
+from invisible_cities.database import download
+import invisible_cities.database.load_db as DB
+import os
+
+
+from pytest import fixture, mark
+
+
+def test_numberOfPMTs():
+    """Check that we retrieve the correct number of PMTs."""
+    pmts = DB.DataPMT()
+    assert pmts.shape[0] == 12
+
+
+def test_numberOfSiPMs():
+    """Check that we retrieve the correct number of SiPMs."""
+    sipms = DB.DataSiPM()
+    assert sipms.shape[0] == 1792
+
+
+def test_SiPMNoise():
+    """Check we have noise for all SiPMs and energy of each bin."""
+    noise, energy, baseline = DB.SiPMNoise()
+    assert noise.shape[0] == baseline.shape[0]
+    assert noise.shape[0] == 1792
+    assert noise.shape[1] == energy.shape[0]
+
+
+def test_DetectorGeometry():
+    """Check Detector Geometry."""
+    geo = DB.DetectorGeo()
+    assert geo['XMIN'][0] == -198
+    assert geo['XMAX'][0] ==  198
+    assert geo['YMIN'][0] == -198
+    assert geo['YMAX'][0] ==  198
+    assert geo['ZMIN'][0] ==    0
+    assert geo['ZMAX'][0] ==  532
+    assert geo['RMAX'][0] ==  198
