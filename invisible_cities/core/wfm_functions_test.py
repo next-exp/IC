374a2ea656e9fe4d2d43b6c4dcc3904a85e7cacb
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-01-24 14:15:02 +0100

    Add utility functions and tests. Minor refactoring
    Two utility functions and a plotting function are added to core.
    
    cwf_from_rwf() creates corrected waveforms from raw waveforms for a
    list of events.
    
    compare_cwf_blr() returns the differences between the CWF and the
    BLR. This difference must be small, since the CWF reproduce the BLR.
    
    Both functions are tested by compare_cwf_blr_test() which:
    1) Computes CWF from RWF (function cwf_from_rwf())
        2) Computes the difference between CWF and BLR (compare_cwf_blr())
        3) Asserts that the differences are small.
        For 10 events and 12 PMTs per event, all differences are less than
        0.1 %
    
    The test requires an input file: electrons_40keV_z250_RWF.h5, which
    has been added to repository (it writes nothing).
    
    In addition, plotting function plot_wfa_wfb() has been
    improved (adding labels) and plotting function
    plot_s2si_map(S2Si) has been moved to pmaps_functions.
    
    Finally assorted minor changes to improve readibility of code

--- invisible_cities/core/wfm_functions_test.py
+++ invisible_cities/core/wfm_functions_test.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
-
+import os
 from hypothesis import given, assume
 from hypothesis.strategies import lists, integers, floats
 from hypothesis.extra.numpy import arrays
 
 from . import wfm_functions as wfm
 import invisible_cities.core.peak_functions_c as cpf
+import invisible_cities.core.tbl_functions as tbl
 import numpy as np
+import tables as tb
 
 
 def ndarrays_of_shape(shape, lo=-1000.0, hi=1000.0):
@@ -17,8 +19,8 @@ def ndarrays_of_shape(shape, lo=-1000.0, hi=1000.0):
 def test_rebin_wf():
     # First, consider a simple test function
 
-    t = np.arange(1.,100., 0.1)
-    e = np.exp(-t/t**2)
+    t = np.arange(1, 100, 0.1)
+    e = np.exp(-t / t ** 2)
 
     T, E   = wfm.rebin_waveform(t, e, stride=10)
     T2, E2 = wfm.rebin_wf(t, e, stride=10)
@@ -35,10 +37,33 @@ def test_rebin_wf2(t):
     """ test using automatic array generation"""
     # First, consider a simple test function
 
-    e = np.exp(-t/t**2)
+    e = np.exp(-t / t ** 2)
 
     T, E   = wfm.rebin_waveform(t, e, stride=10)
     # np.testing.assert_allclose(T, T2, rtol=1e-5, atol=1e-5)
     # np.testing.assert_allclose(T, T3, rtol=1e-5, atol=1e-5)
     # np.testing.assert_allclose(E, E2, rtol=1e-5, atol=1e-5)
     np.testing.assert_allclose(np.sum(e), np.sum(E), rtol=1e-5, atol=1e-5)
+
+
+def compare_cwf_blr_test():
+    """Test functions cwf_from_rwf() and compare_cwf_blr().
+    The test:
+    1) Computes CWF from RWF (function cwf_from_rwf())
+    2) Computes the difference between CWF and BLR (compare_cwf_blr())
+    3) Asserts that the differences are small.
+    For 10 events and 12 PMTs per event, all differences are less than 0.1 %
+    Input file (needed in repository): electrons_40keV_z250_RWF.h5
+    """
+
+    RWF_file = (os.environ['ICDIR']
+             + '/database/test_data/electrons_40keV_z250_RWF.h5')
+    h5rwf = tb.open_file(RWF_file,'r')
+    pmtrwf, pmtblr, sipmrwf = tbl.get_vectors(h5rwf)
+    NEVT, NPMT, PMTWL = pmtrwf.shape
+
+    CWF = wfm.cwf_from_rwf(pmtrwf, event_list=range(NEVT))
+    diff = wfm.compare_cwf_blr(CWF, pmtblr,
+                               event_list=range(NEVT), window_size=200)
+
+    assert max(diff) < 0.1
