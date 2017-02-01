c1d8d08266e5cf41a4ab071cd84644b6a8ac4260
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-02-01 11:14:30 +0100

    Add comprehensive test for configuration parser.
    This is an expansion of the test that appeared in JJ's (withdrawn) PRs
    111 and 112.
    
    Rather than using the real `irene.conf` file (and commenting in the
    test that the test will fail if anyone ever changes that file), we
    create temporary config files from within the test itself. This allows
    us to inject a variety of values into the config file used by the
    test, aswell as ensuring that changes to `irene.conf` will not break
    the test.
    
    We take care to use different values for all the settings wherever
    possible, to minimize the chances of accidentally getting the right
    answer.
    
    The test is parametrized, to allow easy orthogonal testing of
    independent arguments, and easy addition of new test cases.

--- invisible_cities/conftest.py
+++ invisible_cities/conftest.py
@@ -3,3 +3,7 @@ import pytest
 @pytest.fixture(scope='session')
 def irene_diomira_chain_tmpdir(tmpdir_factory):
     return tmpdir_factory.mktemp('irene_diomira_tests')
+
+@pytest.fixture(scope='session')
+def config_tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp('configure_tests')
