03975dbe211c79e7fac782d1d33160b7566f7a7c
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-02-15 11:14:04 +0100

    Further re-factoring of cities
    This commit includes work related with further re-factoring of
    cities.
    
    The initial re-factoring, that derived the "working
    cities" (Diomira, Isidora and Irene) from base class
    DeconvolutionCity (itself derived from City).
    
    Further generalization is possible. While Isidora is indeed a "working
    city" derived from DeconvolutionCity (e.g, attributes and methods that
    permit deconvolution of RWFs are owned by DeconvolutionCity and
    Isidora simply adds the intendence to read, write and run), it turns
    out that there are two further steps between Isidora and Irene.
    
    After Deconvolution, the next logical step is to perform a calibrated
    sum of the PMTs and a to extract the calibrated signals from the
    SiPMs. In fact, it turns out that two calibrated sums of the PMTs are
    possible. In one of them a MAU is used to follow the
    base-line (csum_mau) and in another the MAU is not
    used (csum). csum_mau is useful to search for S1 or other effects
    outside the S2 region (e.g, photoelectron shower in data). csum yields
    a better estimate of the S2 area. Thus the S12 search is now refined
    to used csum_mau for S1 searches and csum for S2 searches.
    
    This leads naturally to the concept of CalibratedCity, as a base
    class (derived from DeconvolutionCity) that performs the
    calibration (csum, csum_mau, and SiPM signals). The searches for S1,
    S2 and SiS2 are delegated to a PmapCity (derived from
    CalibratedCity). Irene is a "working city" derived from PmapCity, but
    other working cities derived from PmapCity are posible (and in fact
    there will be one soon, in charge of photoelectron showers analysis).
    
    The concept appears nicely and naturally extensible further down the
    reconstruction chain. The base class organize the work of
    reconstruction in a sort of Matryoska style: City -->
    DeconvolutionCity --> CalibratedCity ---> PmapCity and hide
    implementation details to the "working cities" (Irene, Isidora, etc),
    since all the calling to specific core functions is now handled by the
    base cities (thus a change of the inner implementation would be
    transparent to the working cities). The chain may be continued with a
    TrackingCity (organizing PMAPS as a collection of (x,y,z) hits, called
    a Ctrack), PaolinaCity (finding blobs in the Ctrack to produce a Ptrack) etc. Working
    cities will take care of saving data objects (Isidora saves CWF
    waveforms, Irene saves PMAPS, further cities can save Ctracks,
    Ptracks, etc).

--- invisible_cities/cities/base_cities.py
+++ invisible_cities/cities/base_cities.py
@@ -1,20 +1,49 @@
+"""
+This module defines base classes for the IC cities. The classes are:
+City: Handles input and output files, compression, and access to data base
+DeconvolutionCity: A City that performs deconvolution of the PMT RWFs
+CalibratedCity: A DeconvolutionCity that perform the calibrated sum of the
+                PMTs and computes the calibrated signals in the SiPM plane.
+PmapCity: A CalibratedCity that computes S1, S2 and S2Si that togehter
+          constitute a PMAP.
+
+Authors: J.J. Gomez-Cadenas and J. Generowicz.
+Feburary, 2017.
+"""
+
+from collections import namedtuple
 import numpy as np
 
 from   invisible_cities.database import load_db
 from   invisible_cities.core.system_of_units_c import SystemOfUnits
+import invisible_cities.sierpe.blr as blr
+import invisible_cities.core.peak_functions_c as cpf
+import invisible_cities.core.pmaps_functions as pmp
 
 units = SystemOfUnits()
+S12Params = namedtuple('S12Params', 'tmin tmax stride lmin lmax rebin')
 
 
 class City:
+    """Base class for all cities.
+       An IC city consumes data stored in the input_files and produce new data
+       which is stored in the output_file. In addition to setting input and
+       output files, the base class sets the print frequency and accesses
+       the data base, storing as attributed several calibration coefficients
 
-    def __init__(self, run_number=0, files_in=None):
+     """
 
-        self.run_number = run_number
-        self.files_in   = files_in
+    def __init__(self,
+                 run_number  = 0,
+                 files_in    = None,
+                 nprint      = 10000):
 
-        DataPMT  = load_db.DataPMT (run_number)
-        DataSiPM = load_db.DataSiPM(run_number)
+        self.run_number     = run_number
+        self.nprint         = nprint  # default print frequency
+        self.input_files    = files_in
+        # access data base
+        DataPMT             = load_db.DataPMT (run_number)
+        DataSiPM            = load_db.DataSiPM(run_number)
 
         # This is JCK-1: text reveals symmetry!
         self.xs              = DataSiPM.X.values
@@ -25,12 +54,8 @@ class City:
         self.coeff_blr       = DataPMT.coeff_blr.values      .astype(np.double)
         self.noise_rms       = DataPMT.noise_rms.values      .astype(np.double)
 
-        # default parameters
-        self.nprint       = 1000000
 
-        self.input_files = files_in
-
-    def set_print(self, nprint=10):
+    def set_print(self, nprint=1000):
         """Print frequency."""
         self.nprint = nprint
 
@@ -38,32 +63,182 @@ class City:
         """Set the input files."""
         self.input_files = input_files
 
+
+
 class DeconvolutionCity(City):
+    """A Deconvolution city extends the City base class adding the
+       deconvolution step, which transforms RWF into CWF.
+       The parameters of the deconvolution are the number of samples
+       used to compute the baseline (n_baseline) and the threshold to
+       thr_trigger in the rising signal (thr_trigger)
+    """
 
     def __init__(self,
                  run_number  = 0,
                  files_in    = None,
+                 nprint      = 10000,
                  n_baseline  = 28000,
-                 thr_trigger = 5 * units.adc,
-                 n_MAU       =   100,
-                 thr_MAU     = 3 * units.adc):
+                 thr_trigger = 5 * units.adc):
 
-        City.__init__(self, run_number=run_number, files_in=files_in)
-
+        City.__init__(self,
+                      run_number  = run_number,
+                      files_in    = files_in,
+                      nprint      = nprint)
         # BLR parameters
         self.n_baseline  = n_baseline
         self.thr_trigger = thr_trigger
 
-        # Parameters of the MAU used to remove low frequency noise.
-        self.  n_MAU =   n_MAU
-        self.thr_MAU = thr_MAU
-
-    def set_BLR(self, n_baseline, thr_trigger):
+    def set_blr(self, n_baseline, thr_trigger):
+        """Set the parameters of the Base Line Restoration (BLR)"""
         self.n_baseline  = n_baseline
         self.thr_trigger = thr_trigger
 
-    def set_MAU(self, n_MAU, thr_MAU):
-        self.  n_MAU =   n_MAU
-        self.thr_MAU = thr_MAU
+    def deconv_pmt(self, RWF):
+        """Deconvolve the RWF of the PMTs"""
+        return blr.deconv_pmt(RWF,
+                              self.coeff_c,
+                              self.coeff_blr,
+                              n_baseline  = self.n_baseline,
+                              thr_trigger = self.thr_trigger)
 
 
+class CalibratedCity(DeconvolutionCity):
+    """A calibrated city extends a DeconvCity, performing two actions.
+       1. Compute the calibrated sum of PMTs, in two flavours:
+          a) csum: PMTs waveforms are equalized to photoelectrons (pes) and
+             added
+          b) csum_mau: waveforms are equalized to photoelectrons;
+              compute a MAU that follows baseline and add PMT samples above
+              MAU + threshold
+       2. Compute the calibrated signal in the SiPMs:
+          a) equalize to pes;
+          b) compute a MAU that follows baseline and keep samples above
+             MAU + threshold.
+       """
+
+    def __init__(self,
+                 run_number  = 0,
+                 files_in    = None,
+                 nprint      = 10000,
+                 n_baseline  = 28000,
+                 thr_trigger = 5 * units.adc,
+                 n_MAU       = 100,
+                 thr_MAU     = 3.0*units.adc,
+                 thr_csum_s1 = 0.2*units.adc,
+                 thr_csum_s2 = 1.0*units.adc,
+                 n_MAU_sipm  = 100,
+                 thr_sipm    = 5.0*units.pes):
+
+        DeconvolutionCity.__init__(self,
+                                   run_number  = run_number,
+                                   files_in    = None,
+                                   nprint      = nprint,
+                                   n_baseline  = n_baseline,
+                                   thr_trigger = thr_trigger)
+
+        # Parameters of the PMT csum.
+        self.n_MAU       = n_MAU
+        self.thr_MAU     = thr_MAU
+        self.thr_csum_s1 = thr_csum_s1
+        self.thr_csum_s2 = thr_csum_s2
+
+        # Parameters of the SiPM signal
+        self.n_MAU_sipm   = n_MAU_sipm
+        self.thr_sipm = thr_sipm
+
+    def set_csum(self, n_MAU, thr_MAU, thr_csum_s1, thr_csum_s2):
+        """Set CSUM parameters"""
+        self.n_MAU       = n_MAU
+        self.thr_MAU     = thr_MAU
+        self.thr_csum_s1 = thr_csum_s1
+        self.thr_csum_s2 = thr_csum_s2
+
+    def set_sipm(self, n_MAU_sipm=100, thr_sipm=5 * units.pes):
+        """Cutoff for SiPMs."""
+        self.thr_sipm = thr_sipm
+        self.n_MAU_sipm   = n_MAU_sipm
+
+    def calibrated_pmt_sum(self, CWF):
+        """Return the csum and csum_mau calibrated sums"""
+        return cpf.calibrated_pmt_sum(CWF,
+                                      self.adc_to_pes,
+                                      n_MAU =   self.n_MAU,
+                                      thr_MAU = self.thr_MAU)
+
+    def csum_zs(self, csum, threshold):
+        """Zero Suppression over csum"""
+        return cpf.wfzs(csum, threshold=threshold)
+
+    def calibrated_signal_sipm(self, SiRWF):
+        """Return the calibrated signal in the SiPMs."""
+        return cpf.signal_sipm(SiRWF,
+                               self.sipm_adc_to_pes,
+                               thr =   self.thr_sipm,
+                               n_MAU = self.n_MAU_sipm)
+
+
+class PmapCity(CalibratedCity):
+    """A PMAP city extends a CalibratedCity, computing the S1, S2 and S2Si
+       objects that togehter constitute a PMAP.
+
+    """
+
+    def __init__(self,
+                 run_number  = 0,
+                 files_in    = None,
+                 nprint      = 10000,
+                 n_baseline  = 28000,
+                 thr_trigger = 5 * units.adc,
+                 n_MAU       = 100,
+                 thr_MAU     = 3.0*units.adc,
+                 thr_csum_s1 = 0.2*units.adc,
+                 thr_csum_s2 = 1.0*units.adc,
+                 n_MAU_sipm  = 100,
+                 thr_sipm    = 5.0*units.pes,
+                 s1_params   = None,
+                 s2_params   = None,
+                 thr_sipm_s2 = 30*units.pes):
+
+        CalibratedCity.__init__(self,
+                                run_number  = run_number,
+                                files_in    = None,
+                                nprint      = nprint,
+                                n_baseline  = n_baseline,
+                                thr_trigger = thr_trigger,
+                                n_MAU       = n_MAU,
+                                thr_MAU     = thr_MAU,
+                                thr_csum_s1 = thr_csum_s1,
+                                thr_csum_s2 = thr_csum_s2,
+                                n_MAU_sipm  = n_MAU_sipm,
+                                thr_sipm    = thr_sipm)
+
+        self.s1_params   = s1_params
+        self.s2_params   = s2_params
+        self.thr_sipm_s2 = thr_sipm_s2
+
+    def set_pmap_params(self,
+                        s1_params,
+                        s2_params,
+                        thr_sipm_s2 = 30*units.pes):
+        """Parameters for PMAP searches."""
+        self.s1_params = s1_params
+        self.s2_params = s2_params
+        self.thr_sipm_s2 = thr_sipm_s2
+
+    def find_S12(self, s1_ene, s1_indx, s2_ene, s2_indx):
+        """Return S1 and S2"""
+        S1 = cpf.find_S12(s1_ene,
+                          s1_indx,
+                          **self.s1_params._asdict())
+
+        S2 = cpf.find_S12(s2_ene,
+                          s2_indx,
+                          **self.s2_params._asdict())
+        return S1, S2
+
+    def find_S2Si(self, S2, sipmzs):
+        """Return S2Si"""
+
+        SIPM = cpf.select_sipm(sipmzs)
+        S2Si = pmp.sipm_s2_dict(SIPM, S2, thr = self.thr_sipm_s2)
+        return S2Si
