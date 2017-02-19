48872baafac1c53f0b279e552174b3caeffd8a8d
Jacek Generowicz <github@my-post-office.net>
2017-02-19 11:08:25 +0100

    Fix Isidora output file management

--- invisible_cities/cities/isidora.py
+++ invisible_cities/cities/isidora.py
@@ -18,7 +18,6 @@ import invisible_cities.sierpe.blr as blr
 import invisible_cities.core.peak_functions_c as cpf
 from   invisible_cities.core.system_of_units_c import SystemOfUnits
 from   invisible_cities.database import load_db
-from   invisible_cities.core.exceptions import NoInputFiles
 
 from   invisible_cities.cities.base_cities import DeconvolutionCity
 
@@ -44,7 +43,7 @@ class Isidora(DeconvolutionCity):
         Load the data base to access calibration and geometry.
         Sets all switches to default value.
         """
-        
+
         DeconvolutionCity.__init__(self,
                                    run_number  = run_number,
                                    files_in    = files_in,
@@ -53,25 +52,15 @@ class Isidora(DeconvolutionCity):
                                    n_baseline  = n_baseline,
                                    thr_trigger = thr_trigger)
 
-    def set_cwf_store(self, cwf_file_name, compression='ZLIB4'):
-        """Set the output files."""
-        self.cwf_file = tb.open_file(cwf_file_name, "w",
-                                     filters = tbl.filters(compression))
-
-        # create a group
-        self.cwf_group = self.cwf_file.create_group(self.cwf_file.root, "BLR")
-
-        self.compression = compression
-
-    def store_cwf(self, cwf):
+    def _store_cwf(self, cwf, cwf_file, cwf_group):
         """Store CWF."""
         NPMT, PMTWL = cwf.shape
-        if "pmtcwf" not in self.cwf_group:
+        if "pmtcwf" not in cwf_group:
             # create earray to store cwf
-            self.pmtcwf = self.cwf_file.create_earray(self.cwf_group, "pmtcwf",
-                                                      atom    = tb.Float32Atom(),
-                                                      shape   = (0, NPMT, PMTWL),
-                                                      filters = tbl.filters(self.compression))
+            self.pmtcwf = cwf_file.create_earray(cwf_group, "pmtcwf",
+                                                 atom    = tb.Float32Atom(),
+                                                 shape   = (0, NPMT, PMTWL),
+                                                 filters = tbl.filters(self.compression))
         self.pmtcwf.append(cwf.reshape(1, NPMT, PMTWL))
 
 
@@ -90,63 +79,61 @@ class Isidora(DeconvolutionCity):
                           """.format(nmax, self.input_files))
 
         # loop over input files
-        first = False
-        for ffile in self.input_files:
-            print("Opening", ffile, end="... ")
-            filename = ffile
-            with tb.open_file(filename, "r") as h5in:
-                # access RWF
-                pmtrwf  = h5in.root.RD.pmtrwf
-                sipmrwf = h5in.root.RD.sipmrwf
+        with tb.open_file(self.output_file, "w",
+                          filters = tbl.filters(self.compression)) as cwf_file:
+            cwf_group = cwf_file.create_group(cwf_file.root, "BLR")
+            first = False
+            for ffile in self.input_files:
+                print("Opening", ffile, end="... ")
+                filename = ffile
+                with tb.open_file(filename, "r") as h5in:
+                    # access RWF
+                    pmtrwf  = h5in.root.RD.pmtrwf
+                    sipmrwf = h5in.root.RD.sipmrwf
 
-                # Copy sensor table if exists (needed for GATE)
-                if self.cwf_file:
+                    # Copy sensor table if exists (needed for GATE)
                     if 'Sensors' in h5in.root:
-                        self.sensors_group = self.cwf_file.create_group(
-                            self.cwf_file.root, "Sensors")
+                        self.sensors_group = cwf_file.create_group(
+                            cwf_file.root, "Sensors")
                         datapmt = h5in.root.Sensors.DataPMT
                         datapmt.copy(newparent=self.sensors_group)
 
-                if not self.monte_carlo:
-                    self.eventsInfo = h5in.root.Run.events
+                    if not self.monte_carlo:
+                        self.eventsInfo = h5in.root.Run.events
 
-                NEVT, NPMT,   PMTWL = pmtrwf .shape
-                NEVT, NSIPM, SIPMWL = sipmrwf.shape
-                print("Events in file = {}".format(NEVT))
+                    NEVT, NPMT,   PMTWL = pmtrwf .shape
+                    NEVT, NSIPM, SIPMWL = sipmrwf.shape
+                    print("Events in file = {}".format(NEVT))
 
-                if first == False:
-                    print_configuration({"# PMT"  : NPMT,
-                                         "PMT WL" : PMTWL,
-                                         "# SiPM" : NSIPM,
-                                         "SIPM WL": SIPMWL})
+                    if first == False:
+                        print_configuration({"# PMT"  : NPMT,
+                                             "PMT WL" : PMTWL,
+                                             "# SiPM" : NSIPM,
+                                             "SIPM WL": SIPMWL})
 
-                    self.signal_t = np.arange(0, PMTWL * 25, 25)
-                    first = True
-                # loop over all events in file unless reach nmax
-                for evt in range(NEVT):
-                    # deconvolve
-                    CWF = blr.deconv_pmt(
-                      pmtrwf[evt],
-                      self.coeff_c,
-                      self.coeff_blr,
-                      n_baseline  = self.n_baseline,
-                      thr_trigger = self.thr_trigger)
+                        self.signal_t = np.arange(0, PMTWL * 25, 25)
+                        first = True
+                    # loop over all events in file unless reach nmax
+                    for evt in range(NEVT):
+                        # deconvolve
+                        CWF = blr.deconv_pmt(
+                            pmtrwf[evt],
+                            self.coeff_c,
+                            self.coeff_blr,
+                            n_baseline  = self.n_baseline,
+                            thr_trigger = self.thr_trigger)
 
-                    if self.cwf_file:
-                        self.store_cwf(CWF)
+                        self._store_cwf(CWF, cwf_file, cwf_group)
 
-                    n_events_tot += 1
-                    if n_events_tot % self.nprint == 0:
-                        print('event in file = {}, total = {}'
-                              .format(evt, n_events_tot))
+                        n_events_tot += 1
+                        if n_events_tot % self.nprint == 0:
+                            print('event in file = {}, total = {}'
+                                  .format(evt, n_events_tot))
 
-                    if n_events_tot >= nmax > -1:
-                        print('reached max nof of events (= {})'
-                              .format(nmax))
-                        break
-
-        if self.cwf_file:
-            self.cwf_file.close()
+                        if n_events_tot >= nmax > -1:
+                            print('reached max nof of events (= {})'
+                                  .format(nmax))
+                            break
 
         return n_events_tot
 
@@ -165,8 +152,8 @@ def ISIDORA(argv = sys.argv):
                   thr_trigger = CFP['THR_TRIGGER'] * units.adc)
 
     #fpp.set_input_files(files_in)
-    fpp.set_cwf_store(CFP['FILE_OUT'],
-                       compression = CFP['COMPRESSION'])
+    fpp.set_output_file(CFP['FILE_OUT'])
+    fpp.set_compression(CFP['COMPRESSION'])
     fpp.set_print(nprint = CFP['NPRINT'])
 
     t0 = time()
