c2bcd58b852da36dc2886a409ecc863cbbc745c5
Jacek Generowicz <github@my-post-office.net>
2017-02-19 10:33:52 +0100

    Use City.set_output_file in Diomira with context manager

--- invisible_cities/cities/diomira.py
+++ invisible_cities/cities/diomira.py
@@ -45,7 +45,6 @@ class Diomira(City):
         City.__init__(self, run_number=run_number, files_in=files_in)
 
         self.sipm_noise_cut = sipm_noise_cut
-        self.h5out = None
 
     def simulate_sipm_response(self, event, sipmrd,
                                sipms_noise_sampler):
@@ -94,16 +93,6 @@ class Diomira(City):
             BLRX.append(signal_blr)
         return np.array(RWF), np.array(BLRX)
 
-    # TODO different cities have different kinds of output files, so,
-    # for the time being they have distinct output file setting
-    # functions. See if this can be unified.
-    def set_output_file(self, output_file, compression='ZLIB4'):
-        """Set the output file."""
-        filename = output_file
-        self.compression = compression
-        self.h5out = tb.open_file(filename, "w",
-                                  filters = tbl.filters(compression))
-
     def set_sipm_noise_cut(self, noise_cut=3.0):
         """Sets the SiPM noise cut (in PES)"""
         self.sipm_noise_cut = noise_cut
@@ -150,7 +139,7 @@ class Diomira(City):
             raise IOError('must set input files before running')
         if len(self.input_files)    == 0:
             raise IOError('input file list is empty')
-        if not self.h5out:
+        if not self.output_file:
             raise IOError('must set output file before running')
         if not self.sipm_noise_cut:
             raise IOError('must set sipm_noise_cut before running')
@@ -159,111 +148,114 @@ class Diomira(City):
                  DIOMIRA will run a max of {} events
                  Input Files = {}
                  Output File = {}
-                          """.format(nmax, self.input_files, self.h5out))
+                          """.format(nmax, self.input_files, self.output_file))
 
         # loop over input files
         first = False
-        for ffile in self.input_files:
+        with tb.open_file(self.output_file, "w",
+                          filters = tbl.filters(self.compression)) as h5out:
 
-            print("Opening file {}".format(ffile))
-            filename = ffile
+            for ffile in self.input_files:
 
-            with tb.open_file(filename, "r") as h5in:
-                pmtrd  = h5in.root.pmtrd
-                sipmrd = h5in.root.sipmrd
+                print("Opening file {}".format(ffile))
+                filename = ffile
 
-                NEVT, NPMT, PMTWL = pmtrd.shape
-                PMTWL_FEE = int(PMTWL / FE.t_sample)
-                NEVENTS_DST, NSIPM, SIPMWL = sipmrd.shape
+                with tb.open_file(filename, "r") as h5in:
+                    pmtrd  = h5in.root.pmtrd
+                    sipmrd = h5in.root.sipmrd
 
-                print("Events in file = {}".format(NEVENTS_DST))
+                    NEVT, NPMT, PMTWL = pmtrd.shape
+                    PMTWL_FEE = int(PMTWL / FE.t_sample)
+                    NEVENTS_DST, NSIPM, SIPMWL = sipmrd.shape
 
-                if not first:
-                    # print configuration, create vectors
-                    # init SiPM noiser, store FEE table
-                    print_configuration({"# PMT"        : NPMT,
-                                         "PMT WL"       : PMTWL,
-                                         "PMT WL (FEE)" : PMTWL_FEE,
-                                         "# SiPM"       : NSIPM,
-                                         "SIPM WL"      : SIPMWL})
-                    # RD group
-                    RD = self.h5out.create_group(self.h5out.root, "RD")
-                    # MC group
-                    MC = self.h5out.create_group(self.h5out.root, "MC")
-                    # create a table to store Energy plane FEE
-                    self.fee_table = self.h5out.create_table(MC, "FEE", FEE,
-                                      "EP-FEE parameters",
-                                      tbl.filters("NOCOMPR"))
-                    # create vectors
-                    self.pmtrwf = self.h5out.create_earray(
-                                RD,
-                                "pmtrwf",
-                                atom = tb.Int16Atom(),
-                                shape = (0, NPMT, PMTWL_FEE),
-                                expectedrows = nmax,
-                                filters = tbl.filters(self.compression))
+                    print("Events in file = {}".format(NEVENTS_DST))
 
-                    self.pmtblr = self.h5out.create_earray(
-                                RD,
-                                "pmtblr",
-                                atom = tb.Int16Atom(),
-                                shape = (0, NPMT, PMTWL_FEE),
-                                expectedrows = nmax,
-                                filters = tbl.filters(self.compression))
+                    if not first:
+                        # print configuration, create vectors
+                        # init SiPM noiser, store FEE table
+                        print_configuration({"# PMT"        : NPMT,
+                                             "PMT WL"       : PMTWL,
+                                             "PMT WL (FEE)" : PMTWL_FEE,
+                                             "# SiPM"       : NSIPM,
+                                             "SIPM WL"      : SIPMWL})
+                        # RD group
+                        RD = h5out.create_group(h5out.root, "RD")
+                        # MC group
+                        MC = h5out.create_group(h5out.root, "MC")
+                        # create a table to store Energy plane FEE
+                        self.fee_table = h5out.create_table(MC, "FEE", FEE,
+                                          "EP-FEE parameters",
+                                          tbl.filters("NOCOMPR"))
+                        # create vectors
+                        self.pmtrwf = h5out.create_earray(
+                                    RD,
+                                    "pmtrwf",
+                                    atom = tb.Int16Atom(),
+                                    shape = (0, NPMT, PMTWL_FEE),
+                                    expectedrows = nmax,
+                                    filters = tbl.filters(self.compression))
 
-                    self.sipmrwf = self.h5out.create_earray(
-                                RD,
-                                "sipmrwf",
-                                atom = tb.Int16Atom(),
-                                shape = (0, NSIPM, SIPMWL),
-                                expectedrows = nmax,
-                                filters = tbl.filters(self.compression))
+                        self.pmtblr = h5out.create_earray(
+                                    RD,
+                                    "pmtblr",
+                                    atom = tb.Int16Atom(),
+                                    shape = (0, NPMT, PMTWL_FEE),
+                                    expectedrows = nmax,
+                                    filters = tbl.filters(self.compression))
 
-                    # Create instance of the noise sampler
-                    self.noise_sampler = SiPMsNoiseSampler(SIPMWL, True)
-                    # thresholds in adc counts
-                    self.sipms_thresholds = (self.sipm_noise_cut
-                                          *  self.sipm_adc_to_pes)
-                    # store FEE parameters in table
-                    self.store_FEE_table()
+                        self.sipmrwf = h5out.create_earray(
+                                    RD,
+                                    "sipmrwf",
+                                    atom = tb.Int16Atom(),
+                                    shape = (0, NSIPM, SIPMWL),
+                                    expectedrows = nmax,
+                                    filters = tbl.filters(self.compression))
 
-                    first = True
+                        # Create instance of the noise sampler
+                        self.noise_sampler = SiPMsNoiseSampler(SIPMWL, True)
+                        # thresholds in adc counts
+                        self.sipms_thresholds = (self.sipm_noise_cut
+                                              *  self.sipm_adc_to_pes)
+                        # store FEE parameters in table
+                        self.store_FEE_table()
 
-                # loop over events in the file. Break when nmax is reached
-                for evt in range(NEVT):
-                    # simulate PMT and SiPM response
-                    # RWF and BLR
-                    dataPMT, blrPMT = self.simulate_pmt_response(
-                                    evt, pmtrd)
-                    # append the data to pytable vectors
-                    self.pmtrwf.append(
-                        dataPMT.astype(int).reshape(1, NPMT, PMTWL_FEE))
-                    self.pmtblr.append(
-                         blrPMT.astype(int).reshape(1, NPMT, PMTWL_FEE))
-                    # SiPMs
-                    dataSiPM = self.simulate_sipm_response(
-                                evt, sipmrd, self.noise_sampler)
-                    # return a noise-supressed waveform
-                    dataSiPM = wfm.noise_suppression(
-                                dataSiPM, self.sipms_thresholds)
+                        first = True
 
-                    self.sipmrwf.append(
-                        dataSiPM.astype(int).reshape(1, NSIPM, SIPMWL))
+                    # loop over events in the file. Break when nmax is reached
+                    for evt in range(NEVT):
+                        # simulate PMT and SiPM response
+                        # RWF and BLR
+                        dataPMT, blrPMT = self.simulate_pmt_response(
+                                        evt, pmtrd)
+                        # append the data to pytable vectors
+                        self.pmtrwf.append(
+                            dataPMT.astype(int).reshape(1, NPMT, PMTWL_FEE))
+                        self.pmtblr.append(
+                             blrPMT.astype(int).reshape(1, NPMT, PMTWL_FEE))
+                        # SiPMs
+                        dataSiPM = self.simulate_sipm_response(
+                                    evt, sipmrd, self.noise_sampler)
+                        # return a noise-supressed waveform
+                        dataSiPM = wfm.noise_suppression(
+                                    dataSiPM, self.sipms_thresholds)
 
-                    n_events_tot +=1
-                    if n_events_tot % self.nprint == 0:
-                        print('event in file = {}, total = {}'
-                              .format(evt, n_events_tot))
+                        self.sipmrwf.append(
+                            dataSiPM.astype(int).reshape(1, NSIPM, SIPMWL))
 
-                    if n_events_tot >= nmax and nmax > -1:
-                        print('reached maximum number of events (={})'
-                              .format(nmax))
-                        break
+                        n_events_tot +=1
+                        if n_events_tot % self.nprint == 0:
+                            print('event in file = {}, total = {}'
+                                  .format(evt, n_events_tot))
+
+                        if n_events_tot >= nmax and nmax > -1:
+                            print('reached maximum number of events (={})'
+                                  .format(nmax))
+                            break
 
         self.pmtrwf.flush()
         self.sipmrwf.flush()
         self.pmtblr.flush()
-        self.h5out.close()
+
 
         return n_events_tot
 
@@ -276,7 +268,8 @@ def DIOMIRA(argv=sys.argv):
     files_in = glob(CFP['FILE_IN'])
     files_in.sort()
     fpp.set_input_files(files_in)
-    fpp.set_output_file(CFP['FILE_OUT'], compression=CFP['COMPRESSION'])
+    fpp.set_output_file(CFP['FILE_OUT'])
+    fpp.set_compression(compression=CFP['COMPRESSION'])
     fpp.set_print(nprint=CFP['NPRINT'])
 
     fpp.set_sipm_noise_cut(noise_cut = CFP["NOISE_CUT"])
