3d90dbbbb5f5cef2129f494dee3ec48f6efab9f4
Jacek Generowicz <github@my-post-office.net>
2017-02-21 15:14:49 +0100

    Refactor City tests
    A significant refactoring of tests has been made to reduce
    duplications and simplify. Tests now make extensive use of fixtures
    and parameterizations.
    
    Creation of the configuration files used in the city tests contained
    tons of repeated code and data. The configuration file formats and
    default values have been moved to the city base classes, and
    subclasses inhert common config file entries from superclasses.
    
    Higher-level config file creation and writing utilities have been
    provided to avoid repetition of low-level code in the tests.
    
    Most of the city test files have now become extremely short, and
    addition of new tests should now be much easier.

--- invisible_cities/cities/diomira_test.py
+++ invisible_cities/cities/diomira_test.py
@@ -6,18 +6,12 @@ IC core team: Jacek Generowicz, JJGC,
 G. Martinez, J.A. Hernando, J.M Benlloch
 package: invisible cities. See release notes and licence
 """
-from __future__ import print_function
-from __future__ import absolute_import
-
 import os
-from os import path
-from glob import glob
 import tables as tb
 import numpy as np
 
-from pytest import mark
+from pytest import mark, fixture
 
-from   invisible_cities.core.configure import configure
 import invisible_cities.core.tbl_functions as tbl
 import invisible_cities.core.wfm_functions as wfm
 import invisible_cities.core.system_of_units as units
@@ -29,35 +23,19 @@ from   invisible_cities.core.random_sampling \
      import NoiseSampler as SiPMsNoiseSampler
 
 
-@mark.serial
-@mark.slow
-def test_diomira_run(irene_diomira_chain_tmpdir):
-    """Test that DIOMIRA runs on default config parameters."""
-    RWF_file = str(irene_diomira_chain_tmpdir.join(
-                   'electrons_40keV_z250_RWF.h5'))
-    conf_file = path.join(os.environ['ICDIR'], 'config/diomira.conf')
-    CFP = configure(['DIOMIRA','-c', conf_file, '-o', RWF_file])
-    fpp = Diomira()
-    files_in = glob(CFP['FILE_IN'])
-    files_in.sort()
-    fpp.set_input_files(files_in)
-    fpp.set_output_file(CFP['FILE_OUT'])
-    fpp.set_compression(CFP['COMPRESSION'])
-    fpp.set_print(nprint=CFP['NPRINT'])
+@fixture
+def conf_file_name(config_tmpdir):
+    conf_file_name = str(config_tmpdir.join('diomira.conf'))
+    Diomira.write_config_file(conf_file_name,
+                               PATH_OUT = str(config_tmpdir))
+    return conf_file_name
 
-    fpp.set_sipm_noise_cut(noise_cut=CFP["NOISE_CUT"])
 
-    nevts = CFP['NEVENTS'] if not CFP['RUN_ALL'] else -1
-    nevt = fpp.run(nmax=nevts)
-
-    assert nevt == nevts
-
-@mark.serial
-@mark.slow # not slow in itself, but in series with test_diomira_run
-def test_diomira_fee_table(irene_diomira_chain_tmpdir):
+def test_diomira_fee_table():
     """Test that FEE table reads back correctly with expected values."""
-    RWF_file = str(irene_diomira_chain_tmpdir.join(
-                   'electrons_40keV_z250_RWF.h5'))
+    RWF_file = os.path.join(os.environ['ICDIR'],
+                            'database/test_data/electrons_40keV_z250_RWF.h5')
+
     with tb.open_file(RWF_file, 'r') as e40rwf:
         fee = tbl.read_FEE_table(e40rwf.root.MC.FEE)
         feep = fee['fee_param']
@@ -85,17 +63,13 @@ def test_diomira_fee_table(irene_diomira_chain_tmpdir):
         assert abs(feep.CEILING - FEE.CEILING)             < eps
 
 
-@mark.serial
-@mark.slow # not slow in itself, but in series with test_diomira_run
-def test_diomira_cwf_blr(irene_diomira_chain_tmpdir):
+def test_diomira_cwf_blr(electron_RWF_file):
     """This is the most rigurous test of the suite. It reads back the
        RWF and BLR waveforms written to disk by DIOMIRA, and computes
        CWF (using deconvoution algorithm), then checks that the CWF match
        the BLR within 1 %.
     """
-    RWF_file = str(irene_diomira_chain_tmpdir.join(
-                   'electrons_40keV_z250_RWF.h5'))
-    with tb.open_file(RWF_file, 'r') as e40rwf:
+    with tb.open_file(electron_RWF_file, 'r') as e40rwf:
 
         pmtrwf = e40rwf.root.RD.pmtrwf
         pmtblr = e40rwf.root.RD.pmtblr
@@ -103,18 +77,19 @@ def test_diomira_cwf_blr(irene_diomira_chain_tmpdir):
         coeff_c = DataPMT.coeff_c.values.astype(np.double)
         coeff_blr = DataPMT.coeff_blr.values.astype(np.double)
 
-        for event in range(10):
-            BLR = pmtblr[event]
-            CWF = blr.deconv_pmt(pmtrwf[event],
-                                 coeff_c,
-                                 coeff_blr,
-                                 n_baseline=28000,
-                                 thr_trigger=5)
+        event = 0
+        BLR = pmtblr[event]
+        CWF = blr.deconv_pmt(pmtrwf[event],
+                             coeff_c,
+                             coeff_blr,
+                             n_baseline=28000,
+                             thr_trigger=5)
 
-
-            for i in range(len(CWF)):
-                assert np.isclose(np.sum(BLR[i][5000:5100]),
-                                  np.sum(CWF[i][5000:5100]), rtol=0.01)
+        diff = wfm.compare_cwf_blr(cwf         = [CWF],
+                                   pmtblr      = [BLR],
+                                   event_list  = [0],
+                                   window_size = 500)
+        assert diff[0] < 1
 
 
 @mark.slow
@@ -151,16 +126,17 @@ def test_diomira_sipm(irene_diomira_chain_tmpdir):
         sipms_thresholds = sipm_noise_cut *  sipm_adc_to_pes
 
         noise_sampler = SiPMsNoiseSampler(SIPMWL, True)
-        for event in range(10):
+        for event in range(5):
             # signal in sipm with noise
             sipmrwf = e40rd.root.sipmrd[event] + noise_sampler.Sample()
             # zs waveform
             sipmzs = wfm.noise_suppression(sipmrwf, sipms_thresholds)
             n_sipm = 0
             for j in range(sipmzs.shape[0]):
-                if np.sum(sipmzs[j] >0):
+                if np.sum(sipmzs[j] > 0):
                     n_sipm+=1
             assert n_sipm < max_sipm_with_signal
+
 
 def test_diomira_identify_bug():
     """Read a one-event file in which the energy of PMTs is equal to zero and
@@ -175,8 +151,8 @@ def test_diomira_identify_bug():
     The same event is later processed with Irene (where a protection
     that skips empty events has been added) to ensure that no crash occur."""
 
-    infile = path.join(os.environ['ICDIR'],
-                       'database/test_data/irene_bug_Kr_ACTIVE_7bar_MCRD.h5')
+    infile = os.path.join(os.environ['ICDIR'],
+                          'database/test_data/irene_bug_Kr_ACTIVE_7bar_MCRD.h5')
     with tb.open_file(infile, 'r') as h5in:
 
         pmtrd  = h5in.root.pmtrd
@@ -185,52 +161,8 @@ def test_diomira_identify_bug():
             assert np.sum(pmtwf[i]) == 0
 
 
-
-config_file_format = """
-# set_input_files
-PATH_IN {PATH_IN}
-FILE_IN {FILE_IN}
-
-# set_cwf_store
-PATH_OUT {PATH_OUT}
-FILE_OUT {FILE_OUT}
-COMPRESSION {COMPRESSION}
-
-# set_print
-NPRINT {NPRINT}
-
-# run
-NEVENTS {NEVENTS}
-RUN_ALL {RUN_ALL}
-
-NOISE_CUT {NOISE_CUT}
-"""
-
-
-def config_file_spec_with_tmpdir(tmpdir):
-    return dict(PATH_IN  = '$ICDIR/database/test_data/',
-                FILE_IN  = 'electrons_40keV_z250_MCRD.h5',
-                PATH_OUT = str(tmpdir),
-                FILE_OUT = 'electrons_40keV_z250_CWF.h5',
-                COMPRESSION = 'ZLIB4',
-                NPRINT      =     1,
-                NEVENTS     =     5,
-                NOISE_CUT   =     3,
-                RUN_ALL     = False)
-
-
-
-# TODO refactor to factor out config file creation: most of this test
-# is noise; duplication of something that also happens in the above
-# test
 @mark.slow
-def test_command_line_diomira(config_tmpdir):
-
-    config_file_spec = config_file_spec_with_tmpdir(config_tmpdir)
-
-    config_file_contents = config_file_format.format(**config_file_spec)
-    conf_file_name = str(config_tmpdir.join('test-4.conf'))
-    with open(conf_file_name, 'w') as conf_file:
-        conf_file.write(config_file_contents)
-
-    DIOMIRA(['DIOMIRA', '-c', conf_file_name])
+def test_command_line_diomira(conf_file_name):
+    nrequired, nactual = DIOMIRA(['DIOMIRA', '-c', conf_file_name])
+    if nrequired > 0:
+        assert nrequired == nactual
