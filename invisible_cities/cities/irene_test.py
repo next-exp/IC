f08c5d6d1bf372855496b454298f821f6aee0466
jmbenlloch <info@jmbenlloch.net>
2017-03-01 15:26:09 +0100

    Add new tests and refactor existing ones for Irene and Isidora
    New test checks that event and run number written by Irene are read
    back correctly.
    
    In addition some re-factoring of existing tests has been
    done. Specifically the followings actions have been taken:
    
    1) Separate fixtures to define configuration file names for
    data and mc. This prevents obvious file-name conflicts, although there
    is some level of duplicity in the code.
    
    2) Add the possibility of setting run number by command
    line in configure.py
    
    3) Set input and outuput paths to None by default, to force
    explicit declaration of paths in tests (clearer code, less bug prone).
    
    4) Most tests re-write input, output, run number and number of events
    in the command line (explicit behaviour, less noise).
    
    5) There is a serial test (marked as such), since one test needs the
    imput of a file written in output by a previous test.

--- invisible_cities/cities/irene_test.py
+++ invisible_cities/cities/irene_test.py
@@ -8,29 +8,102 @@ package: invisible cities. See release notes and licence
 last changed: 01-12-2017
 """
 import os
+import tables as tb
+import numpy  as np
 from pytest import mark, fixture
 
-from   invisible_cities.cities.irene import Irene, IRENE
+from invisible_cities.cities.irene import Irene, IRENE
 
 @fixture(scope='module')
-def conf_file_name(config_tmpdir):
-    conf_file_name = str(config_tmpdir.join('irene.conf'))
+def conf_file_name_mc(config_tmpdir):
+    # Specifies a name for a MC configuration file. Also, default number
+    # of events set to 1.
+
+    conf_file_name = str(config_tmpdir.join('irene_mc.conf'))
     Irene.write_config_file(conf_file_name,
-                            PATH_OUT = str(config_tmpdir))
+                            PATH_OUT = str(config_tmpdir),
+                            NEVENTS    = 1)
+    return conf_file_name
+
+@fixture(scope='module')
+def conf_file_name_data(config_tmpdir):
+    # Specifies a name for a data configuration file. Also, default number
+    # of events set to 1.
+
+    conf_file_name = str(config_tmpdir.join('irene_data.conf'))
+    Irene.write_config_file(conf_file_name,
+                            PATH_OUT = str(config_tmpdir),
+                            NEVENTS    = 1)
     return conf_file_name
 
 
 @mark.slow
-def test_command_line_irene(conf_file_name):
-    nrequired, nactual, _ = IRENE(['IRENE', '-c', conf_file_name])
+def test_command_line_irene_electrons_40keV(conf_file_name_mc, config_tmpdir):
+    # NB: avoid taking defaults for PATH_IN and PATH_OUT
+    # since they are in general test-specific
+    # NB: avoid taking defaults for run number (test-specific)
+
+    PATH_IN = os.path.join(os.environ['ICDIR'],
+              'database/test_data/',
+              'electrons_40keV_z250_RWF.h5')
+    PATH_OUT = os.path.join(str(config_tmpdir),
+              'electrons_40keV_z250_CWF.h5')
+
+    nrequired, nactual, _ = IRENE(['IRENE',
+                                   '-c', conf_file_name_mc,
+                                   '-i', PATH_IN,
+                                   '-o', PATH_OUT,
+                                   '-r', '0'])
     if nrequired > 0:
         assert nrequired == nactual
 
-def test_empty_events_issue_81(conf_file_name):
+@mark.slow
+def test_command_line_irene_run_2983(conf_file_name_data, config_tmpdir):
+    PATH_IN = os.path.join(os.environ['ICDIR'],
+              'database/test_data/',
+              'run_2983.h5')
+    PATH_OUT = os.path.join(str(config_tmpdir),
+              'run_2983_pmaps.h5')
+
+    nrequired, nactual, _ = IRENE(['IRENE',
+                                   '-c', conf_file_name_data,
+                                   '-i', PATH_IN,
+                                   '-o', PATH_OUT,
+                                   '-r', '2983'])
+    if nrequired > 0:
+        assert nrequired == nactual
+
+@mark.serial
+def test_command_line_irene_runinfo_run_2983(config_tmpdir):
+    # Check events numbers & timestamp are copied properly
+    PATH_IN = os.path.join(os.environ['ICDIR'],
+              'database/test_data/',
+              'run_2983.h5')
+    PATH_OUT = os.path.join(str(config_tmpdir),
+              'run_2983_pmaps.h5')
+
+    with tb.open_file(PATH_IN,  mode='r') as h5in, \
+         tb.open_file(PATH_OUT, mode='r') as h5out:
+            evts_in  = h5in .root.Run.events[0]
+            evts_out = h5out.root.Run.events[0]
+            np.testing.assert_array_equal(evts_in, evts_out)
+
+            # Run number is copied
+            assert h5in .root.Run.runInfo[0] == h5out.root.Run.runInfo[0]
+
+
+def test_empty_events_issue_81(conf_file_name_mc, config_tmpdir):
+    # NB: explicit PATH_OUT
     PATH_IN = os.path.join(os.environ['ICDIR'],
            'database/test_data/',
            'irene_bug_Kr_ACTIVE_7bar_RWF.h5')
+    PATH_OUT = os.path.join(str(config_tmpdir),
+              'irene_bug_Kr_ACTIVE_7bar_CWF.h5')
 
-    nrequired, nactual, empty = IRENE(['IRENE', '-c', conf_file_name, '-i', PATH_IN])
+    nrequired, nactual, empty = IRENE(['IRENE',
+                                   '-c', conf_file_name_mc,
+                                   '-i', PATH_IN,
+                                   '-o', PATH_OUT,
+                                   '-r', '0'])
     assert nactual == 0
     assert empty == 1
