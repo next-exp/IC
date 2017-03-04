c772ccc88c617e8256386cfb370172e47131e7a4
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-03-04 22:13:45 +0100

    Demonstrate and fix bug in _store_pmaps
    Fix #151
    
    _store_pmaps calls _store_s12 wihout specifying the table that needs
    to be stored. The function points always to table s1t (the correct
    behaviour is to receive either s1t or s2t by argument and point to
    it).
    
    _store_s12 now receives the tables by argument. In addition the tables
    are not flushed in the store functions but only at the end of the loop
    (flushing every event is not necessary and may hinder performance)
    
    A test has been added, checking that the contents of the tables in
    memory (s1t, s2t and s2sit) are the same than the tables in file.

--- invisible_cities/cities/irene_test.py
+++ invisible_cities/cities/irene_test.py
@@ -13,6 +13,10 @@ import numpy  as np
 from pytest import mark, fixture
 
 from invisible_cities.cities.irene import Irene, IRENE
+from   invisible_cities.core.params import S12Params as S12P
+from   invisible_cities.core.params import SensorParams
+from invisible_cities.core.system_of_units_c import units
+import invisible_cities.core.tbl_functions as tbl
 
 @fixture(scope='module')
 def conf_file_name_mc(config_tmpdir):
@@ -107,3 +111,74 @@ def test_empty_events_issue_81(conf_file_name_mc, config_tmpdir):
                                    '-r', '0'])
     assert nactual == 0
     assert empty == 1
+
+@mark.slow
+def test_pmaps_store_issue_151(config_tmpdir):
+    # Check that PMAPS tables are written correctly
+    PATH_IN = os.path.join(os.environ['ICDIR'],
+              'database/test_data/',
+              'run_2983.h5')
+    PATH_OUT = os.path.join(str(config_tmpdir),
+              'run_2983_pmaps.h5')
+
+    s1par = S12P(tmin   = 10 * units.mus,
+                 tmax   = 590 * units.mus,
+                 stride = 4,
+                 lmin   = 6,
+                 lmax   = 16,
+                 rebin  = False)
+
+    s2par = S12P(tmin   = 590 * units.mus,
+                 tmax   = 620 * units.mus,
+                 stride = 40,
+                 lmin   = 100,
+                 lmax   = 1000,
+                 rebin  = True)
+
+    irene = Irene(run_number  = 2983,
+                 files_in    = [PATH_IN],
+                 file_out    = PATH_OUT,
+                 compression = 'ZLIB4',
+                 nprint      = 1,
+                 n_baseline  = 38000,
+                 thr_trigger = 5.0 * units.adc,
+                 n_MAU       = 100,
+                 thr_MAU     = 3.0 * units.adc,
+                 thr_csum_s1 = 0.2 * units.adc,
+                 thr_csum_s2 = 1.0 * units.adc,
+                 n_MAU_sipm  = 100,
+                 thr_sipm    = 5.0 * units.pes,
+                 s1_params   = s1par,
+                 s2_params   = s2par,
+                 thr_sipm_s2 = 30. * units.pes)
+
+    with tb.open_file(irene.input_files[0], "r") as h5in:
+        with tb.open_file(irene.output_file, "w",
+             filters = tbl.filters(irene.compression)) as pmap_file:
+
+             irene._set_pmap_store(pmap_file)
+             irene.eventsInfo = h5in.root.Run.events
+             pmtrwf  = h5in.root.RD. pmtrwf
+             sipmrwf = h5in.root.RD.sipmrwf
+
+             CWF = irene.deconv_pmt(pmtrwf[0])
+             csum, csum_mau = irene.calibrated_pmt_sum(CWF)
+             s1_ene, s1_indx = irene.csum_zs(csum_mau,
+                                             threshold = irene.thr_csum_s1)
+             s2_ene, s2_indx = irene.csum_zs(csum,
+                                             threshold = irene.thr_csum_s2)
+
+             sipmzs = irene.calibrated_signal_sipm(sipmrwf[0])
+             S1, S2 = irene.find_S12(s1_ene, s1_indx, s2_ene, s2_indx)
+             S2Si = irene.find_S2Si(S2, sipmzs)
+             irene._store_pmaps(0, 0, S1, S2, S2Si)
+
+             # number of rows in file for one event must be the same than
+             # length of time (or energy) array for S1/S2
+             irene.s1t.flush()
+             irene.s2t.flush()
+             irene.s2sit.flush()
+
+             assert irene.s1t.shape[0] == len(S1[0][0])
+             assert irene.s2t.shape[0] == len(S2[0][0])
+             assert irene.s2sit.shape[0] > 0
