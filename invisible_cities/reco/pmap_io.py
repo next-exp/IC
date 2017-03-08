cfbf027cab3234e4da23ef820e3acaa741bb6875
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-03-08 15:08:09 +0100

    Implement pmaps writer
    This work is a complete mess!
    
    The interfaces and other choices made in components brought together
    by this work, need serious review.
    
    However, as the functionality is urgently needed, I have hacked
    together something that seems to work, and am presenting it here for
    immediate inclusion, leaving technical debt reduction until a calmer
    time.
    
    This is a single commit resulting from the squashing of numerous steps
    by @jjgomezcadenas and @jacg. The more important portions of the
    original commit messages follow.
    
    ======================================================================
    
    (This class described just below has now been replaced by a context
    manager.)
    
    PMAPS are saved in three tables. The operation requires to set the
    store file as well as the PMAPS tables before the event loop, save
    PMAPS once per event in the event loop and flush the tables before
    closing PMAPS store table. A class is a convenient way to handle the
    management of tables and hid unncessary details to the client.
    
    The writer has a single public function, that accepts S1, S2 and S2Si
    objects ans saves them to pytables.
    
    ======================================================================
    
    After discussion (see #165) we have agreed to use always an absolute
    event number as key for S12 and S12L. Thus:
    
    S12  = {absolute_evt_number : {peak : namedtuple('t' 'E')}}
    S2Si = {absolute_evt_number : {peak : {nsipm:[E]}}}
    
    It follows that the fields evtDaq and nsample are not needed and can
    be removed from tables.
    
    ======================================================================
    
    Extend writer to add the capability to write run and event info.
    
    When writting PMAPS to file it is necessary to add two additional
    tables to the PMAPS tables, one describing run info the other
    describing event info. This commit modifies the writer interface so
    that run, event and timestamps are saved for each event.
    
    ======================================================================
    
    New format for S2Si.
    
    The new format for S2Si required modifications in both the writer and
    reader functions. The tests have been modified accordingly and
    expanded. The test now writes from transient to file, then reads back
    from file and compares with the original objects, providing a robust
    consistency check.
    
    ======================================================================
    
    Clean up pmaps functions.
    
    module pmaps_functions.py contained a number of functions involving
    manipulations of data frames, which are not used in main stream
    reconstruction and not formally tested. The functions are removed from
    this module and placed in a new module, df_pmaps.py.
    
    ======================================================================
    
    Complete refactoring of Irene.
    
    ======================================================================
    
    Return S12/S2Si instances from find functions
    
    They used to return dictionaries, forcing the client to make the
    conversion.
    
    ======================================================================
    
    Add file property to pmap_writer
    
    This is a (hopefully) temporary workaround to get the new
    MTTracksWriter to work in the half-refactored Irene.
    
    ======================================================================
    
    Move pmaps fixtures up to conftest in invisible_cities
    
    So that they can be reused in the pmap_writer tests
    
    ======================================================================
    
    Use pmaps reading fixtures in writer test
    
    ======================================================================
    
    Make df_to_dict functions return S12/S2Si instances
    
    ======================================================================
    
    Fundamentally, the interfaces are abominable and must be changed. But
    we need this functionality in the short-term, so we'll go with this
    for now.

---
+++ invisible_cities/reco/pmap_io.py
@@ -0,0 +1,115 @@
+import tables as tb
+
+import invisible_cities.reco.nh5 as table_formats
+import invisible_cities.reco.tbl_functions as tbl
+
+
+class pmap_writer:
+
+    def __init__(self, filename, compression = 'ZLIB4'):
+        self._hdf5_file = tb.open_file(filename, 'w')
+        self._pmp_tables, self._run_tables = _make_tables(self._hdf5_file,
+                                                          compression)
+
+    def __call__(self, run_number, event_number, timestamp, s1, s2, s2si):
+        s1  .store(self._pmp_tables[0], event_number)
+        s2  .store(self._pmp_tables[1], event_number)
+        s2si.store(self._pmp_tables[2], event_number)
+        run_writer(self._run_tables[0], run_number)
+        event_writer(self._run_tables[1], event_number, timestamp)
+
+    def close(self):
+        self._hdf5_file.close()
+
+    @property
+    def file(self):
+        return self._hdf5_file
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+
+def _make_tables(hdf5_file, compression):
+
+    c = tbl.filters(compression)
+
+    pmaps_group  = hdf5_file.create_group(hdf5_file.root, 'PMAPS')
+    rungroup     = hdf5_file.create_group(hdf5_file.root, "RunEvtInfo")
+
+    RunInfo, EventInfo = table_formats.RunInfo, table_formats.EventInfo
+    MKT = hdf5_file.create_table
+    s1         = MKT(pmaps_group, 'S1'  , S12 .table_format,   "S1 Table", c)
+    s2         = MKT(pmaps_group, 'S2'  , S12 .table_format,   "S2 Table", c)
+    s2si       = MKT(pmaps_group, 'S2Si', S2Si.table_format, "S2Si Table", c)
+
+    run_info   = MKT(rungroup,   "runInfo",   RunInfo,   "run info table", c)
+    event_info = MKT(rungroup, "eventInfo", EventInfo, "event info table", c)
+
+    pmp_tables = (s1, s2, s2si)
+    run_tables = (run_info, event_info)
+
+    for table in pmp_tables:
+        table.cols.event.create_index()
+
+    return pmp_tables, run_tables
+
+
+def run_writer(table, run_number):
+    row = table.row
+    row['run_number'] = run_number
+    row.append()
+
+def event_writer(table, event_number, timestamp):
+    row = table.row
+    row["evt_number"] = event_number
+    row["timestamp"] = timestamp
+    row.append()
+
+class S12(dict):
+    """Defines an S12 type."""
+
+    table_format = table_formats.S12
+
+    def store(self, table, event_number):
+        row = table.row
+        for peak_number, (ts, Es) in self.items():
+            assert len(ts) == len(Es)
+            for t, E in zip(ts, Es):
+                row["event"] = event_number
+                row["peak"]  =  peak_number
+                row["time"]  = t
+                row["ene"]   = E
+                row.append()
+
+class S2Si(dict):
+    """Defines an S2Si type."""
+
+    table_format = table_formats.S2Si
+
+    def store(self, table, event_number):
+        row = table.row
+        for peak, sipm in self.items():
+            for nsipm, ene in sipm.items():
+                for E in ene:
+                    row["event"]   = event_number
+                    row["peak"]    = peak
+                    row["nsipm"]   = nsipm
+                    #row["nsample"] = nsample
+                    row["ene"]     = E
+                    row.append()
+
+    #
+    # def store(self, table, event_number):
+    #     row = table.row
+    #     for peak_number, sipms in self.items():
+    #         for nsipm, Es in sipms:
+    #             for nsample, E in enumerate(Es):
+    #                 row["event"]   = event_number
+    #                 row["peak"]    =  peak_number
+    #                 row["nsipm"]   = nsipm
+    #                 #row["nsample"] = nsample
+    #                 row["ene"]     = E
+    #                 row.append()
