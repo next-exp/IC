3f1a1934665b1557192e10d99914e98af63d1418
Gonzalo <gonzaponte@gmail.com>
2017-03-09 16:01:48 +0100

    Introduce interface to convert S2Si DF to dictionaries
    A new cython function (df_to_s2si_dict) has been implemented to
    provide a better, more comfortable interface with S2Si PMaps. The
    function takes the S2Si PMaps in DF format and provides a dictionary
    like {evt: {peak: {sipm: E}}}, where evt is the event number, peak is
    the peak number, sipm is the SiPM ID number and E is a numpy array
    with the charge of the corresponding SiPM for each time slice. These
    slices are matched with those of the S2-PMT PMaps, so no time array is
    given. In order to fulfill this condition, the E array is filled with
    zeros when empty (no charge in the SiPM) slices are found.

--- invisible_cities/reco/pmaps_functions_c.pyx
+++ invisible_cities/reco/pmaps_functions_c.pyx
@@ -38,3 +38,43 @@ cpdef df_to_pmaps_dict(df, max_events=None):
             current_event[peak_number] = Peak(np.array(t), np.array(E))
 
     return all_events
+
+
+cpdef dict df_to_s2si_dict(df, max_events=None):
+    cdef dict  all_events = {}
+    cdef dict  current_event
+    cdef dict  current_peak
+    cdef tuple current_sipm
+
+    cdef int   [:] event   = df.event  .values
+    cdef char  [:] peak    = df.peak   .values
+    cdef short [:] nsipm   = df.nsipm  .values
+    cdef short [:] nsample = df.nsample.values
+    cdef float [:] ene     = df.ene    .values
+
+    cdef int  df_size = len(df.index)
+    cdef long limit = np.iinfo(int).max if max_events is None or max_events < 0 else max_events
+
+    cdef int i
+    for i in range(df_size):
+        if event[i] >= limit: break
+
+        current_event = all_events   .setdefault(event[i],  {}     )
+        current_peak  = current_event.setdefault( peak[i],  {}     )
+        current_sipm  = current_peak .setdefault(nsipm[i], ([], []))
+        current_sipm[0].append(nsample[i])
+        current_sipm[1].append(    ene[i])
+
+    cdef int  ID
+    cdef list sample
+    cdef list energy
+    # Postprocessing: Turn lists to numpy arrays before returning and fill
+    # empty slices with zeros
+    for current_event in all_events.values():
+        for current_peak in current_event.values():
+            for ID, (sample, energy) in current_peak.items():
+                maxsample                = np.max(sample) + 1
+                current_peak[ID]         = np.zeros(maxsample)
+                current_peak[ID][sample] = energy
+
+    return all_events
