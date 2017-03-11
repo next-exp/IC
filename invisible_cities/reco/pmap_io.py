e49d7a6ab41502dbfaeb9f6bd105f8a3c555fd9c
Jacek Generowicz <github@my-post-office.net>
2017-03-11 11:17:17 +0100

    Trying to understand S12 etc.

--- invisible_cities/reco/pmap_io.py
+++ invisible_cities/reco/pmap_io.py
@@ -1,6 +1,6 @@
 import tables as tb
 
-from   invisible_cities.reco.nh5 import S12, S2Si
+import invisible_cities.reco.nh5 as table_formats
 import invisible_cities.reco.tbl_functions as tbl
 
 
@@ -11,9 +11,9 @@ class pmap_writer:
         self._tables = _make_tables(self._hdf5_file, compression)
 
     def __call__(self, event_number, s1, s2, s2si):
-        _store_s12 (s1,   self._tables[0], event_number)
-        _store_s12 (s2,   self._tables[1], event_number)
-        _store_s2si(s2si, self._tables[2], event_number)
+        s1  .store(self._tables[0], event_number)
+        s2  .store(self._tables[1], event_number)
+        s2si.store(self._tables[2], event_number)
 
     def close(self):
         self._hdf5_file.close()
@@ -31,9 +31,9 @@ def _make_tables(hdf5_file, compression):
 
     pmaps_group = hdf5_file.create_group(hdf5_file.root, 'PMAPS')
 
-    s1   = hdf5_file.create_table(pmaps_group, 'S1'  , S12 ,   "S1 Table", c)
-    s2   = hdf5_file.create_table(pmaps_group, 'S2'  , S12 ,   "S2 Table", c)
-    s2si = hdf5_file.create_table(pmaps_group, 'S2Si', S2Si, "S2Si Table", c)
+    s1   = hdf5_file.create_table(pmaps_group, 'S1'  , S12 .table_format,   "S1 Table", c)
+    s2   = hdf5_file.create_table(pmaps_group, 'S2'  , S12 .table_format,   "S2 Table", c)
+    s2si = hdf5_file.create_table(pmaps_group, 'S2Si', S2Si.table_format, "S2Si Table", c)
 
     all_tables = (s1, s2, s2si)
 
@@ -43,26 +43,52 @@ def _make_tables(hdf5_file, compression):
     return all_tables
 
 
-def _store_s12(event, table, event_number):
-    row = table.row
-    for peak_number, (ts, Es) in event.items():
-        assert len(ts) == len(Es)
-        for t, E in zip(ts, Es):
-            row["event"] = event_number
-            row["peak"]  =  peak_number
-            row["time"]  = t
-            row["ene"]   = E
-            row.append()
+# I think that an event is made up of a collection of S1s, S2s. The 
 
+# I think that this is something like the (integrated) set of data
+# recorded by the PMTs in single event ... with two instances per
+# event: one for the S1s, one for the S2s
+class S12: # Need a better name
 
-def _store_s2si(event, table, event_number):
-    row = table.row
-    for peak_number, sipms in event.items():
-        for nsipm, Es in sipms:
-            for nsample, E in enumerate(Es):
-                row["event"]   = event_number
-                row["peak"]    =  peak_number
-                row["nsipm"]   = nsipm
-                row["nsample"] = nsample
-                row["ene"]     = E
+    def __init__(self, data):
+        # Temporary solution, can do better
+        self._data = data
+
+    # Other S12 utilities go here, for example statistical functions
+
+    table_format = table_formats.S12
+
+    def store(self, table, event_number):
+        row = table.row
+        for peak_number, (ts, Es) in self._data.items():
+            assert len(ts) == len(Es)
+            for t, E in zip(ts, Es):
+                row["event"] = event_number
+                row["peak"]  =  peak_number
+                row["time"]  = t
+                row["ene"]   = E
                 row.append()
+
+# I think that tis is something like the set of data recorded by the
+# SiPMs during a single event ... only used for S2s
+class S2Si: # Need a better name
+
+    def __init__(self, data):
+        # Temporary solution, can do better
+        self._data = data
+
+    # Other S2si utilities go here, for example statistical functions
+
+    table_format = table_formats.S2Si
+
+    def store(self, table, event_number):
+        row = table.row
+        for peak_number, sipms in self._data.items():
+            for nsipm, Es in sipms:
+                for nsample, E in enumerate(Es):
+                    row["event"]   = event_number
+                    row["peak"]    =  peak_number
+                    row["nsipm"]   = nsipm
+                    row["nsample"] = nsample
+                    row["ene"]     = E
+                    row.append()
--- invisible_cities/reco/pmap_io_test.py
+++ invisible_cities/reco/pmap_io_test.py
@@ -5,7 +5,7 @@ import os
 import tables as tb
 import numpy  as np
 
-from invisible_cities.reco.pmap_io           import pmap_writer
+from invisible_cities.reco.pmap_io           import pmap_writer, S12, S2Si
 from invisible_cities.core.system_of_units_c import units
 from invisible_cities.reco.pmaps_functions   import read_pmaps
 
@@ -16,25 +16,26 @@ def test_pmap_writer_mc(config_tmpdir):
     PMP_file_name = os.path.join(str(config_tmpdir), 'test_pmaps_mc.h5')
 
     run_number = 0
-    S1 = {0:(np.random.rand(5),
-             np.random.rand(5))}
-    S2 = S1
-    S2Si = {0:[(1, np.random.rand(5)),
-               (2, np.random.rand(5)),
-               (3, np.random.rand(5))]}
+    S1 = S12({0:(np.random.rand(5),
+                 np.random.rand(5))})
+    S2 = S12({0:(np.random.rand(5),
+                 np.random.rand(5))})
+    Si = S2Si({0:[(1, np.random.rand(5)),
+                  (2, np.random.rand(5)),
+                  (3, np.random.rand(5))]})
 
     # Automatic closing example
     with pmap_writer(PMP_file_name) as write:
-        write(0, S1, S2, S2Si)
+        write(0, S1, S2, Si)
 
     # Manual closing example
     write = pmap_writer(PMP_file_name)
-    write(0, S1, S2, S2Si)
+    write(0, S1, S2, Si)
     write.close()
 
     s1df, s2df, s2sidf = read_pmaps(PMP_file_name)
-    np.testing.assert_allclose(s1df.time.values, S1[0][0])
-    np.testing.assert_allclose(s2df.time.values, S2[0][0])
-    dim = len(S2Si[0][0][1])
-    assert len(s2sidf) == len(S2Si[0]) * dim
-    np.testing.assert_allclose(s2sidf.ene.values[0:dim], S2Si[0][0][1])
+    np.testing.assert_allclose(s1df.time.values, S1._data[0][0])
+    np.testing.assert_allclose(s2df.time.values, S2._data[0][0])
+    dim = len(Si._data[0][0][1])
+    assert len(s2sidf) == len(Si._data[0]) * dim
+    np.testing.assert_allclose(s2sidf.ene.values[0:dim], Si._data[0][0][1])
