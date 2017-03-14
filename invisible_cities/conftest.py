ce5f79802999646d9bc61794298adee1b60eaf06
Jacek Generowicz <github@my-post-office.net>
2017-03-14 09:47:45 +0100

    Add ICDIR fixture
    `os.environ['ICDIR']` appeared lots of times in lots of tests. It's a
    pain to write. So provide a `ICDIR` fixture to ease the pain.

--- invisible_cities/conftest.py
+++ invisible_cities/conftest.py
@@ -2,6 +2,10 @@ import pytest
 import os
 
 @pytest.fixture(scope = 'session')
+def ICDIR():
+    return os.environ['ICDIR']
+
+@pytest.fixture(scope = 'session')
 def irene_diomira_chain_tmpdir(tmpdir_factory):
     return tmpdir_factory.mktemp('irene_diomira_tests')
 
@@ -14,7 +18,7 @@ def config_tmpdir(tmpdir_factory):
                           'electrons_511keV_z250_RWF.h5',
                           'electrons_1250keV_z250_RWF.h5',
                           'electrons_2500keV_z250_RWF.h5'])
-def electron_RWF_file(request):
-    return os.path.join(os.environ['ICDIR'],
+def electron_RWF_file(request, ICDIR):
+    return os.path.join(ICDIR,
                         'database/test_data',
                         request.param)
