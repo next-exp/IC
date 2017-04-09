3dda4b2c175c8745c7b42a8f099a808d3c78c9a2
Gonzalo <gonzaponte@gmail.com>
2017-04-09 13:50:11 +0200

    Generic class to perform corrections

--- invisible_cities/reco/params.py
+++ invisible_cities/reco/params.py
@@ -1,4 +1,7 @@
 import sys
+
+import numpy as np
+
 from collections import namedtuple
 
 
@@ -29,3 +32,37 @@ for name, attrs in (
 
 # Leave nothing but the namedtuple types in the namespace of this module
 del name, namedtuple, sys, this_module, _add_namedtuple_in_this_module
+
+
+class Correction:
+    def __init__(self, xs, es, ss, norm_opt = "max"):
+        self.xs = np.array(xs, dtype=float, ndmin=2).T
+        self.es = np.array(es, dtype=float)
+        self.ss = np.array(es, dtype=float)
+
+        self.e_norm, self.s_norm = \
+        self._find_norm_values(norm_opt)
+
+        self.s0 = (self.s_norm/self.e_norm)**2
+
+    def _find_norm_values(self, norm_opt):
+        if   norm_opt == "max": # normalize to max energy
+            index  = np.argmax(self.es)
+        elif norm_opt == "center":
+            index  = [i//2 for i in self.es.shape]
+        else:
+            raise ValueError("Normalization option not recognized: {}".format(norm_opt))
+        return self.es[index], self.ss[index]
+
+    def _find_closest(self, x):
+        return np.apply_along_axis(np.argmin, 0, abs(x-self.xs))
+
+    def __call__(self, *x):
+        x_closest   = np.apply_along_axis(self._find_closest, 1, np.array(x, ndmin=2))
+        e_closest   = self.es[x_closest]
+        s_closest   = self.ss[x_closest]
+        correction  = self.e_norm/e_closest
+        uncertainty = correction * (self.s0 + (s_closest/e_closest)**2)**0.5
+        return correction, uncertainty
+
+
