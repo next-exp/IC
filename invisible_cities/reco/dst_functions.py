7ff5045179d52f5fbb50dd7c5290e575a391e0b1
Gonzalo <gonzaponte@gmail.com>
2017-04-17 13:56:08 +0200

    functions to load corrections

--- invisible_cities/reco/dst_functions.py
+++ invisible_cities/reco/dst_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tables as tb
 
+from params import Correction
 
 def load_dst(filename, group, node):
     with tb.open_file(filename) as h5in:
@@ -11,3 +12,13 @@ def load_dst(filename, group, node):
 def load_dsts(dst_list, group, node):
     dsts = [load_dst(filename, group, node) for filename in dst_list]
     return pd.concat(dsts)
+
+
+def load_z_corrections(filename):
+    dst = load_dst(filename, "Corrections", "Zcorrections")
+    return Correction(dst.z, dst.factor, dst.uncertainty)
+
+
+def load_xy_corrections(filename):
+    dst = load_dst(filename, "Corrections", "XYcorrections")
+    return Correction((dst.x, dst.y), dst.factor, dst.uncertainty)
