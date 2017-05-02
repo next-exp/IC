8adc295fa1c2cf1e309180599a31f7a8bbbdb775
jmbenlloch <info@jmbenlloch.net>
2017-05-02 16:49:40 +0200

    Add test

--- invisible_cities/database/load_db_test.py
+++ invisible_cities/database/load_db_test.py
@@ -13,8 +13,18 @@ def test_pmts_pd():
     columns =['SensorID', 'ChannelID', 'PmtID', 'Active', 'X', 'Y',
               'coeff_blr', 'coeff_c', 'adc_to_pes', 'noise_rms', 'Sigma']
     assert columns == list(pmts)
+    assert pmts['PmtID'].str.startswith('PMT').all()
     assert pmts.shape[0] == 12
 
+def test_pmts_MC_pd():
+    """Check that we retrieve the correct number of PMTs."""
+    mc_run = 0
+    pmts = DB.DataPMT(mc_run)
+    columns =['SensorID', 'ChannelID', 'PmtID', 'Active', 'X', 'Y',
+              'coeff_blr', 'coeff_c', 'adc_to_pes', 'noise_rms', 'Sigma']
+    assert columns == list(pmts)
+    assert pmts['PmtID'].str.startswith('PMT').all()
+    assert pmts.shape[0] == 12
 
 def test_sipm_pd():
     """Check that we retrieve the correct number of SiPMs."""
