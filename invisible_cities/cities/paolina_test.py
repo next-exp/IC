a3c66f360108b1927f69026f026fbcb4782e793e
Jacek Generowicz <github@my-post-office.net>
2017-05-10 15:58:46 +0200

    Hypothesize voxelize_hits energy conservation test

--- invisible_cities/cities/paolina_test.py
+++ invisible_cities/cities/paolina_test.py
@@ -1,9 +1,19 @@
+from hypothesis            import given
+from hypothesis.strategies import lists
+from hypothesis.strategies import floats
+from hypothesis.strategies import builds
+
 from . paolina import Hit
 from . paolina import Voxel
 from . paolina import voxelize_hits
 
-def test_voxelize_hits_does_not_lose_energy():
-    hits = [Hit(10.3, 20.9, 30.2, 123.2)]
+posn = floats(min_value=0, max_value=100)
+ener = posn
+
+@given(lists(builds(Hit, posn, posn, posn, ener),
+             min_size = 0,
+             max_size = 30))
+def test_voxelize_hits_does_not_lose_energy(hits):
     voxels = voxelize_hits(hits, None)
 
     def sum_energy(seq):
