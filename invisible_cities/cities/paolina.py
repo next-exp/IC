ae2b25649b272652a6aebdff3279134593793fc1
Jacek Generowicz <github@my-post-office.net>
2017-05-10 17:57:57 +0200

    Add bounding_box function and test

--- invisible_cities/cities/paolina.py
+++ invisible_cities/cities/paolina.py
@@ -1,7 +1,37 @@
+from functools import reduce
+
 from collections import namedtuple
 
-Hit   = namedtuple('Hit',   'x, y, z, E')
-Voxel = namedtuple('Voxel', 'x, y, z, E')
+import numpy as np
+
+class Hit:
+
+    def __init__(self, x,y,z, E):
+        self.pos = np.array((x,y,z))
+        self.E   = E
+
+    def __str__(self):
+        return '<{} {} {}>'.format(self.__class__.__name__,
+                                   self.pos.tolist(), self.E)
+    __repr__ = __str__
+
+class Voxel:
+
+    def __init__(self, x,y,z, E):
+        self.pos = np.array((x,y,z))
+        self.E   = E
+
+    __str__  = Hit.__str__
+    __repr__ =     __str__
+
+MAX3D = np.array([float(' inf')] * 3)
+MIN3D = np.array([float('-inf')] * 3)
+
+def bounding_box(seq):
+    posns = [x.pos for x in seq]
+    return (reduce(np.minimum, posns, MAX3D),
+            reduce(np.maximum, posns, MIN3D))
+
 
 def voxelize_hits(hits, voxel_dimensions):
-    return [Voxel(1,1,1, h.E) for h in hits]
+    return [Voxel(*hits[0].pos, h.E) for h in hits]
--- invisible_cities/cities/paolina_test.py
+++ invisible_cities/cities/paolina_test.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from numpy.testing import assert_almost_equal
+
 from hypothesis            import given
 from hypothesis.strategies import lists
 from hypothesis.strategies import floats
@@ -5,14 +9,18 @@ from hypothesis.strategies import builds
 
 from . paolina import Hit
 from . paolina import Voxel
+from . paolina import bounding_box
 from . paolina import voxelize_hits
 
 posn = floats(min_value=0, max_value=100)
 ener = posn
 
-@given(lists(builds(Hit, posn, posn, posn, ener),
-             min_size = 0,
-             max_size = 30))
+bunch_of_hits = lists(builds(Hit, posn, posn, posn, ener),
+                      min_size = 0,
+                      max_size = 30)
+
+
+@given(bunch_of_hits)
 def test_voxelize_hits_does_not_lose_energy(hits):
     voxels = voxelize_hits(hits, None)
 
@@ -20,3 +28,26 @@ def test_voxelize_hits_does_not_lose_energy(hits):
         return sum(e.E for e in seq)
 
     assert sum_energy(hits) == sum_energy(voxels)
+
+@given(bunch_of_hits)
+def test_bounding_box(hits):
+    if not len(hits): # TODO: deal with empty sequences
+        return
+
+    lo, hi = bounding_box(hits)
+
+    mins = [float(' inf')] * 3
+    maxs = [float('-inf')] * 3
+
+    for hit in hits:
+        assert lo[0] <= hit.pos[0] <= hi[0]
+        assert lo[1] <= hit.pos[1] <= hi[1]
+        assert lo[2] <= hit.pos[2] <= hi[2]
+
+        for d in range(3):
+            mins[d] = min(mins[d], hit.pos[d])
+            maxs[d] = max(maxs[d], hit.pos[d])
+
+    for d in range(3):
+        assert_almost_equal(mins[d], lo[d])
+        assert_almost_equal(maxs[d], hi[d])
