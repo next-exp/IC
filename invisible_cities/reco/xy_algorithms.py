cd4a49cf13ff6a10c1025c787f2eae10ae83baa8
jmbenlloch <info@jmbenlloch.net>
2017-05-18 14:50:15 +0200

    Refactor Corona
    - Decrypt variable names
    
    - Remove select_sipms

--- invisible_cities/reco/xy_algorithms.py
+++ invisible_cities/reco/xy_algorithms.py
@@ -13,9 +13,6 @@ def barycenter(pos, qs):
     std = np.average((pos - mu) ** 2, weights=qs, axis=0)
     return Cluster(sum(qs), mu, std, len(qs))
 
-def select_sipms(sis, pos, qs):
-    return pos[sis], qs[sis]
-
 def discard_sipms(sis, pos, qs):
     return np.delete(pos, sis, axis=0), np.delete(qs, sis)
 
@@ -24,11 +21,11 @@ def get_nearby_sipm_inds(cs, d, pos, qs):
     return np.where(np.linalg.norm(pos - cs, axis=1) <= d)[0]
 
 
-def corona(pos, qs, Qthr  =  0*units.pes,
-                    Qlm   =  5*units.pes,
-                    slm   = 15*units.mm ,
-                    rmax  = 25*units.mm ,
-                    msipm =  3          ):
+def corona(pos, qs, Qthr           =  0 * units.pes,
+                    Qlm            =  5 * units.pes,
+                        lm_radius  = 15 * units.mm,
+                    new_lm_radius  = 25 * units.mm,
+                    msipm          =  3):
     """
     corona creates a list of Clusters by
     first , identifying a loc max (gonz wanted more precise than just max sipm)
@@ -39,35 +36,41 @@ def corona(pos, qs, Qthr  =  0*units.pes,
     kwargs
     Qthr : SiPMs with less than Qthr pes are ignored
     Qlm  : local maxima must have a SiPM with at least T pes
-    slm  : all SiPMs within slm distance from the local max SiPM are used (by
-           barycenter) to compute the approximate center of the local max.
-    rmax : xs,ys,qs, of SiPMs within rmax of a local max are used by barycenter
-           to compute a Cluster.
-    msipm: the minimum number of SiPMs found with
+    lm_radius  : all SiPMs within lm_radius distance from the local max
+           SiPM are used (by barycenter) to compute the approximate center
+            of the local max.
+    new_lm_radius : xs,ys,qs, of SiPMs within new_lm_radius of a local max
+           are used by barycenter to compute a Cluster.
+    msipm: the minimum number of SiPMs needed to make a cluster
 
     returns
     c    : a list of Clusters
     """
     c  = []
     # Keep SiPMs with at least Qthr pes
-    pos, qs = select_sipms(np.where(qs >= Qthr)[0], pos, qs)
+    above_threshold = np.where(qs >= Qthr)[0]
+    pos, qs = pos[above_threshold], qs[above_threshold]
     # While there are more local maxima
     while len(qs) > 0:
-        mx = np.argmax(qs)       # SiPM with largest Q
-        if qs[mx] < Qlm: break   # largest Q remaining is negligible
+        hottest_sipm = np.argmax(qs)       # SiPM with largest Q
+        if qs[hottest_sipm] < Qlm: break   # largest Q remaining is negligible
 
-        # find locmax (the baryc of charge in SiPMs less than slm from mx)
-        sis = get_nearby_sipm_inds(pos[mx], slm, pos, qs)
-        lm  = barycenter(*select_sipms(     sis, pos, qs))
+        # find locmax (the baryc of charge in SiPMs less than lm_radius from hottest_sipm)
+        within_lm_radius = get_nearby_sipm_inds(pos[hottest_sipm], lm_radius, pos, qs)
+        new_local_maximum  = barycenter(pos[within_lm_radius],
+                                        qs [within_lm_radius]).pos
 
-        # rsis is an array of the responsive sipms less than rmax from locmax
-        rsis = get_nearby_sipm_inds(lm.pos, rmax, pos, qs)
+        # new_lm_radius is an array of the responsive sipms less than
+        # new_lm_radius from locmax
+        within_new_lm_radius = get_nearby_sipm_inds(new_local_maximum,
+                                                    new_lm_radius, pos, qs)
 
-        # if rsis have at least msipms, get the barycenter
-        if len(rsis) >= msipm:
-            c.append(barycenter(*select_sipms(rsis, pos, qs)))
+        # if there are at least msipms within_new_lm_radius, get the barycenter
+        if len(within_new_lm_radius) >= msipm:
+            c.append(barycenter(pos[within_new_lm_radius],
+                                qs [within_new_lm_radius]))
 
         # delete the SiPMs contributing to this cluster
-        pos, qs = discard_sipms(rsis, pos, qs)
+        pos, qs = discard_sipms(within_new_lm_radius, pos, qs)
 
     return c
