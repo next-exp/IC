fbcf182fecdc32a77897de1f31feedb0ff23c144
Gonzalo <gonzaponte@gmail.com>
2017-05-18 19:26:06 +0200

    fix import

--- invisible_cities/reco/dst_functions.py
+++ invisible_cities/reco/dst_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tables as tb
 
-from params import Correction
+from . params import Correction
 
 def load_dst(filename, group, node):
     with tb.open_file(filename) as h5in:
