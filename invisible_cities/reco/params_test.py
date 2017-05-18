32b9f8bf9d803d47e9fdf5c17647115c44216dfd
Gonzalo <gonzaponte@gmail.com>
2017-05-18 18:53:34 +0200

    use namedtuples

--- invisible_cities/reco/params_test.py
+++ invisible_cities/reco/params_test.py
@@ -5,13 +5,18 @@ from ..reco.params import Correction
 
 from numpy.testing import assert_equal, assert_allclose
 from pytest        import fixture, mark
+from collections   import namedtuple
+
+
+data_1d = namedtuple("data_1d",   "z E prof")
+data_2d = namedtuple("data_2d", "x y E prof")
 
 
 @fixture(scope='session')
 def toy_data_1d():
     z = np.linspace(  0, 100,     10)
     E = np.linspace(1e3, 2e3, z.size)
-    return z, E
+    return data_1d(z, E, None)
 
 
 @fixture(scope='session')
@@ -19,7 +24,7 @@ def toy_data_2d():
     x    = np.linspace(  0, 100, 10)
     y    = np.linspace(  0, 100, 10)
     E    = np.linspace(1e3, 2e3, x.size*y.size).reshape(x.size, y.size)
-    return x, y, E
+    return data_2d(x, y, E, None)
 
 
 @fixture(scope='session')
@@ -29,7 +34,7 @@ def gauss_data_1d():
     zevt = np.random.uniform(0, 500, size=Nevt)
     Eevt = np.random.normal(mean(zevt), mean(zevt)**0.5)
     prof = fitf.profileX(zevt, Eevt, 50, (0, 500))
-    return zevt, Eevt, prof
+    return data_1d(zevt, Eevt, prof)
 
 
 @fixture(scope='session')
@@ -40,18 +45,18 @@ def gauss_data_2d():
     yevt = np.random.uniform(-200, 200, size=Nevt)
     Eevt = np.random.normal(mean(xevt, yevt), mean(xevt, yevt)**0.5)
     prof = fitf.profileXY(xevt, yevt, Eevt, 50, 50, (-200, 200), (-200, 200))
-    return xevt, yevt, Eevt, prof
+    return data_2d(xevt, yevt, Eevt, prof)
 
 
 def test_correction_normalize_max(gauss_data_1d):
-    x, y, ye = gauss_data_1d[-1]
+    x, y, ye = gauss_data_1d.prof
     c = Correction(x, y, ye, "max")
     assert np.min(c.fs) == 1
     assert np.argmin(c.fs) == 0
 
 
 def test_correction_normalize_center(gauss_data_2d):
-    x, y, z, ze = gauss_data_2d[-1]
+    x, y, z, ze = gauss_data_2d.prof
     c = Correction((x, y), z, ze, "center")
     assert_equal(np.argwhere(c.fs==1)[0], np.array(z.shape)//2)
 
@@ -62,7 +67,7 @@ def test_correction_unnormalized():
 
 
 def test_corrections_toy_1d(toy_data_1d):
-    z, E = toy_data_1d
+    z, E, _ = toy_data_1d
     Emax = np.max(E)
     zcorr = Correction(z, E, np.ones_like(E), "max")
     E *= zcorr(z)[0]
@@ -70,11 +75,11 @@ def test_corrections_toy_1d(toy_data_1d):
 
 
 def test_corrections_toy_2d(toy_data_2d):
-    x, y, E = toy_data_2d
-    xycorr  = Correction((x, y), E, np.ones_like(E), "center")
-    y, x    = map(np.ndarray.flatten, np.meshgrid(x, y))
-    E       = E.flatten()
-    E      *= xycorr(x, y)[0]
+    x, y, E, _  = toy_data_2d
+    xycorr      = Correction((x, y), E, np.ones_like(E), "center")
+    y, x        = map(np.ndarray.flatten, np.meshgrid(x, y))
+    E           = E.flatten()
+    E          *= xycorr(x, y)[0]
     assert_allclose(E, np.mean(E))
 
 
