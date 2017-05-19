8f04b161018e26de8f5d4b804d6ba817f20f4466
Jacek Generowicz <github@my-post-office.net>
2017-05-19 17:24:20 +0200

    Implement and test None correction strategy

--- invisible_cities/reco/corrections.py
+++ invisible_cities/reco/corrections.py
@@ -1,5 +1,7 @@
-def Correction(xs, ys, E, _, __):
-    N = E / E.max()
+import numpy as np
+
+def Correction(xs, ys, E, _, strategy):
+    N = E / E.max() if strategy else np.ones_like(E)
     norm = { (x,y) : N[i,j]
              for i, x in enumerate(xs)
              for j, y in enumerate(ys) }
--- invisible_cities/reco/corrections_test.py
+++ invisible_cities/reco/corrections_test.py
@@ -1,10 +1,18 @@
+from collections import namedtuple
+
 import numpy as np
 
 from numpy.testing import assert_allclose
 
+from pytest import fixture
+
 from . corrections import Correction
 
-def test_correction():
+EnergyMap = namedtuple('EnergyMap',
+                       'E_max, E_00, x_0, y_0, x, y, E')
+
+@fixture(scope='session')
+def toy_energy_map():
     E_max = 120
     E_00  =  30
     x_0   =  10.1
@@ -13,10 +21,22 @@ def test_correction():
     y = np.array([y_0, 10.2, 10.3])
     E = np.array([[E_00,  40,  60],
                   [  80,  90, E_max]])
+    return EnergyMap(E_max, E_00, x_0, y_0, x, y, E)
 
-    U = np.ones_like(E)
 
-    correct = Correction(x, y, E, U, 'max')
 
-    correction = E_00 / E_max
-    assert_allclose(correct(x_0, y_0), correction)
+def test_energy_correction_max(toy_energy_map):
+    m = toy_energy_map
+    U = np.ones_like(m.E)
+    correct = Correction(m.x, m.y, m.E, U, 'max')
+    correction = m.E_00 / m.E_max
+    assert_allclose(correct(m.x_0, m.y_0), correction)
+
+
+def test_energy_correction_None(toy_energy_map):
+    m = toy_energy_map
+    U = np.ones_like(m.E)
+    correct = Correction(m.x, m.y, m.E, U, None)
+    correction = m.E_00 / m.E_max
+    NO_CORRECTION = 1
+    assert_allclose(correct(m.x_0, m.y_0), NO_CORRECTION)
