0347afa83fdee1da06777b3165df2db0c98d0d81
Gonzalo <gonzaponte@gmail.com>
2017-05-25 15:42:53 +0200

    reorganize inheritance chain

--- invisible_cities/cities/penthesilea.py
+++ invisible_cities/cities/penthesilea.py
@@ -11,12 +11,12 @@ import tables as tb
 from invisible_cities.core.configure         import configure
 from invisible_cities.core.system_of_units_c import units
 from invisible_cities.reco.dst_io            import HitCollection_writer
-from invisible_cities.cities.base_cities     import City, HitCollectionCity, merge_two_dicts
+from invisible_cities.cities.base_cities     import City, S12SelectorCity, HitCollectionCity
 from invisible_cities.reco.tbl_functions     import get_event_numbers_and_timestamps
 from invisible_cities.reco.pmaps_functions   import load_pmaps
 from invisible_cities.reco.xy_algorithms     import barycenter, find_algorithm
 
-class Penthesilea(City, HitCollectionCity):
+class Penthesilea(HitCollectionCity):
     def __init__(self,
                  run_number       = 0,
                  files_in         = None,
@@ -52,15 +52,14 @@ class Penthesilea(City, HitCollectionCity):
                  lifetime         = None,
                  reco_algorithm   = barycenter):
 
-
         City             .__init__(self,
-                                   run_number ,
-                                   files_in   ,
-                                   file_out   ,
-                                   compression,
-                                   nprint     )
+                                   run_number       = run_number,
+                                   files_in         = files_in,
+                                   file_out         = file_out,
+                                   compression      = compression,
+                                   nprint           = nprint)
 
-        HitCollectionCity.__init__(self,
+        S12SelectorCity  .__init__(self,
                                    drift_v          = drift_v,
 
                                    S1_Emin          = S1_Emin,
@@ -81,89 +80,15 @@ class Penthesilea(City, HitCollectionCity):
                                    S2_Hmax          = S2_Hmax,
                                    S2_NSIPMmin      = S2_NSIPMmin,
                                    S2_NSIPMmax      = S2_NSIPMmax,
-                                   S2_Ethr          = S2_Ethr,
+                                   S2_Ethr          = S2_Ethr)
 
+
+        HitCollectionCity.__init__(self,
                                    rebin            = rebin,
                                     z_corr_filename =  z_corr_filename,
                                    xy_corr_filename = xy_corr_filename,
                                    lifetime         = lifetime,
                                    reco_algorithm   = reco_algorithm)
-
-    config_file_format = City.config_file_format + """
-
-    RUN_NUMBER       {RUN_NUMBER}
-
-    # set_print
-    NPRINT           {NPRINT}
-
-    DRIFT_V          {DRIFT_V}
-
-    S1_EMIN          {S1_EMIN}
-    S1_EMAX          {S1_EMAX}
-    S1_LMIN          {S1_LMIN}
-    S1_LMAX          {S1_LMAX}
-    S1_HMIN          {S1_HMIN}
-    S1_HMAX          {S1_HMAX}
-    S1_ETHR          {S1_ETHR}
-
-    S2_NMIN          {S2_NMIN}
-    S2_NMAX          {S2_NMAX}
-    S2_EMIN          {S2_EMIN}
-    S2_EMAX          {S2_EMAX}
-    S2_LMIN          {S2_LMIN}
-    S2_LMAX          {S2_LMAX}
-    S2_HMIN          {S2_HMIN}
-    S2_HMAX          {S2_HMAX}
-    S2_ETHR          {S2_ETHR}
-    S2_NSIPMMIN      {S2_NSIPMMIN}
-    S2_NSIPMMAX      {S2_NSIPMMAX}
-
-    REBIN            {REBIN}
-    Z_CORR_FILENAME  {Z_CORR_FILENAME}
-    XY_CORR_FILENAME {XY_CORR_FILENAME}
-    RECO_ALGORITHM   {RECO_ALGORITHM}
-
-    # run
-    NEVENTS          {NEVENTS}
-    RUN_ALL          {RUN_ALL}
-    """
-
-    config_file_format = textwrap.dedent(config_file_format)
-
-    default_config = merge_two_dicts(
-        City.default_config,
-        dict(RUN_NUMBER        =     0,
-             NPRINT            =     1,
-
-             DRIFT_V           =   1.0,
-
-             S1_EMIN           =     0,
-             S1_EMAX           =    30,
-             S1_LMIN           =     4,
-             S1_LMAX           =    20,
-             S1_HMIN           =   0.5,
-             S1_HMAX           =    10,
-             S1_ETHR           =   0.5,
-
-             S2_NMIN           =     1,
-             S2_NMAX           =     5,
-             S2_EMIN           =   1e3,
-             S2_EMAX           =   1e8,
-             S2_LMIN           =     1,
-             S2_LMAX           =    20,
-             S2_HMIN           =   500,
-             S2_HMAX           =   1e5,
-             S2_ETHR           =     1,
-             S2_NSIPMMIN       =     2,
-             S2_NSIPMMAX       =  1000,
-
-             REBIN             = 1,
-             Z_CORR_FILENAME   = "",
-             XY_CORR_FILENAME  = "",
-             RECO_ALGORITHM    = "barycenter",
-
-             NEVENTS           =     3,
-             RUN_ALL           = False))
 
     def run(self, max_evt = -1):
         nevt_in = nevt_out = 0
@@ -258,7 +183,7 @@ def PENTHESILEA(argv = sys.argv):
 
     t0 = time.time()
     nevts = CFP.NEVENTS if not CFP.RUN_ALL else -1
-    # run
+
     nevt_in, nevt_out, ratio = penthesilea.run(max_evt = nevts)
     t1 = time.time()
     dt = t1 - t0
