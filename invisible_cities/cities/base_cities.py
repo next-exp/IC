d07e2727202a1da102b50904c01c0f10396fa40b
Jacek Generowicz <github@my-post-office.net>
2017-06-02 17:08:03 +0200

    Decouple filtering from event creation in Dorothea

--- invisible_cities/cities/base_cities.py
+++ invisible_cities/cities/base_cities.py
@@ -102,8 +102,10 @@ class City:
             print('event in file = {}, total = {}'
                   .format(evt, n_events_tot))
 
-    def max_events_reached(self, nmax, n_events_tot):
-        if n_events_tot >= nmax > -1:
+    def max_events_reached(self, nmax, n_events_in):
+        if nmax < 0:
+            return False
+        if n_events_in == nmax:
             print('reached max nof of events (= {})'
                   .format(nmax))
             return True
@@ -466,88 +468,15 @@ class PmapCity(CalibratedCity):
         if (not self.s1_params) or (not self.s2_params):
             raise IOError('must set S1/S2 parameters before running')
 
-class S12SelectorCity:
-    def __init__(self,
-                 drift_v     = 1 * units.mm / units.mus,
-
-                 S1_Nmin     = 0,
-                 S1_Nmax     = 1000,
-                 S1_Emin     = 0,
-                 S1_Emax     = np.inf,
-                 S1_Lmin     = 0,
-                 S1_Lmax     = np.inf,
-                 S1_Hmin     = 0,
-                 S1_Hmax     = np.inf,
-                 S1_Ethr     = 0,
-
-                 S2_Nmin     = 0,
-                 S2_Nmax     = 1000,
-                 S2_Emin     = 0,
-                 S2_Emax     = np.inf,
-                 S2_Lmin     = 0,
-                 S2_Lmax     = np.inf,
-                 S2_Hmin     = 0,
-                 S2_Hmax     = np.inf,
-                 S2_NSIPMmin = 1,
-                 S2_NSIPMmax = np.inf,
-                 S2_Ethr     = 0):
-
-        self.drift_v     = drift_v
-
-        self.S1_Nmin     = S1_Nmin
-        self.S1_Nmax     = S1_Nmax
-        self.S1_Emin     = S1_Emin
-        self.S1_Emax     = S1_Emax
-        self.S1_Lmin     = S1_Lmin
-        self.S1_Lmax     = S1_Lmax
-        self.S1_Hmin     = S1_Hmin
-        self.S1_Hmax     = S1_Hmax
-        self.S1_Ethr     = S1_Ethr
-
-        self.S2_Nmin     = S2_Nmin
-        self.S2_Nmax     = S2_Nmax
-        self.S2_Emin     = S2_Emin
-        self.S2_Emax     = S2_Emax
-        self.S2_Lmin     = S2_Lmin
-        self.S2_Lmax     = S2_Lmax
-        self.S2_Hmin     = S2_Hmin
-        self.S2_Hmax     = S2_Hmax
-        self.S2_NSIPMmin = S2_NSIPMmin
-        self.S2_NSIPMmax = S2_NSIPMmax
-        self.S2_Ethr     = S2_Ethr
-
-    def select_S1(self, s1s):
-        return pf.select_peaks(s1s,
-                               self.S1_Emin, self.S1_Emax,
-                               self.S1_Lmin, self.S1_Lmax,
-                               self.S1_Hmin, self.S1_Hmax,
-                               self.S1_Ethr)
-
-    def select_S2(self, s2s, sis):
-        s2s = pf.select_peaks(s2s,
-                              self.S2_Emin, self.S2_Emax,
-                              self.S2_Lmin, self.S2_Lmax,
-                              self.S2_Hmin, self.S2_Hmax,
-                              self.S2_Ethr)
-        sis = pf.select_Si(sis,
-                           self.S2_NSIPMmin, self.S2_NSIPMmax)
-
-        valid_peaks = set(s2s) & set(sis)
-        s2s = {peak_no: peak for peak_no, peak in s2s.items() if peak_no in valid_peaks}
-        sis = {peak_no: peak for peak_no, peak in sis.items() if peak_no in valid_peaks}
-        return s2s, sis
 
     def select_event(self, evt_number, evt_time, S1, S2, Si):
         evt       = KrEvent()
         evt.event = evt_number
         evt.time  = evt_time * 1e-3 # s
 
-        S1     = self.select_S1(S1)
-        S2, Si = self.select_S2(S2, Si)
-
-        if (not self.S1_Nmin <= len(S1) <= self.S1_Nmax or
-            not self.S2_Nmin <= len(S2) <= self.S2_Nmax):
-            return None
+        from .. filters.s1s2_filter import s1s2_filter
+        if not s1s2_filter(S1, S2, Si):
+            return
 
         evt.nS1 = len(S1)
         for peak_no, (t, e) in sorted(S1.items()):
--- invisible_cities/cities/dorothea.py
+++ invisible_cities/cities/dorothea.py
@@ -10,15 +10,19 @@ from .. core.configure         import configure
 from .. core.system_of_units_c import units
 
 from .. reco                   import tbl_functions as tbl
+from .. reco                   import pmaps_functions  as pmp
 from .. reco.dst_io            import kr_writer
+from .. reco.dst_io            import KrEvent
 from .. reco.pmaps_functions   import load_pmaps
 from .. reco.tbl_functions     import get_event_numbers_and_timestamps_from_file_name
 
+from .. filters.s1s2_filter    import s1s2_filter
+from .. filters.s1s2_filter    import S12Selector
+
 from .  base_cities            import City
-from .  base_cities            import S12SelectorCity
 
 
-class Dorothea(City, S12SelectorCity):
+class Dorothea(City):
     def __init__(self,
                  run_number  = 0,
                  files_in    = None,
@@ -54,30 +58,28 @@ class Dorothea(City, S12SelectorCity):
                              compression,
                              nprint     )
 
-        S12SelectorCity.__init__(self,
-                                 drift_v     = drift_v,
+        self.drift_v        = drift_v
+        self._s1s2_selector = S12Selector(S1_Nmin     = 1,
+                                          S1_Nmax     = 1,
+                                          S1_Emin     = S1_Emin,
+                                          S1_Emax     = S1_Emax,
+                                          S1_Lmin     = S1_Lmin,
+                                          S1_Lmax     = S1_Lmax,
+                                          S1_Hmin     = S1_Hmin,
+                                          S1_Hmax     = S1_Hmax,
+                                          S1_Ethr     = S1_Ethr,
 
-                                 S1_Nmin     = 1,
-                                 S1_Nmax     = 1,
-                                 S1_Emin     = S1_Emin,
-                                 S1_Emax     = S1_Emax,
-                                 S1_Lmin     = S1_Lmin,
-                                 S1_Lmax     = S1_Lmax,
-                                 S1_Hmin     = S1_Hmin,
-                                 S1_Hmax     = S1_Hmax,
-                                 S1_Ethr     = S1_Ethr,
-
-                                 S2_Nmin     = 1,
-                                 S2_Nmax     = S2_Nmax,
-                                 S2_Emin     = S2_Emin,
-                                 S2_Emax     = S2_Emax,
-                                 S2_Lmin     = S2_Lmin,
-                                 S2_Lmax     = S2_Lmax,
-                                 S2_Hmin     = S2_Hmin,
-                                 S2_Hmax     = S2_Hmax,
-                                 S2_NSIPMmin = S2_NSIPMmin,
-                                 S2_NSIPMmax = S2_NSIPMmax,
-                                 S2_Ethr     = S2_Ethr)
+                                          S2_Nmin     = 1,
+                                          S2_Nmax     = S2_Nmax,
+                                          S2_Emin     = S2_Emin,
+                                          S2_Emax     = S2_Emax,
+                                          S2_Lmin     = S2_Lmin,
+                                          S2_Lmax     = S2_Lmax,
+                                          S2_Hmin     = S2_Hmin,
+                                          S2_Hmax     = S2_Hmax,
+                                          S2_NSIPMmin = S2_NSIPMmin,
+                                          S2_NSIPMmax = S2_NSIPMmax,
+                                          S2_Ethr     = S2_Ethr)
 
     def run(self, nmax):
         self.display_IO_info(nmax)
@@ -107,8 +109,8 @@ class Dorothea(City, S12SelectorCity):
 
             event_numbers, timestamps = get_event_numbers_and_timestamps_from_file_name(filename)
 
-            nevt_in, nevt_out, max_events_reached = (
-             self._event_loop(event_numbers, timestamps, nmax, nevt_in, nevt_out, write_kr, S1s, S2s, S2Sis))
+            nevt_in, nevt_out, max_events_reached = self._event_loop(
+                event_numbers, timestamps, nmax, nevt_in, nevt_out, write_kr, S1s, S2s, S2Sis)
 
             if max_events_reached:
                 print('Max events reached')
@@ -122,27 +124,73 @@ class Dorothea(City, S12SelectorCity):
         max_events_reached = False
         for evt_number, evt_time in zip(event_numbers, timestamps):
             nevt_in +=1
-
+            if self.max_events_reached(nmax, nevt_in):
+                max_events_reached = True
+                break
             S1 = S1s  .get(evt_number, {})
             S2 = S2s  .get(evt_number, {})
             Si = S2Sis.get(evt_number, {})
 
-            evt = self.select_event(evt_number, evt_time, S1, S2, Si)
-
-            if evt:
-                nevt_out += 1
-                write_kr(evt)
+            if not s1s2_filter(self._s1s2_selector, S1, S2, Si):
+                continue
+            nevt_out += 1
+            evt = self._create_kr_event(evt_number, evt_time, S1, S2, Si)
+            write_kr(evt)
 
             if not nevt_in % self.nprint:
                 print("{} evts analyzed".format(nevt_in))
 
-            if self.max_events_reached(nmax, nevt_in):
-                max_events_reached = True
-                break
         return nevt_in, nevt_out, max_events_reached
 
+    def _create_kr_event(self, evt_number, evt_time, S1, S2, Si):
+        evt       = KrEvent()
+        evt.event = evt_number
+        evt.time  = evt_time * 1e-3 # s
+
+        evt.nS1 = len(S1)
+        for peak_no, (t, e) in sorted(S1.items()):
+            evt.S1w.append(pmp.width(t))
+            evt.S1h.append(np.max(e))
+            evt.S1e.append(np.sum(e))
+            evt.S1t.append(t[np.argmax(e)])
+
+        evt.nS2 = len(S2)
+        for peak_no, (t, e) in sorted(S2.items()):
+            s2time  = t[np.argmax(e)]
+
+            evt.S2w.append(pmp.width(t, to_mus=True))
+            evt.S2h.append(np.max(e))
+            evt.S2e.append(np.sum(e))
+            evt.S2t.append(s2time)
+
+            IDs, Qs = pmp.integrate_charge(Si[peak_no])
+            xsipms  = self.xs[IDs]
+            ysipms  = self.ys[IDs]
+            x       = np.average(xsipms, weights=Qs)
+            y       = np.average(ysipms, weights=Qs)
+            q       = np.sum    (Qs)
+
+            evt.Nsipm.append(len(IDs))
+            evt.S2q  .append(q)
+
+            evt.X    .append(x)
+            evt.Y    .append(y)
+
+            evt.Xrms .append((np.sum(Qs * (xsipms-x)**2) / (q - 1))**0.5)
+            evt.Yrms .append((np.sum(Qs * (ysipms-y)**2) / (q - 1))**0.5)
+
+            evt.R    .append((x**2 + y**2)**0.5)
+            evt.Phi  .append(np.arctan2(y, x))
+
+            dt  = s2time - evt.S1t[0] if len(evt.S1t) > 0 else -1e3
+            dt *= units.ns / units.mus
+            evt.DT   .append(dt)
+            evt.Z    .append(dt * units.mus * self.drift_v)
+
+        return evt
+
+
 def DOROTHEA(argv = sys.argv):
-    """Dorothea DRIVER"""
 
     # get parameters dictionary
     CFP = configure(argv)
