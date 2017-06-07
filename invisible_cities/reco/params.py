afc99fc525b585f2dab5755759275a93d4004d8d
Jacek Generowicz <github@my-post-office.net>
2017-06-07 13:33:12 +0200

    Add minmax type and use it in S12 and Pmap Params
    The type `minmax` (a named tuple with attributes `min` and `max`) is
    used to store pairs of values which together place limits on some
    quantity.

--- invisible_cities/reco/params.py
+++ invisible_cities/reco/params.py
@@ -10,13 +10,14 @@ def _add_namedtuple_in_this_module(name, attribute_names):
     setattr(this_module, name, new_nametuple)
 
 for name, attrs in (
+        ('minmax'         , 'min max'),
         ('RawVectors'     , 'event pmtrwf sipmrwf pmt_active sipm_active'),
         ('SensorParams'   , 'NPMT PMTWL NSIPM SIPMWL'),
         ('CalibParams'    , 'coeff_c, coeff_blr, adc_to_pes_pmt adc_to_pes_sipm'),
         ('DeconvParams'   , 'n_baseline thr_trigger'),
         ('CalibVectors'   , 'channel_id coeff_blr coeff_c adc_to_pes adc_to_pes_sipm pmt_active'),
-        ('S12Params'      , 'tmin tmax stride lmin lmax rebin'),
-        ('PmapParams'     ,'s1_params s2_params s1p_params s1_PMT_params s1p_PMT_params'),
+        ('S12Params'      , 'time stride length rebin'),
+        ('PmapParams'     , 's1_params s2_params s1p_params s1_PMT_params s1p_PMT_params'),
         ('ThresholdParams', 'thr_s1 thr_s2 thr_MAU thr_sipm thr_SIPM'),
         ('CalibratedSum'  , 'csum csum_mau'),
         ('CalibratedPMT'  , 'CPMT CPMT_mau'),
