e3bc678711e832807bde7bca12ca4aaad857aecd
Jacek Generowicz <github@my-post-office.net>
2017-06-07 15:24:29 +0200

    Turn minmax into a more powerful class and test
    + asserts that min does not exceed max
    + supports `*`, `+` and `-` with scalars
    + `__str__` and `__repr__`

--- invisible_cities/reco/params.py
+++ invisible_cities/reco/params.py
@@ -9,8 +9,35 @@ def _add_namedtuple_in_this_module(name, attribute_names):
     new_nametuple = namedtuple(name, attribute_names)
     setattr(this_module, name, new_nametuple)
 
+
+# TODO: move this into a more appropriate module
+class minmax:
+
+    def __init__(self, min, max):
+        assert min <= max
+        self.min = min
+        self.max = max
+
+    def __mul__(self, factor):
+        return minmax(self.min * factor, self.max * factor)
+
+    def __add__(self, scalar):
+        return minmax(self.min + scalar, self.max + scalar)
+
+    def __sub__(self, scalar):
+        return minmax(self.min - scalar, self.max - scalar)
+
+    def __str__(self):
+        return 'minmax(min={.min}, max={.max})'.format(self, self)
+    __repr__ = __str__
+
+    def __getitem__(self, n):
+        if n == 0: return self.min
+        if n == 1: return self.max
+        raise IndexError
+
+
 for name, attrs in (
-        ('minmax'         , 'min max'),
         ('RawVectors'     , 'event pmtrwf sipmrwf pmt_active sipm_active'),
         ('SensorParams'   , 'NPMT PMTWL NSIPM SIPMWL'),
         ('CalibParams'    , 'coeff_c, coeff_blr, adc_to_pes_pmt adc_to_pes_sipm'),
