b00cf2cbda63469ecdda8c61d5a9641029420a8e
Gonzalo <gonzaponte@gmail.com>
2017-06-19 10:35:09 -0700

    cosmetics

--- invisible_cities/io/dst_io.py
+++ invisible_cities/io/dst_io.py
@@ -1,20 +1,17 @@
-# import tables as tb
-#
-# from .. reco import tbl_functions as tbl
-# from .. reco import nh5           as table_formats
-
-from . table_io import make_table
-from .. reco import nh5           as table_formats
+from .  table_io import make_table
+from .. reco.nh5 import HitsTable
 
 
 def hits_writer(hdf5_file, *, compression='ZLIB4'):
     hits_table  = make_table(hdf5_file,
-                          group='RECO',
-                          name='Events',
-                          fformat=table_formats.HitsTable,
-                          description='Hits',
-                          compression=compression)
+                             group       = 'RECO',
+                             name        = 'Events',
+                             fformat     = HitsTable,
+                             description = 'Hits',
+                             compression = compression)
+
     hits_table.cols.event.create_index()
+
     def write_hits(hits_event):
         hits_event.store(hits_table)
     return write_hits
--- invisible_cities/io/kdst_io.py
+++ invisible_cities/io/kdst_io.py
@@ -1,14 +1,15 @@
-from . table_io import make_table
-from .. reco import nh5           as table_formats
+from .  table_io import make_table
+from .. reco.nh5 import KrTable
+from .. reco.nh5 import XYfactors
 
 
 def kr_writer(hdf5_file, *, compression='ZLIB4'):
     kr_table = make_table(hdf5_file,
-                          group='DST',
-                          name='Events',
-                          fformat=table_formats.KrTable,
-                          description='KDST Events',
-                          compression=compression)
+                          group       = 'DST',
+                          name        = 'Events',
+                          fformat     = KrTable,
+                          description = 'KDST Events',
+                          compression = compression)
     #kr_table = _make_kr_tables(file, compression)
     def write_kr(kr_event):
         kr_event.store(kr_table)
@@ -17,11 +18,11 @@ def kr_writer(hdf5_file, *, compression='ZLIB4'):
 
 def xy_writer(hdf5_file, *, compression='ZLIB4'):
     xy_table = make_table(hdf5_file,
-                          group='Corrections',
-                          name='XYcorrections',
-                          fformat=table_formats.XYfactors,
-                          description='x,y corrections',
-                          compression=compression)
+                          group       = 'Corrections',
+                          name        = 'XYcorrections',
+                          fformat     = XYfactors,
+                          description = 'x,y corrections',
+                          compression = compression)
     
     def write_xy(xs, ys, fs, us, ns):
         row = xy_table.row
