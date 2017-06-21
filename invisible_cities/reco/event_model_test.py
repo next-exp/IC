f767cb020a36dc65ed26ccdd3b4a1ee8ad7da3d1
Gonzalo <gonzaponte@gmail.com>
2017-06-20 18:13:27 -0700

    Add bunch of tests for the event_model module

--- invisible_cities/reco/event_model_test.py
+++ invisible_cities/reco/event_model_test.py
@@ -1,13 +1,50 @@
 import numpy as np
 
+from numpy.testing import assert_equal
+from pytest        import mark
+
 from hypothesis             import given
 from hypothesis.strategies  import floats
 from hypothesis.strategies  import integers
 from hypothesis.strategies  import composite
+from hypothesis.extra.numpy import arrays
 
 from .. core.ic_types    import xy
+from .. core.ic_types    import minmax
+from .       event_model import SensorParams
+from .       event_model import Event
+from .       event_model import Waveform
+from .       event_model import S12
 from .       event_model import Cluster
 from .       event_model import Hit
+from .       event_model import HitCollection
+from .       event_model import PersistentHitCollection
+from .       event_model import KrEvent
+from .       event_model import PersistentKrEvent
+
+
+@composite
+def sensor_params_input(draw):
+    npmt   = draw(integers())
+    pmtwl  = draw(integers())
+    nsipm  = draw(integers())
+    sipmwl = draw(integers())
+    return npmt, pmtwl, nsipm, sipmwl
+
+
+@composite
+def event_input(draw):
+    evt_no = draw(integers())
+    time   = draw(floats  (allow_nan=False))
+    return evt_no, time
+
+
+@composite
+def waveform_input(draw, min_size=1, max_size=1000):
+    size = draw(integers(min_size, max_size))
+    t    = draw(arrays(float, size, floats())); t.sort() # times in order!
+    E    = draw(arrays(float, size, floats()))
+    return size, t, E
 
 
 @composite
@@ -27,6 +64,69 @@ def hit_input(draw, min_value=0, max_value=100):
     s2_energy   = draw(floats  (50, 100))
     peak_number = draw(integers( 1,  20))
     return peak_number, s2_energy, z
+
+
+@given(sensor_params_input())
+def test_sensor_params(sensor_pars):
+    npmt, pmtwl, nsipm, sipmwl = sensor_pars
+    sp =  SensorParams(*sensor_pars)
+
+    assert sp.npmt   == sp.NPMT   == npmt
+    assert sp.nsipm  == sp.NSIPM  == nsipm
+    assert sp.pmtwl  == sp.PMTWL  == pmtwl
+    assert sp.sipmwl == sp.SIPMWL == sipmwl
+
+
+@mark.parametrize("test_class",
+                  (Event,
+                   HitCollection,
+                   PersistentHitCollection,
+                   KrEvent,
+                   PersistentKrEvent))
+@given(event_input())
+def test_event(test_class, event_pars):
+    evt_no, time = event_pars
+    evt =  test_class(*event_pars)
+
+    assert evt.event == evt_no
+    assert evt.time  == time
+
+
+@given(waveform_input())
+def test_waveform(waveform_pars):
+    size, t, E = waveform_pars
+    wf =  Waveform(t, E)
+
+    assert_equal(wf.t, t)
+    assert_equal(wf.E, E)
+    assert wf == wf
+    assert wf.total_energy      == np.sum(E)
+    assert wf.height            == np.max(E)
+    assert wf.number_of_samples == size
+    assert wf.tpeak             == t[np.argmax(E)]
+    assert wf.tmin_tmax         == minmax(t[0], t[-1])
+    assert wf.width             == t[-1] - t[0]
+
+
+@given(integers(min_value=31, max_value=40)) # pick a random event, limits from KrMC_pmaps fixture in conftest.py
+def test_s12(KrMC_pmaps, evt_no):
+    *_, (s1data, s2data, _) = KrMC_pmaps
+
+    s1data = s1data[evt_no]
+    s2data = s2data[evt_no]
+
+    s1 = S12(s1data, "S1")
+    s2 = S12(s2data, "S2")
+
+    assert s1.type == "S1"
+    assert s2.type == "S2"
+
+    assert s1.number_of_peaks == len(s1data)
+    assert s2.number_of_peaks == len(s2data)
+
+    assert sorted(s1.peaks) == sorted(s1data)
+    for peak_no, peak in s1data.items():
+        assert s1.peak_waveform(peak_no) == Waveform(*peak)
 
 
 @given(cluster_input(1))
@@ -68,3 +168,28 @@ def test_hit(ci, hi):
     np.isclose (h.E        ,   E, rtol=1e-4)
     np.allclose(h.XYZ      , xyz, rtol=1e-4)
     np.allclose(h.VXYZ     , xyz, rtol=1e-4)
+
+
+@mark.parametrize("test_class",
+                  (HitCollection,
+                   PersistentHitCollection))
+def test_hit_collection_empty(test_class):
+    hc = test_class(-1, -1)
+    assert hc.hits == []
+
+
+@mark.parametrize("test_class",
+                  (KrEvent,
+                   PersistentKrEvent))
+def test_kr_event_attributes(test_class):
+    evt =  test_class(-1, -1)
+
+    for attr in ["nS1", "nS2"]:
+        assert getattr(evt, attr) == -1
+
+    for attr in ["S1w", "S1h", "S1e", "S1t",
+                 "S2w", "S2h", "S2e", "S2t", "S2q",
+                 "Nsipm", "DT", "Z",
+                 "X", "Y", "R", "Phi",
+                 "Xrms", "Yrms"]:
+        assert getattr(evt, attr) == []
