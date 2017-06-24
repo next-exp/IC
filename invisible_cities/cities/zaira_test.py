8db6a6e2b50ffcb11c816c7f9ed430daeb4ac706
Jacek Generowicz <github@my-post-office.net>
2017-06-24 14:39:09 +0200

    Adapt test_zaira_KrMC to new structure

--- invisible_cities/cities/zaira_test.py
+++ invisible_cities/cities/zaira_test.py
@@ -2,6 +2,8 @@ import os
 
 from pytest import mark
 
+from .. core.configure import configure
+
 from . zaira import Zaira
 
 @mark.slow
@@ -10,13 +12,21 @@ def test_zaira_KrMC(config_tmpdir, ICDIR):
     # since they are in general test-specific
     # NB: avoid taking defaults for run number (test-specific)
 
+
     PATH_IN =  os.path.join(ICDIR, 'database/test_data', 'KrDST_MC.h5')
-    PATH_OUT = os.path.join(config_tmpdir,                 'KrCorr.h5')
+    PATH_OUT = os.path.join(config_tmpdir,               'KrCorr.h5')
 
-    zaira = Zaira(run_number = 0,
-                  files_in   = [PATH_IN],
-                  file_out   = PATH_OUT,
-                  lifetime   = 1e6)
+    nrequired = 1
+
+    from .. core.system_of_units import ns
+
+    conf = vars(configure('dummy -c invisible_cities/config/zaira.conf'.split()))
+    conf.update(dict(run_number = 0,
+                     files_in   = PATH_IN,
+                     file_out   = PATH_OUT,
+                     nmax       = nrequired,
+                     lifetime   = 1e6 * ns))
+
+    zaira = Zaira(**conf)
     dst_size = zaira.run()
-
     assert dst_size > 0
