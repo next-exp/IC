af7e8644d2e7f49648f3575d98a90ab6d136fa6e
Jacek Generowicz <github@my-post-office.net>
2017-06-24 13:10:20 +0200

    Clean up Penthesilea config and constructors

--- invisible_cities/cities/penthesilea.py
+++ invisible_cities/cities/penthesilea.py
@@ -18,86 +18,47 @@ from ..cities.base_cities     import HitCollectionCity
 from ..reco                   import tbl_functions as tbl
 from ..reco.tbl_functions     import get_event_numbers_and_timestamps_from_file_name
 from ..reco.pmaps_functions   import load_pmaps
-from ..reco.xy_algorithms     import barycenter
-from ..reco.xy_algorithms     import find_algorithm
 from ..filters.s1s2_filter    import s1s2_filter
 from ..filters.s1s2_filter    import s2si_filter
 from ..filters.s1s2_filter    import S12Selector
 
 class Penthesilea(HitCollectionCity):
-    def __init__(self,
-                 run_number       = 0,
-                 files_in         = None,
-                 file_out         = None,
-                 compression      = "ZLIB4",
-                 nprint           = 10000,
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        conf = self.conf
 
-                 drift_v          = 1 * units.mm / units.mus,
+        self.drift_v        = conf.drift_v
+        self._s1s2_selector = S12Selector(S1_Nmin     = conf.s1_nmin,
+                                          S1_Nmax     = conf.s1_nmax,
+                                          S1_Emin     = conf.s1_emin,
+                                          S1_Emax     = conf.s1_emax,
+                                          S1_Lmin     = conf.s1_lmin,
+                                          S1_Lmax     = conf.s1_lmax,
+                                          S1_Hmin     = conf.s1_hmin,
+                                          S1_Hmax     = conf.s1_hmax,
+                                          S1_Ethr     = conf.s1_ethr,
 
-                 S1_Emin          = 0,
-                 S1_Emax          = np.inf,
-                 S1_Lmin          = 0,
-                 S1_Lmax          = np.inf,
-                 S1_Hmin          = 0,
-                 S1_Hmax          = np.inf,
-                 S1_Ethr          = 0,
+                                          S2_Nmin     = conf.s2_nmin,
+                                          S2_Nmax     = conf.s2_nmax,
+                                          S2_Emin     = conf.s2_emin,
+                                          S2_Emax     = conf.s2_emax,
+                                          S2_Lmin     = conf.s2_lmin,
+                                          S2_Lmax     = conf.s2_lmax,
+                                          S2_Hmin     = conf.s2_hmin,
+                                          S2_Hmax     = conf.s2_hmax,
+                                          S2_NSIPMmin = conf.s2_nsipmmin,
+                                          S2_NSIPMmax = conf.s2_nsipmmax,
+                                          S2_Ethr     = conf.s2_ethr)
 
-                 S2_Nmin          = 1,
-                 S2_Nmax          = 1000,
-                 S2_Emin          = 0,
-                 S2_Emax          = np.inf,
-                 S2_Lmin          = 0,
-                 S2_Lmax          = np.inf,
-                 S2_Hmin          = 0,
-                 S2_Hmax          = np.inf,
-                 S2_NSIPMmin      = 1,
-                 S2_NSIPMmax      = np.inf,
-                 S2_Ethr          = 0,
-
-                 rebin            = 1,
-                 reco_algorithm   = barycenter):
-
-        HitCollectionCity.__init__(self,
-                         run_number       = run_number,
-                         files_in         = files_in,
-                         file_out         = file_out,
-                         compression      = compression,
-                         nprint           = nprint,
-                         rebin            = rebin,
-                         reco_algorithm   = reco_algorithm)
-
-        self.drift_v        = drift_v
-        self._s1s2_selector = S12Selector(S1_Nmin     = 1,
-                                          S1_Nmax     = 1,
-                                          S1_Emin     = S1_Emin,
-                                          S1_Emax     = S1_Emax,
-                                          S1_Lmin     = S1_Lmin,
-                                          S1_Lmax     = S1_Lmax,
-                                          S1_Hmin     = S1_Hmin,
-                                          S1_Hmax     = S1_Hmax,
-                                          S1_Ethr     = S1_Ethr,
-
-                                          S2_Nmin     = 1,
-                                          S2_Nmax     = S2_Nmax,
-                                          S2_Emin     = S2_Emin,
-                                          S2_Emax     = S2_Emax,
-                                          S2_Lmin     = S2_Lmin,
-                                          S2_Lmax     = S2_Lmax,
-                                          S2_Hmin     = S2_Hmin,
-                                          S2_Hmax     = S2_Hmax,
-                                          S2_NSIPMmin = S2_NSIPMmin,
-                                          S2_NSIPMmax = S2_NSIPMmax,
-                                          S2_Ethr     = S2_Ethr)
-
-    def run(self, nmax):
-        self.display_IO_info(nmax)
+    def run(self):
+        self.display_IO_info()
 
         # with HitCollection_writer(self.output_file, "DST", "w",
         #                           self.compression, "Tracks") as write:
         with tb.open_file(self.output_file, "w",
                           filters = tbl.filters(self.compression)) as h5out:
             write_hits = hits_writer(h5out)
-            nevt_in, nevt_out = self._file_loop(write_hits, nmax)
+            nevt_in, nevt_out = self._file_loop(write_hits)
         print(textwrap.dedent("""
                               Number of events in : {}
                               Number of events out: {}
@@ -105,7 +66,7 @@ class Penthesilea(HitCollectionCity):
                               """.format(nevt_in, nevt_out, nevt_out / nevt_in)))
         return nevt_in, nevt_out
 
-    def _file_loop(self, write_hits, nmax):
+    def _file_loop(self, write_hits):
         nevt_in = nevt_out = 0
 
         for filename in self.input_files:
@@ -120,7 +81,7 @@ class Penthesilea(HitCollectionCity):
             event_numbers, timestamps = get_event_numbers_and_timestamps_from_file_name(filename)
 
             nevt_in, nevt_out, max_events_reached = self._event_loop(
-                event_numbers, timestamps, nmax, nevt_in, nevt_out, write_hits, S1s, S2s, S2Sis)
+                event_numbers, timestamps, nevt_in, nevt_out, write_hits, S1s, S2s, S2Sis)
 
             if max_events_reached:
                 print('Max events reached')
@@ -130,11 +91,12 @@ class Penthesilea(HitCollectionCity):
 
         return nevt_in, nevt_out
 
-    def _event_loop(self, event_numbers, timestamps, nmax, nevt_in, nevt_out, write_hits, S1s, S2s, S2Sis):
+    def _event_loop(self, event_numbers, timestamps,
+                    nevt_in, nevt_out, write_hits, S1s, S2s, S2Sis):
         max_events_reached = False
         for evt_number, evt_time in zip(event_numbers, timestamps):
             nevt_in += 1
-            if self.max_events_reached(nmax, nevt_in):
+            if self.max_events_reached(nevt_in):
                 max_events_reached = True
                 break
             S1   = S1s  .get(evt_number, {})
@@ -178,52 +140,3 @@ class Penthesilea(HitCollectionCity):
             npeak += 1
 
         return hitc
-
-
-
-def PENTHESILEA(argv = sys.argv):
-    """Penthesilea DRIVER"""
-
-    # get parameters dictionary
-    CFP = configure(argv)
-
-    #class instance
-    penthesilea = Penthesilea(run_number       = CFP.RUN_NUMBER,
-                              files_in         = sorted(glob.glob(CFP.FILE_IN)),
-                              file_out         = CFP.FILE_OUT,
-                              compression      = CFP.COMPRESSION,
-                              nprint           = CFP.NPRINT,
-
-                              drift_v          = CFP.DRIFT_V * units.mm/units.mus,
-
-                              S1_Emin          = CFP.S1_EMIN * units.pes,
-                              S1_Emax          = CFP.S1_EMAX * units.pes,
-                              S1_Lmin          = CFP.S1_LMIN,
-                              S1_Lmax          = CFP.S1_LMAX,
-                              S1_Hmin          = CFP.S1_HMIN * units.pes,
-                              S1_Hmax          = CFP.S1_HMAX * units.pes,
-                              S1_Ethr          = CFP.S1_ETHR * units.pes,
-
-                              S2_Nmin          = CFP.S2_NMIN,
-                              S2_Nmax          = CFP.S2_NMAX,
-                              S2_Emin          = CFP.S2_EMIN * units.pes,
-                              S2_Emax          = CFP.S2_EMAX * units.pes,
-                              S2_Lmin          = CFP.S2_LMIN,
-                              S2_Lmax          = CFP.S2_LMAX,
-                              S2_Hmin          = CFP.S2_HMIN * units.pes,
-                              S2_Hmax          = CFP.S2_HMAX * units.pes,
-                              S2_NSIPMmin      = CFP.S2_NSIPMMIN,
-                              S2_NSIPMmax      = CFP.S2_NSIPMMAX,
-                              S2_Ethr          = CFP.S2_ETHR * units.pes,
-
-                              rebin            = CFP.REBIN if "REBIN" in CFP else 1,
-                              reco_algorithm   = find_algorithm(CFP.RECO_ALGORITHM))
-
-    t0 = time.time()
-    nevts = CFP.NEVENTS if not CFP.RUN_ALL else -1
-
-    nevt_in, nevt_out = penthesilea.run(nmax = nevts)
-    t1 = time.time()
-    dt = t1 - t0
-
-    print("run {} evts in {} s, time/event = {}".format(nevt_in, dt, dt/nevt_in))
