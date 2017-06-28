225e5b7f394eaed017c91dcb6d66691d2228e83e
Alejandro Botas <alej@nextp18.ific.uv.es>
2017-06-28 18:08:54 +0200

    make PmapCity uses cython peak functions

--- invisible_cities/cities/base_cities.py
+++ invisible_cities/cities/base_cities.py
@@ -386,18 +386,11 @@ class PmapCity(CalibratedCity):
 
     def find_S12(self, csum, s1_indx, s2_indx):
         """Return S1 and S2."""
-        # S1 = cpf.find_S12(s1_ene,
-        #                   s1_indx,
-        #                   **self.s1_params._asdict())
-        #
-        # S2 = cpf.find_S12(s2_ene,
-        #                   s2_indx,
-        #                   **self.s2_params._asdict())
-        S1 = pf.find_S12(csum,
-                         s1_indx,
+        S1 = cpf.find_S12(csum,
+                          s1_indx,
                           **self.s1_params._asdict())
 
-        S2 = pf.find_S12(csum,
+        S2 = cpf.find_S12(csum,
                           s2_indx,
                           **self.s2_params._asdict())
         return S1, S2
