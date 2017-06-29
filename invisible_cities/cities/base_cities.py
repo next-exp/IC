49d6f09145bd8d3fec075e82b949045d4782abf7
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-06-29 12:34:32 +0200

    Move intelligence of simulate_pmt_response to FF API
    Following the IC credo, simulate_pmt_response lives now as a free
    function in sensor_response (reco). The base city (SensorResponseCity)
    calls the FF and provides the interface to clients such as DIOMIRA.

--- invisible_cities/cities/base_cities.py
+++ invisible_cities/cities/base_cities.py
@@ -34,6 +34,7 @@ from .. database import load_db
 from ..io                 import pmap_io          as pio
 
 from ..reco               import peak_functions_c as cpf
+from ..reco               import sensor_functions as sf
 from ..reco               import peak_functions   as pf
 from ..reco               import pmaps_functions  as pmp
 from ..reco               import dst_functions    as dstf
@@ -230,32 +231,9 @@ class SensorResponseCity(City):
         front end electronics (LPF, HPF filters)
         array of BLR waveforms (only decimation)
         """
-        # Single Photoelectron class
-        spe = FE.SPE()
-        # FEE, with noise PMT
-        fee  = FE.FEE(noise_FEEPMB_rms=FE.NOISE_I, noise_DAQ_rms=FE.NOISE_DAQ)
-        NPMT = pmtrd.shape[1]
-        RWF  = []
-        BLRX = []
+        return sf.simulate_pmt_response(event, pmtrd, self.adc_to_pes)
 
-        for pmt in range(NPMT):
-            # normalize calibration constants from DB to MC value
-            cc = self.adc_to_pes[pmt] / FE.ADC_TO_PES
-            # signal_i in current units
-            signal_i = FE.spe_pulse_from_vector(spe, pmtrd[event, pmt])
-            # Decimate (DAQ decimation)
-            signal_d = FE.daq_decimator(FE.f_mc, FE.f_sample, signal_i)
-            # Effect of FEE and transform to adc counts
-            signal_fee = FE.signal_v_fee(fee, signal_d, pmt) * FE.v_to_adc()
-            # add noise daq
-            signal_daq = cc * FE.noise_adc(fee, signal_fee)
-            # signal blr is just pure MC decimated by adc in adc counts
-            signal_blr = cc * FE.signal_v_lpf(fee, signal_d) * FE.v_to_adc()
-            # raw waveform stored with negative sign and offset
-            RWF.append(FE.OFFSET - signal_daq)
-            # blr waveform stored with positive sign and no offset
-            BLRX.append(signal_blr)
-        return np.array(RWF), np.array(BLRX)
+
 
     @property
     def FE_t_sample(self):
