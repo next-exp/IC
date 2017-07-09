0e1df09a43ac94948b69e244e347768926d7b698
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-07-09 09:58:49 +0200

    Refactoring of PMAPS classes.
    PMAPS objects have been refactored to enhance the high-level
    interface. The refactoring includes:
    1) declaration of types inside cython functions (to boost
    performance).
    2) The dictionaries underlying the S1, S2 and S2Si classes are made
    public. Thus, for and s1 object the s1d attribute gives access to the
    {peak_no:(t,E) underlying dict.
    3) The high level classes own now the method store, and thus are
    capable to write themselves.
    
    In summary, **finally** PMAPS are represented by high-level objects.
    
    Tests have been added to demonstrate the new functionality.

--- invisible_cities/evm/pmaps_test.py
+++ invisible_cities/evm/pmaps_test.py
@@ -27,12 +27,15 @@ def peak_input(draw, min_size=1, max_size=100):
     return size, t, E
 
 
-
-
 @given(peak_input())
-def test_waveform(waveform_pars):
-    size, t, E = waveform_pars
+def test_peak(peak_pars):
+    size, t, E = peak_pars
     wf =  Peak(t, E)
+    if (np.any(np.isnan(t))  or
+       np.any(np.isnan(E))):
+       assert  wf.good_waveform == False
+    else:
+       assert  wf.good_waveform == True
 
     np.allclose (wf.t , t, rtol=1e-4)
     np.allclose (wf.E , E, rtol=1e-4)
@@ -40,39 +43,37 @@ def test_waveform(waveform_pars):
     np.isclose (wf.height , np.max(E), rtol=1e-4)
     np.isclose (wf.tpeak , t[np.argmax(E)], rtol=1e-4)
     np.isclose (wf.width , t[-1] - t[0], rtol=1e-4)
-    assert wf.number_of_samples == size
+    np.isclose (wf.tmin_tmax.min , t[0], rtol=1e-4)
+    np.isclose (wf.tmin_tmax.max , t[-1], rtol=1e-4)
+    assert wf.number_of_samples == len(t)
 
 
 @given(peak_input())
-def test_s12c(wform):
+def test_s1__(wform):
     _, t1, E1 = wform
     _, t2, E2 = wform
 
-    s12d = {0: [t1, E1], 1: [t2, E2] }
-    s12 = S12(s12d)
+    s1d = {0: [t1, E1], 1: [t2, E2] }
+    s1 = S1(s1d)
 
-    assert s12.number_of_peaks == len(s12d)
-    for i in range(s12.number_of_peaks):
-        if s12.peak_waveform(i).good_waveform:
-            np.allclose (s12.peak_waveform(i).t , s12d[i][0], rtol=1e-4)
-            np.allclose (s12.peak_waveform(i).E , s12d[i][1], rtol=1e-4)
+    assert s1.number_of_peaks == len(s1d)
+    for i in s1.peak_collection():
+        if s1.peak_waveform(i).good_waveform:
+            np.allclose (s1.peak_waveform(i).t , s1d[i][0], rtol=1e-4)
+            np.allclose (s1.peak_waveform(i).E , s1d[i][1], rtol=1e-4)
 
 
 @given(integers(min_value=31, max_value=40)) # pick a random event, limits from KrMC_pmaps fixture in conftest.py
 def test_s1_s2(KrMC_pmaps, evt_no):
-    *_, (s1data, s2data, _) = KrMC_pmaps
+    *_, (s1_dict, s2_dict, _) = KrMC_pmaps
 
-    s1data = s1data[evt_no]
-    s2data = s2data[evt_no]
+    s1 = s1_dict[evt_no]
+    s2 = s2_dict[evt_no]
 
-    s1 = S1(s1data)
-    s2 = S2(s2data)
+    assert s1.number_of_peaks == len(s1.peaks)
+    assert s2.number_of_peaks == len(s2.peaks)
 
-    assert s1.number_of_peaks == len(s1data)
-    assert s2.number_of_peaks == len(s2data)
-
-    assert sorted(s1.peaks) == sorted(s1data)
-    for peak_no, (t,E) in s1data.items():
+    for peak_no, (t,E) in s1.s1d.items():
         pwf = s1.peak_waveform(peak_no)
         pk =  Peak(t,E)
         np.allclose (pwf.t , pk.t, rtol=1e-4)
@@ -81,25 +82,21 @@ def test_s1_s2(KrMC_pmaps, evt_no):
 
 @given(integers(min_value=31, max_value=40))
 def test_s2si(KrMC_pmaps, evt_no):
-    *_, (_, s2data, s2sidata) = KrMC_pmaps
+    *_, (_, _, s2si_dict) = KrMC_pmaps
 
-    s2d   = s2data[evt_no]
-    s2sid = s2sidata[evt_no]
+    s2si = s2si_dict[evt_no]
 
-    s2si = S2Si(s2d, s2sid)
-    assert s2si.number_of_peaks == len(s2d)
-
-    for peak_number in range(s2si.number_of_peaks):
+    for peak_number in s2si.peak_collection():
         assert (s2si.number_of_sipms_in_peak(peak_number) ==
-                len(s2sid[peak_number]))
+                len(s2si.s2sid[peak_number]))
 
         np.array_equal(np.array(s2si.sipms_in_peak(peak_number)),
-                       np.array(s2sid[peak_number].keys()))
+                       np.array(s2si.s2sid[peak_number].keys()))
 
         for sipm_number in s2si.sipms_in_peak(peak_number):
             w   = s2si.sipm_waveform(peak_number, sipm_number)
             wzs = s2si.sipm_waveform_zs(peak_number, sipm_number)
-            E   = s2sid[peak_number][sipm_number]
+            E   = s2si.s2sid[peak_number][sipm_number]
             t = s2si.peak_waveform(peak_number).t
             np.allclose(w.E , E)
             np.allclose(wzs.E , E[E>0])
