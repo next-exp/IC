fd605e4740335c57b6353d724f794dcc33805744
Alejandro Botas <alejbotas@gmail.com>
2017-07-11 17:14:50 +0200

    coordination of rebin_s2si from base_cities to pf

--- invisible_cities/reco/pmaps_functions.py
+++ invisible_cities/reco/pmaps_functions.py
@@ -8,11 +8,11 @@ Last revised, JJGC, July, 2017.
 
 """
 import numpy  as np
+from .. core.core_functions_c  import rebin_array
 from .. core.system_of_units_c import units
-from .. core.core_functions    import rebin_array
 
 
-def _integrate_sipm_charges_in_peak_as_dict(Si):
+def _integrate_sipm_charges_in_peak_as_dict(s2si):
     """Return dict of integrated charges from a SiPM dictionary.
 
     S2Si = {  peak : Si }
@@ -20,10 +20,10 @@ def _integrate_sipm_charges_in_peak_as_dict(Si):
 
     Returns an integrated Si = { nsipm : sum(q_n) }
     """
-    return { sipm : sum(qs) for (sipm, qs) in Si.items() }
+    return { sipm : sum(qs) for (sipm, qs) in s2si.items() }
 
 
-def _integrate_sipm_charges_in_peak(Si):
+def _integrate_sipm_charges_in_peak(s2si):
     """Return arrays of nsipm and integrated charges from SiPM dictionary.
 
     S2Si = {  peak : Si }
@@ -32,30 +32,38 @@ def _integrate_sipm_charges_in_peak(Si):
     Returns (np.array[[nsipm_1 ,     nsipm_2, ...]],
              np.array[[sum(q_1), sum(nsipm_2), ...]])
     """
-    sipms_and_Q_totals = _integrate_sipm_charges_in_peak_as_dict(Si)
+    sipms_and_Q_totals = _integrate_sipm_charges_in_peak_as_dict(s2si)
     sipms = np.array(tuple(sipms_and_Q_totals.keys()))
     Qs    = np.array(tuple(sipms_and_Q_totals.values()))
     return sipms, Qs
 
 
-def _integrate_S2Si_charge(S2Si):
-    """
-    Return S2Si containing integrated charges.
+def _integrate_S2Si_charge(s2sid):
+    """Return s2Si containing integrated charges.
 
-    S2Si = {  peak : Si }
-      Si = { nsipm : [ q1, q2, ...] }
-
-    Returns S2Si where Si = { nsipm : sum([q1, q2, ...])}
+    s2sid = { peak_no : {nsipm : [ q1, q2, ...]} }
+    Returns s2sid where s2sid = {peak_no: { nsipm : sum([q1, q2, ...])} }
     """
     return { peak_no : _integrate_sipm_charges_in_peak_as_dict(peak)
-             for (peak_no, peak) in S2Si.items() }
+             for (peak_no, peak) in s2sid.items() }
 
-def _rebin_S2(t, e, sipms, stride):
-    """rebin: s2 times, s2 energies, s2 sipm qs, by stride"""
-    return rebin_array(t, stride),
-           rebin_array(e, stride),
-           {sipm: rebin_array(qs, stride) for sipm, qs in sipms.items()}
 
+def rebin_s2si(s2, s2si, rf):
+    """given an s2 and a corresponding s2si, rebin them by a factor rf"""
+    s2_rebin = {}
+    s2si_rebin = {}
+    for peak in s2d:
+        t, e, sipms = rebin_s2si_peak(s2d[peak][0], s2d[peak][1], s2sid[peak], rf)
+        s2d_rebin  [peak] = Peak(t, e)
+        s2sid_rebin[peak] = sipms
+    return S2(s2d_rebin), S2Si(s2sid_rebin)
+
+
+def _rebin_s2si_peak(t, e, sipms, stride):
+    """rebin: s2 times (taking mean), s2 energies, s2 sipm qs, by stride"""
+    return rebin_array(t, stride, remainder=True, mean=True), \
+           rebin_array(e, stride, remainder=True, \
+          {sipm: rebin_array(qs, stride, mean=True, remainder=True) for sipm, qs in sipms.items()}
 
 # def select_si_slice(si, slice_no):
 #     # This is a temporary fix! The number of slices in the SiPM arrays
