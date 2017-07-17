1d823193e72a1ce8cb88ad0186e3b7b6b2bbf48c
jmbenlloch <info@jmbenlloch.net>
2017-07-17 19:15:36 +0200

    Remove blocks of old commented code

--- invisible_cities/cities/penthesilea.py
+++ invisible_cities/cities/penthesilea.py
@@ -46,64 +46,15 @@ class Penthesilea(HitCity):
         self.drift_v        = conf.drift_v
         self._s1s2_selector = S12Selector(**kwds)
 
-    # def run(self):
-    #     self.display_IO_info()
-    #
-    #     # with HitCollection_writer(self.output_file, "DST", "w",
-    #     #                           self.compression, "Tracks") as write:
-    #     with tb.open_file(self.output_file, "w",
-    #                       filters = tbl.filters(self.compression)) as h5out:
-    #         write_hits = hits_writer(h5out)
-    #         nevt_in, nevt_out = self._file_loop(write_hits)
-    #     print(textwrap.dedent("""
-    #                           Number of events in : {}
-    #                           Number of events out: {}
-    #                           Ratio               : {}
-    #                           """.format(nevt_in, nevt_out, nevt_out / nevt_in)))
-    #     return nevt_in, nevt_out
-
-    # def file_loop(self, write_hits):
-    #     """
-    #     actions:
-    #     1. init counters
-    #     2. access pmaps (si_dicts )
-    #     3. access run and event info
-    #     4. call event_loop
-    #     """
-    #     write_kr = self.writers
-    #     self.cnt.init_counters(('n_events_tot', 'nevt_out'))
-    #
-    #     for filename in self.input_files:
-    #         print("Opening {filename}".format(**locals()), end="... ")
-    #
-    #         try:
-    #             s1_dict, s2_dict, s2si_dict = self.get_pmaps_dicts(filename)
-    #         except (ValueError, tb.exceptions.NoSuchNodeError):
-    #             print("Empty file. Skipping.")
-    #             continue
-    #
-    #         event_numbers, timestamps = self.event_numbers_and_timestamps_from_file_name(filename)
-    #
-    #         max_events_reached = self._event_loop(
-    #             event_numbers, timestamps, nevt_in, nevt_out, write_hits, S1_dict, S2_dict, S2Si_dict)
-    #
-    #         if max_events_reached:
-    #             print('Max events reached')
-    #             break
-    #         else:
-    #             print("OK")
-    #
-    #     return nevt_in, nevt_out
 
     def event_loop(self, event_numbers, timestamps, s1_dict, s2_dict, s2si_dict):
         """actions:
         1. loops over all PMAPS
         2. filter pmaps
-        3. write hit_event 
+        3. write hit_event
         """
 
         write_hits = self.writers
-        #max_events_reached = False
         for evt_number, evt_time in zip(event_numbers, timestamps):
             # Count events in and break if necessary before filtering
             if self.max_events_reached(self.cnt.counter_value('n_events_tot')):
@@ -111,20 +62,11 @@ class Penthesilea(HitCity):
             else:
                 self.cnt.increment_counter('n_events_tot')
 
-            # nevt_in += 1
-            # if self.max_events_reached(nevt_in):
-            #     max_events_reached = True
-            #     break
-
             # get pmaps
             s1, s2, s2si = self. get_pmaps_from_dicts(s1_dict,
                                                       s2_dict,
                                                       s2si_dict,
                                                       evt_number)
-            # s1   = S1_dict  .get(evt_number, {})
-            # s2   = S2_dict  .get(evt_number, {})
-            # s2si = S2Si_dict.get(evt_number, {})
-
             # filtering
             # loop event away if any signal (s1, s2 or s2si) not present
             if s1 == None or s2 == None or s2si == None:
@@ -137,33 +79,22 @@ class Penthesilea(HitCity):
             # event passed selection: increment counter and write
             self.cnt.increment_counter('nevt_out')
 
-#            nevt_out += 1
-
             evt = self._create_hits_event(evt_number, evt_time, s1, s2, s2si)
             write_hits(evt)
             self.conditional_print(self.cnt.counter_value('n_events_tot'),
             self.cnt.counter_value('nevt_out'))
 
-            #self.conditional_print(evt, nevt_in)
-
-        #return nevt_in, nevt_out, max_events_reached
 
     def _create_hits_event(self, evt_number, evt_time,
                            s1, s2, s2si):
 
         hitc = PersistentHitCollection(evt_number, evt_time * 1e-3)
-        #hitc.evt   = evt_number
-        #hitc.time  = evt_time * 1e-3 # s
 
         # in order to compute z one needs to define one S1
         # for time reference. By default the filter will only
         # take events with exactly one s1. Otherwise, the
         # convention is to take the first peak in the S1 object
         # as reference.
-
-        # peak  = S1.peak_waveform(0)
-        # t, e = next(iter(S1.values()))
-        # S1t  = t[np.argmax(e)]
 
         s1_t = s1.peak_waveform(0).tpeak
 
