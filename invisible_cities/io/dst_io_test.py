6900bc6924c2d5f357ab5ecbc8cc04c2b37878b9
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-07-19 12:49:38 +0200

    Various tests involving cluster fixed.
    The change of definition in Cluster (takes std, not rms) requires to
    fix a number of tests.

--- invisible_cities/io/dst_io_test.py
+++ invisible_cities/io/dst_io_test.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import tables as tb
 
 from numpy.testing import assert_allclose
@@ -15,25 +15,24 @@ from . dst_io              import hits_writer
 def test_hits_writer(config_tmpdir, hits_toy_data):
     output_file = os.path.join(config_tmpdir, "test_hits.h5")
 
-    _, (npeak, nsipm, x, y, xrms, yrms, z, q, e) = hits_toy_data
+    _, (npeak, nsipm, x, y, xstd, ystd, z, q, e) = hits_toy_data
 
     with tb.open_file(output_file, 'w') as h5out:
         write = hits_writer(h5out)
         hits = PersistentHitCollection(-1, -1)
         for i in range(len(x)):
-            c = Cluster(q[i], xy(x[i], y[i]), xy(xrms[i], yrms[i]), nsipm[i])
+            c = Cluster(q[i], xy(x[i], y[i]), xy(xstd[i], ystd[i]), nsipm[i])
             h = Hit(npeak[i], c, z[i], e[i])
             hits.hits.append(h)
         write(hits)
-
 
     dst = load_dst(output_file, group = "RECO", node = "Events")
     assert_allclose(npeak, dst.npeak.values)
     assert_allclose(nsipm, dst.nsipm.values)
     assert_allclose(x    , dst.X    .values)
     assert_allclose(y    , dst.Y    .values)
-    assert_allclose(xrms , dst.Xrms .values)
-    assert_allclose(yrms , dst.Yrms .values)
+    assert_allclose(np.sqrt(xstd), dst.Xrms .values)
+    assert_allclose(np.sqrt(ystd), dst.Yrms .values)
     assert_allclose(z    , dst.Z    .values)
     assert_allclose(q    , dst.Q    .values)
     assert_allclose(e    , dst.E    .values)
--- invisible_cities/reco/test_xy_algorithms.py
+++ invisible_cities/reco/test_xy_algorithms.py
@@ -66,7 +66,7 @@ def test_corona_barycenter_are_same_with_one_cluster(toy_sipm_signal):
     # assert len(c_cluster)  == len(b_cluster)
     # A cluster object has no length!
     np.array_equal(c_cluster.pos, b_cluster.pos)
-    np.array_equal(c_cluster.rms, b_cluster.rms)
+    np.array_equal(c_cluster.std.XY, b_cluster.std.XY)
     assert c_cluster.nsipm      == b_cluster.nsipm
     assert c_cluster.Q          == b_cluster.Q
     assert c_cluster.nsipm      == b_cluster.nsipm
