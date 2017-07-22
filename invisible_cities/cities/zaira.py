f6c493cd74f7ecd27462eb6577647b0ed940abe1
J. J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-07-22 17:28:52 +0200

    Propagate refactoring of cities to Diomira and Zaira.

--- invisible_cities/cities/zaira.py
+++ invisible_cities/cities/zaira.py
@@ -12,18 +12,17 @@ from functools import partial
 import numpy  as np
 import tables as tb
 import pandas as pd
+from operator import attrgetter
 
-from .  base_cities         import City
-from .  base_cities         import MapCity
+from .  base_cities         import DstCity
 from .. core.fit_functions  import in_range
 from .. core.configure      import configure
-from .. io. dst_io          import load_dst
 from .. io.kdst_io          import xy_writer
+from ..reco.corrections     import Correction
+from ..reco.corrections     import LifetimeCorrection
+from .. core                import fit_functions  as fitf
 
-from .  diomira     import Diomira
-
-
-class Zaira(MapCity):
+class Zaira(DstCity):
 
     def __init__(self, **kwds):
         """Zaira Init:
@@ -34,37 +33,58 @@ class Zaira(MapCity):
 
         """
         super().__init__(**kwds)
-        self.cnt.set_name('irene')
+        self.cnt.set_name('zaira')
         self.cnt.set_counter('nmax', value=self.conf.nmax)
+        self.set_lifetime_correction()
+        self.set_xybins_and_range()
+        self.set_z_and_e_fiducial()
 
+    def set_z_and_e_fiducial(self):
         conf = self.conf
-
         fiducial_z, fiducial_e = conf.fiducial_z, conf.fiducial_e
 
-        self._fiducial_z = ((self.det_geo.ZMIN[0], self.det_geo.ZMAX[0])
+        self.fiducial_z = ((self.det_geo.ZMIN[0], self.det_geo.ZMAX[0])
                              if fiducial_z is None
                              else fiducial_z)
-        self._fiducial_e = (0, np.inf) if fiducial_e is None else fiducial_e
+        self.fiducial_e = (0, np.inf) if fiducial_e is None else fiducial_e
 
-        self._dst_group  = conf.dst_group
-        self._dst_node   = conf.dst_node
+    def set_lifetime_correction(self):
+        conf = self.conf
+        lifetime =   conf.lifetime
+        u_lifetime = conf.u_lifetime
+
+        lifetimes = [lifetime]     if not np.shape(  lifetime) else lifetime
+        u_lifetimes = [u_lifetime] if not np.shape(u_lifetime) else u_lifetime
+        self.lifetime_corrections = tuple(map(LifetimeCorrection,
+                                              lifetimes,
+                                              u_lifetimes))
+
+    def set_xybins_and_range(self):
+        conf = self.conf
+        xmin = self.det_geo.XMIN[0] if conf.xmin is None else conf.xmin
+        xmax = self.det_geo.XMAX[0] if conf.xmax is None else conf.xmax
+        ymin = self.det_geo.YMIN[0] if conf.ymin is None else conf.ymin
+        ymax = self.det_geo.YMAX[0] if conf.ymax is None else conf.ymax
+        self.xbins  = conf.xbins
+        self.ybins  = conf.ybins
+        self.xrange = xmin, xmax
+        self.yrange = ymin, ymax
+
 
     def run(self):
         """Zaira overwrites the default run method """
 
-        dsts = [load_dst(input_file, self._dst_group, self._dst_node)
-                for input_file in self.input_files]
-
         # Correct each dataset with the corresponding lifetime
-        for dst, correct in zip(dsts, self._lifetime_corrections):
+        for dst, correct in zip(self.dsts, self.lifetime_corrections):
             dst.S2e *= correct(dst.Z.values).value
 
         # Join datasets
-        dst = pd.concat(dsts)
-        dst = dst[in_range(dst.S2e.values, *self._fiducial_e)]
-        dst = dst[in_range(dst.Z  .values, *self._fiducial_z)]
-        dst = dst[in_range(dst.X  .values, *self._xrange    )]
-        dst = dst[in_range(dst.Y  .values, *self._yrange    )]
+        dst = pd.concat(self.dsts)
+        # select fiducial region
+        dst = dst[in_range(dst.S2e.values, *self.fiducial_e)]
+        dst = dst[in_range(dst.Z  .values, *self.fiducial_z)]
+        dst = dst[in_range(dst.X  .values, *self.xrange    )]
+        dst = dst[in_range(dst.Y  .values, *self.yrange    )]
 
         # Compute corrections and stats
         xycorr = self.xy_correction(dst.X.values, dst.Y.values, dst.S2e.values)
@@ -76,3 +96,16 @@ class Zaira(MapCity):
 
         self.cnt.set_counter('n_events_tot', value=len(dst))
         return self.cnt
+
+
+    def xy_correction(self, X, Y, E):
+        xs, ys, es, us = fitf.profileXY(X, Y, E,
+                                        self.xbins, self.ybins,
+                                        self.xrange, self.yrange)
+
+        norm_index = xs.size//2, ys.size//2
+        return Correction((xs, ys), es, us, norm_strategy="index", index=norm_index)
+
+    def xy_statistics(self, X, Y):
+        return np.histogram2d(X, Y, (self.xbins, self.ybins),
+                                    (self.xrange, self.yrange))
