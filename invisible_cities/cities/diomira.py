9a9ea694811c1d5f01a16ff4a4b64140cb8823d0
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-07-24 10:30:23 +0200

    Refactor Diomira
    Diomira has been refactored so that, in addition to simulate sensor
    response it can now emulate the S2 trigger (and select events that
    pass the trigger). Tests have been fixed accordingly.

--- invisible_cities/cities/diomira.py
+++ invisible_cities/cities/diomira.py
@@ -13,6 +13,7 @@ from time      import time
 from functools import partial
 from argparse  import Namespace
 
+import numpy as np
 import tables as tb
 
 from .. core.system_of_units_c  import units
@@ -37,13 +38,13 @@ from .. evm.ic_containers       import PeakData
 from .. database                import load_db          as db
 from .. types.ic_types          import minmax
 from .  base_cities             import MonteCarloCity
-
+from .. filters.trigger_filters import TriggerFilter
 
 class Diomira(MonteCarloCity):
     """
-    The city of DIOMIRA simulates the response of the energy and
-    traking plane sensors.
-
+    The city of DIOMIRA simulates:
+    1. the response of the energy and traking plane sensors.
+    2. the response of the trigger
     """
 
     def __init__(self, **kwds):
@@ -52,6 +53,7 @@ class Diomira(MonteCarloCity):
         2. inits counters
         3. get sensor parameters
         4. inits the noise sampler and the sipms thesholds
+        5. instantiates the Trigger Filter.
         """
         super().__init__(**kwds)
         conf = self.conf
@@ -59,6 +61,14 @@ class Diomira(MonteCarloCity):
         self.cnt.set_name('diomira')
         self.cnt.set_counter('nmax', value=self.conf.nmax)
         self.cnt.init_counters(('n_events_tot', 'nevt_out'))
+
+        self.sipm_noise_cut   = conf.sipm_noise_cut
+
+        # thresholds in adc counts
+        self.sipms_thresholds = self.sipm_noise_cut *  self.sipm_adc_to_pes
+
+        self.trigger_filter   = TriggerFilter(self.trigger_params)
+
 
     def event_loop(self, NEVT, first_event_no, dataVectors):
         """
@@ -72,10 +82,16 @@ class Diomira(MonteCarloCity):
         write       = self.writers
         pmtrd       = dataVectors.pmt
         sipmrd      = dataVectors.sipm
-        mc_tracks    = dataVectors.mc
+        mc_tracks   = dataVectors.mc
         events_info = dataVectors.events
 
         for evt in range(NEVT):
+            # Count events in and break if necessary before filtering
+            if self.max_events_reached(self.cnt.counter_value('n_events_tot')):
+                break
+            else:
+                self.cnt.increment_counter('n_events_tot')
+
             # Simulate detector response
             dataPMT, blrPMT = self.simulate_pmt_response(evt, pmtrd,
                                                          self.sipm_adc_to_pes)
@@ -83,9 +99,18 @@ class Diomira(MonteCarloCity):
                                                          self.noise_sampler,
                                                          self.sipm_adc_to_pes)
             dataSiPM = wfm.noise_suppression(dataSiPM_noisy, self.sipms_thresholds)
+            RWF = dataPMT.astype(np.int16)
+            BLR = blrPMT.astype(np.int16)
 
             # simulate trigger
+            peak_data = self.emulate_trigger(RWF)
+            # filter events as a function of trigger
 
+            if not self.trigger_filter(peak_data):
+                continue
+            self.cnt.increment_counter('nevt_out')
+
+            #write
             event_number, timestamp = self.event_and_timestamp(evt, events_info)
             local_event_number = event_number + first_event_no
 
@@ -93,15 +118,12 @@ class Diomira(MonteCarloCity):
                 write.mc(mc_tracks, local_event_number)
 
             write.run_and_event(self.run_number, local_event_number, timestamp)
-            write.rwf(dataPMT.astype(int))
-            write.cwf( blrPMT.astype(int))
+            write.rwf(RWF)
+            write.cwf(BLR)
             write.sipm(dataSiPM)
 
-            self.conditional_print(evt, self.cnt.counter_value('n_events_tot'))
-            if self.max_events_reached(self.cnt.counter_value('n_events_tot')):
-                break
-            else:
-                self.cnt.increment_counter('n_events_tot')
+            self.conditional_print(self.cnt.counter_value('n_events_tot'),
+            self.cnt.counter_value('nevt_out'))
 
     def write_parameters(self, h5out):
         """Write deconvolution parameters to output file"""
