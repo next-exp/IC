946e71d02100225c6ab2bee3a7e17769100205a6
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-08-08 18:50:27 +0200

    Expand paolina_functions.
    Add two new functions and complete annotation.

--- invisible_cities/reco/paolina_functions.py
+++ invisible_cities/reco/paolina_functions.py
@@ -5,11 +5,16 @@ from itertools   import combinations
 import numpy    as np
 import networkx as nx
 
-from networkx           import Graph
-from .. evm.event_model import Voxel
-from .. core.exceptions import NoHits
-from .. core.exceptions import NoVoxels
-from .. evm.event_model import BHit
+from networkx                   import Graph
+from .. evm.event_model         import Voxel
+from .. core.exceptions         import NoHits
+from .. core.exceptions         import NoVoxels
+from .. evm.event_model         import BHit
+from .. evm.event_model         import Voxel
+from .. evm.event_model         import Track
+from .. evm.event_model         import Blob
+from ..evm.event_model          import TrackCollection
+from .. core.system_of_units_c  import units
 
 from typing import Sequence
 from typing import List
@@ -26,7 +31,8 @@ def bounding_box(seq : BHit) ->Sequence[np.ndarray]:
             reduce(np.maximum, posns, MIN3D))
 
 
-def voxelize_hits(hits : Sequence[BHit], voxel_dimensions : np.ndarray) ->List[Voxel]:
+def voxelize_hits(hits             : Sequence[BHit],
+                  voxel_dimensions : np.ndarray)->List[Voxel]:
     """1. Hits are enclosed by a bounding box.
        2. Boundix box is discretized (via a hitogramdd).
        3. The energy of all the hits insidex each discreet "voxel" is added.
@@ -46,27 +52,30 @@ def voxelize_hits(hits : Sequence[BHit], voxel_dimensions : np.ndarray) ->List[V
 
     cx, cy, cz = map(centres, edges)
     nz = np.nonzero(E)
-
-    return [Voxel(cx[x], cy[y], cz[z], E[x,y,z])
+    voxels = [Voxel(cx[x], cy[y], cz[z], E[x,y,z])
             for (x,y,z) in np.stack(nz).T]
 
+    return voxels
 
-def make_track_graphs(voxels : Voxel,  voxel_dimensions : np.ndarray) ->Sequence[Graph]:
+
+def make_track_graphs(voxels           : Voxel,
+                      voxel_dimensions : np.ndarray,
+                      contiguity       : float = 1) ->Sequence[Graph]:
     """Creates a graph where the voxels are the nodes and the edges are any
     pair of neighbour voxel. Two voxels are considered to be neighbours if
-    their distance normalized to their size is smaller than a scale factor.
+    their distance normalized to their size is smaller than a
+    contiguity factor .
 
     """
 
-    def neighbours(va : Voxel, vb : Voxel, scale : float = 1.5) ->bool:
-        return ((abs(va.pos - vb.pos) / voxel_dimensions) < scale).all()
+    def neighbours(va : Voxel, vb : Voxel, scale : float = 1.0) ->bool:
+        return ((abs(va.pos - vb.pos) / voxel_dimensions) < contiguity).all()
 
     voxel_graph = nx.Graph()
     voxel_graph.add_nodes_from(voxels)
     for va, vb in combinations(voxels, 2):
         if neighbours(va, vb):
-            voxel_graph.add_edge(va, vb,
-                                 distance = np.linalg.norm(va.pos - vb.pos))
+            voxel_graph.add_edge(va, vb, distance = np.linalg.norm(va.pos - vb.pos))
 
     return tuple(nx.connected_component_subgraphs(voxel_graph))
 
@@ -121,11 +130,34 @@ def blob_energies(track_graph : Graph, radius : float) ->Tuple[float, float]:
     return (Ea, Eb) if Ea < Eb else (Eb, Ea)
 
 
-def blobs(track_graph : Graph, radius : float) ->Tuple[List[Voxel], List[Voxel]]:
+def compute_blobs(track_graph : Graph, radius : float) ->Tuple[List[Voxel], List[Voxel]]:
     """Return the blobs (list of voxels) around the extrema of the track. """
     distances = shortest_paths(track_graph)
     a,b = find_extrema(distances)
     ba = voxels_within_radius(distances[a], radius)
     bb = voxels_within_radius(distances[b], radius)
 
-    return (ba, bb)
+    return a, b, ba, bb
+
+
+def make_tracks(evt_number       : float,
+                evt_time         : float,
+                voxels           : List[Voxel],
+                voxel_dimensions : np.ndarray,
+                contiguity       : float = 1,
+                blob_radius      : float = 30*units.mm) ->TrackCollection:
+    """Makes a track collection. """
+
+    tc = TrackCollection(evt_number, evt_time) # type: TrackCollection
+    track_graphs = make_track_graphs(voxels, voxel_dimensions) # type: Sequence[Graph]
+    for trk in track_graphs:
+        # distances = shortest_paths(trk) # type: Dict[Voxel, Dict[Voxel, float]]
+        # a,b       = find_extrema(distances) # type: Tuple[Voxel, Voxel]
+        a, b, voxels_a, voxels_b    = compute_blobs(trk, blob_radius)
+
+        blob_a = Blob(a, voxels_a) # type: Blob
+        blob_b = Blob(b, voxels_b)
+        blobs = (blob_a, blob_b) if blob_a.E < blob_b.E else (blob_b, blob_a)
+        track = Track(voxels_from_track_graph(trk), blobs)
+        tc.tracks.append(track)
+    return tc
