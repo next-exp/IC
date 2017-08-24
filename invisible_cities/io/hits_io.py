2a01e943127daaad432bbc125c371b1ac25e7c2a
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2017-08-24 19:34:18 +0200

    Refactor dst_io splitting hits_io. Add tests.
    dst_io has been refactored. The file dst_io contains now only general
    reading functions (e.g, load_dst), while hits_io now contains all the
    functionality related with reading and writing hits.
    
    Tests have been added to exercize the new reading functionality.

---
+++ invisible_cities/io/hits_io.py
@@ -0,0 +1,53 @@
+
+from . dst_io              import load_dst
+from ..evm.event_model     import Hit
+from ..evm.event_model     import Cluster
+from ..evm.event_model     import HitCollection
+from .. types.ic_types     import xy
+from .  table_io           import make_table
+from .. evm .nh5           import HitsTable
+
+
+# reader
+def load_hits(DST_file_name):
+    """Return the Hits as PD DataFrames."""
+
+    dst = load_dst(DST_file_name,'RECO','Events')
+    dst_size = len(dst)
+    all_events = {}
+
+    event = dst.event.values
+    time  = dst.time .values
+    npeak = dst.npeak.values
+    nsipm = dst.nsipm.values
+    X     = dst.X    .values
+    Y     = dst.Y    .values
+    Xrms  = dst.Xrms .values
+    Yrms  = dst.Yrms .values
+    Z     = dst.Z    .values
+    Q     = dst.Q    .values
+    E     = dst.E    .values
+
+    for i in range(dst_size):
+        current_event = all_events.setdefault(event[i],
+                                              HitCollection(event[i], time[i] * 1e-3))
+        hit = Hit(npeak[i],
+                 Cluster(Q[i], xy(X[i],Y[i]), xy(Xrms[i],Yrms[i]),
+                         nsipm[i], Z[i], E[i]), Z[i], E[i])
+        current_event.hits.append(hit)
+    return all_events
+
+# writers
+def hits_writer(hdf5_file, *, compression='ZLIB4'):
+    hits_table  = make_table(hdf5_file,
+                             group       = 'RECO',
+                             name        = 'Events',
+                             fformat     = HitsTable,
+                             description = 'Hits',
+                             compression = compression)
+
+    hits_table.cols.event.create_index()
+
+    def write_hits(hits_event):
+        hits_event.store(hits_table)
+    return write_hits
