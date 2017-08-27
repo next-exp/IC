e8a04aca02105e1c14d11fe98ffff7816195a7a9
J. J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-08-27 11:49:47 +0200

    Refactor mchits_io and add a second test.
    A minor refactoring to return data in a more consistent way.
    
    load_mcparticles(file_name) --> returns event_dicts for particles
    load_mchits(file_name) --> returns event_dicts for hits.
    
    In addition a second test has been added. The test checks that the
    first particle and all its hits in disk is correctly loaded into the
    high level class.
    
    The two existing tests, while simple, check that the data format is
    correct and the high level classes are correctly instantiated.

--- invisible_cities/io/mchits_io_test.py
+++ invisible_cities/io/mchits_io_test.py
@@ -1,21 +1,43 @@
 import os
 import numpy  as np
 from . mchits_io import load_mchits
+from . mchits_io import load_mcparticles
 
-def test_load_mchits(electron_MCRD_file):
+def test_load_mchits(mc_particle_and_hits_data):
+    efile, _, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_data
 
-    X = [-0.10718990862369537, -0.16415221989154816, -0.18664051592350006, -0.19431403279304504]
-    Y = [-0.01200979482382536, -0.07335199415683746, -0.09059777110815048, -0.09717071801424026,]
-    Z = [25.12295150756836, 25.140811920166016, 25.11968994140625, 25.115009307861328]
-    E = [0.006218845956027508, 0.014433029107749462, 0.010182539001107216, 0.009165585972368717]
-    t = [0.0009834024822339416, 0.0018070531077682972, 0.002247565658763051, 0.002446305239573121]
-
-    mchits_dict = load_mchits(electron_MCRD_file)
+    mchits_dict = load_mchits(efile)
     hX = [hit.X for hit in mchits_dict[0]]
     hY = [hit.Y for hit in mchits_dict[0]]
     hZ = [hit.Z for hit in mchits_dict[0]]
     hE = [hit.E for hit in mchits_dict[0]]
     ht = [hit.time for hit in mchits_dict[0]]
+
+    assert np.isclose(X,hX).all()
+    assert np.isclose(Y,hY).all()
+    assert np.isclose(Z,hZ).all()
+    assert np.isclose(E,hE).all()
+    assert np.isclose(t,ht).all()
+
+def test_load_mcparticles(mc_particle_and_hits_data):
+    efile, name, pdg, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_data
+
+    mcparticles_dict = load_mcparticles(efile)
+    particle = mcparticles_dict[0][0]
+
+    assert particle.name == name
+    assert particle.pdg  == pdg
+    assert np.isclose(particle.vi,vi).all()
+    assert np.isclose(particle.vf,vf).all()
+    assert np.isclose(particle.p,p).all()
+    assert particle.E    == Ep
+    assert len(particle.hits) == nhits
+
+    hX = [hit.X for hit in particle.hits]
+    hY = [hit.Y for hit in particle.hits]
+    hZ = [hit.Z for hit in particle.hits]
+    hE = [hit.E for hit in particle.hits]
+    ht = [hit.time for hit in particle.hits]
 
     assert np.isclose(X,hX).all()
     assert np.isclose(Y,hY).all()
