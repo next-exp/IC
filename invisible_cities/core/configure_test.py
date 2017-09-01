864a87bcdff017e5a3ae6456edff96aaf70fb1e9
Jacek Generowicz <github@my-post-office.net>
2017-09-01 10:20:02 +0100

    Test run-number
    It fails! Two problems appear.
    
    + The config printer picks up the value specified on the command line,
      but reports that it came from the configuration file.
    
    + The config object contains a value of zero, ignoring both the value
      on the command line and the value in the config file.

--- invisible_cities/core/configure_test.py
+++ invisible_cities/core/configure_test.py
@@ -307,8 +307,11 @@ def test_config_drive_flags(simple_conf_file_name, tmpdir_factory):
     assert conf.first_event == 0
 
 @mark.parametrize(  'name          flags            value'.split(),
-                  (('first_event',            '-f 21', 21),
-                   ('first_event', '--first-event 22', 22)))
+                  (('first_event',              '-f 21', 21),
+                   ('first_event',   '--first-event 22', 22),
+                   ('run_number',               '-r 23', 23),
+                   ('run_number',     '--run-number 24', 24),
+                  ))
 def test_config_drive_first_event_short(simple_conf_file_name, tmpdir_factory, name, flags, value):
     conf   = simple_conf_file_name
     infile = conf # any existing file will do as a dummy for now
