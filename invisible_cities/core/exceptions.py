4d0a30759800250efed36416b183fd0789157bcf
Jacek Generowicz <github@my-post-office.net>
2017-09-07 23:23:44 +0200

    Remove pointless passes
    `pass` is not required if there is *any* code in the block at all. A
    docstring is code!

--- invisible_cities/core/exceptions.py
+++ invisible_cities/core/exceptions.py
@@ -4,12 +4,9 @@ Define IC-specific exceptions
 
 class ICException(Exception):
     """ Base class for IC exceptions hierarchy """
-    pass
 
 class NoInputFiles(ICException):
     """ Input files list is not defined """
-    pass
-
 
 class NoOutputFile(ICException):
     pass
