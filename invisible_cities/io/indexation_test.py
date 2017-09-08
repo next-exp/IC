1e1ccd574b17ff7776ef323fa159d82555d7af4f
Alejandro Botas <alejbotas@gmail.com>
2017-09-08 16:02:29 +0900

    create city.index_tables() and call in city.end()

--- invisible_cities/io/indexation_test.py
+++ invisible_cities/io/indexation_test.py
@@ -2,24 +2,40 @@ import os
 import tables as tb
 
 from pytest import mark
+from pytest import fixture
 
+from .. cities.base_cities import City
+from .. core.configure   import configure
 from . hits_io import     hits_writer
 from . kdst_io import       kr_writer
 from .   mc_io import mc_track_writer
 from . pmap_io import     pmap_writer
 
 
-@mark.parametrize("writer content group node column".split(),
-                  [(    hits_writer, "hits"   , "RECO" , "Events"  , "event"     ),
-                   (      kr_writer, "kr"     , "DST"  , "Events"  , "event"     ),
-                   (mc_track_writer, "mctrk"  , "MC"   , "MCTracks", "event_indx"),
-                   (    pmap_writer, "S1pmaps", "PMAPS", "S1"      , "event"     ),
-                   (    pmap_writer, "S2pmaps", "PMAPS", "S2"      , "event"     ),
-                   (    pmap_writer, "Sipmaps", "PMAPS", "S2Si"    , "event"     )])
-def test_hits_table_is_indexed(config_tmpdir, writer, content, group, node, column):
-    outputfilename = os.path.join(config_tmpdir, f"empty_{content}_file.h5")
 
-    with tb.open_file(outputfilename, 'w') as h5out:
-        writer(h5out)
+@fixture
+def init_city(ICDIR, config_tmpdir):
+        conf = configure(('city ' + ICDIR +  'config/city.conf').split())
+        file_out = os.path.join(config_tmpdir, f"empty_file.h5")
+        conf.update(dict(file_out = file_out))
+        city = City(**conf)
+        city.display_IO_info = lambda: None
+        city.file_loop       = lambda: None
+        return city, file_out
+
+
+@mark.parametrize("writer group node column".split(),
+                  [(    hits_writer, "RECO" , "Events"  , "event"     ),
+                   (      kr_writer, "DST"  , "Events"  , "event"     ),
+                   (mc_track_writer, "MC"   , "MCTracks", "event_indx"),
+                   (    pmap_writer, "PMAPS", "S1"      , "event"     ),
+                   (    pmap_writer, "PMAPS", "S2"      , "event"     ),
+                   (    pmap_writer, "PMAPS", "S2Si"    , "event"     )])
+def test_hits_table_is_indexed(init_city, writer, group, node, column):
+    city, file_out = init_city
+    city.get_writers = writer
+    city.run()
+    city.end() # or city.index_tables(), but this checks that city.end() calls city.index_tables()
+    with tb.open_file(file_out, 'r') as h5out:
         table = getattr(getattr(h5out.root, group), node)
         assert getattr(table.cols, column).is_indexed
