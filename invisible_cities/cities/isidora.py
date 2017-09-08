4787840bcea44d2475bc615476ed01743c41185b
Gonzalo <gonzaponte@gmail.com>
2017-09-08 11:40:53 +0200

    move loop control to the top

--- invisible_cities/cities/isidora.py
+++ invisible_cities/cities/isidora.py
@@ -61,6 +61,11 @@ class Isidora(DeconvolutionCity):
         events_info = dataVectors.events
 
         for evt in range(NEVT):
+            what_next = self.event_range_step()
+            if what_next is EventLoop.skip_this_event: continue
+            if what_next is EventLoop.terminate_loop : break
+            self.cnt.increment_counter('n_events_tot')
+
             CWF = self.deconv_pmt(pmtrwf[evt])  # deconvolution
 
             # write stuff
@@ -71,12 +76,7 @@ class Isidora(DeconvolutionCity):
             if self.monte_carlo:
                 write.mc(mc_tracks, self.cnt.counter_value('n_events_tot'))
 
-            # conditional print and exit of loop condition
             self.conditional_print(evt, self.cnt.counter_value('n_events_tot'))
-            what_next = self.event_range_step()
-            if what_next is EventLoop.skip_this_event: continue
-            if what_next is EventLoop.terminate_loop : break
-            self.cnt.increment_counter('n_events_tot')
 
     def get_writers(self, h5out):
         """Get the writers needed by Isidora"""
