a8d93791adea4a754d4e05ee43969ee13e518a93
Gonzalo <gonzaponte@gmail.com>
2017-09-08 11:42:11 +0200

    move conditional prints to the top of the loop

--- invisible_cities/cities/penthesilea.py
+++ invisible_cities/cities/penthesilea.py
@@ -56,6 +56,9 @@ class Penthesilea(HitCity):
         s2si_dict = pmapVectors.s2si
 
         for evt_number, evt_time in zip(event_numbers, timestamps):
+            self.conditional_print(self.cnt.counter_value('n_events_tot'),
+                                   self.cnt.counter_value('nevt_out'))
+
             # Count events in and break if necessary before filtering
             what_next = self.event_range_step()
             if what_next is EventLoop.skip_this_event: continue
@@ -83,8 +86,6 @@ class Penthesilea(HitCity):
                                       timestamps = evt_time)
             evt = self.create_hits_event(pmapVectors)
             write_hits(evt)
-            self.conditional_print(self.cnt.counter_value('n_events_tot'),
-                                   self.cnt.counter_value('nevt_out'))
 
     def get_writers(self, h5out):
         """Get the writers needed by dorothea"""
