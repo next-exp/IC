c1aed89f087ea38901d968caf3397fcdd0f547f9
J.J. Gómez-Cadenas <jjgomezcadenas@gmail.com>
2017-09-14 22:27:15 +0200

    Refactor (simplify) Dorothea and Penthesilea.
    Dotty and Patty loose a lot of fat!

--- invisible_cities/cities/dorothea.py
+++ invisible_cities/cities/dorothea.py
@@ -6,20 +6,14 @@ credits: see ic_authors_and_legal.rst in /doc
 last revised: JJGC, September-2017
 """
 
-rom .. io.kdst_io               import kr_writer
-from .. evm.ic_containers       import PmapVectors
-
-from .. filters.s1s2_filter    import S12Selector
-from .  base_cities            import KrCity
-
+from .. io.kdst_io               import kr_writer
+from .  base_cities              import KrCity
 
 class Dorothea(KrCity):
+    """Read PMAPS and produces a KDST"""
 
     def __init__(self, **kwds):
         super().__init__(**kwds)
-
-        self.drift_v = self.conf.drift_v
-        self._s1s2_selector = S12Selector(**kwds)
 
     def get_writers(self, h5out):
         """Get the writers needed by dorothea"""
@@ -29,6 +23,5 @@ class Dorothea(KrCity):
         """Get the writers needed by dorothea"""
         return  self.create_kr_event(pmapVectors)
 
-    def s12_selector(self):
-        """Return the selector defined for Dorothea"""
-        return self._s1s2_selector
+    def write_parameters(self, h5out):
+        pass
