e24d2ea3b1d37039bc0a19f7b3ad66920eb6ca00
Jacek Generowicz <github@my-post-office.net>
2017-09-18 17:02:56 +0200

    Stop suppressing ModuleNotFoundError for unknown daemons

--- invisible_cities/daemons/idaemon.py
+++ invisible_cities/daemons/idaemon.py
@@ -2,15 +2,9 @@ from importlib import import_module
 import traceback
 
 
-
 def summon_daemon(daemon_name):
-    """Takes a daemon name and returns an instance of the daemon"""
-    try:
-        module_name = 'invisible_cities.daemons.' + daemon_name
-        daemon_class  = getattr(import_module(module_name),
-                                          daemon_name.capitalize())
-    except ModuleNotFoundError:
-        traceback.print_exc()
-    else:
+    """Take a daemon name and return a new instance of the daemon"""
+    module_name = 'invisible_cities.daemons.' + daemon_name
+    daemon_class  = getattr(import_module(module_name), daemon_name.capitalize())
+    return daemon_class()
 
-        return daemon_class()
