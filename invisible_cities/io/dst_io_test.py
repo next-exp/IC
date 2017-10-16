7824082e9359505d537faa8ddd9eea8f70e513f5
Gonzalo <gonzaponte@gmail.com>
2017-10-16 13:03:58 +0200

    Rewrite dst_io_test in terms of the new fixture

--- invisible_cities/io/dst_io_test.py
+++ invisible_cities/io/dst_io_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from . dst_io              import load_dst
 from numpy.testing import assert_allclose
 
-from .. core.testing_utils    import assert_dataframes_equal
+from .. core.testing_utils    import assert_dataframes_close
 # from ..types.ic_types      import xy
 # from ..evm.event_model     import Cluster
 # from ..evm.event_model     import Hit
@@ -15,23 +15,23 @@ from . dst_io                 import load_dst
 from . dst_io                 import load_dsts
 
 
-def test_load_dst(Kr_dst_data):
-    (filename, group, node), df = Kr_dst_data
-    dst = load_dst(filename, group, node)
+def test_load_dst(KrMC_kdst):
+    # The function load_dst is actually used in the fixture
+    *_, df_read, df_true = KrMC_kdst
 
-    assert_dataframes_equal(dst, df, False)
-
-
-def test_load_dsts_single_file(Kr_dst_data):
-    (filename, group, node), df = Kr_dst_data
-    dst = load_dsts([filename], group, node)
-
-    assert_dataframes_equal(dst, df, False)
+    assert_dataframes_close(df_read, df_true, False)
 
 
-def test_load_dsts_double_file(Kr_dst_data):
-    (filename, group, node), df = Kr_dst_data
-    dst = load_dsts([filename]*2, group, node)
-    df  = pd.concat([df, df])
+def test_load_dsts_single_file(KrMC_kdst):
+    (filename, group, node), *_, df_true = KrMC_kdst
+    df_read = load_dsts([filename], group, node)
 
-    assert_dataframes_equal(dst, df, False)
+    assert_dataframes_close(df_read, df_true, False)
+
+
+def test_load_dsts_double_file(KrMC_kdst):
+    (filename, group, node), *_, df_true = KrMC_kdst
+    df_read = load_dsts([filename]*2, group, node)
+    df_true = pd.concat([df_true, df_true])
+
+    assert_dataframes_close(df_read, df_true, False)
