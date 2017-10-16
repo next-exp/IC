4401e8f94fb5a39cc5f726a7d27633aba9855c05
Gonzalo <gonzaponte@gmail.com>
2017-10-16 12:02:45 +0200

    Fix pmaps tests
    They contained hard-coded values based on the previous
    test file. Besides fixing the test itself, this commit
    removes that dependence.

--- invisible_cities/evm/pmaps_test.py
+++ invisible_cities/evm/pmaps_test.py
@@ -111,51 +111,49 @@ def test_s1__(wform):
             np.allclose (s1.peak_waveform(i).E , s1d[i][1], rtol=1e-4)
 
 
-@given(integers(min_value=31, max_value=40)) # pick a random event, limits from KrMC_pmaps fixture in conftest.py
-def test_s1_s2(KrMC_pmaps, evt_no):
+def test_s1_s2(KrMC_pmaps):
     *_, (s1_dict, s2_dict, _) = KrMC_pmaps
 
-    s1 = s1_dict[evt_no]
-    s2 = s2_dict[evt_no]
+    for s12_evts, peak_attr in zip(( s1_dict,  s2_dict),
+                                   ("s1d"   , "s2d"   )):
+        for evt_no, s12 in s12_evts.items():
+            assert s12.number_of_peaks == len(s12.peaks)
 
-    assert s1.number_of_peaks == len(s1.peaks)
-    assert s2.number_of_peaks == len(s2.peaks)
-
-    for peak_no, (t,E) in s1.s1d.items():
-        pwf = s1.peak_waveform(peak_no)
-        pk =  Peak(t,E)
-        np.allclose (pwf.t , pk.t, rtol=1e-4)
-        np.allclose (pwf.E , pk.E, rtol=1e-4)
+            peak_dict = getattr(s12, peak_attr)
+            for peak_no, (t, E) in peak_dict.items():
+                pwf = s12.peak_waveform(peak_no)
+                pk =  Peak(t, E)
+                np.allclose(pwf.t , pk.t, rtol=1e-4)
+                np.allclose(pwf.E , pk.E, rtol=1e-4)
 
 
-@given(integers(min_value=31, max_value=40))
-def test_s2si(KrMC_pmaps, evt_no):
+def test_s2si(KrMC_pmaps):
     *_, (_, _, s2si_dict) = KrMC_pmaps
 
-    s2si = s2si_dict[evt_no]
-    Q_dict = s2si.peak_and_sipm_total_energy_dict()
+    for evt_no, s2si in s2si_dict.items():
+        Q_dict = s2si.peak_and_sipm_total_energy_dict()
 
-    for peak_number in s2si.peak_collection():
-        assert (s2si.number_of_sipms_in_peak(peak_number) ==
-                len(s2si.s2sid[peak_number]))
+        for peak_number in s2si.peak_collection():
+            assert (s2si.number_of_sipms_in_peak(peak_number) ==
+                    len(s2si.s2sid[peak_number]))
 
-        np.array_equal(np.array(s2si.sipms_in_peak(peak_number)),
-                       np.array(s2si.s2sid[peak_number].keys()))
+            np.array_equal(np.array(s2si.sipms_in_peak(peak_number)),
+                           np.array(s2si.s2sid[peak_number].keys()))
 
-        Q_sipm_dict = s2si.sipm_total_energy_dict(peak_number)
-        qdict = Q_dict[peak_number]
+            Q_sipm_dict = s2si.sipm_total_energy_dict(peak_number)
+            qdict = Q_dict[peak_number]
 
-        for sipm_number in s2si.sipms_in_peak(peak_number):
-            Q = np.sum(s2si.s2sid[peak_number][sipm_number])
-            np.allclose(Q_sipm_dict[sipm_number] , Q)
-            np.allclose(qdict[sipm_number] , Q)
-            w   = s2si.sipm_waveform(peak_number, sipm_number)
-            wzs = s2si.sipm_waveform_zs(peak_number, sipm_number)
-            E   = s2si.s2sid[peak_number][sipm_number]
-            t = s2si.peak_waveform(peak_number).t
-            np.allclose(w.E , E)
-            np.allclose(wzs.E , E[E>0])
-            np.allclose(wzs.t , t[E>0])
+            for sipm_number in s2si.sipms_in_peak(peak_number):
+                Q = np.sum(s2si.s2sid[peak_number][sipm_number])
+                np.allclose(Q_sipm_dict[sipm_number] , Q)
+                np.allclose(qdict[sipm_number] , Q)
+                w   = s2si.sipm_waveform(peak_number, sipm_number)
+                wzs = s2si.sipm_waveform_zs(peak_number, sipm_number)
+                E   = s2si.s2sid[peak_number][sipm_number]
+                t = s2si.peak_waveform(peak_number).t
+                np.allclose(w.E , E)
+                np.allclose(wzs.E , E[E>0])
+                np.allclose(wzs.t , t[E>0])
 
 
 @given(s2si_input())
