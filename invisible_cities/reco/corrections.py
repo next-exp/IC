Fix "max" normalization option
The "max" normalization option worked only for 1D corrections and
crashed if more dimensions where present. This was a consequence of
`np.argmax` returning a flat index.

invisible_cities/reco/corrections.py
@@ -79,9 +79,10 @@ class Correction:
             u_ref = 0
 
         elif   strategy == "max"  :
-            index = np.argmax(self._fs)
-            f_ref = self._fs[index]
-            u_ref = self._us[index]
+            flat_index = np.argmax(self._fs)
+            mult_index = np.unravel_index(flat_index, self._fs.shape)
+            f_ref = self._fs[mult_index]
+            u_ref = self._us[mult_index]
 
         elif   strategy == "index":
             if "index" not in opts: