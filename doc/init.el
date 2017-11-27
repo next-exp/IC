0e0a52c7e7c09244529e7bca8b09901b36b7ad60
Jacek Generowicz <github@my-post-office.net>
2017-11-27 15:48:10 +0100

    402 Translate emacs init file from el-get to use-package
    https://github.com/nextic/IC/pull/402
    
    [jacg]
    
    el-get was getting rather flaky down the years. use-package seems much
    more stable.
    
    [approved by jjgomezcadenas]
    
    Indeed, solves #401

--- doc/init.el
+++ doc/init.el
@@ -1,110 +1,82 @@
-;;; bootstrapping elget
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
-;; (setq el-get-user-package-directory "~/.emacs.d/el-get/el-get-init-files/")
-(setq
- el-get-sources
- '(el-get
-
-   (:name magit
-          :after (progn
-                   (require 'magit)
-                   ;;(setq magit-last-seen-setup-instructions "1.4.0")
-                   (global-set-key (kbd "C-x g") 'magit-status)))
-
-   (:name helm
-          :after (progn
-                   (require 'helm)
-                   (require 'helm-config)
-                   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-                   (define-key helm-map (kbd "C-z") 'helm-select-action)
-
-                   (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-                         helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source
-                         helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp
-                         helm-scroll-amount                    8 ; scroll 8 lines other window usin M-<next>/M-<prior>
-                         helm-ff-file-name-history-use-recentf t)
-
-                   (helm-mode 1)
-                   (helm-autoresize-mode t)
-                   (setq helm-M-x-fuzzy-match t)
-                   (magit-define-popup-switch 'magit-log-popup ?p "first parent" "--first-parent")
-                   ;; the default is dangerously close to C-x C-c which kills emacs
-                   ;; (global-unset-key (kbd "C-x c")) ; done in custom
-                   (global-set-key (kbd "C-x C-f") 'helm-find-files)
-                   (global-set-key (kbd "M-x") 'helm-M-x)
-                   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-                   (global-set-key (kbd "C-x b") 'ido-switch-buffer)))
-
-   (:name helm-ag
-          :after (progn (require 'helm-ag)))
-
-   (:name helm-swoop
-          :after (progn
-                   (require 'helm-swoop)
-
-                   ;; I feel a hydra on a key-chord coming on. Or maybe just access it via isearch forward
-                   (global-set-key (kbd "M-i") 'helm-swoop)
-                   (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
-                   (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
-                   (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
-
-                   ;; When doing isearch, hand the word over to helm-swoop
-                   (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-                   ;; From helm-swoop to helm-multi-swoop-all
-                   (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-
-                   ;; Move up and down like isearch
-                   (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-                   (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-                   (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
-                   (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
-
-                   ;; Save buffer when helm-multi-swoop-edit complete
-                   (setq helm-multi-swoop-edit-save t)
-
-                   ;; If this value is t, split window inside the current window
-                   (setq helm-swoop-split-with-multiple-windows nil)
-
-                   ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
-                   (setq helm-swoop-split-direction 'split-window-horizontally)
-
-                   ;; If nil, you can slightly boost invoke speed in exchange for text color
-                   (setq helm-swoop-speed-or-color nil)
-
-                   ;; Go to the opposite side of line from the end or beginning of line
-                   (setq helm-swoop-move-to-line-cycle t)
-
-                   ;; Optional face for line numbers
-                   ;; Face name is `helm-swoop-line-number-face`
-                   (setq helm-swoop-use-line-number-face t)))))
-
-(require 'el-get-elpa)
-(unless (file-directory-p el-get-recipe-path-elpa)
- (el-get-elpa-build-local-recipes))
-
-;; install new packages and init already installed packages
-(el-get 'sync (mapcar 'el-get-source-name el-get-sources))
-
 (require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
+(setq use-package-always-ensure t)
+(put 'use-package 'lisp-indent-function 1)
+
+
+(use-package magit
+
+  :bind  (("M-g M-s" . magit-status)
+          ("M-g M-d" . magit-dispatch-popup)
+          ("C-x g"   . magit-status)
+          ("C-x C-g" . magit-dispatch-popup))
+
+  :config
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  (global-magit-file-mode 1)
+  (magit-define-popup-switch 'magit-log-popup ?p "first parent" "--first-parent"))
+
+
+(use-package helm
+  :bind (("C-c h h" . helm-command-prefix)
+         ("M-x"     . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("M-y"     . helm-show-kill-ring)
+         ("C-x b"   . ido-switch-buffer)
+
+         :unbind "C-x c"                ; Too similar to C-x C-c !
+
+         :map helm-map
+         ;; ("<tab>" . helm-execute-persistent-action) ; Bug: gets
+         ;; bound in global map ! so I'm doing it below with
+         ;; define-key instead
+         ;;("C-i"   . helm-execute-persistent-action)
+         ("C-z"   . helm-select-action)
+
+         :map
+         helm-command-prefix
+         ("g"     . helm-do-grep)
+         ("o"     . helm-occur))
+
+  :config
+  (require 'helm-config)
+  ;; Bug: :bind :map doesn't seem to work properly for special keys,
+  ;; and bind <tab> in the global map.
+  (bind-key "<tab>" #'helm-execute-persistent-action helm-map)
+
+  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+        helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source
+        helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp
+        helm-scroll-amount                    8 ; scroll 8 lines other window usin M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t
+        helm-M-x-fuzzy-match                  t
+        helm-move-to-line-cycle-in-source     nil
+        helm-command-prefix-key               "C-z")
+
+  (helm-mode 1)
+  (helm-autoresize-mode t))
+
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-command-prefix-key "C-z")
  '(indent-tabs-mode nil)
+ '(package-selected-packages (quote (helm magit use-package)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
