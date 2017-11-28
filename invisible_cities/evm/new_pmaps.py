4f68363e9ff69abcabdf1d93c3ed93014f92e15c
Alejandro Botas <alejbotas@gmail.com>
2017-11-28 13:11:58 +0100

    Implement SensorResponses class

--- invisible_cities/evm/new_pmaps.py
+++ invisible_cities/evm/new_pmaps.py
@@ -1,3 +1,7 @@
+from textwrap import dedent
+
+import numpy as np
+
 class PMap:
 
     @property
@@ -51,20 +55,66 @@ class S1(_Peak): pass
 class S2(_Peak): pass
 
 
-class SensorResponses:
+class _SensorResponses:
+    def __init__(self, ids, wfs):
+        self._check_valid_input(ids, wfs)
+
+        self._ids              = np.array(ids, copy=False, ndmin=1)
+        self._all_waveforms    = np.array(wfs, copy=False, ndmin=2)
+        self._wfs_dict         = dict(zip(self._ids, self._all_waveforms))
+        self._sum_over_sensors = np.sum(self._all_waveforms, axis=0)
+        self._sum_over_times   = np.sum(self._all_waveforms, axis=1)
 
     @property
-    def all_waveforms(self): pass
+    def all_waveforms(self):
+        return self._all_waveforms
 
-    def waveform(self, sensor_id): pass
+    def waveform(self, sensor_id):
+        return self._wfs_dict[sensor_id]
 
-    def time_slice(self, slice_number): pass
-
-    @property
-    def ids(self): pass
-
-    @property
-    def sum_over_times(self): pass
+    def time_slice(self, slice_number):
+        return self._all_waveforms[:, slice_number]
 
     @property
-    def sum_over_sensors(self): pass
+    def ids(self):
+        return self._ids
+
+    @property
+    def sum_over_times(self):
+        return self._sum_over_times
+
+    @property
+    def sum_over_sensors(self):
+        return self._sum_over_sensors
+
+    def where_above_threshold(self, thr):
+        return np.where(self.sum_over_sensors > thr)[0]
+
+    @classmethod
+    def build_empty_instance(cls):
+        return cls([], [])
+
+    def __repr__(self):
+        n_sensors = len(self.ids)
+        header = dedent(f"""
+            ------------------------
+            {self.__class__.__name__} instance
+            ------------------------
+            Number of sensors: {n_sensors}""")
+
+        sensors = [f"""
+            | ID: {ID}
+            | WF: {wf}
+            """ for ID, wf in self._wfs_dict.items()]
+        return header + "".join(map(dedent, sensors))
+
+    def _check_valid_input(self, ids, wfs):
+        if len(ids) != len(wfs):
+            msg  =  "Shapes do not match\n"
+            msg += f"ids has length {len(ids)}\n"
+            msg += f"wfs has length {len(wfs)}\n"
+            raise ValueError(msg)
+
+
+class PMTResponses (_SensorResponses): pass
+class SiPMResponses(_SensorResponses): pass
