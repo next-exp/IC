1570af90e9969f1687a898c534a5047a50b7087d
Alejandro Botas <alejbotas@gmail.com>
2017-11-28 13:24:42 +0100

    Implement _Peak class

--- invisible_cities/evm/new_pmaps.py
+++ invisible_cities/evm/new_pmaps.py
@@ -2,6 +2,10 @@ from textwrap import dedent
 
 import numpy as np
 
+from .. core.system_of_units_c import units
+from .. core.core_functions    import weighted_mean_and_std
+
+
 class PMap:
 
     @property
@@ -19,37 +23,107 @@ class PMap:
 
 
 class _Peak:
+    def __init__(self, times, pmts, sipms):
+        self._check_valid_input(times, pmts, sipms)
+
+        self._times = np.asarray(times)
+        self._pmts  = pmts
+        self._sipms = sipms
 
     @property
-    def sipms(self): pass
+    def times(self):
+        return self._times
 
     @property
-    def pmts(self): pass
+    def pmts(self):
+        return self._pmts
 
     @property
-    def times(self): pass
+    def sipms(self):
+        return self._sipms
 
     @property
-    def time_at_max_energy(self): pass
+    def time_at_max_energy(self):
+        return self.times[np.argmax(self.pmts.sum_over_sensors)]
 
     @property
-    def total_energy(self): pass
+    def total_energy(self):
+        return self.energy_above_threshold(0)
 
     @property
-    def height(self): pass
+    def total_charge(self):
+        return self.charge_above_threshold(0)
 
     @property
-    def width(self): pass
+    def height(self):
+        return np.max(self.pmts.sum_over_sensors)
 
     @property
-    def rms(self): pass
+    def width(self):
+        return self.width_above_threshold(0)
 
-    def energy_above_threshold(self, thr): pass
+    @property
+    def rms(self):
+        return self.rms_above_threshold(0)
 
-    def  width_above_threshold(self, thr): pass
+    def energy_above_threshold(self, thr):
+        i_above_thr  = self.pmts.where_above_threshold(thr)
+        wf_above_thr = self.pmts.sum_over_sensors[i_above_thr]
+        return np.sum(wf_above_thr)
 
-    def    rms_above_threshold(self, thr): pass
+    def charge_above_threshold(self, thr):
+        i_above_thr  = self.sipms.where_above_threshold(thr)
+        wf_above_thr = self.sipms.sum_over_sensors[i_above_thr]
+        return np.sum(wf_above_thr)
 
+    def  width_above_threshold(self, thr):
+        i_above_thr = self.pmts.where_above_threshold(thr)
+        if np.size(i_above_thr) < 1:
+            return 0
+
+        times_above_thr = self.times[i_above_thr]
+        return times_above_thr[-1] - times_above_thr[0]
+
+    def    rms_above_threshold(self, thr):
+        i_above_thr     = self.pmts.where_above_threshold(thr)
+        times_above_thr = self.times[i_above_thr]
+        wf_above_thr    = self.pmts.sum_over_sensors[i_above_thr]
+        if np.size(i_above_thr) < 2 or np.sum(wf_above_thr) == 0:
+            return 0
+
+        return weighted_mean_and_std(times_above_thr, wf_above_thr)[1]
+
+    def __repr__(self):
+        s  = f"""
+        ---------------------
+        {self.__class__.__name__} instance
+        ---------------------
+        Number of samples: {len(self.times)}
+        Times: {self.times / units.mus} µs
+        Time @ max energy: {self.time_at_max_energy / units.mus}
+        Width: {self.width / units.mus} µs
+        Height: {self.height} pes
+        Energy: {self.total_energy} pes
+        Charge: {self.total_charge} pes
+        RMS: {self.rms / units.mus} µs
+        """
+        return dedent(s)
+
+    def _check_valid_input(self, times, pmts, sipms):
+        length_times = len(times)
+        length_pmts  = pmts .all_waveforms.shape[1]
+        length_sipms = sipms.all_waveforms.shape[1]
+
+        if length_times == 0:
+            raise ValueError("Attempt to initialize an empty"
+                            f"{self.__class__.__name} instance.")
+        if ((length_times != length_pmts ) or
+            (length_times != length_sipms != 0)):
+            msg  =  "Shapes don't match!\n"
+            msg += f"times has length {length_times}\n"
+            msg += f"pmts  has length {length_pmts} \n"
+            msg += f"sipms has length {length_sipms}\n"
+            raise ValueError(msg)
 
 class S1(_Peak): pass
 class S2(_Peak): pass
