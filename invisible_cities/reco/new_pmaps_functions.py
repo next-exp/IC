f34bfbb99503e99d42974c3297783a4f275b5eb1
Gonzalo <gonzaponte@gmail.com>
2017-12-18 19:15:54 +0100

    Add new_pmaps_functions module

---
+++ invisible_cities/reco/new_pmaps_functions.py
@@ -0,0 +1,21 @@
+from ..evm.new_pmaps      import  PMTResponses
+from ..evm.new_pmaps      import SiPMResponses
+from . new_peak_functions import rebin_times_and_waveforms
+
+
+def rebin_peak(peak, rebin_factor):
+    if rebin_factor == 1: return peak
+
+    times, pmt_wfs = rebin_times_and_waveforms(peak.times,
+                                               peak.pmts.all_waveforms,
+                                               rebin_factor)
+    pmt_r  = PMTResponses(peak.pmts.ids, pmt_wfs)
+
+    sipm_r = SiPMResponses.build_empty_instance()
+    if peak.sipms.ids.size:
+        _, sipms = rebin_times_and_waveforms(peak.times,
+                                             peak.sipms.all_waveforms,
+                                             rebin_factor)
+        sipm_r = SiPMResponses(peak.sipms.ids, sipms)
+
+    return type(peak)(times, pmt_r, sipm_r)
