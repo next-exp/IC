347331c91b5eb4e1d4b0fffac954ef1f0ba01a5d
coreyjadams <coreyadams@fas.harvard.edu>
2018-01-09 07:46:24 -0600

    Splitting Event Meta to allow for other sized detectors (100, demo, etc)

--- invisible_cities/viewer/manager/EventMeta.py
+++ invisible_cities/viewer/manager/EventMeta.py
@@ -1,51 +1,31 @@
 from invisible_cities.database import load_db
 
-
-
-class NEWMeta(object):
-    """Wrapper for geometry interface for NEW
+class EventMeta(object):
+    """Baseclass for accessing geometry information
     
-    Contains easy access functions for NEW geometry properties, particularly
-    detector dimensions, pmt and sipm locations
+    Core meta information class for 3D geometry description.  Only one inherited class
+    for now, but in the future I expect more.
+    
     """
+
     def __init__(self):
-        super(NEWMeta, self).__init__()
-
-
-        # Load the database objects
-        self._det_geo   = load_db.DetectorGeo()
-        self._pmt_data  = load_db.DataPMT()
-        self._sipm_data = load_db.DataSiPM()
-
-        self.skim_detector_info(self._det_geo)
-
-
-    def skim_detector_info(self, det_geo):
-        """Capture and store basic geometry information
-        
-        Gets basic geometry information from the database (x min/max, etc)
-        Also specifies the size of pixels and number of pixels in each direction
-
-        Units are implicitly in millimeters
-        
-        Arguments:
-            det_geo {DetectorGeo Database} -- the output of load_db.DetectorGeo()
-        """
-        self._x_min   = det_geo.XMIN[0]
-        self._y_min   = det_geo.YMIN[0]
-        self._z_min   = det_geo.ZMIN[0]
-        self._x_max   = det_geo.XMAX[0]
-        self._y_max   = det_geo.YMAX[0]
-        self._z_max   = det_geo.ZMAX[0]
-        self._r_max   = det_geo.RMAX[0]
+        super(EventMeta, self).__init__
+        self._x_min        = None
+        self._y_min        = None
+        self._z_min        = None
+        self._x_max        = None
+        self._y_max        = None
+        self._z_max        = None
+        self._r_max        = None
         # Use 1 pixel per sipm
-        self._x_n_pixels = (self._x_max - self._x_min) / 10.
-        self._y_n_pixels = (self._y_max - self._y_min) / 10.
+        self._x_n_pixels   = None
+        self._y_n_pixels   = None
         # Use 1 pixel per mm of drift
-        self._z_n_pixels = self._z_max - self._z_min
-        self._size_voxel_x = 10
-        self._size_voxel_y = 10
-        self._size_voxel_z = 1
+        self._z_n_pixels   = None
+        self._size_voxel_x = None
+        self._size_voxel_y = None
+        self._size_voxel_z = None
+
 
     def pmt_data(self):
         return self._pmt_data
@@ -109,3 +89,49 @@ class NEWMeta(object):
 
     def max_z(self):
         return self._z_max
+
+class NEWMeta(EventMeta):
+    """Wrapper for geometry interface for NEW
+    
+    Contains easy access functions for NEW geometry properties, particularly
+    detector dimensions, pmt and sipm locations
+    """
+    def __init__(self):
+        super(NEWMeta, self).__init__()
+
+
+        # Load the database objects
+        self._det_geo   = load_db.DetectorGeo()
+        self._pmt_data  = load_db.DataPMT()
+        self._sipm_data = load_db.DataSiPM()
+
+        self.skim_detector_info(self._det_geo)
+
+
+    def skim_detector_info(self, det_geo):
+        """Capture and store basic geometry information
+        
+        Gets basic geometry information from the database (x min/max, etc)
+        Also specifies the size of pixels and number of pixels in each direction
+
+        Units are implicitly in millimeters
+        
+        Arguments:
+            det_geo {DetectorGeo Database} -- the output of load_db.DetectorGeo()
+        """
+        self._x_min   = det_geo.XMIN[0]
+        self._y_min   = det_geo.YMIN[0]
+        self._z_min   = det_geo.ZMIN[0]
+        self._x_max   = det_geo.XMAX[0]
+        self._y_max   = det_geo.YMAX[0]
+        self._z_max   = det_geo.ZMAX[0]
+        self._r_max   = det_geo.RMAX[0]
+        # Use 1 pixel per sipm
+        self._x_n_pixels = (self._x_max - self._x_min) / 10.
+        self._y_n_pixels = (self._y_max - self._y_min) / 10.
+        # Use 1 pixel per mm of drift
+        self._z_n_pixels = self._z_max - self._z_min
+        self._size_voxel_x = 10
+        self._size_voxel_y = 10
+        self._size_voxel_z = 1
+
