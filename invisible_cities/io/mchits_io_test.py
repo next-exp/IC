7800f557d625f02e24ef7da7f14c0b3de07900fa
Paola Ferrario <paola.ferrario@gmail.com>
2018-02-23 20:37:18 +0100

    Fix way of writing particles and hits tables

--- invisible_cities/io/mchits_io_test.py
+++ invisible_cities/io/mchits_io_test.py
@@ -2,16 +2,16 @@ import os
 import numpy  as np
 import tables as tb
 
-from . mchits_io import load_mchits
-from . mchits_io import load_mcparticles
 from . mchits_io import load_mchits_nexus
 from . mchits_io import load_mcparticles_nexus
 from . mchits_io import load_mcsensor_response_nexus
 from . mchits_io import mc_info_writer
 
+from .. reco.tbl_functions import get_mc_info
+
 def test_non_consecutive_events(config_tmpdir, ICDIR):
     filein  = os.path.join(ICDIR, 'database/test_data/', 'kr.3evts.MCRD.h5')
-    fileout = os.path.join(config_tmpdir, 'test_mcextents.h5')
+    fileout = os.path.join(config_tmpdir, 'test_mcinfo.h5')
     h5in    = tb.open_file(filein)
     h5out   = tb.open_file(fileout, 'w')
 
@@ -19,10 +19,12 @@ def test_non_consecutive_events(config_tmpdir, ICDIR):
     mc_extents = h5in.root.MC.extents
     events_in = np.unique(h5in.root.MC.extents[:]['evt_number'])
 
+    mc_info = get_mc_info(h5in)
+
     #Skip one event (there are only 3 in the file)
     events_to_copy = events_in[::2]
     for evt in events_to_copy:
-        mc_writer(mc_extents, evt)
+        mc_writer(mc_info, evt)
 
     events_out = np.unique(h5out.root.MC.extents[:]['evt_number'])
 
@@ -30,53 +32,53 @@ def test_non_consecutive_events(config_tmpdir, ICDIR):
 
 def test_load_all_mchits(mc_all_hits_data):
     efile, number_of_hits, evt_number = mc_all_hits_data
-    mchits_dict = load_mchits(efile)
+    mchits_dict = load_mchits_nexus(efile)
 
     assert len(mchits_dict[evt_number]) == number_of_hits
 
-def test_load_mchits(mc_particle_and_hits_data):
-    efile, _, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_data
+#def test_load_mchits(mc_particle_and_hits_data):
+#    efile, _, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_data
 
-    mchits_dict = load_mchits(efile)
-    hX = [hit.X for hit in mchits_dict[0]]
-    hY = [hit.Y for hit in mchits_dict[0]]
-    hZ = [hit.Z for hit in mchits_dict[0]]
-    hE = [hit.E for hit in mchits_dict[0]]
-    ht = [hit.time for hit in mchits_dict[0]]
+#    mchits_dict = load_mchits(efile)
+#    hX = [hit.X for hit in mchits_dict[0]]
+#    hY = [hit.Y for hit in mchits_dict[0]]
+#    hZ = [hit.Z for hit in mchits_dict[0]]
+ #   hE = [hit.E for hit in mchits_dict[0]]
+ #   ht = [hit.time for hit in mchits_dict[0]]
 
-    assert np.isclose(X,hX).all()
-    assert np.isclose(Y,hY).all()
-    assert np.isclose(Z,hZ).all()
-    assert np.isclose(E,hE).all()
-    assert np.isclose(t,ht).all()
+ #   assert np.isclose(X,hX).all()
+ #   assert np.isclose(Y,hY).all()
+ #   assert np.isclose(Z,hZ).all()
+ #   assert np.isclose(E,hE).all()
+ #   assert np.isclose(t,ht).all()
 
-def test_load_mcparticles(mc_particle_and_hits_data):
-    efile, name, pdg, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_data
+#def test_load_mcparticles(mc_particle_and_hits_data):
+#    efile, name, pdg, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_data
 
-    mcparticles_dict = load_mcparticles(efile)
-    particle = mcparticles_dict[0][0]
+#    mcparticles_dict = load_mcparticles(efile)
+#    particle = mcparticles_dict[0][0]
 
-    assert particle.name == name
-    assert np.isclose(particle.initial_vertex,vi).all()
-    assert np.isclose(particle.final_vertex,vf).all()
-    assert np.isclose(particle.p,p).all()
-    assert particle.E    == Ep
-    assert len(particle.hits) == nhits
+#    assert particle.name == name
+#    assert np.isclose(particle.initial_vertex,vi).all()
+#    assert np.isclose(particle.final_vertex,vf).all()
+#    assert np.isclose(particle.p,p).all()
+#    assert particle.E    == Ep
+#    assert len(particle.hits) == nhits
 
-    hX = [hit.X for hit in particle.hits]
-    hY = [hit.Y for hit in particle.hits]
-    hZ = [hit.Z for hit in particle.hits]
-    hE = [hit.E for hit in particle.hits]
-    ht = [hit.time for hit in particle.hits]
+ #   hX = [hit.X for hit in particle.hits]
+ #   hY = [hit.Y for hit in particle.hits]
+ #   hZ = [hit.Z for hit in particle.hits]
+ #   hE = [hit.E for hit in particle.hits]
+ #   ht = [hit.time for hit in particle.hits]
 
-    assert np.isclose(X,hX).all()
-    assert np.isclose(Y,hY).all()
-    assert np.isclose(Z,hZ).all()
-    assert np.isclose(E,hE).all()
-    assert np.isclose(t,ht).all()
+ #   assert np.isclose(X,hX).all()
+ #   assert np.isclose(Y,hY).all()
+ #   assert np.isclose(Z,hZ).all()
+ #   assert np.isclose(E,hE).all()
+ #   assert np.isclose(t,ht).all()
 
 def test_load_mchits_nexus(mc_particle_and_hits_nexus_data):
-    efile, _, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
+    efile, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
 
     mchits_dict = load_mchits_nexus(efile)
     hX = [hit.X for hit in mchits_dict[0]]
@@ -92,7 +94,7 @@ def test_load_mchits_nexus(mc_particle_and_hits_nexus_data):
     assert np.isclose(t,ht).all()
 
 def test_load_mcparticles_nexus(mc_particle_and_hits_nexus_data):
-    efile, name, pdg, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
+    efile, name, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
 
     mcparticles_dict = load_mcparticles_nexus(efile)
     particle = mcparticles_dict[0][1]
