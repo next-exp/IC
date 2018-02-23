892bc8ac083edff6309ab6c1df411675cdf00d11
Paola Ferrario <paola.ferrario@gmail.com>
2018-02-23 21:39:21 +0100

    Remove casting, not needed with the new file

--- invisible_cities/cities/isidora_test.py
+++ invisible_cities/cities/isidora_test.py
@@ -54,8 +54,5 @@ def test_isidora_electrons_40keV(config_tmpdir, ICDIR):
             # check events numbers & timestamps
             evts_in     = h5in .root.Run.events[:nactual]
             evts_out_u8 = h5out.root.Run.events[:nactual]
-            # The old format used <i4 for th event number; the new one
-            # uses <u8. Casting the latter to the former allows us to
-            # re-use the old test data files.
-            evts_out_i4 = evts_out_u8.astype([('evt_number', '<i4'), ('timestamp', '<u8')])
-            np.testing.assert_array_equal(evts_in, evts_out_i4)
+
+            np.testing.assert_array_equal(evts_in, evts_out_u8)
