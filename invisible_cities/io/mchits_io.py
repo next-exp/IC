c823c4cf627811e9b33609122b0a3a5e3ba588b4
Paola Ferrario <paola.ferrario@gmail.com>
2018-02-23 20:37:18 +0100

    Fix way of writing particles and hits tables

--- invisible_cities/io/mchits_io.py
+++ invisible_cities/io/mchits_io.py
@@ -63,20 +63,41 @@ class mc_info_writer:
 
         hits, particles = read_mcinfo_evt_by_evt(mctables, evt_number)
 
-        (self.hit_table.append(h[:]) for h in hits)
+        for h in hits:
+            new_row = self.hit_table.row
+            new_row['hit_position']  = h[0]
+            new_row['hit_time']      = h[1]
+            new_row['hit_energy']    = h[2]
+            new_row['label']         = h[3]
+            new_row['particle_indx'] = h[4]
+            new_row['hit_indx']      = h[5]
+            new_row.append()
         self.hit_table.flush()
 
-        (self.particle_table.append(p[:]) for p in particles)
+        for p in particles:
+            new_row = self.particle_table.row
+            new_row['particle_indx']  = p[0]
+            new_row['particle_name']  = p[1]
+            new_row['primary']        = p[2]
+            new_row['mother_indx']    = p[3]
+            new_row['initial_vertex'] = p[4]
+            new_row['final_vertex']   = p[5]
+            new_row['initial_volume'] = p[6]
+            new_row['final_volume']   = p[7]
+            new_row['momentum']       = p[8]
+            new_row['kin_energy']     = p[9]
+            new_row['creator_proc']   = p[10]
+            new_row.append()
         self.particle_table.flush()
 
 
-def load_mchits(file_name: str, max_events:int =1e+9) -> Mapping[int, MCHit]:
-
-    with tables.open_file(file_name,mode='r') as h5in:
-        mctable = h5in.root.MC.MCTracks
-        mcevents = read_mcinfo (mctable, max_events)
-        mchits_dict = compute_mchits_dict(mcevents)
-    return mchits_dict
+#def load_mchits(file_name: str, max_events:int =1e+9) -> Mapping[int, MCHit]:
+#
+#    with tables.open_file(file_name,mode='r') as h5in:
+#        mctable = h5in.root.MC.MCTracks
+#        mcevents = read_mcinfo (mctable, max_events)
+ #       mchits_dict = compute_mchits_dict(mcevents)
+  #  return mchits_dict
 
 def load_mchits_nexus(file_name: str,
                       event_range=(0,int(1e9))) -> Mapping[int, MCHit]:
@@ -87,11 +108,11 @@ def load_mchits_nexus(file_name: str,
 
     return mchits_dict
 
-def load_mcparticles(file_name: str, max_events:int =1e+9) -> Mapping[int, MCParticle]:
+#def load_mcparticles(file_name: str, max_events:int =1e+9) -> Mapping[int, MCParticle]:
 
-    with tables.open_file(file_name,mode='r') as h5in:
-        mctable = h5in.root.MC.MCTracks
-        return read_mcinfo (mctable, max_events)
+#    with tables.open_file(file_name,mode='r') as h5in:
+#        mctable = h5in.root.MC.MCTracks
+#        return read_mcinfo (mctable, max_events)
 
 def load_mcparticles_nexus(file_name: str,
                                event_range=(0,int(1e9))) -> Mapping[int, MCParticle]:
@@ -105,56 +126,56 @@ def load_mcsensor_response_nexus(file_name: str,
     with tables.open_file(file_name,mode='r') as h5in:
         return read_mcsns_response_nexus(h5in, event_range)
 
-def read_mcinfo (mc_table: tables.table.Table,
-                   max_events:int =1e+9) ->Mapping[int, Mapping[int, MCParticle]]:
+#def read_mcinfo (mc_table: tables.table.Table,
+#                   max_events:int =1e+9) ->Mapping[int, Mapping[int, MCParticle]]:
 
-    all_events = {}
-    current_event = {}
-#    convert table to numpy.ndarray
-    data       = mc_table[:]
-    data_size  = len(data)
+#    all_events = {}
+#    current_event = {}
+# #   convert table to numpy.ndarray
+ #   data       = mc_table[:]
+ #   data_size  = len(data)
 
-    event            =  data["event_indx"]
-    particle         =  data["mctrk_indx"]
-    particle_name    =  data["particle_name"]
-#    pdg_code         =  data["pdg_code"]
-    initial_vertex   =  data["initial_vertex"]
-    final_vertex     =  data["final_vertex"]
-    momentum         =  data["momentum"]
-    energy           =  data["energy"]
-    nof_hits         =  data["nof_hits"]
-    hit              =  data["hit_indx"]
-    hit_position     =  data["hit_position"]
-    hit_time         =  data["hit_time"]
-    hit_energy       =  data["hit_energy"]
+ #   event            =  data["event_indx"]
+ #   particle         =  data["mctrk_indx"]
+ #   particle_name    =  data["particle_name"]
+##    pdg_code         =  data["pdg_code"]
+ #   initial_vertex   =  data["initial_vertex"]
+ #   final_vertex     =  data["final_vertex"]
+ #   momentum         =  data["momentum"]
+ #   energy           =  data["energy"]
+ #   nof_hits         =  data["nof_hits"]
+ #   hit              =  data["hit_indx"]
+ #   hit_position     =  data["hit_position"]
+ #   hit_time         =  data["hit_time"]
+ #   hit_energy       =  data["hit_energy"]
 
-    for i in range(data_size):
-        if event[i] >= max_events:
-            break
+ #   for i in range(data_size):
+ #       if event[i] >= max_events:
+ #           break
 
-        current_event = all_events.setdefault(event[i], {})
+ #       current_event = all_events.setdefault(event[i], {})
 
-        current_particle = current_event.setdefault( particle[i],
-                                                     MCParticle(particle_name[i],
-                                                     -1, # primariness not saved
-                                                     -1, # mother index not saved
-                                                     initial_vertex[i],
-                                                     final_vertex[i],
-                                                     '', # initial volume not saved
-                                                     '', # final volume not saved
-                                                     momentum[i],
-                                                     energy[i],
-                                                     '' # creator process not saved
-                                                      ))
-        hit = MCHit(hit_position[i], hit_time[i], hit_energy[i], '') # label not saved
-        current_particle.hits.append(hit)
+ #       current_particle = current_event.setdefault( particle[i],
+ #                                                    MCParticle(particle_name[i],
+ #                                                    -1, # primariness not saved
+ #                                                    -1, # mother index not saved
+ #                                                    initial_vertex[i],
+ #                                                    final_vertex[i],
+ #                                                    '', # initial volume not saved
+ #                                                    '', # final volume not saved
+ #                                                    momentum[i],
+ #                                                    energy[i],
+ #                                                    '' # creator process not saved
+ #                                                     ))
+ #       hit = MCHit(hit_position[i], hit_time[i], hit_energy[i], '') # label not saved
+ #       current_particle.hits.append(hit)
 
-    return all_events
+ #   return all_events
 
 def read_mcinfo_nexus (h5f, event_range=(0,int(1e9))) ->Mapping[int, Mapping[int, MCParticle]]:
-    h5extents   = h5f.root.Run.extents
-    h5hits      = h5f.root.Run.hits
-    h5particles = h5f.root.Run.particles
+    h5extents   = h5f.root.MC.extents
+    h5hits      = h5f.root.MC.hits
+    h5particles = h5f.root.MC.particles
 
     all_events = {}
     particles  = {}
@@ -287,15 +308,18 @@ def read_mcinfo_evt_by_evt (mctables: tuple(),
 
     particle_rows = []
     hit_rows = []
-
+    
     event_range = (0,int(1e9))
-    ihit = 0; ipart = 0
-
     for iext in range(*event_range):
         if h5extents[iext]['evt_number'] == event_number:
+            ihit = 0; ipart = 0
+            if iext > 0:
+                ihit = h5extents[iext-1]['last_hit'] + 1
+                ipart = h5extents[iext-1]['last_particle'] + 1
 
-            ipart_end = h5extents[iext]['last_particle']
             ihit_end  = h5extents[iext]['last_hit']
+            ipart_end = h5extents[iext]['last_particle']
+            
 
             while ihit <= ihit_end:
                 hit_rows.append(h5hits[ihit])
