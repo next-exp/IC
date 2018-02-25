0101c0468b90a24f5d33415f780cf107e2008eab
Paola Ferrario <paola.ferrario@gmail.com>
2018-02-25 11:26:40 +0100

    Make search for event faster in evt by evt reader

--- invisible_cities/io/mcinfo_io_test.py
+++ invisible_cities/io/mcinfo_io_test.py
@@ -36,47 +36,6 @@ def test_load_all_mchits(mc_all_hits_data):
 
     assert len(mchits_dict[evt_number]) == number_of_hits
 
-#def test_load_mchits(mc_particle_and_hits_data):
-#    efile, _, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_data
-
-#    mchits_dict = load_mchits(efile)
-#    hX = [hit.X for hit in mchits_dict[0]]
-#    hY = [hit.Y for hit in mchits_dict[0]]
-#    hZ = [hit.Z for hit in mchits_dict[0]]
- #   hE = [hit.E for hit in mchits_dict[0]]
- #   ht = [hit.time for hit in mchits_dict[0]]
-
- #   assert np.isclose(X,hX).all()
- #   assert np.isclose(Y,hY).all()
- #   assert np.isclose(Z,hZ).all()
- #   assert np.isclose(E,hE).all()
- #   assert np.isclose(t,ht).all()
-
-#def test_load_mcparticles(mc_particle_and_hits_data):
-#    efile, name, pdg, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_data
-
-#    mcparticles_dict = load_mcparticles(efile)
-#    particle = mcparticles_dict[0][0]
-
-#    assert particle.name == name
-#    assert np.isclose(particle.initial_vertex,vi).all()
-#    assert np.isclose(particle.final_vertex,vf).all()
-#    assert np.isclose(particle.p,p).all()
-#    assert particle.E    == Ep
-#    assert len(particle.hits) == nhits
-
- #   hX = [hit.X for hit in particle.hits]
- #   hY = [hit.Y for hit in particle.hits]
- #   hZ = [hit.Z for hit in particle.hits]
- #   hE = [hit.E for hit in particle.hits]
- #   ht = [hit.time for hit in particle.hits]
-
- #   assert np.isclose(X,hX).all()
- #   assert np.isclose(Y,hY).all()
- #   assert np.isclose(Z,hZ).all()
- #   assert np.isclose(E,hE).all()
- #   assert np.isclose(t,ht).all()
-
 def test_load_mchits(mc_particle_and_hits_nexus_data):
     efile, _, _, _, _, _, _, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
 
