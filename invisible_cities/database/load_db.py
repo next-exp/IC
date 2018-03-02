999e1689fdc7ab055da31eb423c145eeb10cc564
jmbenlloch <info@jmbenlloch.net>
2018-03-02 11:08:10 +0100

    450 Take the proper interval from database

--- invisible_cities/database/load_db.py
+++ invisible_cities/database/load_db.py
@@ -14,58 +14,31 @@ def tmap(*args):
 # 3012 was the first SiPM calibration after remapping.
 runNumberForMC = 3012
 
-def get_min_run_values(db, run_number, sensors):
-    ''' Due to ART database design MaxRun is not being used currently,
-    so we need to get the exact MinRun value for a given run number to
-    get only one row per sensor...
-    db is sqlite3 connection and sensors pmt or sipm.'''
-
-    if sensors == 'pmt':  bound = '<' #pmt id's are < 100
-    if sensors == 'sipm': bound = '>' #sipms id's are > 100
-
-    sql = '''select Max(MinRun) from ChannelGain
-where SensorID {} 100 and MinRun < {}'''.format(bound, abs(run_number))
-    cursor = db.execute(sql)
-    minrun_gain = cursor.fetchone()[0]
-
-    sql = '''select Max(MinRun) from ChannelPosition
-where SensorID {} 100 and MinRun < {}'''.format(bound, abs(run_number))
-    cursor = db.execute(sql)
-    minrun_position = cursor.fetchone()[0]
-
-    sql = '''select Max(MinRun) from ChannelMapping
-where SensorID {} 100 and MinRun < {}'''.format(bound, abs(run_number))
-    cursor = db.execute(sql)
-    minrun_map = cursor.fetchone()[0]
-
-    return minrun_gain, minrun_position, minrun_map
-
-
 def DataPMT(run_number=1e5):
     if run_number == 0:
         run_number = runNumberForMC
     dbfile = DATABASE_LOCATION
     conn = sqlite3.connect(dbfile)
 
-    minrun_gain, minrun_position, minrun_map = \
-            get_min_run_values(conn, run_number, 'pmt')
-
     sql = '''select pos.SensorID, map.ElecID "ChannelID", Label "PmtID",
 case when msk.SensorID is NULL then 1 else 0 end "Active",
 X, Y, coeff_blr, coeff_c, abs(Centroid) "adc_to_pes", noise_rms, Sigma
 from ChannelPosition as pos INNER JOIN ChannelMapping
 as map ON pos.SensorID = map.SensorID LEFT JOIN
-(select * from PmtNoiseRms where MinRun < {3} and (MaxRun >= {3} or MaxRun is NULL))
+(select * from PmtNoiseRms where MinRun <= {0} and (MaxRun >= {0} or MaxRun is NULL))
 as noise on map.ElecID = noise.ElecID LEFT JOIN
-(select * from ChannelMask where MinRun < {3} and {3} < MaxRun)
+(select * from ChannelMask where MinRun <= {0} and {0} <= MaxRun)
 as msk ON pos.SensorID = msk.SensorID LEFT JOIN
-(select * from ChannelGain where MinRun={1})
+(select * from ChannelGain where  MinRun <= {0} and {0} <= MaxRun)
 as gain ON pos.SensorID = gain.SensorID LEFT JOIN
-(select * from PmtBlr where MinRun < {3} and (MaxRun >= {3} or MaxRun is NULL))
+(select * from PmtBlr where MinRun <= {0} and (MaxRun >= {0} or MaxRun is NULL))
 as blr ON map.ElecID = blr.ElecID
-where pos.SensorID < 100 and pos.MinRun={0} and map.MinRun={2} and pos.Label LIKE 'PMT%'
-order by Active desc, pos.SensorID'''\
-    .format(minrun_position, minrun_gain, minrun_map, abs(run_number))
+where pos.SensorID < 100
+and pos.MinRun <= {0} and {0} <= pos.MaxRun
+and map.MinRun <= {0} and {0} <= map.MaxRun
+and pos.Label LIKE 'PMT%'
+order by Active desc, pos.SensorID
+'''.format(abs(run_number))
     data = pd.read_sql_query(sql, conn)
     data.fillna(0, inplace=True)
     conn.close()
@@ -77,20 +50,19 @@ def DataSiPM(run_number=1e5):
     dbfile = DATABASE_LOCATION
     conn = sqlite3.connect(dbfile)
 
-    minrun_gain, minrun_position, minrun_map = \
-            get_min_run_values(conn, run_number, 'sipm')
-
-    sql = '''select pos.SensorID, map.ElecID "ChannelID",
+    sql='''select pos.SensorID, map.ElecID "ChannelID",
 case when msk.SensorID is NULL then 1 else 0 end "Active",
 X, Y, Centroid "adc_to_pes"
 from ChannelPosition as pos INNER JOIN ChannelGain as gain
 ON pos.SensorID = gain.SensorID INNER JOIN ChannelMapping as map
 ON pos.SensorID = map.SensorID LEFT JOIN
-(select * from ChannelMask where MinRun < {3} and {3} < MaxRun) as msk
+(select * from ChannelMask where MinRun <= {0} and {0} <= MaxRun) as msk
 ON pos.SensorID = msk.SensorID
-where pos.SensorID > 100 and pos.MinRun={0} and gain.MinRun={1}
-and map.MinRun={2} order by pos.SensorID'''\
-    .format(minrun_position, minrun_gain, minrun_map, abs(run_number))
+where pos.SensorID > 100
+and pos.MinRun <= {0} and {0} <= pos.MaxRun
+and gain.MinRun <= {0} and {0} <= gain.MaxRun
+and map.MinRun <= {0} and {0} <= map.MaxRun
+order by pos.SensorID'''.format(abs(run_number))
     data = pd.read_sql_query(sql, conn)
     conn.close()
     return data
