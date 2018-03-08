4d37df8c9df62b496f9a7bd400eb12e4f147f76a
andLaing <a.b.r.laing@gmail.com>
2018-03-08 13:45:20 +0100

    Fix typo

--- invisible_cities/io/hist_io.py
+++ invisible_cities/io/hist_io.py
@@ -5,12 +5,12 @@ from .. reco import tbl_functions as tbl
 
 def hist_writer(file,
                 *,
-                group_name      : 'options: HIST, HIST2D',
-                table_name      : 'options: pmt, pmtMAU, sipm, sipmMAU',
-                compression     = 'ZLIB4',
-                n_sensors       : 'number of pmts or sipms',
-                n_bins          : 'length of bin range used',
-                bin_centres     : 'np.array of bin centres'):
+                group_name  : 'options: HIST, HIST2D',
+                table_name  : 'options: pmt, pmtMAU, sipm, sipmMAU',
+                compression = 'ZLIB4',
+                n_sensors   : 'number of pmts or sipms',
+                n_bins      : 'length of bin range used',
+                bin_centres : 'np.array of bin centres'):
     try:                       hist_group = getattr          (file.root, group_name)
     except tb.NoSuchNodeError: hist_group = file.create_group(file.root, group_name)
 
@@ -19,11 +19,11 @@ def hist_writer(file,
                                     atom    = tb.Int32Atom(),
                                     shape   = (0, n_sensors, n_bins),
                                     filters = tbl.filters(compression))
-    
+
     ## The bins can be written just once at definition of the writer
     file.create_array(hist_group, table_name+'_bins', bin_centres)
-    
-    def write_hist(waveform : 'np.array: RWF, CWF, SiPM'):
-        hist_table.append(waveform.reshape(1, n_sensors, n_bins))
-        
+
+    def write_hist(histo : 'np.array: RWF, CWF, SiPM'):
+        hist_table.append(histo.reshape(1, n_sensors, n_bins))
+
     return write_hist
