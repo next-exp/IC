92f6acd3c2d352d8b2f0f523977cdc5adcd3cddd
andLaing <a.b.r.laing@gmail.com>
2018-03-08 13:59:04 +0100

    Add tests for new calibration functions

---
+++ invisible_cities/calib/calib_functions_test.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from numpy.testing import assert_array_equal
+from pytest        import mark
+from pytest        import raises
+
+from .                         import calib_functions as cf
+from .. core.system_of_units_c import units
+
+
+def test_bin_waveforms():
+    bins = np.arange(0, 20)
+    data = np.arange(1, 51).reshape(2, 25)
+
+    expected = np.stack((np.histogram(data[0], bins)[0],
+                         np.histogram(data[1], bins)[0]))
+    actual   = cf.bin_waveforms(data, bins)
+    assert_array_equal(actual, expected)
+
+
+def test_spaced_integrals():
+    limits = np.array([2, 4, 6])
+    data   = np.arange(20).reshape(2, 10)
+
+    expected = np.array([[5, 9, 30], [25, 29, 70]])
+    actual   = cf.spaced_integrals(data, limits)
+    assert_array_equal(actual, expected)
+
+
+@mark.parametrize("limits",
+                  ([-1, 0,  2],
+                   [-1, 1, 10],
+                   [ 0, 2, 10],
+                   [ 0, 3, 11]))
+def test_spaced_integrals_raises_ValueError_with_wrong_limits(limits):
+    limits = np.array(limits)
+    data   = np.arange(20).reshape(2, 10)
+    with raises(ValueError):
+        cf.spaced_integrals(data, limits)
+
+
+def test_integral_limits():
+    sampling    =  1 * units.mus
+    n_integrals = 10
+    start_int   =  5
+    width_int   =  1
+    period      = 50
+
+    expected_llim = np.array([  5,   6,  55,  56, 105, 106, 155, 156, 205, 206, 255, 256, 305, 306, 355, 356, 405, 406, 455, 456])
+    expected_dlim = np.array([  2,   3,  52,  53, 102, 103, 152, 153, 202, 203, 252, 253, 302, 303, 352, 353, 402, 403, 452, 453])
+
+    (actual_llimits,
+     actual_dlimits) = cf.integral_limits(sampling, n_integrals, start_int, width_int, period)
+
+    assert_array_equal(actual_llimits, expected_llim)
+    assert_array_equal(actual_dlimits, expected_dlim)
+
+
+def test_filter_limits_inside():
+    sampling         =   1 * units.mus
+    n_integrals      =  10
+    start_int        =   5
+    width_int        =   1
+    period           =  50
+    fake_data_length = 500
+
+    (expected_llimits,
+     expected_dlimits) = cf.integral_limits(sampling, n_integrals, start_int, width_int, period)
+
+    actual_llimits = cf.filter_limits(expected_llimits, fake_data_length)
+    actual_dlimits = cf.filter_limits(expected_dlimits, fake_data_length)
+
+    assert_array_equal(actual_llimits, expected_llimits)
+    assert_array_equal(actual_dlimits, expected_dlimits)
+
+
+def test_filter_limits_outside():
+    sampling         =   1 * units.mus
+    n_integrals      =  10
+    start_int        =   5
+    width_int        =   1
+    period           =  50
+    fake_data_length = 400
+
+    (unfiltered_llimits,
+     unfiltered_dlimits) = cf.integral_limits(sampling, n_integrals, start_int, width_int, period)
+
+    filtered_llimits = cf.filter_limits(unfiltered_llimits, fake_data_length)
+    filtered_dlimits = cf.filter_limits(unfiltered_dlimits, fake_data_length)
+
+    assert len(filtered_llimits) < len(unfiltered_llimits)
+    assert len(filtered_dlimits) < len(unfiltered_dlimits)
+    assert len(filtered_llimits) % 2 == 0
+    assert len(filtered_dlimits) % 2 == 0
