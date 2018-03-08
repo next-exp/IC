b83886adf7fbda042fdf776554536c8c9930b6fa
Gonzalo <gonzaponte@gmail.com>
2018-03-08 13:39:22 +0100

    Add function to compute poisson pmf
    The scipy implementation is significatively slower.

--- invisible_cities/core/stat_functions_test.py
+++ invisible_cities/core/stat_functions_test.py
@@ -10,6 +10,7 @@ from hypothesis.strategies  import floats
 from hypothesis.strategies  import sampled_from
 from hypothesis.extra.numpy import arrays
 
+from . stat_functions import poisson_factor
 from . stat_functions import poisson_sigma
 
 
@@ -19,6 +20,23 @@ def arrays_of_positive_numbers(draw):
     _type, strategy = draw(sampled_from(list(_strategies.items())))
     size            = draw(integers(1, 10))
     return draw(arrays(_type, size, strategy(0, 100)))
+
+
+@mark.parametrize("mean", np.linspace(0.1, 3, 3))
+def test_poisson_factor_mean(mean):
+    k        = 1
+    expected = np.exp(-mean) * mean
+    actual   = poisson_factor(k, mean)
+
+    assert actual == approx(expected)
+
+
+@mark.parametrize("k", range(10))
+def test_poisson_factor_k(k):
+    mean  = 1
+    first = poisson_factor(0, mean)
+    nth   = poisson_factor(k, mean)
+    assert first / nth == approx(np.math.factorial(k))
 
 
 @mark.parametrize("default", (0, 1, 2))
