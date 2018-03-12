fc367b4a712932a9419f3cbff9dd3025c116792a
Jacek Generowicz <github@my-post-office.net>
2018-03-12 17:17:32 +0100

    Add command line execution tests for liquid cities

---
+++ invisible_cities/liquid_cities/command_line_execution_test.py
@@ -0,0 +1,32 @@
+from os      import getenv
+from os.path import join
+
+from subprocess import check_output
+from subprocess import CalledProcessError
+from subprocess import STDOUT
+
+from pytest import mark
+
+@mark.slow
+# @mark.parametrize('city',
+#                   'diomira isidora irene dorothea zaira penthesilea'.split())
+# TODO understand what's wrong with isidora (in Travis)
+@mark.parametrize('city',
+                  'diomira isidora irene dorothea penthesilea zaira zemrude phyllis moriana'.split())
+
+def test_command_line_run(city, tmpdir_factory):
+    ICTDIR = getenv('ICTDIR')
+    # Use the example config file included in the repository
+    config_file_name = join(ICTDIR, 'invisible_cities/config/', f'liquid_{city}.conf')
+    # Ensure that output does not pollute: send it to a temporary dir
+    temp_dir = tmpdir_factory.mktemp('output_files')
+    out_file_name = join(temp_dir, f'{city}.out')
+    # The actual command that we want to test
+    command = ('liquidcity {city} {config_file_name} -o {out_file_name}'
+               .format(**locals()))
+    try:
+        check_output(command, shell = True, stderr=STDOUT)
+    except CalledProcessError as e:
+        # Ensure that stdout and stderr are visible when test fails
+        print(e.stdout.decode())
+        raise
