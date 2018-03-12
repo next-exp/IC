69ebd875f7bf4d172231683f0ab62b26268db9d3
Jacek Generowicz <github@my-post-office.net>
2018-03-12 14:06:57 +0100

    Implement and test Diomira as a liquid city

---
+++ invisible_cities/liquid_cities/diomira.py
@@ -0,0 +1,187 @@
+from functools import partial
+
+import numpy  as np
+import tables as tb
+
+from .. reco                    import    tbl_functions as tbl
+from .. reco                    import sensor_functions as sf
+from .. reco                    import   peak_functions as pkf
+from .. reco                    import    wfm_functions as wfm
+from .. sierpe                  import fee              as FE
+from .. core.random_sampling    import NoiseSampler     as SiPMsNoiseSampler
+from .. io.rwf_io               import           rwf_writer
+from .. io.       mcinfo_io     import       mc_info_writer
+from .. io.run_and_event_io     import run_and_event_writer
+from .. filters.trigger_filters import TriggerFilter
+from .. database                import load_db
+from .. evm.ic_containers       import TriggerParams
+from .. evm.pmaps               import S2
+from .. types.ic_types          import minmax
+
+from .. dataflow          import dataflow as fl
+from .. dataflow.dataflow import push
+from .. dataflow.dataflow import pipe
+from .. dataflow.dataflow import fork
+
+from .  components import city
+from .  components import print_every
+from .  components import sensor_data
+from .  components import deconv_pmt
+from .  components import WfType
+from .  components import wf_from_files
+
+
+@city
+def diomira(files_in, file_out, compression, event_range, print_mod, run_number,
+            sipm_noise_cut, filter_padding, trigger_type,
+            trigger_params = dict(), s2_params = dict()):
+
+    sd = sensor_data(files_in[0], WfType.mcrd)
+
+    simulate_pmt_response_  = fl.map(simulate_pmt_response (run_number),
+                                     args="pmt" , out= ("pmt_sim", "blr_sim"))
+    simulate_sipm_response_ = fl.map(simulate_sipm_response(run_number, sd.SIPMWL, sipm_noise_cut, filter_padding),
+                                     args="sipm", out="sipm_sim"             )
+    trigger_filter_         = select_trigger_filter(trigger_type, trigger_params, s2_params)
+    emulate_trigger_        = fl.map(emulate_trigger(run_number, trigger_type, trigger_params, s2_params), args="pmt_sim", out="trigger_sim")
+    trigger_filter          = fl.count_filter(trigger_filter_, args="trigger_sim")
+
+    with tb.open_file(file_out, "w", filters=tbl.filters(compression)) as h5out:
+        RWF        = partial(rwf_writer, h5out, group_name='RD')
+        write_pmt  = fl.sink(RWF(table_name='pmtrwf' , n_sensors=sd.NPMT , waveform_length=sd. PMTWL // int(FE.t_sample)), args= "pmt_sim")
+        write_blr  = fl.sink(RWF(table_name='pmtblr' , n_sensors=sd.NPMT , waveform_length=sd. PMTWL // int(FE.t_sample)), args= "blr_sim")
+        write_sipm = fl.sink(RWF(table_name='sipmrwf', n_sensors=sd.NSIPM, waveform_length=sd.SIPMWL                    ), args="sipm_sim")
+
+        write_event_info_ = run_and_event_writer(h5out)
+        write_mc_         = mc_info_writer      (h5out) if run_number <= 0 else (lambda *_: None)
+
+        write_event_info = fl.sink(write_event_info_, args=("run_number", "event_number", "timestamp"))
+        write_mc         = fl.sink(write_mc_        , args=(        "mc", "event_number"             ))
+
+        event_count_in = fl.spy_count()
+
+        return push(
+            source = wf_from_files(files_in, WfType.mcrd),
+            pipe   = pipe(fl.slice(*event_range, close_all=True),
+                          event_count_in.spy                    ,
+                          print_every(print_mod)                ,
+                          simulate_pmt_response_                ,
+                          emulate_trigger_                      ,
+                          trigger_filter.filter                 ,
+                          simulate_sipm_response_               ,
+                          fork(write_pmt                        ,
+                               write_blr                        ,
+                               write_sipm                       ,
+                               write_mc                         ,
+                               write_event_info)                ),
+            result = dict(events_in     = event_count_in.future,
+                          events_filter = trigger_filter.future))
+
+
+def compute_pe_resolution(rms, adc_to_pes):
+    return np.divide(rms                              ,
+                     adc_to_pes                       ,
+                     out   = np.zeros_like(adc_to_pes),
+                     where = adc_to_pes != 0          )
+
+
+def simulate_pmt_response(run_number):
+    datapmt       = load_db.DataPMT(run_number)
+    adc_to_pes    = np.abs(datapmt.adc_to_pes.values).astype(np.double)
+    single_pe_rms = datapmt.Sigma.values.astype(np.double)
+    pe_resolution = compute_pe_resolution(single_pe_rms, adc_to_pes)
+
+    def simulate_pmt_response(pmtrd):
+        rwf, blr = sf.simulate_pmt_response(0, pmtrd[np.newaxis],
+                                            adc_to_pes, pe_resolution,
+                                            run_number)
+        return rwf.astype(np.int16), blr.astype(np.int16)
+    return simulate_pmt_response
+
+
+def simulate_sipm_response(run_number, wf_length, noise_cut, filter_padding):
+    datasipm      = load_db.DataSiPM (run_number)
+    baselines     = load_db.SiPMNoise(run_number)[-1]
+    noise_sampler = SiPMsNoiseSampler(run_number, wf_length, True)
+
+    adc_to_pes    = datasipm.adc_to_pes.values
+    thresholds    = noise_cut * adc_to_pes + baselines
+    single_pe_rms = datasipm.Sigma.values.astype(np.double)
+    pe_resolution = compute_pe_resolution(single_pe_rms, adc_to_pes)
+
+    def simulate_sipm_response(sipmrd):
+        wfs = sf.simulate_sipm_response(0, sipmrd[np.newaxis],
+                                        noise_sampler, adc_to_pes,
+                                        pe_resolution, run_number)
+        return wfm.noise_suppression(wfs, thresholds, filter_padding)
+    return simulate_sipm_response
+
+
+def select_trigger_filter(trigger_type, trigger_params, s2_params):
+    if   trigger_type is None:
+        def always_pass(*args):
+            return True
+        return always_pass
+
+    elif trigger_type == "S2":
+        chann  =              trigger_params["tr_channels"]
+        nchann =              trigger_params["min_number_channels"]
+        ratio  =              trigger_params["data_mc_ratio"]
+        height = minmax(min = trigger_params["min_height"],
+                        max = trigger_params["max_height"])
+        charge = minmax(min = trigger_params["min_charge"],
+                        max = trigger_params["max_charge"])
+        width  = minmax(min = trigger_params["min_width" ],
+                        max = trigger_params["max_width" ])
+        params = TriggerParams(trigger_channels    =  chann,
+                               min_number_channels = nchann,
+                               charge              = charge * ratio,
+                               height              = height * ratio,
+                               width               = width)
+        return TriggerFilter(params)
+
+    else:
+        raise ValueError(f"Invalid trigger type: {repr(trigger_type)}")
+
+
+def emulate_trigger(run_number, trigger_type, trigger_params, s2_params):
+    if   trigger_type is None:
+        def do_nothing(*args, **kwargs):
+            pass
+        return do_nothing
+    elif trigger_type == "S2":
+        channels   = trigger_params["tr_channels"]
+        min_height = trigger_params["min_height"]
+        datapmt    = load_db.DataPMT(run_number)
+        IC_ids     = sf.convert_channel_id_to_IC_id(datapmt, channels).tolist()
+        n_baseline = s2_params.pop("n_baseline")
+        s2_params  = dict(time         = minmax(min = s2_params["s2_tmin"        ],
+                                                max = s2_params["s2_tmax"        ]),
+                          stride                    = s2_params["s2_stride"      ] ,
+                          length       = minmax(min = s2_params["s2_lmin"        ],
+                                                max = s2_params["s2_lmax"        ]),
+                          rebin_stride =              s2_params["s2_rebin_stride"])
+
+
+        deconvolver = deconv_pmt(run_number, n_baseline, IC_ids)
+
+        def get_indices(cwf):
+            return pkf.indices_and_wf_above_threshold(cwf, thr=min_height)[0]
+
+        def find_peaks(cwf, idx):
+            return pkf.find_peaks(cwf, idx, Pk=S2, pmt_ids=[-1], **s2_params)
+
+        def emulate_trigger(rwfs):
+            open("cwf_liquid.txt", "w").write(str(rwfs.tolist()))
+            cwfs = deconvolver(rwfs)
+            peak_data = dict()
+            for ID, cwf in zip(IC_ids, cwfs):
+                idx = get_indices(cwf)
+                s2  = find_peaks (cwf, idx)
+                peak_data[ID] = s2
+            return peak_data
+
+        return emulate_trigger
+
+    else:
+        raise ValueError(f"Invalid trigger type: {repr(trigger_type)}")
---
+++ invisible_cities/liquid_cities/diomira_test.py
@@ -0,0 +1,187 @@
+import os
+import tables as tb
+import numpy  as np
+
+from pytest import mark
+from pytest import raises
+
+from .. core                 import system_of_units as units
+from .. core.configure       import configure
+
+from .. reco     import tbl_functions as tbl
+from .. sierpe   import fee as FEE
+
+from .  diomira  import diomira
+
+
+@mark.skip(reason="Table not written in liquid cities anymore")
+def test_diomira_fee_table(ICDATADIR):
+    "Test that FEE table reads back correctly with expected values."
+    RWF_file = os.path.join(ICDATADIR, 'electrons_40keV_z250_RWF.h5')
+
+    with tb.open_file(RWF_file, 'r') as e40rwf:
+        fee = tbl.read_FEE_table(e40rwf.root.FEE.FEE)
+        feep = fee.fee_param
+        eps = 1e-04
+        # Ignoring PEP8 to improve readability by making symmetry explicit.
+        assert len(fee.adc_to_pes)    == e40rwf.root.RD.pmtrwf.shape[1]
+        assert len(fee.coeff_blr)     == e40rwf.root.RD.pmtrwf.shape[1]
+        assert len(fee.coeff_c)       == e40rwf.root.RD.pmtrwf.shape[1]
+        assert len(fee.pmt_noise_rms) == e40rwf.root.RD.pmtrwf.shape[1]
+        assert feep.NBITS == FEE.NBITS
+        assert abs(feep.FEE_GAIN - FEE.FEE_GAIN)           < eps
+        assert abs(feep.LSB - FEE.LSB)                     < eps
+        assert abs(feep.NOISE_I - FEE.NOISE_I)             < eps
+        assert abs(feep.NOISE_DAQ - FEE.NOISE_DAQ)         < eps
+        assert abs(feep.C2/units.nF - FEE.C2/units.nF)     < eps
+        assert abs(feep.C1/units.nF - FEE.C1/units.nF)     < eps
+        assert abs(feep.R1/units.ohm - FEE.R1/units.ohm)   < eps
+        assert abs(feep.ZIN/units.ohm - FEE.Zin/units.ohm) < eps
+        assert abs(feep.t_sample - FEE.t_sample)           < eps
+        assert abs(feep.f_sample - FEE.f_sample)           < eps
+        assert abs(feep.f_mc - FEE.f_mc)                   < eps
+        assert abs(feep.f_LPF1 - FEE.f_LPF1)               < eps
+        assert abs(feep.f_LPF2 - FEE.f_LPF2)               < eps
+        assert abs(feep.OFFSET - FEE.OFFSET)               < eps
+        assert abs(feep.CEILING - FEE.CEILING)             < eps
+
+
+def test_diomira_identify_bug(ICDATADIR):
+    """Read a one-event file in which the energy of PMTs is equal to zero and
+    asset it must be son. This test would fail for a normal file where there
+    is always some energy in the PMTs. It's purpose is to provide an automaic
+    documentation of a problem (not really a bug but a feature of the
+    simulation) that can cause a crash in some events. NEXUS simulation
+    can produce eventually events where none of the PMTs of the EP record
+    energy. This test simply ensures that the event read in the example file
+    in indeed of this type.
+
+    The same event is later processed with Irene (where a protection
+    that skips empty events has been added) to ensure that no crash occur."""
+
+    infile = os.path.join(ICDATADIR, 'irene_bug_Kr_ACTIVE_7bar_MCRD.h5')
+    with tb.open_file(infile, 'r') as h5in:
+
+        pmtrd  = h5in.root.pmtrd
+        pmtwf = pmtrd[0]
+        for i in range(pmtrd.shape[1]):
+            assert np.sum(pmtwf[i]) == 0
+
+
+@mark.slow
+def test_diomira_copy_mc_and_offset(ICDATADIR, config_tmpdir):
+    PATH_IN  = os.path.join(ICDATADIR    , 'electrons_40keV_z250_MCRD.h5')
+    PATH_OUT = os.path.join(config_tmpdir, 'electrons_40keV_z250_RWF.h5')
+
+    start_evt  = tbl.event_number_from_input_file_name(PATH_IN)
+    run_number = 0
+    nrequired = 2
+
+    conf = configure('diomira invisible_cities/config/liquid_diomira.conf'.split())
+
+    conf.update(dict(run_number  = run_number,
+                     files_in    = PATH_IN,
+                     file_out    = PATH_OUT,
+                     event_range = (start_evt, start_evt + nrequired)))
+
+    cnt     = diomira(**conf)
+    nactual = cnt.events_in
+    assert nrequired == nactual
+
+    with tb.open_file(PATH_IN,  mode='r') as h5in, \
+         tb.open_file(PATH_OUT, mode='r') as h5out:
+            # check event & run number
+            assert h5out.root.Run.runInfo[0]['run_number'] == run_number
+            assert h5out.root.Run.events [0]['evt_number'] == start_evt
+
+            # check mcextents
+            # we have to convert manually into a tuple because MCTracks[0]
+            # returns an object of type numpy.void where we cannot index
+            # using ranges like mctracks_in[1:]
+            mcextents_in  = tuple(h5in .root.MC.extents[0])
+            mcextents_out = tuple(h5out.root.MC.extents[0])
+            #evt number is not equal if we redefine first event number
+            assert mcextents_out[0] == start_evt
+            for e in zip(mcextents_in[1:], mcextents_out[1:]):
+                np.testing.assert_array_equal(e[0],e[1])
+
+            # check event number is different for each event
+            first_evt_number = h5out.root.MC.extents[ 0][0]
+            last_evt_number  = h5out.root.MC.extents[-1][0]
+            assert first_evt_number != last_evt_number
+
+
+@mark.skip(reason="Feature removed")
+@mark.parametrize('filename first_evt'.split(),
+                  (('dst_NEXT_v0_08_09_Co56_INTERNALPORTANODE_74_0_7bar_MCRD_10000.root.h5', 740000),
+                   ('NEXT_v0_08_09_Co56_2_0_7bar_MCRD_1000.root.h5'                        ,   2000),
+                   ('electrons_40keV_z250_MCRD.h5'                                         ,      0)))
+def test_event_number_from_input_file_name(filename, first_evt):
+    assert Diomira.event_number_from_input_file_name(filename) == first_evt
+
+
+@mark.slow
+def test_diomira_mismatch_between_input_and_database(ICDATADIR, output_tmpdir):
+    file_in  = os.path.join(ICDATADIR    , 'electrons_40keV_z250_MCRD.h5')
+    file_out = os.path.join(output_tmpdir, 'electrons_40keV_z250_RWF_test_mismatch.h5')
+
+    conf = configure('diomira invisible_cities/config/liquid_diomira.conf'.split())
+    conf.update(dict(run_number  = -4500, # Must be a run number with dead pmts
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     event_range = (0, 1)))
+    conf["trigger_params"].update(dict(tr_channels = (18, 19)))
+
+    cnt = diomira(**conf)
+
+    # we are just interested in checking whether the code runs or not
+    assert cnt.events_in == 1
+
+
+@mark.skip(reason="Trigger not implemented in liquid cities")
+@mark.slow
+def test_diomira_trigger_on_masked_pmt_raises_ValueError(ICDATADIR, output_tmpdir):
+    file_in  = os.path.join(ICDATADIR    , 'electrons_40keV_z250_MCRD.h5')
+    file_out = os.path.join(output_tmpdir, 'electrons_40keV_z250_RWF_test_trigger.h5')
+
+    conf = configure('diomira invisible_cities/config/liquid_diomira.conf'.split())
+    conf.update(dict(run_number   = -4500, # Must be a run number with dead pmts
+                     files_in     = file_in,
+                     file_out     = file_out,
+                     trigger_type = "S2",
+                     tr_channels  = (0,), # This is a masked PMT for this run
+                     event_range  = (0, 1)))
+
+    with raises(ValueError):
+        diomira(**conf)
+
+def test_diomira_read_multiple_files(ICDATADIR, output_tmpdir):
+    file_in     = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts*.MCRD.h5")
+    file_out    = os.path.join(output_tmpdir, "Kr83_nexus_v5_03_00_ACTIVE_7bar_6evts.RWF.h5")
+    second_file = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts_1.MCRD.h5")
+
+    nevents_per_file = 3
+
+    nrequired = 10
+    conf = configure('dummy invisible_cities/config/liquid_diomira.conf'.split())
+    conf.update(dict(run_number   = -4734,
+                     files_in     = file_in,
+                     file_out     = file_out,
+                     event_range  = (0, nrequired)))
+    conf["trigger_params"].update(dict(tr_channels = [18, 19]))
+
+    diomira(**conf)
+
+    with tb.open_file(file_out) as h5out:
+        last_particle_list = h5out.root.MC.extents[:]['last_particle']
+        last_hit_list      = h5out.root.MC.extents[:]['last_hit'     ]
+
+        assert all(x<y for x, y in zip(last_particle_list, last_particle_list[1:]))
+        assert all(x<y for x, y in zip(last_hit_list     , last_hit_list     [1:]))
+
+        with tb.open_file(second_file) as h5second:
+            nparticles_in_first_event = h5second.root.MC.extents[0]['last_particle'] + 1
+            nhits_in_first_event      = h5second.root.MC.extents[0]['last_hit'     ] + 1
+
+            assert last_particle_list[nevents_per_file] - last_particle_list[nevents_per_file - 1] == nparticles_in_first_event
+            assert last_hit_list     [nevents_per_file] - last_hit_list     [nevents_per_file - 1] == nhits_in_first_event
