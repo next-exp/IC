19811b335ea4fbafbfc3d199a664d1d2015d6433
Ander Simón <aretno@gmail.com>
2018-03-14 12:41:15 +0100

    Add and test monitoring functions for pmaps and rwf

---
+++ invisible_cities/reco/monitor_functions.py
@@ -0,0 +1,206 @@
+import numpy as np
+import tables as tb
+import glob
+
+from invisible_cities.evm.histos  import HistoManager
+from invisible_cities.io.pmaps_io import load_pmaps
+from invisible_cities.reco.tbl_functions import get_rwf_vectors
+
+from collections import defaultdict
+
+import invisible_cities.database.load_db         as dbf
+import invisible_cities.reco.histogram_functions as histf
+from   invisible_cities.core import system_of_units as units
+
+def pmap_bins(config_dict):
+    """
+    Generates the binning arrays and label of the monitor plots from the a
+    config dictionary that contains the ranges, number of bins and labels.
+
+    Returns a dictionary with the bins and another with the labels.
+    """
+    var_bins   = {}
+    var_labels = {}
+
+    for k, v in config_dict.items():
+        if "_bins" in k:
+            var_bins[k.replace("_bins", "")] = [ np.linspace(v[0], v[1], v[2] + 1) ]
+        elif "_labels" in k:
+            var_labels[k.replace("_labels", "")] = v
+
+    exception = ['S1_Energy', 'S1_Number', 'S1_Time']
+    bin_sel = lambda x: ('S2' not in x) and (x not in exception)
+    for param in filter(bin_sel, list(var_bins)):
+        var_bins  ['S1_Energy_' + param] = var_bins['S1_Energy']   + var_bins[param]
+        var_labels['S1_Energy_' + param] = var_labels['S1_Energy'] + var_labels[param]
+
+    var_bins  ['S1_Time_S1_Energy'] = var_bins['S1_Time']   + var_bins['S1_Energy']
+    var_labels['S1_Time_S1_Energy'] = var_labels['S1_Time'] + var_labels['S1_Energy']
+
+    exception = ['S2_Energy', 'S2_Number', 'S2_Time']
+    bin_sel = lambda x: ('S1' not in x) and (x not in exception) and ('SiPM' not in x)
+    for param in filter(bin_sel, list(var_bins)):
+        var_bins  ['S2_Energy_' + param] = var_bins['S2_Energy']   + var_bins[param]
+        var_labels['S2_Energy_' + param] = var_labels['S2_Energy'] + var_labels[param]
+
+    var_bins  ['S2_Time_S2_Energy'] = var_bins['S2_Time']   + var_bins['S2_Energy']
+    var_labels['S2_Time_S2_Energy'] = var_labels['S2_Time'] + var_labels['S2_Energy']
+
+    var_bins  ['S2_Energy_S1_Energy'] = var_bins['S2_Energy']   + var_bins['S1_Energy']
+    var_labels['S2_Energy_S1_Energy'] = var_labels['S2_Energy'] + var_labels['S1_Energy']
+
+    var_bins  ['S2_XYSiPM'] = var_bins['S2_XSiPM']   + var_bins['S2_XSiPM']
+    var_labels['S2_XYSiPM'] = var_labels['S2_XSiPM'] + ['Y (mm)']
+
+    del var_bins['S2_XSiPM']
+
+    return var_bins, var_labels
+
+def fill_pmap_var_1d(speaks, var_dict, ptype, DataSiPM=None):
+    """
+    Fills a passed dictionary of lists with the pmap variables to monitor.
+
+    Arguments:
+    speaks   = List of S1 or S2s.
+    var_dict = Dictionary that stores the variable values.
+    ptype    = Type of pmap ('S1' or 'S2')
+    DataSiPM = Database with the SiPM information. Only needed in case of 'S2'
+               ptype
+    """
+
+    var_dict[ptype + '_Number'].append(len(speaks))
+    for speak in speaks:
+        var_dict[ptype + '_Width'] .append(speak.width / units.mus)
+        var_dict[ptype + '_Height'].append(speak.height)
+        var_dict[ptype + '_Energy'].append(speak.total_energy)
+        var_dict[ptype + '_Charge'].append(speak.total_charge)
+        var_dict[ptype + '_Time']  .append(speak.time_at_max_energy / units.mus)
+
+        if ptype == 'S2':
+            nS1 = var_dict['S1_Number'][-1]
+            var_dict[ptype + '_SingleS1'].append(nS1)
+            if nS1 == 1:
+                var_dict[ptype + '_SingleS1_Energy'].append(var_dict['S1_Energy'][-1])
+
+            sipm_ids = speak.sipms.ids
+            sipm_Q   = speak.sipms.sum_over_times
+            var_dict[ptype + '_NSiPM'] .append(len(sipm_ids))
+            var_dict[ptype + '_QSiPM'] .extend(sipm_Q)
+            var_dict[ptype + '_IdSiPM'].extend(sipm_ids)
+            if len(sipm_ids) > 0:
+                var_dict[ptype + '_XSiPM'].extend(DataSiPM.X.values[sipm_ids])
+                var_dict[ptype + '_YSiPM'].extend(DataSiPM.Y.values[sipm_ids])
+
+def fill_pmap_var_2d(var_dict, ptype):
+    """
+    Makes 2d combinations of the variables stored in a dictionary that contains
+    the pmaps variables.
+
+    Arguments:
+    var_dict = Dictionary that stores the variable values.
+    ptype    = Type of pmap ('S1' or 'S2')
+    """
+
+    param_list = ['Width', 'Height', 'Charge']
+    for param in param_list:
+        var_dict[ptype + '_Energy_' + ptype + '_' + param] = np.array([var_dict[ptype + '_Energy'], var_dict[ptype + '_' + param]])
+
+    var_dict[ptype + '_Time_' + ptype + '_Energy'] = np.array([var_dict[ptype + '_Time'], var_dict[ptype + '_Energy']])
+
+    if ptype == 'S2':
+        sel = np.asarray(var_dict['S2_SingleS1']) == 1
+        var_dict[ptype + '_Energy_S1_Energy'] = np.array([np.asarray(var_dict[ptype + '_Energy'])[sel],
+                                                          np.asarray(var_dict[ptype + '_SingleS1_Energy'])])
+        var_dict[ptype + '_XYSiPM']           = np.array([var_dict[ptype + '_XSiPM'], var_dict[ptype + '_YSiPM']])
+
+def fill_pmap_histos(in_path, run_number, config_dict):
+    """
+    Creates and returns an HistoManager object with the pmap histograms.
+
+    Arguments:
+    in_path     = String with the path to the file(s) to be monitored.
+    run_number  = Run number of the dataset (used to obtain the SiPM database).
+    config_dict = Dictionary with the configuration parameters (bins, labels).
+    """
+
+    var_bins, var_labels = pmap_bins(config_dict)
+
+    histo_manager = histf.create_histomanager_from_dicts(var_bins, var_labels)
+
+    SiPM_db = dbf.DataSiPM(run_number)
+
+    for in_file in glob.glob(in_path):
+        pmaps = load_pmaps(in_file)
+        var = defaultdict(list)
+        for ti, pi in enumerate(pmaps):
+            fill_pmap_var_1d(pmaps[pi].s1s, var, 'S1')
+            fill_pmap_var_1d(pmaps[pi].s2s, var, 'S2', SiPM_db)
+
+        fill_pmap_var_2d(var, 'S1')
+        fill_pmap_var_2d(var, 'S2')
+
+        del var['S2_XSiPM']
+        del var['S2_YSiPM']
+        del var['S2_SingleS1']
+        del var['S2_SingleS1_Energy']
+
+        histo_manager.fill_histograms(var)
+    return histo_manager
+
+def rwf_bins(config_dict):
+    """
+    Generates the binning arrays and label of the monitor plots from the a
+    config dictionary that contains the ranges, number of bins and labels.
+
+    Returns a dictionary with the bins and another with the labels.
+    """
+
+    var_bins   = {}
+    var_labels = {}
+
+    for k, v in config_dict.items():
+        if "_bins" in k:
+            var_bins[k.replace("_bins", "")] = [ np.linspace(v[0], v[1], v[2] + 1) ]
+        elif "_labels" in k:
+            var_labels[k.replace("_labels", "")] = v
+
+    return var_bins, var_labels, config_dict['n_baseline']
+
+def fill_rwf_var(rwf, var_dict, sensor_type):
+    """
+    Fills a passed dictionary of lists with the pmap variables to monitor.
+
+    Arguments:
+    rwf            = Raw waveforms
+    var_dict       = Dictionary that stores the variable values.
+    sensor_type    = Type of sensor('PMT' or 'SiPM')
+    """
+
+    bls = np.mean(rwf, axis=1)[:, np.newaxis]
+    rms = np.std (rwf, axis=1)[:, np.newaxis]
+    var_dict[sensor_type + '_Baseline']   .extend(bls.flatten())
+    var_dict[sensor_type + '_BaselineRMS'].extend(rms.flatten())
+
+def fill_rwf_histos(in_path, config_dict):
+    """
+    Creates and returns an HistoManager object with the waveform histograms.
+
+    Arguments:
+    in_path     = String with the path to the file(s) to be monitored.
+    config_dict = Dictionary with the configuration parameters (bins, labels)
+    """
+
+    var_bins, var_labels, n_baseline = rwf_bins(config_dict)
+
+    histo_manager = histf.create_histomanager_from_dicts(var_bins, var_labels)
+
+    for in_file in glob.glob(in_path):
+        with tb.open_file(in_file, "r") as h5in:
+            var = defaultdict(list)
+            nevt, pmtrwf, sipmrwf, _ = get_rwf_vectors(h5in)
+            for evt in range(nevt):
+                fill_rwf_var(pmtrwf[evt,:,0:n_baseline], var, "PMT")
+                fill_rwf_var(              sipmrwf[evt], var,"SiPM")
+
+        histo_manager.fill_histograms(var)
+    return histo_manager
