84aebacf7098589c7a71f7de58cb546bf5c53eb4
Ander Simón <aretno@gmail.com>
2018-03-19 23:26:19 +0100

    Variable name change to common criteria.

--- invisible_cities/reco/olivia.py
+++ invisible_cities/reco/olivia.py
@@ -14,11 +14,11 @@ data_types = ['rwf', 'pmaps']
 
 def olivia(conf):
     print(conf)
-    in_path      =     os.path.expandvars(conf.in_path)
-    out_path     =     os.path.expandvars(conf.out_path)
-    run_number   =                    int(conf.run_number)
-    data_type    =                        conf.data_type
-    histo_config =                        conf.histo_config
+    files_in     = os.path.expandvars(conf.files_in)
+    file_out     = os.path.expandvars(conf.file_out)
+    run_number   =                int(conf.run_number)
+    data_type    =                    conf.data_type
+    histo_config =                    conf.histo_config
 
     if data_type not in data_types:
         print(f'Error: Data type {data_type} is not recognized.')
@@ -28,10 +28,10 @@ def olivia(conf):
     with open(histo_config) as  config_file:
         config_dict = json.load(config_file)
         if   data_type == 'rwf'  :
-            histo_manager = monf.fill_rwf_histos (in_path, config_dict)
+            histo_manager = monf.fill_rwf_histos (files_in, config_dict)
         elif data_type == 'pmaps':
-            histo_manager = monf.fill_pmap_histos(in_path, run_number, config_dict)
-        save_histomanager_to_file(histo_manager, out_path)
+            histo_manager = monf.fill_pmap_histos(files_in, run_number, config_dict)
+        save_histomanager_to_file(histo_manager, file_out)
 
 
 if __name__ == "__main__":
