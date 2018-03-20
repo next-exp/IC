420860585f02dc70e1dc5ce369825554708f1729
Josh Renner <jrenner@Joshs-MacBook-Pro.local>
2018-03-20 22:58:24 +0100

    Removes unnecessary test fixture

--- invisible_cities/detsim/detsim_functions_test.py
+++ invisible_cities/detsim/detsim_functions_test.py
@@ -12,10 +12,6 @@ from numpy.testing       import assert_allclose
 from .. io.mcinfo_io     import load_mchits
 from .  detsim_functions import diffuse_and_smear_hits
 
-@pytest.fixture(scope = 'module')
-def SE_nexus_filename(ICDATADIR):
-    return os.path.join(ICDATADIR, "electron_26keV_nexus.h5")
-
 def test_diffuse_and_smear_hits(mc_particle_and_hits_nexus_data):
     hfile, name, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
 
