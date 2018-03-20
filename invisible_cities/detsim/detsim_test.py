f6c7edb08dcbc4598674d4dd865691e754f361f2
Josh Renner <jrenner@Joshs-MacBook-Pro.local>
2018-03-20 18:00:52 +0100

    Updates code to recent version of master

--- invisible_cities/detsim/detsim_test.py
+++ invisible_cities/detsim/detsim_test.py
@@ -1,20 +1,29 @@
-from . detsim            import Detsim
+import os
 
-def test_detsim_SE(SE_nexus_filename, config_tmpdir):
-    PATH_IN   = SE_nexus_filename
-    PATH_OUT  = os.path.join(config_tmpdir,'SE_true_voxels.h5')
+
+from .  detsim         import Detsim
+from .. core.configure import configure
+
+def test_detsim(mc_particle_and_hits_nexus_data, config_tmpdir):
+    hfile, name, vi, vf, p, Ep, nhits, X, Y, Z, E, t = mc_particle_and_hits_nexus_data
+
+    PATH_IN   = hfile
+    PATH_OUT  = os.path.join(config_tmpdir,'detsim_true_voxels.h5')
     conf      = configure('dummy invisible_cities/config/detsim.conf'.split())
     nevt_req  = 1
 
-    conf.update(dict(files_in      = PATH_IN,
-                     file_out      = PATH_OUT,
-                     event_range   = (nevt_req,),
-                     **KrMC_hdst.config))
+    conf.update(dict(files_in              = PATH_IN,
+                     file_out              = PATH_OUT,
+                     event_range           = (nevt_req,),
+                     zmin                  = 50,
+                     zmax                  = 500,
+                     diff_transv           = 1.0,
+                     diff_long             = 0.3,
+                     resolution_FWHM       = 0.8,
+                     Qbb                   = 2.45784,
+                     true_voxel_dimensions = [5,5,5]))
 
     cdetsim = Detsim(**conf)
     cdetsim.run()
     cnt         = cdetsim.end()
     assert cnt.n_events_tot      == nevt_req
-
-    #df_penthesilea = dio.load_dst(PATH_OUT , 'RECO', 'Events')
-    #assert_dataframes_close(df_penthesilea, DF_TRUE, check_types=False)
--- invisible_cities/io/voxels_io_test.py
+++ invisible_cities/io/voxels_io_test.py
@@ -1,17 +1,18 @@
 import os
 import tables as tb
+import numpy  as np
 
 from numpy.testing import assert_allclose
 
 from .. evm.event_model import Voxel
 from .. evm.event_model import VoxelCollection
-from .  dst_io          import load_dst
 from .  voxels_io       import true_voxels_writer
+from .  voxels_io       import load_voxels
 
 def test_true_voxels_writer(config_tmpdir, voxels_toy_data):
-    output_file = os.path.join(config_tmpdir, "test_voxels.h5")
 
-    _, (event, X, Y, Z, E, size) = voxels_toy_data
+    voxels_filename, (event, X, Y, Z, E, size) = voxels_toy_data
+    output_file = os.path.join(config_tmpdir, 'toy_voxels.h5')
 
     with tb.open_file(output_file, 'w') as h5out:
         write = true_voxels_writer(h5out)
@@ -29,4 +30,19 @@ def test_true_voxels_writer(config_tmpdir, voxels_toy_data):
     assert_allclose(E,     vdst.root.TrueVoxels.Voxels[:]['E'])
     assert_allclose(size,  vdst.root.TrueVoxels.Voxels[:]['size'])
 
-def test_load_voxels():
+def test_load_voxels(config_tmpdir, voxels_toy_data):
+
+    voxels_filename, (event, X, Y, Z, E, size) = voxels_toy_data
+
+    voxels_dict = load_voxels(voxels_filename)
+    vX =    [voxel.X    for voxel in voxels_dict[0].voxels]
+    vY =    [voxel.Y    for voxel in voxels_dict[0].voxels]
+    vZ =    [voxel.Z    for voxel in voxels_dict[0].voxels]
+    vE =    [voxel.E    for voxel in voxels_dict[0].voxels]
+    vsize = [voxel.size for voxel in voxels_dict[0].voxels]
+
+    assert np.allclose(X, vX)
+    assert np.allclose(Y, vY)
+    assert np.allclose(Z, vZ)
+    assert np.allclose(E, vE)
+    assert np.allclose(size, vsize)
