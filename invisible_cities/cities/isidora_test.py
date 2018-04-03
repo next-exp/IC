d1031cec950c3a712be7c8f44d033786fb03c281
Gonzalo <gonzaponte@gmail.com>
2018-04-03 11:33:15 +0200

    Add test to check exact result in isidora

--- invisible_cities/cities/isidora_test.py
+++ invisible_cities/cities/isidora_test.py
@@ -20,6 +20,8 @@ from pytest import fixture
 from .  isidora import Isidora
 from .. core                 import system_of_units as units
 from .. core.configure       import configure
+from .. core.configure       import all as all_events
+from .. core.testing_utils   import assert_tables_equality
 
 
 @mark.slow
@@ -58,3 +60,29 @@ def test_isidora_electrons_40keV(config_tmpdir, ICDIR):
             evts_out_u8 = evts_out_i4.astype([('evt_number', '<u8'), ('timestamp', '<u8')])
 
             np.testing.assert_array_equal(evts_in, evts_out_u8)
+
+
+def test_isidora_exact_result(ICDATADIR, output_tmpdir):
+    file_in     = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts.RWF.h5")
+    file_out    = os.path.join(output_tmpdir,                      "exact_result_isidora.h5")
+    true_output = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts.BLR.h5")
+
+    conf = configure("isidora invisible_cities/config/isidora.conf".split())
+    conf.update(dict(run_number  = 0,
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     event_range = all_events))
+
+    Isidora(**conf).run()
+
+    tables = ( "MC/extents",  "MC/hits"   , "MC/particles", "MC/generators",
+              "BLR/pmtcwf" , "BLR/sipmrwf",
+              "Run/events" , "Run/runInfo")
+    with tb.open_file(true_output)  as true_output_file:
+        with tb.open_file(file_out) as      output_file:
+            print(true_output_file)
+            print(output_file)
+            for table in tables:
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
