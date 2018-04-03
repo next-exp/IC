12d0f909b6c9f5aad9f00e380d55c2ecb69cc1ad
Gonzalo <gonzaponte@gmail.com>
2018-04-03 20:56:09 +0200

    Add test to check exact result in pmtgain

--- invisible_cities/calib/pmtgain_test.py
+++ invisible_cities/calib/pmtgain_test.py
@@ -15,8 +15,10 @@ import tables as tb
 from pytest        import mark
 from numpy.testing import assert_array_equal
 
-from .  pmtgain        import Pmtgain
-from .. core.configure import configure
+from .  pmtgain            import Pmtgain
+from .. core.configure     import configure
+from .. core.configure     import all as all_events
+from .. core.testing_utils import assert_tables_equality
 
 
 @mark.parametrize("proc_opt", ('gain', 'gain_mau', 'gain_nodeconv'))
@@ -49,3 +51,29 @@ def test_pmtgain_pulsedata(config_tmpdir, ICDATADIR, proc_opt):
                                                            ('timestamp' , '<u8')])
 
         assert_array_equal(evts_in, evts_out)
+
+
+@mark.parametrize("proc_opt", ('gain', 'gain_mau', 'gain_nodeconv'))
+def test_pmtgain_exact_result(ICDATADIR, output_tmpdir, proc_opt):
+    file_in     = os.path.join(ICDATADIR    ,                  "pmtledpulsedata.h5")
+    file_out    = os.path.join(output_tmpdir, f"exact_result_pmtgain_{proc_opt}.h5")
+    true_output = os.path.join(ICDATADIR    , f"pmtledpulsedata_hist_{proc_opt}.h5")
+
+    conf = configure("pmtgain invisible_cities/config/pmtgain.conf".split())
+    conf.update(dict(run_number  = 5000,
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     proc_mode   = proc_opt,
+                     event_range = all_events))
+
+    Pmtgain(**conf).run()
+
+    tables = ("HIST/pmt_dark", "HIST/pmt_dark_bins",
+              "HIST/pmt_spe" , "HIST/pmt_spe_bins" ,
+               "Run/events"  ,  "Run/runInfo"      )
+    with tb.open_file(true_output)  as true_output_file:
+        with tb.open_file(file_out) as      output_file:
+            for table in tables:
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
