d9c2d4adb8152178c6d940a1cbee892dbc4c353a
Gonzalo <gonzaponte@gmail.com>
2018-04-04 00:00:43 +0200

    Add test to check exact result in sipmpdf

--- invisible_cities/calib/sipmpdf_test.py
+++ invisible_cities/calib/sipmpdf_test.py
@@ -14,8 +14,10 @@ import tables as tb
 from numpy.testing import assert_array_equal
 from pytest        import mark
 
-from .  sipmpdf           import Sipmpdf
-from .. core   .configure import configure
+from .  sipmpdf               import Sipmpdf
+from .. core   .configure     import configure
+from .. core   .configure     import all as all_events
+from .. core   .testing_utils import assert_tables_equality
 
 
 @mark.parametrize("adc_plots", (True, False))
@@ -48,3 +50,51 @@ def test_sipmpdf_sipmdarkcurrent(config_tmpdir, ICDATADIR, adc_plots):
                                                            ('timestamp' , '<u8')])
 
         assert_array_equal(evts_in, evts_out)
+
+
+def test_sipmpdf_exact_result(ICDATADIR, output_tmpdir):
+    file_in     = os.path.join(ICDATADIR    ,      "sipmdarkcurrentdata.h5")
+    file_out    = os.path.join(output_tmpdir,     "exact_result_sipmpdf.h5")
+    true_output = os.path.join(ICDATADIR    , "sipmdarkcurrentdata_hist.h5")
+
+    conf = configure("sipmpdf invisible_cities/config/sipmpdf.conf".split())
+    conf.update(dict(run_number  = 5000,
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     event_range = all_events))
+
+    Sipmpdf(**conf).run()
+
+    tables = ("HIST/sipm_median", "HIST/sipm_median_bins",
+              "HIST/sipm_mode"  , "HIST/sipm_mode_bins"  ,
+               "Run/events"     ,  "Run/runInfo"         )
+    with tb.open_file(true_output)  as true_output_file:
+        with tb.open_file(file_out) as      output_file:
+            for table in tables:
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
+
+
+def test_sipmpdf_exact_result_adc(ICDATADIR, output_tmpdir):
+    file_in     = os.path.join(ICDATADIR    ,          "sipmdarkcurrentdata.h5")
+    file_out    = os.path.join(output_tmpdir,     "exact_result_sipmpdf_adc.h5")
+    true_output = os.path.join(ICDATADIR    , "sipmdarkcurrentdata_hist_adc.h5")
+
+    conf = configure("sipmpdf invisible_cities/config/sipmpdf.conf".split())
+    conf.update(dict(run_number  = 5000,
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     event_range = all_events,
+                     adc_only    = True))
+
+    Sipmpdf(**conf).run()
+
+    tables = ("HIST/sipm_adc",
+               "Run/events"  ,  "Run/runInfo"         )
+    with tb.open_file(true_output)  as true_output_file:
+        with tb.open_file(file_out) as      output_file:
+            for table in tables:
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
