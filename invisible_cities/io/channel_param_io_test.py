c5dbb32314b64b3432c18d0a7badb08858db3266
Andrew Laing <laingandrew@alaing.ific.uv.es>
2018-04-18 18:11:00 +0200

    Generalised test to check multiple sensor entries

--- invisible_cities/io/channel_param_io_test.py
+++ invisible_cities/io/channel_param_io_test.py
@@ -17,25 +17,30 @@ def test_generic_parameters(config_tmpdir):
     filename = os.path.join(config_tmpdir, 'test_param.h5')
 
     outDict = {}
+    val_list = []
+    n_rows = 5
     with tb.open_file(filename, 'w') as dataOut:
         pWrite = channel_param_writer(dataOut, sensor_type="test",
                                       func_name="generic",
                                       param_names=generic_params)
 
-        for i, par in enumerate(generic_params):
-            outDict[par] = [i, i / 10]
+        for sens in range(n_rows):
+            for i, par in enumerate(generic_params):
+                outDict[par] = [i, (i + sens) / 10]
 
-        pWrite(0, outDict)
+            pWrite(sens, outDict)
+            val_list.append(list(outDict.values()))
 
     col_list = ['SensorID'] + list(outDict.keys())
-    val_list = np.concatenate(list(outDict.values()))
+    val_list = np.concatenate(val_list)
     with tb.open_file(filename) as dataIn:
 
         tblRead = dataIn.root.FITPARAMS.FIT_test_generic
-        assert tblRead.nrows == 1
+        assert tblRead.nrows == n_rows
         assert len(tblRead.colnames) == len(generic_params) + 1
         assert tblRead.colnames == col_list
-        assert_allclose(np.concatenate(list(tblRead[0])[1:]), val_list)
+        all_values = [ list(x)[1:] for x in tblRead[:] ]
+        assert_allclose(np.concatenate(all_values), val_list)
 
 
 def test_simple_parameters_with_covariance(config_tmpdir):
@@ -76,8 +81,7 @@ def test_make_table_dictionary():
 def test_store_fit_values(config_tmpdir):
     filename = os.path.join(config_tmpdir, 'test_param.h5')
 
-    dummy_dict = {'SensorID' : tb.UInt32Col(pos=0),
-                  'par0' : tb.UInt32Col(pos=1)}
+    dummy_dict = make_table_dictionary(['par0'])
 
     with tb.open_file(filename, 'w') as dataOut:
         PARAM_group = dataOut.create_group(dataOut.root, "testgroup")
