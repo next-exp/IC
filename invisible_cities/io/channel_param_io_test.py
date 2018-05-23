ec6ee59d63d16c7ca4cdeaba94a6147880e1cccf
Andrew Laing <a.b.r.laing@gmail.com>
2018-05-23 16:50:02 +0200

    Added readers to tests

--- invisible_cities/io/channel_param_io_test.py
+++ invisible_cities/io/channel_param_io_test.py
@@ -7,10 +7,12 @@ from numpy.testing import assert_allclose
 
 from .. reco     import tbl_functions as tbl
 
-from . channel_param_io import        generic_params
-from . channel_param_io import      store_fit_values
-from . channel_param_io import  channel_param_writer
-from . channel_param_io import make_table_dictionary
+from . channel_param_io import         generic_params
+from . channel_param_io import       store_fit_values
+from . channel_param_io import   channel_param_writer
+from . channel_param_io import  make_table_dictionary
+from . channel_param_io import     basic_param_reader
+from . channel_param_io import generator_param_reader
 
 
 def test_generic_parameters(config_tmpdir):
@@ -35,12 +37,44 @@ def test_generic_parameters(config_tmpdir):
     val_list = np.concatenate(val_list)
     with tb.open_file(filename) as dataIn:
 
-        tblRead = dataIn.root.FITPARAMS.FIT_test_generic
-        assert tblRead.nrows == n_rows
-        assert len(tblRead.colnames) == len(generic_params) + 1
-        assert tblRead.colnames == col_list
-        all_values = [ list(x)[1:] for x in tblRead[:] ]
+        tbl_names, param_names, tbls = basic_param_reader(dataIn)
+        assert len(tbls) == 1
+        assert tbls[0].nrows == n_rows
+        assert len(param_names[0]) == len(col_list)
+        assert param_names[0] == col_list
+        all_values = [ list(x)[1:] for x in tbls[0][:] ]
         assert_allclose(np.concatenate(all_values), val_list)
+
+
+def test_generator_param_reader(config_tmpdir):
+    filename = os.path.join(config_tmpdir, 'test_param.h5')
+
+    outDict = {}
+    val_list = []
+    n_rows = 5
+    with tb.open_file(filename, 'w') as dataOut:
+        pWrite = channel_param_writer(dataOut, sensor_type="test",
+                                      func_name="generic",
+                                      param_names=generic_params)
+
+        for sens in range(n_rows):
+            for i, par in enumerate(generic_params):
+                outDict[par] = [i, (i + sens) / 10]
+
+            pWrite(sens, outDict)
+            val_list.append(list(outDict.values()))
+
+    val_list = np.array(val_list)
+    with tb.open_file(filename) as dataIn:
+        counter = 0
+        for sens, (vals, errs) in generator_param_reader(dataIn, 'FIT_test_generic'):
+            assert sens == counter
+            assert len(vals) == len(errs) == len(outDict)
+            assert_allclose(val_list[sens, :, 0], np.array(list(vals.values())))
+            assert_allclose(val_list[sens, :, 1], np.array(list(errs.values())))
+            counter += 1
+        assert counter == n_rows
+            
 
 
 def test_simple_parameters_with_covariance(config_tmpdir):
