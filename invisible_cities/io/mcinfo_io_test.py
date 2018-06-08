2205a33e9d383f9f18e35ca2945fe0401720f1f6
Paola Ferrario <paola.ferrario@gmail.com>
2018-06-08 12:56:14 +0200

    Divide one big test into two smaller ones

--- invisible_cities/io/mcinfo_io_test.py
+++ invisible_cities/io/mcinfo_io_test.py
@@ -215,6 +215,13 @@ def test_load_sensors_data(mc_sensors_nexus_data):
 
     assert np.allclose(samples, sipm)
 
+
+def test_read_last_sensor_response(mc_sensors_nexus_data):
+    efile, _, _, _, _, _ = mc_sensors_nexus_data
+
+    mcsensors_dict = load_mcsensor_response(efile)
+    waveforms = mcsensors_dict[0]
+
     with tb.open_file(efile, mode='r') as h5in:
         last_written_id = h5in.root.MC.sensor_positions[-1][0]
         last_read_id = list(waveforms.keys())[-1]
