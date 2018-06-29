5a8ba34f0feec23ba19fd6138a29a66fd6d75f2d
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-06-29 19:23:21 +0900

    Remove commented code
    The proposed behavior of the function when an exception of type
    NoSuchNodeError is found is simply to print an error message, to
    inform the user of the existence of an offending file and then ignore
    the file. This is a case where it is justified not to abort the concat
    of good files (which can take many minutes) because a spurious or
    corrupted file is found. No need to throw a new exception either,
    although it could be done if one wanted to let the user decide what to do.

--- invisible_cities/io/dst_io.py
+++ invisible_cities/io/dst_io.py
@@ -9,22 +9,9 @@ def load_dst(filename, group, node):
             return pd.DataFrame.from_records(table)
         except NoSuchNodeError:
             print(f' error loading {filename}')
-            #raise UnknownDST
 
-            #return pd.DataFrame.from_records(table)
 
 
 def load_dsts(dst_list, group, node):
     dsts = [load_dst(filename, group, node) for filename in dst_list]
     return pd.concat(dsts)
-
-# def load_dsts(dst_list, group, node):
-#     dsts=[]
-#     for i, filename in enumerate(dst_list):
-#         try:
-#             dsts.append(dstio.load_dst(filename, group, node))
-#         except:
-#             print(f' error loading {filename}')
-#
-#     print(f'{i} files are being concatenated')
-#     return pd.concat(dsts)
