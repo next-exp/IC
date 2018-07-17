ff3782455a09398a87bff98772f58b4e9b4d897e
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-07-17 16:28:23 -0500

    Add further protections to load_dst.
    Function load_dst() can fail also due to corrupted files or non
    existing files. In all the cases the action is to intercept the
    excpetion and send a warning.

--- invisible_cities/io/dst_io.py
+++ invisible_cities/io/dst_io.py
@@ -1,16 +1,24 @@
 import tables as tb
 import pandas as pd
 from tables import NoSuchNodeError
+from tables import HDF5ExtError
 import warnings
 
-def load_dst(filename, group, node):
-    with tb.open_file(filename) as h5in:
-        try:
-            table = getattr(getattr(h5in.root, group), node).read()
-            return pd.DataFrame.from_records(table)
-        except NoSuchNodeError:
-            warnings.warn(f' warning: could no load  {filename}')
 
+
+def load_dst(filename, group, node):
+    try:
+        with tb.open_file(filename) as h5in:
+            try:
+                table = getattr(getattr(h5in.root, group), node).read()
+                return pd.DataFrame.from_records(table)
+            except NoSuchNodeError:
+                warnings.warn(f' warning:  {filename} not of kdst type')
+    except HDF5ExtError:
+        warnings.warn(f' warning:  {filename} corrupted')
+    except IOError:
+        warnings.warn(f' warning:  {filename} does not exist')
+        
 
 def load_dsts(dst_list, group, node):
     dsts = [load_dst(filename, group, node) for filename in dst_list]
