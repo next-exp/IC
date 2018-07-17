60904bf013d298036e02e5b8dd04f46b50c1945a
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-07-17 17:24:35 -0500

    Improve tests.
    Warnings now catched properly, two different tests for the two common warnings.

--- invisible_cities/io/dst_io_test.py
+++ invisible_cities/io/dst_io_test.py
@@ -1,9 +1,12 @@
+import os
 import pandas as pd
 
 from ..core.testing_utils import assert_dataframes_close
 from .      dst_io        import load_dst
 from .      dst_io        import load_dsts
 
+import warnings
+import pytest
 
 def test_load_dst(KrMC_kdst):
     df_read = load_dst(*KrMC_kdst[0].file_info)
@@ -25,10 +28,31 @@ def test_load_dsts_double_file(KrMC_kdst):
 
     assert_dataframes_close(df_read, df_true, False)
 
+def test_load_dsts_reads_good_kdst(ICDATADIR):
+    good_file = "Kr83_nexus_v5_03_00_ACTIVE_7bar_10evts_KDST.h5"
+    good_file = os.path.join(ICDATADIR, good_file)
+    group = "DST"
+    node  = "Events"
+    load_dsts([good_file], group, node)
 
-def test_load_dsts_warn_if_wrong_file(KrMC_kdst):
-    tbl     = KrMC_kdst[0].file_info
-    df_true = KrMC_kdst[0].true
-    wrong_file =KrMC_kdst[1].filename
-    non_existing_file ="non_existing.hdf5"
-    cdst = load_dsts([tbl.filename, non_existing_file, wrong_file], tbl.group, tbl.node)
+def test_load_dsts_warns_not_of_kdst_type(ICDATADIR):
+    good_file = "Kr83_nexus_v5_03_00_ACTIVE_7bar_10evts_KDST.h5"
+    good_file = os.path.join(ICDATADIR, good_file)
+
+    wrong_file = "kdst_5881_map_lt.h5"
+    wrong_file = os.path.join(ICDATADIR, wrong_file)
+    group = "DST"
+    node  = "Events"
+    with pytest.warns(UserWarning, match='not of kdst type'):
+        load_dsts([good_file, wrong_file], group, node)
+
+def test_load_dsts_warns_if_not_existing_file(ICDATADIR):
+    good_file = "Kr83_nexus_v5_03_00_ACTIVE_7bar_10evts_KDST.h5"
+    good_file = os.path.join(ICDATADIR, good_file)
+
+    wrong_file = "non_existing_file.h5"
+
+    group = "DST"
+    node  = "Events"
+    with pytest.warns(UserWarning, match='does not exist'):
+        load_dsts([good_file, wrong_file], group, node)
