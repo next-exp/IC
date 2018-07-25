9e8571ada656453beb9fbab629a28c86b7f6878d
Michel Sorel <sorel@fnal.gov>
2018-07-25 10:29:16 -0500

    removed unused variable

--- invisible_cities/io/mcinfo_io.py
+++ invisible_cities/io/mcinfo_io.py
@@ -234,7 +234,6 @@ def read_mcinfo(h5f, event_range=(0, int(1e9))) ->Mapping[int, Mapping[int, MCPa
     h5extents = mc_info.extents
     h5ehits = mc_info.hits
     h5particles = mc_info.particles
-    h5generators = mc_info.generators
 
     events_in_file = len(h5extents)
 
