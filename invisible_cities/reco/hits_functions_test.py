aa144fd214efe2b78b0737bd24e10317660b866d
mmkekic <mmkekic@gmail.com>
2018-07-31 19:58:53 +0200

    Added test for hitc_corrections

--- invisible_cities/reco/hits_functions_test.py
+++ invisible_cities/reco/hits_functions_test.py
@@ -1,4 +1,5 @@
 import numpy                     as     np
+from   numpy.testing             import assert_allclose
 
 from   .. evm.event_model        import Hit
 from   .. evm.event_model        import HitCollection
@@ -8,11 +9,9 @@ from   .. core.system_of_units_c import units
 from   .. types.ic_types         import NN
 from   .. reco                   import dst_functions    as dstf
 from   .. reco.hits_functions    import merge_NN_hits
+from   .. reco.hits_functions    import hitc_corrections
 
-from   numpy.testing             import assert_allclose
-
-
-from pytest import fixture
+from  pytest                     import fixture
 
 @fixture
 def toy_hitc():
@@ -26,10 +25,33 @@ def toy_hitc():
     for i in range(len(xs)):
         hitc.hits.append(Hit(0, Cluster(qs[i], xy(xs[i],ys[i]), xy(0,0), 0), zs[i], es[i], xy(x_peak,y_peak)))
     return hitc
-    
+@fixture
+def toy_emap():
+    raw_e = [1000.     , 1500.     , 3000.     ]
+    lt_e  = [1002.45422, 1503.68134, 3017.80762]
+    cor_e = [2948.58558, 4422.87837, 8929.88863]
+    raw_q = [ 100.     ,  150.     ,  200.     ]
+    lt_q  = [ 100.24542,  150.36813,  201.18717]
+    cor_q = [ 294.85855,  442.28783,  595.32590]
+    return raw_e, lt_e, cor_e, raw_q, lt_q, cor_q
+
 def test_merge_NN_hits(toy_hitc):
     hitc = toy_hitc
     hitc = merge_NN_hits(hitc)
     ecs_test = [h.E for h in hitc.hits]
     ecs      = np.array([ 1020,1530,3000]) * units.pes
     assert_allclose(ecs_test  , ecs, rtol=1e-4)
+
+def test_hitc_corrections(toy_hitc, toy_emap, corr_toy_data):
+    corr_filename, _ = corr_toy_data
+    XYcor    = dstf.load_xy_corrections         (corr_filename, group = "Corrections", node =  "XYcorrections", norm_strategy = 'max')
+    LTcor    = dstf.load_lifetime_xy_corrections(corr_filename, group = "Corrections", node  = "LifetimeXY"                         )
+    hitc_emap= hitc_corrections(toy_hitc, XYcor, LTcor, XYcor, LTcor)
+    raw_e, lt_e, cor_e, raw_q, lt_q, cor_q = toy_emap
+    assert_allclose(raw_e , hitc_emap.E.raw, rtol=1e-4)
+    assert_allclose( lt_e , hitc_emap.E.lt , rtol=1e-4)
+    assert_allclose(cor_e , hitc_emap.E.cor, rtol=1e-4)
+    assert_allclose(raw_q , hitc_emap.Q.raw, rtol=1e-4)
+    assert_allclose( lt_q , hitc_emap.Q.lt , rtol=1e-4)
+    assert_allclose(cor_q , hitc_emap.Q.cor, rtol=1e-4)
+
