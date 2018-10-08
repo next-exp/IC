40e4fa6a6a755f840d65a26fe9e87088bfb3787b
Carmen Romo <carmenromoluque@gmail.com>
2018-10-08 13:19:54 +0200

    Add seeds_and_bounds_function

--- invisible_cities/reco/calib_functions.py
+++ invisible_cities/reco/calib_functions.py
@@ -130,3 +130,114 @@ def copy_sensor_table(h5in, h5out):
         if 'DataSiPM' in dIn.root.Sensors:
             datasipm = dIn.root.Sensors.DataSiPM
             datasipm.copy(newparent=group)
+
+
+def seeds_and_bounds(sensor_type, run_no, n_chann, scaler, bins, spec, ped_vals,
+                     ped_errs, func='dfunc', use_db_gain_seeds=True):
+
+    """
+    Define the seeds and bounds to be used for calibration fits.
+
+    Parameters
+    ----------
+    sensor_type   : string
+    Input of type of sensor: sipm or pmt.
+    run_no        : int
+    Run number.
+    n_chann       : int
+    Channel number (sensor ID).
+    scaler        : callable
+    Scale function.
+    bins          : np.array
+    Number of divisions in the x axis.
+    spec          : np.array
+    Spectra, charge values of the signal.
+    ped_vals      : np.array
+    Values for the pedestal fit.
+    ped_errs      : np.array
+    Errors of the values for the pedestal fit.
+    func          : callable, optional
+    Function used for fitting. Defaults to dfunc.
+    use_db_gain_seeds : bool, optional
+    If True, seeds are taken from previous runs in database.
+    If False, peaks are found with find_peaks_cwt function.
+
+    Returns
+    -------
+    sd0 : sequence
+    Seeds for normalization, mu, gain and sigma.
+    bd0 : sequence
+    Minimum and maximum limits for the previous variables.
+    """
+
+    norm_seed = spec.sum()
+
+    GSeed  = 0
+    GSSeed = 0
+
+    if n_chann > 1000: #SiPMs
+        GainSeeds  = DB.DataSiPM(run_no).adc_to_pes.values
+        SigSeeds   = DB.DataSiPM(run_no).Sigma.values
+        spectra    = spec
+        peak_range = np.arange(4, 20)
+        min_bin    = 10
+        max_bin    = 22
+        hpw        = 5
+        mins       = 5
+        dscale     = spec[bins<5].sum() / fitf.gauss(bins[bins<5], *ped_vals).sum()
+        lim_ped    = 10000
+
+    else: #PMTs
+        GainSeeds  = DB.DataPMT(run_no).adc_to_pes.values
+        SigSeeds   = DB.DataPMT(run_no).Sigma.values
+        dscale     = spec[bins<0].sum() / fitf.gauss(bins[bins<0], *ped_vals).sum()
+        spectra    = spec - fitf.gauss(bins, *ped_vals) * dscale
+        peak_range = np.arange(10, 20)
+        min_bin    = 15
+        max_bin    = 50
+        hpw        = 10
+        mins       = 0
+        lim_ped    = 10000
+
+    if use_db_gain_seeds:
+        GSeed     = GainSeeds[n_chann]
+        GSSeed    = SigSeeds[n_chann]
+
+    else:
+        pDL  = find_peaks_cwt(spectra, peak_range, min_snr=1, noise_perc=5)
+        p1pe = pDL[(bins[pDL]>10) & (bins[pDL]<22)][0]
+        if not p1pe:
+            p1pe = np.argwhere(bins==(min_bin+max_bin)/2)[0][0]
+        else:
+            p1pe = p1pe[spec[p1pe].argmax()]
+
+        fgaus = fitf.fit(fitf.gauss, bins[p1pe-hpw:p1pe+hpw], spectra[p1pe-hpw:p1pe+hpw],
+                         seed=(spectra[p1pe], bins[p1pe], 7), sigma=np.sqrt(spectra[p1pe-hpw:p1pe+hpw]))
+
+        GSeed = fgaus.values[1] - ped_vals[1]
+        if fgaus.values[2] <= ped_vals[2]:
+            GSSeed = 0.5
+        else:
+            GSSeed = np.sqrt(fgaus.values[2]**2 - ped_vals[2]**2)
+
+    errs          = np.sqrt(spectra[bins<mins])
+    errs[errs==0] = 1
+    fscale        = fitf.fit(scaler, bins[bins<mins], spec[bins<mins], (dscale), sigma=errs)
+    mu_seed        = -np.log(fscale.values[0])
+    if mu_seed < 0: mu_seed = 0.001
+
+    if 'gau' in func:
+        ped_seed     = ped_vals[1]
+        ped_min      = ped_seed - lim_ped * ped_errs[1]
+        ped_max      = ped_seed + lim_ped * ped_errs[1]
+        ped_sig_seed = ped_vals[2]
+        ped_sig_min  = max(0.001, ped_sig_seed - lim_ped * ped_errs[2])
+        ped_sig_max  = ped_sig_seed + lim_ped * ped_errs[2]
+
+        sd0          = (norm_seed, mu_seed, ped_seed, ped_sig_seed, GSeed, GSSeed)
+        bd0          = [(0, 0, ped_min, ped_sig_min, 0, 0.001), (1e10, 10000, ped_max, ped_sig_max, 10000, 10000)]
+        return sd0, bd0
+
+    sd0 = (norm_seed, mu_seed, GSeed, GSSeed)
+    bd0 = [(0, 0, 0, 0.001), (1e10, 10000, 10000, 10000)]
+    return sd0, bd0
