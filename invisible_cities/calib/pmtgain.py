b2f92789579bb136662fb1d3a7d81c674db58218
Gonzalo <gonzaponte@gmail.com>
2018-10-10 18:23:06 +0200

    Move calib functions to reco

--- invisible_cities/calib/pmtgain.py
+++ invisible_cities/calib/pmtgain.py
@@ -11,13 +11,12 @@ from functools import partial
 
 import numpy  as np
 
-from .                         import      calib_functions as cf
+from .. reco                   import         calib_functions as cf
 from .. reco                   import calib_sensors_functions as csf
 from .. io   .         hist_io import          hist_writer
 from .. io   .run_and_event_io import run_and_event_writer
-from .. icaro.hst_functions    import shift_to_bin_centers
+from .. core .core_functions    import shift_to_bin_centers
 
-from .. core  .system_of_units_c import units
 from .. cities.base_cities       import CalibratedCity
 from .. cities.base_cities       import EventLoop
 from .. sierpe                   import fee
--- invisible_cities/calib/sipmpdf.py
+++ invisible_cities/calib/sipmpdf.py
@@ -11,11 +11,11 @@ from functools import partial
 
 import numpy  as np
 
-from .                        import calib_functions         as cf
-from .. reco                  import calib_sensors_functions as csf
-from .. io  .         hist_io import          hist_writer
-from .. io  .run_and_event_io import run_and_event_writer
-from .. core.core_functions   import shift_to_bin_centers
+from .. reco                   import calib_functions         as cf
+from .. reco                   import calib_sensors_functions as csf
+from .. io   .         hist_io import          hist_writer
+from .. io   .run_and_event_io import run_and_event_writer
+from .. core.core_functions    import shift_to_bin_centers
 
 from .. cities.base_cities import CalibratedCity
 from .. cities.base_cities import EventLoop
