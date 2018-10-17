298068fad1e128eccf7116b5a9a50a1ea87a8483
Jacek Generowicz <github@my-post-office.net>
2018-10-17 20:46:21 +0200

    Initial import

---
+++ step1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: step1.sh git_repo_path"
+	exit
+fi
+
+DSTFOLDER=$1
+
+UPSTREAM="git@github.com:jmbenlloch/IC-1"
+LFSOBJECTS="https://cern.ch/jobenllo/lfs_objects.tar.bz2"
+
+git clone $UPSTREAM $DSTFOLDER
+cd $DSTFOLDER/.git/lfs
+
+if which wget; then
+	wget $LFSOBJECTS
+else
+	curl $LFSOBJECTS
+fi
+
+rm -r objects
+tar xvf lfs_objects.tar.bz2
+rm lfs_objects.tar.bz2
---
+++ step4.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: step3.sh repo_folder remote_name branch_name"
+	exit
+fi
+
+IFS=$'\n'
+
+REPO=$1
+REMOTE=$2
+BRANCH=$3
+
+cd $REPO
+MERGEBASE=`git merge-base origin/pre-lfs-master $REMOTE/$BRANCH`
+
+echo MERGEBASE: $MERGEBASE
+
+DATE=`git show -s --format=%ci $MERGEBASE`
+SUBJECT=`git show -s --format=%s $MERGEBASE`
+
+
+for COMMIT in `git rev-list origin/master`; do
+	NEWDATE=`git show -s --format=%ci $COMMIT`
+	
+	if [ $NEWDATE != $DATE ]; then
+		continue
+	fi
+
+	NEWSUBJECT=`git show -s --format=%s $COMMIT`
+	if [ $NEWSUBJECT == $SUBJECT ]; then
+		NEWMERGEBASE=$COMMIT
+		echo $DATE, $SUBJECT, $MERGEBASE
+		echo $NEWDATE, $NEWSUBJECT, $NEWMERGEBASE
+		break
+	fi
+done
+
+
+FOUND=""
+for COMMIT in `git rev-list $REMOTE/$BRANCH`; do
+	PARENT=`git show -s --format=%P $COMMIT`
+	if [ $PARENT == $MERGEBASE ]; then
+		FOUND=1
+		MERGEBASECHILD=$COMMIT
+		break
+	fi
+done
+
+if [ -z "$FOUND" ]; then
+	echo "Did not find cherry pick range"
+	exit
+fi
+
+
+echo NEWMERGEBASE:   $NEWMERGEBASE
+echo MERGEBASECHILD: $MERGEBASECHILD
+
+git checkout -b new-"$BRANCH" $NEWMERGEBASE
+
+BRANCHHASH=`git show -s --format=%H $REMOTE/$BRANCH`
+echo BRANCHHASH: $BRANCHHASH
+
+if [ $MERGEBASECHILD == $BRANCHHASH ]; then
+	git cherry-pick $BRANCHHASH
+else
+	git cherry-pick $MERGEBASECHILD..$REMOTE/$BRANCH
+fi
+
