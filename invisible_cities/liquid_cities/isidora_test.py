70360a81dac95047f9586fd3484a0fc54c68a5b4
Gonzalo <gonzaponte@gmail.com>
2018-10-18 18:01:35 +0200

    Add test checking exact result for Isidora

--- invisible_cities/liquid_cities/isidora_test.py
+++ invisible_cities/liquid_cities/isidora_test.py
@@ -7,10 +7,11 @@ import numpy  as np
 from pytest import mark
 from pytest import fixture
 
-from .  isidora        import isidora
-from .. core           import system_of_units as units
-from .. core.configure import configure
-
+from .  isidora            import isidora
+from .. core               import system_of_units as units
+from .. core.configure     import             all as all_events
+from .. core.configure     import configure
+from .. core.testing_utils import assert_tables_equality
 
 @mark.slow
 def test_isidora_electrons_40keV(config_tmpdir, ICDATADIR):
@@ -41,3 +42,29 @@ def test_isidora_electrons_40keV(config_tmpdir, ICDATADIR):
             evts_in  = h5in .root.Run.events[:nactual].astype([('evt_number', '<i4'), ('timestamp', '<u8')])
             evts_out = h5out.root.Run.events[:nactual]
             np.testing.assert_array_equal(evts_in, evts_out)
+
+
+def test_isidora_exact_result(ICDATADIR, output_tmpdir):
+    file_in     = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts.RWF.h5")
+    file_out    = os.path.join(output_tmpdir,                      "exact_result_isidora.h5")
+    true_output = os.path.join(ICDATADIR    , "Kr83_nexus_v5_03_00_ACTIVE_7bar_3evts.BLR.h5")
+
+    conf = configure("isidora invisible_cities/config/liquid_isidora.conf".split())
+    conf.update(dict(run_number  = -10000,
+                     files_in    = file_in,
+                     file_out    = file_out,
+                     event_range = all_events))
+
+    isidora(**conf)
+
+    tables = ( "MC/extents",  "MC/hits"   , "MC/particles", "MC/generators",
+              "BLR/pmtcwf" , "BLR/sipmrwf",
+              "Run/events" , "Run/runInfo")
+    with tb.open_file(true_output)  as true_output_file:
+        with tb.open_file(file_out) as      output_file:
+            print(true_output_file)
+            print(output_file)
+            for table in tables:
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
