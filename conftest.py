2a93fb7d67bad74cb3edbb74878ae8156c680370
Jacek Generowicz <github@my-post-office.net>
2018-10-20 13:50:06 +0200

    Remove noshrink hypothesis profile
    It relied on `max_shrinks` which has been disabled by the Hypothesis
    authors, and now generates a warning.

--- conftest.py
+++ conftest.py
@@ -8,8 +8,6 @@ settings.register_profile("hard"      , settings(max_examples = 1000))
 settings.register_profile("dev"       , settings(max_examples =   10))
 settings.register_profile("hard_nocov", settings(max_examples = 1000, use_coverage=False))
 settings.register_profile("dev_nocov" , settings(max_examples =   10, use_coverage=False))
-settings.register_profile("noshrink"  , settings(max_examples =   10,
-                                                 max_shrinks  =    0))
 settings.register_profile("debug"     , settings(max_examples =   10,
                                                 verbosity=Verbosity.verbose))
 settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'dev'))
