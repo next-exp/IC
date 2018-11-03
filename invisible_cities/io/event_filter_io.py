d5d0663cddb453e8d209310060d629d12c4c8ef9
Gonzalo <gonzaponte@gmail.com>
2018-11-03 15:21:43 +0100

    Add reader for event filter info

--- invisible_cities/io/event_filter_io.py
+++ invisible_cities/io/event_filter_io.py
@@ -1,3 +1,6 @@
+import tables as tb
+import pandas as pd
+
 from .. evm      import nh5        as table_formats
 from .  table_io import make_table
 
@@ -16,3 +19,15 @@ def event_filter_writer(file, filter_name, *, compression='ZLIB4'):
         table.row.append()
 
     return write_event_passed
+
+
+def event_filter_reader(filename):
+    def read_as_df(table):
+        df = pd.DataFrame.from_records(table.read(), index="event")
+        df.rename(columns=dict(passed = table.name), inplace=True)
+        return df
+
+    with tb.open_file(filename) as file:
+        tables  = map(read_as_df, file.root.Filters)
+        full_df = pd.concat(list(tables), axis=1)
+        return full_df.fillna(False)
