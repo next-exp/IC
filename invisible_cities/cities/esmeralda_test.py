e0bd3bb961c3df7cd10cff9aacfbc7e5b9188f2e
Marija Kekic <mmkekic@gmail.com>
2018-11-28 15:46:46 +0100

    Added test for threshold_hits

--- invisible_cities/cities/esmeralda_test.py
+++ invisible_cities/cities/esmeralda_test.py
@@ -1,7 +1,9 @@
+import os
 from . esmeralda import merge_NN_hits
+from . esmeralda import threshold_hits
 import numpy                     as     np
-from   numpy.testing             import assert_allclose,assert_equal 
-
+from   numpy.testing             import assert_allclose,assert_equal, assert_almost_equal
+from   .. core.configure         import configure
 from   .. evm.event_model        import Hit
 from   .. evm.event_model        import HitCollection
 from   .. evm.event_model        import Cluster
@@ -9,7 +11,8 @@ from   .. types.ic_types         import xy
 from   .. core.system_of_units_c import units
 from   .. types.ic_types         import NN
 from   .. reco                   import dst_functions    as dstf
-
+from    . penthesilea            import penthesilea
+from   .. io                     import hits_io          as hio
 from  pytest                     import fixture
 
 @fixture
@@ -46,4 +49,42 @@ def test_merge_NN_hits(toy_hitc):
     assert passed_same_peak == True
     assert passed == True
     
-
+def test_threshold_hits(config_tmpdir, Kr_pmaps_run4628_filename):
+    PATH_IN =  Kr_pmaps_run4628_filename
+    nrequired = 1
+    conf = configure('dummy invisible_cities/config/penthesilea.conf'.split())
+    PATH_OUT_2 = os.path.join(config_tmpdir, 'KrDST_4628_2th.h5')
+    conf.update(dict(run_number = 4628,
+                     files_in   = PATH_IN,
+                     file_out   = PATH_OUT_2,
+                     event_range = (0, nrequired),
+                     slice_reco_params = dict(
+                         Qthr          = 2 * units.pes,
+                         Qlm           = 0 * units.pes,
+                         lm_radius     = 0 * units.mm ,
+                         new_lm_radius = 0 * units.mm ,
+                         msipm         = 1      )))
+    cnt = penthesilea (**conf)
+    PATH_OUT_10 = os.path.join(config_tmpdir, 'KrDST_4628_10th.h5')
+    conf.update(dict(run_number = 4628,
+                     files_in   = PATH_IN,
+                     file_out   = PATH_OUT_10,
+                     event_range = (0, nrequired),
+                     slice_reco_params = dict(
+                         Qthr          = 10 * units.pes,
+                         Qlm           = 0 * units.pes,
+                         lm_radius     = 0 * units.mm ,
+                         new_lm_radius = 0 * units.mm ,
+                         msipm         = 1      )))
+    cnt = penthesilea (**conf)
+    hits_pent_2  = hio.load_hits(PATH_OUT_2)
+    hits_pent_10 = hio.load_hits(PATH_OUT_10)
+    ev_num = 1
+    hits_thresh  = threshold_hits (hits_pent_2[ev_num], th=10)
+    assert len(hits_pent_10[1].hits)==len(hits_thresh.hits)
+    for h1, h2 in zip(hits_pent_10[1].hits,hits_thresh.hits):
+        assert_almost_equal (h1.E, h2.E, 3)
+        assert_almost_equal (h1.Q, h2.Q, 3)
+        assert h1.Z==h2.Z
+        assert h1.X==h2.X
+        assert h1.Y==h2.Y
