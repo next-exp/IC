16aa866820ba3ae1b6d11b1eea75e8fa226fb9d1
Andrew Laing <a.b.r.laing@gmail.com>
2019-01-31 17:46:35 +0100

    tidying and type hints

--- invisible_cities/reco/pmaps_functions.py
+++ invisible_cities/reco/pmaps_functions.py
@@ -2,10 +2,13 @@ from enum import Enum
 
 import numpy as np
 
-from ..evm.pmaps           import  PMTResponses
-from ..evm.pmaps           import SiPMResponses
+from typing import List
+
+from ..evm.pmaps           import                     _Peak
+from ..evm.pmaps           import              PMTResponses
+from ..evm.pmaps           import             SiPMResponses
 from . peak_functions      import rebin_times_and_waveforms
-from ..core.core_functions import dict_filter_by_key
+from ..core.core_functions import        dict_filter_by_key
 
 
 class RebinMethod(Enum):
@@ -13,26 +16,36 @@ class RebinMethod(Enum):
     threshold = 1
 
 
-def get_even_slices(bins, stride):
+def get_even_slices(bins : int, stride : int) -> List[slice]:
     new_bins = int(np.ceil(bins / stride))
     return [slice(stride * i, stride * (i + 1)) for i in range(new_bins)]
 
 
-def get_theshold_slices(pmt_sum, threshold):
+def get_threshold_slices(pmt_sum   : np.array,
+                         threshold :    float) -> List[slice]:
     slices     = []
     last_index = 0
     last_sum   = 0
+
+    def slice_condition(indx, charge, prev_charge):
+        if i == len(pmt_sum) - 1 or charge - prev_charge >= threshold:
+            return True
+        return False
+
     for i, sum_val in enumerate(np.cumsum(pmt_sum)):
-        if sum_val - last_sum >= threshold or i + 1 == len(pmt_sum):
+        if slice_condition(i, sum_val, last_sum):
             slices.append(slice(last_index, i + 1))
+
             last_index = i + 1
             last_sum   = sum_val
-    if pmt_sum[slices[-1]].sum() < threshold and len(slices) > 1:
+
+    if len(slices) > 1 and pmt_sum[slices[-1]].sum() < threshold:
         slices[-2:] = [slice(slices[-2].start, slices[-1].stop)]
     return slices
 
 
-def rebin_peak(peak, rebin_factor, model=RebinMethod.stride):
+def rebin_peak(peak : _Peak, rebin_factor : int or float,
+               model=RebinMethod.stride) -> _Peak:
     """
     Rebin a peak either using a set stride of
     a set number of existing bins or a charge
@@ -54,8 +67,8 @@ def rebin_peak(peak, rebin_factor, model=RebinMethod.stride):
     The rebinned version of the peak
     """
     if model == RebinMethod.threshold:
-        slices = get_theshold_slices(peak.pmts.sum_over_sensors,
-                                     rebin_factor)
+        slices = get_threshold_slices(peak.pmts.sum_over_sensors,
+                                      rebin_factor)
     else:
         if rebin_factor <= 1: return peak
         slices = get_even_slices(peak.times.shape[0], rebin_factor)
@@ -63,7 +76,7 @@ def rebin_peak(peak, rebin_factor, model=RebinMethod.stride):
     return rebin_peak_to_slices(peak, slices)
 
 
-def rebin_peak_to_slices(peak, slices):
+def rebin_peak_to_slices(peak : _Peak, slices : List[slice]) -> _Peak:
 
     (times,
      widths,
@@ -71,7 +84,7 @@ def rebin_peak_to_slices(peak, slices):
                                           peak.bin_widths,
                                           peak.pmts.all_waveforms,
                                           slices = slices)
-    
+
     pmt_r  = PMTResponses(peak.pmts.ids, pmt_wfs)
 
     sipm_r = SiPMResponses.build_empty_instance()
@@ -80,63 +93,10 @@ def rebin_peak_to_slices(peak, slices):
                                               peak.bin_widths,
                                               peak.sipms.all_waveforms,
                                               slices = slices)
-        
+
         sipm_r = SiPMResponses(peak.sipms.ids, sipms)
 
     return type(peak)(times, widths, pmt_r, sipm_r)
-
-
-## def rebin_peak_threshold(peak, threshold):
-##     """
-##     Peak rebinning function based on a minimum
-##     charge in the pmt sum to form a slice.
-
-##     Parameters
-##     ----------
-##     peak : object
-##         The peak object to be rebinned
-##     threshold : float
-##         Minimum charge (pe) required in the pmt sum
-##         to be counted as a slice.
-
-##     Returns
-##     -------
-##     rebinned_peak : peak object
-##         The peak rebinned according to the threshold.
-##     """
-##     pmt_sum = peak.pmts.sum_over_sensors
-
-##     slices     = []
-##     last_index = 0
-##     last_sum   = 0
-##     for i, sum_val in enumerate(np.cumsum(pmt_sum)):
-##         if sum_val - last_sum >= threshold or i + 1 == len(pmt_sum) - 1:
-##             slices.append(slice(last_index, i + 1))
-##             last_index = i + 1
-##             last_sum   = sum_val
-##     if pmt_sum[slices[-1]].sum() < threshold:
-##         last_slice = [slice(slices[-2].start, slices[-1].stop)]
-##         slices[-2:] = last_slice
-
-##     n_bins = len(slices)
-##     times  = peak.times
-##     pmts   = peak.pmts.all_waveforms
-##     sipms  = peak.sipms.all_waveforms
-##     rebinned_times = np.zeros(                 n_bins )
-##     rebinned_pmts  = np.zeros((pmts.shape[0] , n_bins))
-##     rebinned_sipms = np.zeros((sipms.shape[0], n_bins))
-
-##     for i, s in enumerate(slices):
-##         t = times[   s]
-##         e = pmts [:, s]
-##         q = sipms[:, s]
-##         w = np.sum(e, axis=0) if np.any(e) else None
-##         rebinned_times[   i] = np.average(t, weights=w)
-##         rebinned_pmts [:, i] = np.sum    (e,    axis=1)
-##         rebinned_sipms[:, i] = np.sum    (q,    axis=1)
-##     pmt_r  = PMTResponses(peak.pmts.ids, rebinned_pmts)
-##     sipm_r = SiPMResponses(peak.sipms.ids, rebinned_sipms)
-##     return type(peak)(rebinned_times, pmt_r, sipm_r)
 
 
 def pmap_event_id_selection(data, event_ids):
