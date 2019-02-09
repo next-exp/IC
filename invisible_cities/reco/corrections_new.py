43928a0cff1be6638a1a45a003e8ec3c9a79d211
Marija Kekic <mmkekic@gmail.com>
2019-02-09 21:32:34 +0100

    Replaced functions for applying geometric and lifetime corrections.

--- invisible_cities/reco/corrections_new.py
+++ invisible_cities/reco/corrections_new.py
@@ -18,91 +18,6 @@ class ASectorMap:  # Map in chamber sector containing average of pars
     ltu     : DataFrame
     mapinfo : Series
 
-
-
-def e0_xy_correction(E   : np.array,
-                     X   : np.array,
-                     Y   : np.array,
-                     E0M : DataFrame,
-                     xr  : Tuple[int, int],
-                     yr  : Tuple[int, int],
-                     nx  : int,
-                     ny  : int)->np.array:
-    """
-    Computes the energy vector corrected by geometry in bins of XY.
-    Parameters
-    ----------
-    E
-        The uncorrected energy vector.
-    X
-        Array of X bins.
-    Y
-        Array of Y bins.
-    E0M
-        Map of geometrical corrections (E0 map).
-    xr
-        Range of X (e.g, (-220,220)).
-    yr
-        Range of Y (e.g, (-220,220)).
-    nx
-        Number of bins in X.
-    ny
-        Number of bins in Y.
-    Returns
-    -------
-    np.array
-        The corrected energy vector (by energy).
-    """
-    CE = xy_correction_matrix_(X, Y, E0M, xr, yr, nx, ny)
-    return E / CE
-
-
-def lt_xy_correction(E    : np.array,
-                     X    : np.array,
-                     Y    : np.array,
-                     Z    : np.array,
-                     LTM  : DataFrame,
-                     xr   : Tuple[int, int],
-                     yr   : Tuple[int, int],
-                     nx   : int,
-                     ny   : int)->np.array:
-    """
-    Computes the energy vector corrected by lifetime in bins of XY.
-    Parameters
-    ----------
-    E
-        The uncorrected energy vector.
-    X
-        Array of X bins.
-    Y
-        Array of Y bins.
-    LTM
-        Map of lifetime corrections (LT map).
-    xr
-        Range of X (e.g, (-220,220)).
-    yr
-        Range of Y (e.g, (-220,220)).
-    nx
-        Number of bins in X.
-    ny
-        Number of bins in Y.
-    Returns
-    -------
-    np.array
-        The corrected energy vector (by lifetime).
-    """
-    LT = xy_correction_matrix_(X, Y, LTM, xr, yr, nx, ny)
-    return E * np.exp(Z / LT)
-
-def temporal_correction(E   : np.array,
-                        X   : np.array,
-                        Y   : np.array,
-                        tmp_pars : DataFrame,
-                        xr  : Tuple[int, int],
-                        yr  : Tuple[int, int],
-                        nx  : int,
-                        ny  : int)->np.array:
-    raise NotImplementedError 
 class CorrectionsDF(AutoNameEnumBase):
     e0      = auto()
     lt      = auto()
@@ -128,6 +43,29 @@ def maps_coefficient_getter(maps : ASectorMap, corrections_df : CorrectionsDF) -
         iy = np.digitize(y, binsy)-1
         return np.array([df.get(j, {}).get(i, np.nan) for i, j in zip(iy,ix)])
     return get_maps_coefficient
+
+def correct_geometry_(E : np.array, CE : np.array) -> np.array:
+    return E/CE
+
+def correct_lifetime_(E : np.array, Z : np.array, LT : np.array) -> np.array:
+    return E * np.exp(Z / LT)
+
+def correct_temporal(E : np.array, X : np.array, Y : np.array, **kwargs):
+    raise NotImplementedError
+
+def e0_xy_corrections(E : np.array, X : np.array, Y : np.array, maps : ASectorMap)-> np.array:
+    get_maps_coefficient= maps_coefficient_getter(maps, CorrectionsDF.e0)
+    CE  = get_maps_coefficient(X,Y)
+    return correct_geometry_(E,CE)
+
+def lt_xy_corrections(E : np.array, X : np.array, Y : np.array, Z : np.array, maps : ASectorMap)-> np.array:
+    get_maps_coefficient= maps_coefficient_getter(maps, CorrectionsDF.lt)
+    LT  = get_maps_coefficient(X,Y)
+    return correct_lifetime_(E,Z,LT)
+
+def temporal_corrections(E : np.array, X : np.array, Y : np.array, Z : np.array, maps : ASectorMap)-> np.array:
+    raise NotImplementedError
+
 def apply_all_correction(cmap:ASectorMap, hits : List[Hit]) -> List[Hit]:
     """ This function calls all three - geometric, lifetime and temporal corrections """ 
     raise NotImplementedError 
