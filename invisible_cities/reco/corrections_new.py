e74bab077b0ac02d22bed0fb52275acf36f0221d
Marija Kekic <mmkekic@gmail.com>
2019-02-11 15:51:27 +0100

    Removed CorrectionsDF class; functiones changed accordingly.

--- invisible_cities/reco/corrections_new.py
+++ invisible_cities/reco/corrections_new.py
@@ -18,12 +18,6 @@ class ASectorMap:  # Map in chamber sector containing average of pars
     ltu     : DataFrame
     mapinfo : Series
 
-class CorrectionsDF(AutoNameEnumBase):
-    e0      = auto()
-    lt      = auto()
-    e0u     = auto()
-    ltu     = auto()
-
 def read_maps(filename : str)->ASectorMap:
     chi2     = pd.read_hdf(filename, 'chi2')
     e0       = pd.read_hdf(filename, 'e0')
@@ -33,15 +27,13 @@ def read_maps(filename : str)->ASectorMap:
     mapinfo  = pd.read_hdf(filename, 'mapinfo')
     return  ASectorMap(chi2, e0, lt, e0u, ltu, mapinfo)
 
-def maps_coefficient_getter(maps : ASectorMap, corrections_df : CorrectionsDF) -> Callable:
-    mapinfo = maps.mapinfo
+def maps_coefficient_getter(mapinfo : Series, map_df : DataFrame) -> Callable:
     binsx   = np.linspace(mapinfo.xmin,mapinfo.xmax,mapinfo.nx+1)
     binsy   = np.linspace(mapinfo.ymin,mapinfo.ymax,mapinfo.ny+1)
-    df      = getattr(maps,corrections_df.value)
     def get_maps_coefficient(x : np.array, y : np.array) -> np.array:
         ix = np.digitize(x, binsx)-1
         iy = np.digitize(y, binsy)-1
-        return np.array([df.get(j, {}).get(i, np.nan) for i, j in zip(iy,ix)])
+        return np.array([map_df.get(j, {}).get(i, np.nan) for i, j in zip(iy,ix)])
     return get_maps_coefficient
 
 def correct_geometry_(E : np.array, CE : np.array) -> np.array:
@@ -54,12 +46,16 @@ def correct_temporal(E : np.array, X : np.array, Y : np.array, **kwargs):
     raise NotImplementedError
 
 def e0_xy_corrections(E : np.array, X : np.array, Y : np.array, maps : ASectorMap)-> np.array:
-    get_maps_coefficient= maps_coefficient_getter(maps, CorrectionsDF.e0)
+    mapinfo = maps.mapinfo
+    map_df  = maps.e0
+    get_maps_coefficient= maps_coefficient_getter(mapinfo, map_df)
     CE  = get_maps_coefficient(X,Y)
     return correct_geometry_(E,CE)
 
 def lt_xy_corrections(E : np.array, X : np.array, Y : np.array, Z : np.array, maps : ASectorMap)-> np.array:
-    get_maps_coefficient= maps_coefficient_getter(maps, CorrectionsDF.lt)
+    mapinfo = maps.mapinfo
+    map_df  = maps.lt
+    get_maps_coefficient= maps_coefficient_getter(mapinfo, map_df)
     LT  = get_maps_coefficient(X,Y)
     return correct_lifetime_(E,Z,LT)
 
