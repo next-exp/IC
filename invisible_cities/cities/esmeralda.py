7668386810637666087864389183e0cd91513bf4
Marija Kekic <mmkekic@gmail.com>
2019-03-12 09:42:54 +0100

    Adds make_event_summary method
    Fix bugs in event_summary
    
    Updates event summary method

--- invisible_cities/cities/esmeralda.py
+++ invisible_cities/cities/esmeralda.py
@@ -13,6 +13,8 @@ import os
 import tables as tb
 import numpy  as np
 import pandas as pd
+import warnings
+
 from functools   import partial
 from typing      import Tuple
 from typing      import Callable
@@ -132,15 +134,56 @@ def track_blob_info_extractor(vox_size, energy_type, energy_threshold, min_voxel
     return extract_track_blob_info
 
 
-def final_summary_maker(**kargs)-> Callable:
-    """I am not sure this is a new function or goes under extract_track_blob_info. To be discussed"""
-    return partial(make_final_summary, **locals())
+def make_event_summary(event_number, timestamp, topology_info, paolina_hits, kdst) -> pd.DataFrame:
+    """Compute the quantities to be placed in the final event summary"""
+    es = pd.DataFrame(columns=['event_number', 'timestamp', 'S1e', 'S1t',
+                               'nS2', 'ntrks', 'nhits', 'S2e0', 'S2ec',
+                               'S2q0', 'S2qc', 'x_avg', 'y_avg', 'z_avg',
+                               'r_avg', 'x_min', 'y_min', 'z_min', 'r_min',
+                               'x_max', 'y_max', 'z_max', 'r_max'])
 
+    if(len(kdst.s1_peak.unique()) != 1):
+        warnings.warn("Number of recorded S1 energies differs from 1 in event {}.  Choosing first S1".format(event_number), UserWarning)
 
-def make_final_summary(class_to_store_info_per_track, kdst_info_table,**kargs)-> class_to_store_event_summary:
-    """I am not sure this is a new function or goes under extract_track_blob_info. To be discussed"""
-    raise NotImplementedError
+    S1e = kdst.S1e.values[0]
+    S1t = kdst.S1t.values[0]
 
+    nS2 = kdst.nS2.values[0]
+
+    ntrks = len(topology_info.index)
+    nhits = len(paolina_hits.hits)
+
+    S2e0 = np.sum(kdst.S2e.values)
+    S2ec = sum([h.E for h in paolina_hits.hits])
+
+    S2q0 = np.sum(kdst.S2q.values)
+    S2qc = sum([h.Q for h in paolina_hits.hits])
+
+    x_avg = sum([h.X*h.E for h in paolina_hits.hits])
+    y_avg = sum([h.Y*h.E for h in paolina_hits.hits])
+    z_avg = sum([h.Z*h.E for h in paolina_hits.hits])
+    r_avg = sum([(h.X**2 + h.Y**2)**0.5*h.E for h in paolina_hits.hits])
+    if(S2ec > 0):
+        x_avg /= S2ec
+        y_avg /= S2ec
+        z_avg /= S2ec
+        r_avg /= S2ec
+
+    x_min = min([h.X for h in paolina_hits.hits])
+    y_min = min([h.Y for h in paolina_hits.hits])
+    z_min = min([h.Z for h in paolina_hits.hits])
+    r_min = min([(h.X**2 + h.Y**2)**0.5 for h in paolina_hits.hits])
+
+    x_max = max([h.X for h in paolina_hits.hits])
+    y_max = max([h.Y for h in paolina_hits.hits])
+    z_max = max([h.Z for h in paolina_hits.hits])
+    r_max = max([(h.X**2 + h.Y**2)**0.5 for h in paolina_hits.hits])
+
+    es.loc[0] = [event_number, timestamp, S1e, S1t, nS2, ntrks, nhits, S2e0,
+                S2ec, S2q0, S2qc, x_avg, y_avg, z_avg, r_avg, x_min, y_min,
+                z_min, r_min, x_max, y_max, z_max, r_max]
+    
+    return es
 
 
 def track_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name='Tracks', descriptive_string='Track information',str_col_length=32):
