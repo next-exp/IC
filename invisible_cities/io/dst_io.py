369f639018176460f2d3d89080e083d2a9850ac7
Marija Kekic <mmkekic@gmail.com>
2019-03-13 19:32:22 +0100

    Add function to store pandas dataframe in pytables open_file.

--- invisible_cities/io/dst_io.py
+++ invisible_cities/io/dst_io.py
@@ -1,10 +1,12 @@
 import tables as tb
 import pandas as pd
+import numpy  as np
 from tables import NoSuchNodeError
 from tables import HDF5ExtError
 import warnings
 
-
+from .. reco import tbl_functions as tbl
+from .  table_io           import make_table
 
 def load_dst(filename, group, node):
     """load a kdst if filename, group and node correctly found"""
@@ -24,3 +26,43 @@ def load_dst(filename, group, node):
 def load_dsts(dst_list, group, node):
     dsts = [load_dst(filename, group, node) for filename in dst_list]
     return pd.concat(dsts)
+
+class TableMismatchError(Exception):
+    def __init__(self):
+        s  = 'The table and dataframe dont match! '
+        Exception.__init__(self, s)
+
+def _store_pandas_as_tables(h5out, df, group_name, table_name, compression='ZLIB4', descriptive_string=None, str_col_length=32):
+    if len(df) == 0:
+        warnings.warn(f' dataframe is empty', UserWarning)
+    if '/'+group_name not in h5out:
+        group = h5out.create_group(h5out.root,group_name)
+    else:
+        group = getattr(h5out.root,group_name)
+    if table_name in group:
+        table=getattr(group,table_name)
+    else:
+        tabledef={}
+        for indx, colname in enumerate(df.columns):
+            coltype = df[colname].dtype.name
+            try:
+                tabledef[colname] = tb.Col.from_type(coltype, pos = indx)
+            except ValueError:
+                tabledef[colname] = tb.StringCol(32, pos = indx)
+
+        if descriptive_string is None:
+            descriptive_string = ''
+        table = make_table(h5out,
+                           group       = group_name,
+                           name        = table_name,
+                           fformat     = tabledef,
+                           description = descriptive_string,
+                           compression = compression)
+    if not np.array_equal(df.columns,table.colnames):
+        raise TableMismatchError
+    for indx in df.index:
+        tablerow = table.row
+        for colname in table.colnames:
+            tablerow[colname] = df.at[indx,colname]
+        tablerow.append()
+    table.flush()
