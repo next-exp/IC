b9e7bfd45b66867b19bd14f1efc861d9646b14ba
mmkekic <mmkekic@gmail.com>
2019-04-18 10:55:22 +0200

    Change overlap from flat to float value.
    Change overlap energy name

--- invisible_cities/cities/esmeralda.py
+++ invisible_cities/cities/esmeralda.py
@@ -111,7 +111,7 @@ def track_blob_info_extractor(vox_size : [float, float, float], energy_type : ev
                                    'extreme2_x', 'extreme2_y', 'extreme2_z',
                                    'blob1_x', 'blob1_y', 'blob1_z',
                                    'blob2_x', 'blob2_y', 'blob2_z',
-                                   'eblob1', 'eblob2', 'blob_overlap',
+                                   'eblob1', 'eblob2', 'ovlp_blob_energy',
                                    'vox_size_x', 'vox_size_y', 'vox_size_z'])
         for c, t in enumerate(tracks, 0):
             tID = c
@@ -141,8 +141,7 @@ def track_blob_info_extractor(vox_size : [float, float, float], energy_type : ev
 
             e_blob1, e_blob2, hits_blob1, hits_blob2 = plf.blob_energies_and_hits(t, blob_radius)
             overlap = False
-            if len(set(hits_blob1).intersection(hits_blob2)) > 0:
-                overlap = True
+            overlap = sum([h.Ec for h in set(hits_blob1).intersection(hits_blob2)])
             list_of_vars = [hitc.event, tID, energy, length, numb_of_voxels, numb_of_hits, numb_of_tracks, min_x, min_y, min_z, max_x, max_y, max_z, max_r, ave_pos[0], ave_pos[1], ave_pos[2], extr1_pos[0], extr1_pos[1], extr1_pos[2], extr2_pos[0], extr2_pos[1], extr2_pos[2], blob_pos1[0], blob_pos1[1], blob_pos1[2], blob_pos2[0], blob_pos2[1], blob_pos2[2], e_blob1, e_blob2, overlap, vox_size_x, vox_size_y, vox_size_z]
 
             df.loc[c] = list_of_vars
