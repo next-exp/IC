f020ad59caf15424fe77e39aafdfd4f79c0a6d13
Marija Kekic <mmkekic@gmail.com>
2019-05-30 19:22:54 +0200

    Improves docstrings and alignment.

--- invisible_cities/cities/esmeralda.py
+++ invisible_cities/cities/esmeralda.py
@@ -3,12 +3,17 @@
                               Esmeralda
 -----------------------------------------------------------------------
 From Spanish esmeralda (“emerald”), as first used in the novel Notre-Dame de Paris (1831) by Victor Hugo.
-This city is correcting hits and voxelizing them. The input is penthesilea output containing hits, kdst global information and mc info. The output contains tables:
-- corrected hits
-- summary of topology analysis
-- mc info
-
+This city corrects hits energy and extracts topology information.
+The input is penthesilea output containing hits, kdst global information and mc info.
+The city outputs :
+    - RECO corrected hits table, according to the given map and hits threshold and merging parameters
+    - MC info (if run number <=0)
+    - PAOLINA Summary of topological anlaysis containing:
+        - corrected (paolina) hits, hits table after performing paolina drop_voxels
+        - summary of per track information
+        - summary of per event information
 """
+
 import os
 import tables as tb
 import numpy  as np
@@ -24,14 +29,15 @@ from .. reco                import tbl_functions        as tbl
 from .. reco                import paolina_functions    as plf
 from .. reco                import hits_functions       as hif
 from .. reco                import corrections_new      as cof
-from .. dataflow            import dataflow as fl
+from .. evm                 import event_model          as evm
+from .. dataflow            import dataflow             as fl
 from .. dataflow.dataflow   import push
 from .. dataflow.dataflow   import pipe
 
 from .  components import city
 from .  components import print_every
 from .  components import hits_and_kdst_from_files
-from .. evm  import event_model as evm
+
 from .. types.      ic_types import NN
 from .. types.      ic_types import xy
 from .. io.         hits_io  import          hits_writer
@@ -40,18 +46,49 @@ from .. io.run_and_event_io  import run_and_event_writer
 from .. io. event_filter_io  import  event_filter_writer
 from .. io.          dst_io  import _store_pandas_as_tables
 
+def hits_threshold_and_corrector(map_fname        : str  ,
+                                 threshold_charge : float,
+                                 same_peak        : bool ,
+                                 apply_temp       : bool ,
+                                 norm_strat       : cof.norm_strategy
+                                 ) -> Callable:
+    """
+    For a given correction map and hit threshold/ merging parameters returns a function that applies thresholding, merging and
+    energy and Z corrections to a given HitCollection object.
 
-def hits_threshold_and_corrector(map_fname: str, threshold_charge : float, same_peak : bool, apply_temp : bool, norm_strat : cof.norm_strategy) -> Callable:
-    """Wrapper of correct_hits"""
-    map_fname=os.path.expandvars(map_fname)
-    maps=cof.read_maps(map_fname)
-    get_coef=cof.apply_all_correction(maps, apply_temp = apply_temp, norm_strat = norm_strat)
+    Parameters
+    ----------
+    map_fname        : string (filepath)
+        filename of the map
+    threshold_charge : float
+        minimum pes of a hit
+    same_peak        : bool
+        if True energy of NN hits is assigned only to the hits from the same peak
+    apply_temp       : bool
+        whether to apply temporal corrections
+        must be set to False if no temporal correction dataframe exists in map file
+    norm_strat       :  norm_strategy
+    class norm_strategy(AutoNameEnumBase):
+        mean   = auto()
+        max    = auto()
+        kr     = auto()
+        custom = auto()
+    strategy to normalize the energy
+
+
+    Returns
+    ----------
+    A function that takes HitCollection as input and returns HitCollection that containes
+    only non NN hits of charge above threshold_charge with modified Ec and Z fields.
+    """
+    map_fname = os.path.expandvars(map_fname)
+    maps      = cof.read_maps(map_fname)
+    get_coef  = cof.apply_all_correction(maps, apply_temp = apply_temp, norm_strat = norm_strat)
     if maps.t_evol is not None:
         time_to_Z = cof.get_df_to_z_converter(maps)
     else:
         time_to_Z = lambda x: x
     def threshold_and_correct_hits(hitc : evm.HitCollection) -> evm.HitCollection:
-        """ This function threshold the hits on the charge, redistribute the energy of NN hits to the surrouding ones and applies energy correction."""
         t = hitc.time
         thr_hits = hif.threshold_hits(hitc.hits, threshold_charge     )
         mrg_hits = hif.merge_NN_hits ( thr_hits, same_peak = same_peak)
@@ -65,7 +102,8 @@ def hits_threshold_and_corrector(map_fname: str, threshold_charge : float, same_
         Zc = time_to_Z(Z)
         cor_hits = []
         for idx, hit in enumerate(mrg_hits):
-            hit = evm.Hit(hit.npeak, evm.Cluster(hit.Q, xy(hit.X, hit.Y), hit.var, hit.nsipm), Zc[idx], hit.E, xy(hit.Xpeak, hit.Ypeak), s2_energy_c = Ec[idx])
+            hit = evm.Hit(hit.npeak, evm.Cluster(hit.Q, xy(hit.X, hit.Y), hit.var, hit.nsipm),
+                          Zc[idx], hit.E, xy(hit.Xpeak, hit.Ypeak), s2_energy_c = Ec[idx])
             cor_hits.append(hit)
         new_hitc      = evm.HitCollection(hitc.event, t)
         new_hitc.hits = cor_hits
@@ -74,6 +112,11 @@ def hits_threshold_and_corrector(map_fname: str, threshold_charge : float, same_
 
 
 def events_filter(allow_nans : bool) -> Callable:
+    """
+    Filter for Esmeralda flow. The flow stops if:
+        1. there are no hits
+        2. there are hits with NaN energy and allow_nans is set to False
+    """
     def filter_events(hitc : Optional[evm.HitCollection]) -> bool:
         if hitc == None:
             return False
@@ -85,10 +128,42 @@ def events_filter(allow_nans : bool) -> Callable:
     return filter_events
 
 
-def track_blob_info_creator_extractor(vox_size : [float, float, float], energy_type : evm.HitEnergy, strict_vox_size : bool, energy_threshold : float, min_voxels : int, blob_radius : float) -> Callable:
-    """ Wrapper of extract_track_blob_info"""
+def track_blob_info_creator_extractor(vox_size         : [float, float, float],
+                                      energy_type      : evm.HitEnergy        ,
+                                      strict_vox_size  : bool                 ,
+                                      energy_threshold : float                ,
+                                      min_voxels       : int                  ,
+                                      blob_radius      : float
+                                     ) -> Callable:
+    """
+    For a given paolina parameters returns a function that extract tracks / blob information from a HitCollection.
+
+    Parameters
+    ----------
+    vox_size         : [float, float, float]
+        (maximum) size of voxels for track reconstruction
+    energy_type      : HitEnergy
+        class HitEnergy(AutoNameEnumBase):
+            E        = auto()
+            Ec       = auto()
+        energy attribute to use for voxelization/ tracks
+
+    strict_vox_size  : bool
+        if False allows per event adaptive voxel size,
+        smaller of equal thatn vox_size
+    energy_threshold : float
+        if energy of end-point voxel is smaller
+        the voxel will be dropped and energy redistributed to the neighbours
+    min_voxels       : int
+        after min_voxel number of voxels is reached no dropping will happen.
+    blob_radius      : float
+        radius of blob
+
+    Returns
+    ----------
+    A function that from a given HitCollection returns a pandas DataFrame with per track information.
+    """
     def create_extract_track_blob_info(hitc):
-        """This function extract relevant info about the tracks and blobs, as well as assigning new field of energy, track_id etc to the HitCollection object (NOTE: we don't want to erase any hits, just redifine some attributes. If we need to cut away some hits to apply paolina functions, it has to be on the copy of the original hits)"""
         voxels     = plf.voxelize_hits(hitc.hits, vox_size, strict_vox_size, energy_type)
         mod_voxels = plf.drop_end_point_voxels(voxels, energy_threshold, min_voxels)
         tracks     = plf.make_track_graphs(mod_voxels)
@@ -97,8 +172,10 @@ def track_blob_info_creator_extractor(vox_size : [float, float, float], energy_t
         vox_size_y = voxels[0].size[1]
         vox_size_z = voxels[0].size[2]
 
+        def get_track_energy(track):
+            return sum([vox.Ehits for vox in track.nodes()])
         #sort tracks in energy
-        tracks     = sorted(tracks, key = lambda t: sum([vox.Ehits for vox in t.nodes()]), reverse = True)
+        tracks     = sorted(tracks, key = get_track_energy, reverse = True)
 
         track_hits = []
         df = pd.DataFrame(columns=['event', 'trackID', 'energy', 'length', 'numb_of_voxels',
@@ -112,7 +189,7 @@ def track_blob_info_creator_extractor(vox_size : [float, float, float], energy_t
                                    'vox_size_x', 'vox_size_y', 'vox_size_z'])
         for c, t in enumerate(tracks, 0):
             tID = c
-            energy = sum([vox.Ehits for vox in t.nodes()])
+            energy = get_track_energy(t)
             length = plf.length(t)
             numb_of_hits = len([h for vox in t.nodes() for h in vox.hits])
             numb_of_voxels = len(t.nodes())
@@ -139,7 +216,16 @@ def track_blob_info_creator_extractor(vox_size : [float, float, float], energy_t
             e_blob1, e_blob2, hits_blob1, hits_blob2 = plf.blob_energies_and_hits(t, blob_radius)
             overlap = False
             overlap = sum([h.Ec for h in set(hits_blob1).intersection(hits_blob2)])
-            list_of_vars = [hitc.event, tID, energy, length, numb_of_voxels, numb_of_hits, numb_of_tracks, min_x, min_y, min_z, max_x, max_y, max_z, max_r, ave_pos[0], ave_pos[1], ave_pos[2], extr1_pos[0], extr1_pos[1], extr1_pos[2], extr2_pos[0], extr2_pos[1], extr2_pos[2], blob_pos1[0], blob_pos1[1], blob_pos1[2], blob_pos2[0], blob_pos2[1], blob_pos2[2], e_blob1, e_blob2, overlap, vox_size_x, vox_size_y, vox_size_z]
+            list_of_vars = [hitc.event, tID, energy, length, numb_of_voxels,
+                            numb_of_hits, numb_of_tracks,
+                            min_x, min_y, min_z, max_x, max_y, max_z, max_r,
+                            ave_pos[0], ave_pos[1], ave_pos[2],
+                            extr1_pos[0], extr1_pos[1], extr1_pos[2],
+                            extr2_pos[0], extr2_pos[1], extr2_pos[2],
+                            blob_pos1[0], blob_pos1[1], blob_pos1[2],
+                            blob_pos2[0], blob_pos2[1], blob_pos2[2],
+                            e_blob1, e_blob2, overlap,
+                            vox_size_x, vox_size_y, vox_size_z]
 
             df.loc[c] = list_of_vars
             try:
@@ -163,8 +249,34 @@ def track_blob_info_creator_extractor(vox_size : [float, float, float], energy_t
     return create_extract_track_blob_info
 
 
-def make_event_summary(event_number : int, timestamp : int, topology_info : pd.DataFrame, paolina_hits : evm.HitCollection, kdst : pd.DataFrame) -> pd.DataFrame:
-    """Compute the quantities to be placed in the final event summary"""
+def make_event_summary(event_number  : int              ,
+                       timestamp     : int              ,
+                       topology_info : pd.DataFrame     ,
+                       paolina_hits  : evm.HitCollection,
+                       kdst          : pd.DataFrame
+                       ) -> pd.DataFrame:
+    """
+    For a given event number, timestamp, topology info dataframe, paolina hits and kdst information returns a 
+    dataframe with the whole event summary.
+
+    Parameters
+    ----------
+    event_number  : int
+    timestamp     : long int
+    topology_info : DataFrame
+        Dataframe containing track information,
+        output of track_blob_info_creator_extractor
+    paolina_hits  : HitCollection
+        Hits table passed through paolina functions,
+        output of track_blob_info_creator_extractor
+    kdst          : DataFrame
+        Kdst information read from penthesilea output
+
+
+    Returns
+    ----------
+    DataFrame containing relevant per event information.
+    """
     es = pd.DataFrame(columns=['event', 'time', 'S1e', 'S1t',
                                'nS2', 'ntrks', 'nhits', 'S2e0', 'S2ec',
                                'S2q0', 'S2qc', 'x_avg', 'y_avg', 'z_avg',
@@ -172,7 +284,7 @@ def make_event_summary(event_number : int, timestamp : int, topology_info : pd.D
                                'x_max', 'y_max', 'z_max', 'r_max'])
 
     if(len(kdst.s1_peak.unique()) != 1):
-        warnings.warn("Number of recorded S1 energies differs from 1 in event {}.  Choosing first S1".format(event_number), UserWarning)
+        warnings.warn("Number of recorded S1 energies differs from 1 in event {}.Choosing first S1".format(event_number), UserWarning)
 
     S1e = kdst.S1e.values[0]
     S1t = kdst.S1t.values[0]
@@ -220,15 +332,21 @@ def make_event_summary(event_number : int, timestamp : int, topology_info : pd.D
     return es
 
 
-def track_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name='Tracks', descriptive_string='Track information',str_col_length=32):
+def track_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name='Tracks', descriptive_string='Track information', str_col_length=32):
+    """
+    For a given open table returns a writer for topology info dataframe
+    """
     def write_tracks(df):
-        return _store_pandas_as_tables(h5out=h5out, df=df,compression = compression, group_name=group_name, table_name=table_name, descriptive_string=descriptive_string, str_col_length=str_col_length)
+        return _store_pandas_as_tables(h5out = h5out, df = df, compression = compression, group_name = group_name, table_name = table_name, descriptive_string = descriptive_string, str_col_length = str_col_length)
     return write_tracks
 
 
-def summary_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name='Summary', descriptive_string='Event summary information',str_col_length=32):
+def summary_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name='Summary', descriptive_string='Event summary information', str_col_length=32):
+    """
+    For a given open table returns a writer for summary info dataframe
+    """
     def write_summary(df):
-        return _store_pandas_as_tables(h5out=h5out, df=df,compression = compression, group_name=group_name, table_name=table_name, descriptive_string=descriptive_string, str_col_length=str_col_length)
+        return _store_pandas_as_tables(h5out = h5out, df = df, compression = compression, group_name = group_name, table_name = table_name, descriptive_string = descriptive_string, str_col_length = str_col_length)
     return write_summary
 
 
@@ -236,10 +354,70 @@ def summary_writer(h5out, compression='ZLIB4', group_name='PAOLINA', table_name=
 def esmeralda(files_in, file_out, compression, event_range, print_mod, run_number,
               cor_hits_params = dict(),
               paolina_params  = dict()):
-    # cor_hits_params_NN   are map_fname, threshold_charge, same_peak, apply_temp, to_kr_scale
-    # cor_hits_params_PL   are map_fname, threshold_charge, same_peak, apply_temp, to_kr_scale
-    # paolina_params       are vox_size,  energy_type, energy_threshold, min_voxels, blob_radius, z_factor
-    # energy_type parameter in paolina_params has to be translated to enum class
+    """
+    The city corrects Penthesilea hits energy and extracts topology information.
+    ----------
+    Parameters
+    ----------
+    files_in  : str, filepath
+         input file
+    file_out  : str, filepath
+         output file
+    compression : str
+         Default  'ZLIB4'
+    event_range : int /'all_events'
+         number of events from files_in to process
+    print_mode : int
+         how frequently to print events
+    run_number : int
+         has to be negative for MC runs
+
+    cor_hits_params              : dict
+        map_fname                : string (filepath)
+            filename of the map
+        threshold_charge_NN      : float
+            minimum pes for a RECO hit
+        threshold_charge_paolina : float
+            minimum pes for a PAOLINA hit
+        same_peak                : bool
+            if True energy of NN hits is assigned only to the hits from the same peak
+        apply_temp               : bool
+            whether to apply temporal corrections
+            must be set to False if no temporal correction dataframe exists in map file
+        norm_strat               : 'max', 'mean' or 'kr'
+           strategy to normalize the energy
+
+    paolina_params               :dict
+        vox_size                 : [float, float, float]
+            (maximum) size of voxels for track reconstruction
+        energy_type              : 'corrected' ot 'uncorrected'
+            energy attribute to use for voxelization/ tracks
+        strict_vox_size          : bool
+            if False allows per event adaptive voxel size,
+            smaller of equal thatn vox_size
+        energy_threshold        : float
+            if energy of end-point voxel is smaller
+            the voxel will be dropped and energy redistributed to the neighbours
+        min_voxels              : int
+            after min_voxel number of voxels is reached no dropping will happen.
+        blob_radius             : float
+            radius of blob
+    ----------
+    Input
+    ----------
+    Penthesilea output
+    ----------
+    Output
+    ----------
+    RECO    : corrected hits table, according to the given map and hits threshold and merging parameters
+    MC info : (if run number <=0)
+    PAOLINA : Summary of topological anlaysis containing:
+        Events  : corrected (paolina) hits, hits table after performing paolina drop_voxels
+        Tracks  : summary  of per track topological information
+        Summary : summary of per event information
+
+"""
+
     if   paolina_params ['energy_type'] == 'corrected'   : energy_type = evm.HitEnergy.Ec
     elif paolina_params ['energy_type'] == 'uncorrected' : energy_type = evm.HitEnergy.E
     else                                                 : raise ValueError(f"Unrecognized processing mode: {paolina_params['energy_type']}")
