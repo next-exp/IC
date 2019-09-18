9f469324d8f460b1a6734e673525591646689b51
Ander <aretno@gmail.com>
2019-09-18 12:53:16 +0200

    Adds PSF writer

--- invisible_cities/io/kdst_io.py
+++ invisible_cities/io/kdst_io.py
@@ -1,6 +1,7 @@
 from .  table_io import make_table
 from .. evm.nh5  import KrTable
 from .. evm.nh5  import XYfactors
+from .. evm.nh5  import PSFfactors
 
 
 def kr_writer(hdf5_file, *, compression='ZLIB4'):
@@ -57,3 +58,26 @@ def xy_lifetime_writer(hdf5_file, * ,
                      description  = "XY-dependent lifetime values",
                      compression  = compression)
 
+def psf_writer(hdf5_file, **kwargs):
+    psf_table = make_table(hdf5_file,
+                           group       = "PSF",
+                           name        = "PSFs",
+                           fformat     = PSFfactors,
+                           description = "XYZ dependent point spread functions",
+                           compression = 'ZLIB4')
+
+    def write_psf(xr, yr, zr, xp, yp, zp, fr, nr):
+        row = psf_table.row
+        for i, x in enumerate(xr):
+            for j, y in enumerate(yr):
+                for k, z in enumerate(zr):
+                    row["xr"    ] = x
+                    row["yr"    ] = y
+                    row["zr"    ] = z
+                    row["x"     ] = xp
+                    row["y"     ] = yp
+                    row["z"     ] = zp
+                    row["factor"] = fr[i,j,k]
+                    row["nevt"  ] = nr[i,j,k]
+                    row.append()
+    return write_psf
