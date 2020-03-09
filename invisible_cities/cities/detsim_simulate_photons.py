9da4e94727934f6f8e78c663315c13bbbcd4b977
gondiaz <gonzalodiazlopez10@gmail.com>
2020-03-09 16:52:32 +0100

    Cosmetics
    Change typing np.array->np.ndarray

--- invisible_cities/cities/detsim_simulate_photons.py
+++ invisible_cities/cities/detsim_simulate_photons.py
@@ -6,15 +6,15 @@ from typing import Callable
 #######################################
 ########## PHOTON SIMULATION ##########
 #######################################
-def generate_s1_photons(energies : np.array,
-                        ws       : float) -> np.array:
+def generate_s1_photons(energies : np.ndarray,
+                        ws       : float) -> np.ndarray:
     """generate s1 photons"""
     return np.random.poisson(energies / ws)
 
 
-def generate_s2_photons(x              : np.array,
+def generate_s2_photons(x              : np.ndarray,
                         el_gain        : float,
-                        el_gain_sigma  : float) -> np.array:
+                        el_gain_sigma  : float) -> np.ndarray:
     """generate number of EL-photons produced by secondary electrons that reach
     the EL (after drift and diffusion)
     """
@@ -23,14 +23,14 @@ def generate_s2_photons(x              : np.array,
     return nphs
 
 
-def pes_at_sensors(xs         : np.array,
-                   ys         : np.array,
-                   zs         : np.array,
-                   photons    : np.array,
-                   x_sensors  : np.array,
-                   y_sensors  : np.array,
+def pes_at_sensors(xs         : np.ndarray,
+                   ys         : np.ndarray,
+                   zs         : np.ndarray,
+                   photons    : np.ndarray,
+                   x_sensors  : np.ndarray,
+                   y_sensors  : np.ndarray,
                    z_sensors  : float   ,
-                   psf : Callable) -> np.array:
+                   psf : Callable) -> np.ndarray:
     """compute the pes that reach each sensor, based on
     the sensor psf"""
 
