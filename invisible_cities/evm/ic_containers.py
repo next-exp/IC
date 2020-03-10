21ac43da8e46c1054452e9049313ae0e00f7da2d
Gonzalo <gonzaponte@gmail.com>
2020-03-10 11:52:01 +0200

    Remove deprecated namedtuples

--- invisible_cities/evm/ic_containers.py
+++ invisible_cities/evm/ic_containers.py
@@ -20,28 +20,13 @@ def _add_namedtuple_in_this_module(name, attribute_names):
     setattr(this_module, name, new_nametuple)
 
 for name, attrs in (
-        ('DataVectors'    , 'pmt sipm mc events trg_type trg_channels'),
         ('SensorData'     , 'NPMT PMTWL NSIPM SIPMWL '),
-        ('PmapVectors'    , 'pmaps events timestamps mc'),
-        ('RawVectors'     , 'event pmtrwf sipmrwf pmt_active sipm_active'),
-        ('CalibParams'    , 'coeff_c coeff_blr adc_to_pes_pmt adc_to_pes_sipm'),
         ('DeconvParams'   , 'n_baseline thr_trigger'),
         ('CalibVectors'   , 'channel_id coeff_blr coeff_c adc_to_pes adc_to_pes_sipm pmt_active'),
         ('S12Params'      , 'time stride length rebin_stride'),
-        ('PmapParams'     , 's1_params s2_params s1p_params s1_PMT_params s1p_PMT_params'),
-        ('ThresholdParams', 'thr_s1 thr_s2 thr_MAU thr_sipm thr_SIPM'),
-        ('CSum'           , 'csum csum_mau'),
-        ('CCWf'           , 'ccwf ccwf_mau'),
-        ('S12Sum'         , 's1_ene s1_indx s2_ene s2_indx'),
         ('ZsWf'           , 'indices energies'),
-        ('CalibratedPMT'  , 'CPMT CPMT_mau'),
-        ('S1PMaps'        , 'S1 S1_PMT S1p S1p_PMT'),
-        ('PMaps'          , 'S1 S2 S2Si'),
-        ('Peak'           , 't E'),
         ('FitFunction'    , 'fn values errors chi2 pvalue cov'),
         ('TriggerParams'  , 'trigger_channels min_number_channels charge height width'),
-        ('PeakData'       , 'charge height width'),
-        ('Measurement'    , 'value uncertainty'),
         ('SensorParams'   , 'spectra peak_range min_bin_peak max_bin_peak half_peak_width p1pe_seed lim_ped'),
         ('PedestalParams' , 'gain gain_min gain_max sigma sigma_min sigma_max')):
     _add_namedtuple_in_this_module(name, attrs)
