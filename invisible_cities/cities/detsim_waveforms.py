07366f5a49665a44e595e759bf841a2ac3af3194
gondiaz <gonzalodiazlopez10@gmail.com>
2020-03-11 20:18:20 +0100

    Add docstrings

--- invisible_cities/cities/detsim_waveforms.py
+++ invisible_cities/cities/detsim_waveforms.py
@@ -3,10 +3,27 @@ import numpy as np
 ##################################
 ######### WAVEFORMS ##############
 ##################################
-def create_waveform(times : np.array,
-                    pes   : np.array,
-                    bins  : np.array,
-                    nsamples    : int) -> np.array:
+def create_waveform(times : np.ndarray,
+                    pes   : np.ndarray,
+                    bins  : np.ndarray,
+                    nsamples : int) -> np.ndarray:
+    """
+    This function builds a waveform from a set of (buffer_time, pes) values.
+    This set is of values come from the times and pes arguments.
+
+    Input
+    times: a vector with the buffer times in which a photoelectron is produced in the
+    detector.
+    pes (from photoelecton): a vector with the photoelectrons produced in the detector in
+    each of the buffer times in times argument.
+    bins: a vector with the output waveform bin times (for example [0, 25, 50, ...] if
+    the detector has a sampling time of 25).
+    nsamples: an integer that controlls the distribution of the photoelectrons in each of
+    the waveform bins. The counts (N) in a given time bin (T) are distributed in the waveform
+    such as the nsamples posterior to T would have N/nsamples counts (included T).
+    """
+
+
     wf = np.zeros(len(bins))
 
     if np.sum(pes)==0:
@@ -33,12 +50,24 @@ def create_waveform(times : np.array,
     return wf
 
 
-def create_sensor_waveforms(times   : np.array,
-                            pes_at_sensors : np.array,
+def create_sensor_waveforms(times   : np.ndarray,
+                            pes_at_sensors : np.ndarray,
                             wf_buffer_time : float,
                             bin_width    : float,
                             nsamples : int,
-                            poisson  : bool =False) -> np.array:
+                            poisson  : bool =False) -> np.ndarray:
+    """
+    This function calls recursively to create_waveform. See create_waveform for
+    an explanation of the arguments not explained below.
+
+    Input:
+    pes_at_sensors: an array with size (#sensors, len(times)). It is the same
+    as pes argument in create_waveform but for each sensor in axis 0.
+    wf_buffer_time: a float with the waveform extent (in default IC units)
+    bin_width: a float with the time distance between bins in the waveform buffer.
+    poisson: a bool. This must be set to True to poisson distribute each bin count.
+    """
+
     bins = np.arange(0, wf_buffer_time, bin_width)
     wfs = np.array([create_waveform(times, pes, bins, nsamples) for pes in pes_at_sensors])
 
