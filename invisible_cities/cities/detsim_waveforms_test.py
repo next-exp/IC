7ab64adce96fb6a649df57446c3d50fe6cf06645
gondiaz <gonzalodiazlopez10@gmail.com>
2020-03-14 18:38:29 +0100

    Add test for Exception, Warning and Sum conservation.
    create_waveform contains now an Exception and a Warning that must be
    tested. Besides, the test cointains an assertion for Sum conservation:
    that the number of initial pes is equal to the number of final counts,
    if times values are restricted to the binning. Note this is not true
    if times values are outside binning edges.

--- invisible_cities/cities/detsim_waveforms_test.py
+++ invisible_cities/cities/detsim_waveforms_test.py
@@ -1,13 +1,19 @@
+import pytest
+from hypothesis import given
+from hypothesis.strategies import composite
+from hypothesis.strategies import floats
+from hypothesis.strategies import integers
+from hypothesis.strategies import lists
+
 import numpy as np
 
 from invisible_cities.cities.detsim_waveforms import create_waveform
 
 
-
 def test_create_waveform():
     #data
-    wf_bin_time = 1.
-    bins = np.arange(0., 10. + wf_bin_time, wf_bin_time)
+    bin_width = 1.
+    bins = np.arange(0., 10. + bin_width, bin_width)
     nsamples = 2
     times = np.array([1, 2, 5, 6, 9, 10])
     pes   = np.array([2, 2, 2, 4, 1,  3])
@@ -15,6 +21,28 @@ def test_create_waveform():
     expected = np.array([0, 1, 2, 1, 0, 1, 3, 2, 0, 2, 2], dtype=float)
 
     # test
-    waveform = create_waveform(times, pes, bins, wf_bin_time, nsamples)
+    waveform = create_waveform(times, pes, bins, nsamples)
 
-    assert np.all(waveform == expected)
+    assert np.allclose(waveform, expected)
+
+
+@composite
+def times_and_pes_from_bins(draw, bins):
+    min, max = bins[0], bins[-1]
+
+    times = draw(lists(integers(min_value=min, max_value=max), min_size=1))
+    pes   = draw(lists(integers(min_value=0), min_size=len(times), max_size=len(times)))
+    return times, pes, bins
+
+@given(times_and_pes_from_bins(bins=np.arange(0, 10)),
+       integers())
+def test_create_waveform_Exception_Warning_Sum(times_and_pes, nsamples):
+    times, pes, bins = times_and_pes
+
+    if nsamples<1:
+        pytest.raises(Exception,  create_waveform, times, pes, bins, nsamples)
+    elif nsamples>len(bins):
+        pytest.warns(UserWarning, create_waveform, times, pes, bins, nsamples)
+    else:
+        waveform = create_waveform(times, pes, bins, nsamples)
+        assert np.allclose(np.sum(pes), np.sum(waveform))
