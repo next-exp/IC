50a9c8bbb9e79b7a3746984764b50e2d7e0369ca
gondiaz <gonzalodiazlopez10@gmail.com>
2020-03-23 19:33:19 +0100

    Update test to account for new create_xyz_function

--- invisible_cities/cities/detsim_get_psf_test.py
+++ invisible_cities/cities/detsim_get_psf_test.py
@@ -5,7 +5,11 @@ import pandas as pd
 
 from pytest import fixture
 
-from invisible_cities.cities.detsim_get_psf import *
+from invisible_cities.reco.corrections_new import read_maps
+
+from invisible_cities.cities.detsim_get_psf import create_xyz_function
+from invisible_cities.cities.detsim_get_psf import get_psf_from_krmap
+from invisible_cities.cities.detsim_get_psf import get_sipm_psf_from_file
 
 ####################################
 ######### CREATE XY FUNC ###########
@@ -13,36 +17,32 @@ from invisible_cities.cities.detsim_get_psf import *
 @fixture
 def extent_and_function():
     xmin, xmax, dx = 12, 100, 3.
-    ymin, ymax, dy =-50, 50, 13.
+    ymin, ymax, dy = 10, 20, 1.
+    zmin, zmax, dz =-50, 50, 13.
 
-    xaxis = (xmin, xmax, dx)
-    yaxis = (ymin, ymax, dy)
+    xbins = np.arange(xmin, xmax+dz, dx)
+    ybins = np.arange(ymin, ymax+dz, dy)
+    zbins = np.arange(zmin, zmax+dz, dz)
+    bins = [xbins, ybins, zbins]
 
-    func = lambda x, y: - x**2
+    func = lambda x, y, z: - x**2
 
-    return xaxis, yaxis, func
+    return bins, func
 
 
-def test_create_xy_function(extent_and_function):
+def test_create_xyz_function(extent_and_function):
 
-    xaxis, yaxis, func = extent_and_function
+    bins, func = extent_and_function
 
-    xmin, xmax, dx = xaxis
-    ymin, ymax, dy = yaxis
-
-    xbins = np.arange(xmin, xmax+dx, dx)
-    ybins = np.arange(ymin, ymax+dy, dy)
-
-    x, y = np.meshgrid(xbins, ybins)
-    x, y = x.flatten(), y.flatten()
-    f = func(x, y)
+    x, y, z = np.meshgrid(bins[0][:-1], bins[1][:-1], bins[2][:-1]) #remove bin extreme
+    x, y, z = x.flatten(), y.flatten(), z.flatten()
+    f = func(x, y, z)
 
     #test create_function
-    H, _ = np.histogramdd((x, y), weights=f, bins=(xbins, ybins))
-    callable = create_xy_function(H, xaxis, yaxis)
+    H, _ = np.histogramdd((x, y, z), weights=f, bins=bins)
+    callable = create_xyz_function(H, bins)
 
-    noedges = (x<xbins[-2]) & (y<ybins[-2])
-    assert (f[noedges] == callable(x[noedges], y[noedges])).all()
+    assert np.all(f == callable(x, y, z))
 
 
 ####################################
@@ -61,10 +61,11 @@ def test_xyaxis_get_psf_from_krmap(correction_map_filename):
     #select a random slice in, for example, x
     f = np.random.randint(nx)
     xslice = e0map[nx//f, :]
-    x = xmin + nx//f * dx
+    x = np.full(len(xslice) ,xmin + nx//f * dx)
     y = ymin + np.arange(0, ny) * dy
+    z = np.zeros(len(x))
 
-    assert (psf(x, y) == xslice).all()
+    assert np.all(psf(x, y, z) == xslice)
 
 
 ####################################
@@ -85,4 +86,4 @@ def test_get_sipm_psf_from_file(sipmpsf_filename):
     psf = psf[sel]
     xr, yr, factor = psf["xr"], psf["yr"], psf["factor"]
 
-    assert (psffunc(xr, yr) == factor).all()
+    assert np.all(psffunc(xr, yr, np.zeros(len(xr))) == factor)
