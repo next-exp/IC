dfb0e5d4fdfde12ea09a54749120274238e3a8ea
gondiaz <gonzalodiazlopez10@gmail.com>
2020-03-23 17:03:21 +0100

    Remove redundancy
    It is not necessary to have create_xy_function and
    create_xyz_function. The xyz function is enough. Functions that
    use xy, will be modified to include a thrid z dummy dimension.

--- invisible_cities/cities/detsim_get_psf.py
+++ invisible_cities/cities/detsim_get_psf.py
@@ -6,6 +6,49 @@ from typing import Tuple, Callable
 
 from invisible_cities.reco.corrections_new import read_maps
 
+from invisible_cities.core.core_functions  import in_range
+
+
+###################################
+############# UTILS ###############
+###################################
+def create_xyz_function(H, bins):
+    """Given a 3D array and a list of bins for
+    each dim, it returns a x,y,z function"""
+
+    xbins, ybins, zbins = bins
+    if not H.shape == (len(xbins)-1, len(ybins)-1, len(zbins)-1):
+        raise Exception("bins and array shapes not consistent")
+
+    def function(x, y, z):
+        if not len(x)==len(y)==len(z):
+            raise Exception("x, y and z must have same size")
+
+        out = np.full(len(x), np.nan)
+        #select values inside bin extremes
+        selx = in_range(x, xbins[0], xbins[-1])
+        sely = in_range(y, ybins[0], ybins[-1])
+        selz = in_range(z, zbins[0], zbins[-1])
+        sel = selx & sely & selz
+
+        ix = np.digitize(x[sel], xbins)-1
+        iy = np.digitize(y[sel], ybins)-1
+        iz = np.digitize(z[sel], zbins)-1
+
+        out[sel] = H[ix, iy, iz]
+        return out
+    return function
+
+
+def binedges_from_bincenters(bincenters):
+    ds = np.diff(bincenters)
+    if ~np.all(ds == ds[0]):
+        raise Exception("Bin distances must be equal")
+
+    d = ds[0]
+    return np.arange(bincenters[0]-d/2., bincenters[-1]+d/2.+d, d)
+
+
 ##################################
 ############# PSF ################
 ##################################
@@ -13,20 +56,6 @@ def _psf(dx, dy, dz, factor = 1.):
     """ generic analytic PSF function
     """
     return factor * np.abs(dz) / (2 * np.pi) / (dx**2 + dy**2 + dz**2)**1.5
-
-
-def create_xy_function(array : np.array,
-                       xaxis : Tuple[float],
-                       yaxis : Tuple[float])->Callable:
-    xmin, xmax, dx = xaxis
-    ymin, ymax, dy = yaxis
-    def function(x, y, z=0):
-        x = np.clip(x, xmin, xmax-0.1*dx)
-        y = np.clip(y, ymin, ymax-0.1*dy)
-        ix = ((x-xmin)//dx).astype(int)
-        iy = ((y-ymin)//dy).astype(int)
-        return array[ix, iy]
-    return function
 
 
 def get_psf_from_krmap(filename : str,
@@ -68,30 +97,6 @@ def get_sipm_psf_from_file(filename : str)->Callable:
 ##################################
 ######### LIGTH TABLE ############
 ##################################
-def binedges_from_bincenters(bincenters):
-    ds = np.diff(bincenters)
-    if ~np.all(ds == ds[0]):
-        raise Exception("Bin distances must be equal")
-
-    d = ds[0]
-    return np.arange(bincenters[0]-d/2., bincenters[-1]+d/2.+d, d)
-
-
-def create_xyz_function(array, xaxis, yaxis, zaxis):
-    xmin, xmax, dx = xaxis
-    ymin, ymax, dy = yaxis
-    zmin, zmax, dz = zaxis
-    def function(x, y, z):
-        x = np.clip(x, xmin, xmax-0.1*dx)
-        y = np.clip(y, ymin, ymax-0.1*dx)
-        z = np.clip(z, zmin, zmax-0.1*dz)
-        ix = ((x-xmin)//dx).astype(int)
-        iy = ((y-ymin)//dy).astype(int)
-        iz = ((z-zmin)//dz).astype(int)
-        return array[ix, iy, iz]
-    return function
-
-
 def get_ligthtables(filename: str)->Callable:
     ##### Load LT ######
     with tb.open_file(filename) as h5file:
