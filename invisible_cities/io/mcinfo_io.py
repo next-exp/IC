776a148b4b0a4534e36b217d9408f3d76b366558
Andrew Laing <a.b.r.laing@gmail.com>
2020-05-12 21:26:43 +0200

    Change Pmt binning name for old mcconfiguration

--- invisible_cities/io/mcinfo_io.py
+++ invisible_cities/io/mcinfo_io.py
@@ -313,7 +313,7 @@ def load_mcconfiguration(file_name : str) -> pd.DataFrame:
     config           = load_dst(file_name, 'MC', 'configuration')
     if is_oldformat_file(file_name):
         config.param_key = config.param_key.str.replace('.*Pmt.*\_binning.*' ,
-                                                        'Pmt_binning'        )
+                                                        'PmtR11410_binning'  )
         config.param_key = config.param_key.str.replace('.*SiPM.*\_binning.*',
                                                         'SiPM_binning'       )
         config           = config.drop_duplicates('param_key').reset_index(drop=True)
