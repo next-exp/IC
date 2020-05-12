jmbenlloch/IC-1

Add tests for Hypathia

invisible_cities/cities/command_line_execution_test.py
@@ -12,8 +12,7 @@ from pytest import mark
 #                   'diomira isidora irene dorothea zaira penthesilea'.split())
 # TODO understand what's wrong with isidora (in Travis)
 @mark.parametrize('city',
-                  'diomira isidora irene dorothea penthesilea zaira berenice phyllis trude esmeralda'.split())
-
+                  'diomira isidora irene dorothea penthesilea zaira berenice phyllis trude hypathia esmeralda'.split())
 def test_command_line_run(city, tmpdir_factory):
     ICTDIR = getenv('ICTDIR')
     # Use the example config file included in the repository

next-exp/IC

invisible_cities/cities/command_line_execution_test.py
@@ -12,7 +12,7 @@ from pytest import mark
 #                   'diomira isidora irene dorothea zaira penthesilea'.split())
 # TODO understand what's wrong with isidora (in Travis)
 @mark.parametrize('city',
-                  'diomira isidora irene dorothea penthesilea berenice phyllis trude esmeralda beersheba'.split())
+                  'diomira isidora irene dorothea penthesilea berenice phyllis trude esmeralda beersheba hypathia'.split())
 
 def test_command_line_run(city, tmpdir_factory):
     ICTDIR = getenv('ICTDIR')