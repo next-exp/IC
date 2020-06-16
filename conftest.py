2d003d1ebf4cf9dc3a924a68c14aa8aaeadb7229
mmkekic <mmkekic@gmail.com>
2020-06-16 12:01:09 +0200

    Remove nocov profiles since use_coverage is deprecated.

--- conftest.py
+++ conftest.py
@@ -7,8 +7,6 @@ from hypothesis import Verbosity
 settings.register_profile("travis-ci" , settings(max_examples = 1000, deadline=300))
 settings.register_profile("hard"      , settings(max_examples = 1000))
 settings.register_profile("dev"       , settings(max_examples =   10))
-settings.register_profile("hard_nocov", settings(max_examples = 1000, use_coverage=False))
-settings.register_profile("dev_nocov" , settings(max_examples =   10, use_coverage=False))
 settings.register_profile("debug"     , settings(max_examples =   10,
                                                 verbosity=Verbosity.verbose))
 settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'dev'))
