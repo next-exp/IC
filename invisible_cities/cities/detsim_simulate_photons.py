2fbaaebd5692f910d2a70c77bfa4cd42e2708378
gondiaz <gonzalodiazlopez10@gmail.com>
2020-07-24 11:35:12 +0200

    Modify photon simulation module

--- invisible_cities/cities/detsim_simulate_photons.py
+++ invisible_cities/cities/detsim_simulate_photons.py
@@ -1,44 +1,39 @@
 import numpy  as np
 
-from typing import Callable
-
-
 #######################################
 ########## PHOTON SIMULATION ##########
 #######################################
-def generate_s1_photons(energies : np.ndarray,
-                        ws       : float) -> np.ndarray:
-    """generate s1 photons"""
+def generate_s1_photons(ws       : float,
+                        energies : np.ndarray) -> np.ndarray:
+    """generates s1 photons from the energy hits
+
+    Parameters:
+        :ws: float
+            scintillation yield
+        :energies: np.ndarray
+            energy hits
+    Returns:
+        :: np.ndarray
+            s1 photons per hit
+    """
     return np.random.poisson(energies / ws)
 
 
-def generate_s2_photons(x              : np.ndarray,
-                        el_gain        : float,
-                        el_gain_sigma  : float) -> np.ndarray:
+def generate_s2_photons(el_gain        : float,
+                        el_gain_sigma  : float,
+                        nes            : np.ndarray) -> np.ndarray:
     """generate number of EL-photons produced by secondary electrons that reach
     the EL (after drift and diffusion)
+
+    Parameters:
+        :el_gain: float
+            number of phtotons emitted per ionization electron at EL
+        :el_gain_sigma: float
+            standard deviation of the el_gain.
+        :nes: np.ndarray
+            number of ionization electrons at arriving at some EL X,Y position.
+    Returns:
+        :: np.ndarray
+            s2 photons electrons at the EL
     """
-    n = len(x)
-    nphs = np.random.normal(el_gain, el_gain_sigma, size = n)
-    return nphs
-
-
-def pes_at_sensors(xs         : np.ndarray,
-                   ys         : np.ndarray,
-                   zs         : np.ndarray,
-                   photons    : np.ndarray,
-                   x_sensors  : np.ndarray,
-                   y_sensors  : np.ndarray,
-                   z_sensors  : float   ,
-                   psf : Callable) -> np.ndarray:
-    """compute the pes that reach each sensor, based on
-    the sensor psf"""
-
-    dxs = xs[:, np.newaxis] - x_sensors
-    dys = ys[:, np.newaxis] - y_sensors
-    dzs = zs[:, np.newaxis] - z_sensors
-    photons = photons[:, np.newaxis]
-
-    pes = photons * psf(dxs, dys, dzs)
-    pes = np.random.poisson(pes)
-    return pes.T
+    return np.random.normal(el_gain*nes, np.sqrt(el_gain_sigma**2*nes))
