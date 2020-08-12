3cc8c46a0790c92fffda20d266f07ee5b9b2e6a9
gondiaz <gonzalodiazlopez10@gmail.com>
2020-08-12 09:48:36 +0200

    Add test for binedges_from_bincenters

--- invisible_cities/cities/detsim_get_psf_test.py
+++ invisible_cities/cities/detsim_get_psf_test.py
@@ -4,9 +4,11 @@ import tables as tb
 import pandas as pd
 
 from pytest import fixture
+from pytest import raises
 
 from invisible_cities.cities.detsim_get_psf import create_xyz_function
 from invisible_cities.cities.detsim_get_psf import create_xy_function
+from invisible_cities.cities.detsim_get_psf import binedges_from_bincenters
 
 
 def test_create_xy_function():
@@ -28,6 +30,33 @@ def test_create_xy_function():
     obtained_values = func(x, y)
 
     assert np.all(obtained_values == expected_values)
+
+
+def test_create_xy_function_Exceptions():
+
+    # create discrete function
+    H = np.array([[1, 2, 3],
+                  [4, 5, 6]])
+
+    xbins = np.array([0, 1, 2])
+    ybins = np.array([0, 1, 2, 3])
+    bins = [xbins, ybins]
+
+    func = create_xy_function(H, bins)
+
+    # test for some values
+    x = np.array([1.5, 0.7, 0.1, -1, 1 ])
+    y = np.array([2.9, 1.4, 2.2, 3.])
+
+    with raises(Exception) as E:
+        assert func(x, y)
+    assert str(E.value) == "x and y must have same size"
+
+    xbins = np.array([0, 1, 2, 3])
+    bins = [xbins, ybins]
+    with raises(Exception) as E:
+        assert create_xy_function(H, bins)
+    assert str(E.value) == "bins and array shapes not consistent"
 
 
 def test_create_xyz_function():
@@ -53,6 +82,26 @@ def test_create_xyz_function():
     assert np.all(obtained_values == expected_values)
 
 
+def test_binedges_from_bincenters():
+
+    centers = np.array([0, 1, 2, 3])
+    assert np.all(binedges_from_bincenters(centers) == np.array([0, 0.5, 1.5, 2.5, 3.]))
+
+    # Exceptions
+    centers = np.array([])
+    with raises(Exception) as E:
+        assert binedges_from_bincenters(centers)
+    assert str(E.value) == "Inconsistent array shape"
+
+    centers = np.array([[0, 1], [2, 3]])
+    with raises(Exception) as E:
+        assert binedges_from_bincenters(centers)
+    assert str(E.value) == "Inconsistent array shape"
+
+    centers = np.array([10, 9, 8])
+    with raises(Exception) as E:
+        assert binedges_from_bincenters(centers)
+    assert str(E.value) == "Unordered bin centers"
 
 
 
