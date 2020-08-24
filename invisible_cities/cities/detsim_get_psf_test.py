cd045f1aea426710a685158a5e05a31a74470c43
gondiaz <gonzalodiazlopez10@gmail.com>
2020-08-24 13:23:40 +0200

    Add psf fixture and test function

--- invisible_cities/cities/detsim_get_psf_test.py
+++ invisible_cities/cities/detsim_get_psf_test.py
@@ -3,12 +3,17 @@ import numpy  as np
 import tables as tb
 import pandas as pd
 
+import inspect
+
 from pytest import fixture
 from pytest import raises
+
+from invisible_cities.core import system_of_units as units
 
 from invisible_cities.cities.detsim_get_psf import create_xyz_function
 from invisible_cities.cities.detsim_get_psf import create_xy_function
 from invisible_cities.cities.detsim_get_psf import binedges_from_bincenters
+from invisible_cities.cities.detsim_get_psf import create_effective_psf
 
 
 def test_create_xy_function():
@@ -102,3 +107,27 @@ def test_binedges_from_bincenters():
     with raises(Exception) as E:
         assert binedges_from_bincenters(centers)
     assert str(E.value) == "Unordered bin centers"
+
+
+@fixture(scope="session",
+         params = ["detsim_psf_next100.h5"])
+def detsim_sipm_psf(request, ICDATADIR):
+    return os.path.join(ICDATADIR, request.param)
+
+
+def test_create_effective_psf(detsim_sipm_psf):
+
+    drift_velocity_EL = 2.5 * units.mm / units.mus
+    wf_sipm_bin_width =  1  * units.mus
+
+    psf, info = create_effective_psf(detsim_sipm_psf, drift_velocity_EL, wf_sipm_bin_width)
+    (n_time_bins, s2_sipm_nsamples) = info
+
+    # test shape of a calling psf function
+    distances = np.array([0, 10, 100])
+    assert inspect.isfunction(psf)
+    assert psf(distances).shape == (len(distances), n_time_bins)
+
+    # test null values for long distances
+    distances = np.array([200])
+    assert np.all(psf(distances) == np.zeros((len(distances), n_time_bins)))
