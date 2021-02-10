a83a56bd05ec153675b7cb0780329a2f9be6c50f
Jacek Generowicz <jacg@my-post-office.net>
2021-02-10 17:56:57 +0100

    Replace deprecated check_less_precise

--- invisible_cities/core/testing_utils.py
+++ invisible_cities/core/testing_utils.py
@@ -108,16 +108,15 @@ def _compare_dataframes(assertion, df1, df2, check_types=True, **kwargs):
 def assert_dataframes_equal(df1, df2, check_types=True, **kwargs):
     pd.testing.assert_frame_equal(df1.sort_index(axis=1), df2.sort_index(axis=1), check_names=True, check_dtype=check_types, check_exact=True, **kwargs)
 
-def assert_dataframes_close(df1, df2, check_types=True, rtol=None, atol=None, **kwargs):
-    if rtol:
-        check_less_precise = int(np.log10(1./rtol))
-    elif atol:
-        check_less_precise = int(np.log10(atol))
-    else:
-        check_less_precise = True
 
-    pd.testing.assert_frame_equal(df1.sort_index(axis=1), df2.sort_index(axis=1), 
-    check_names=True, check_dtype=check_types, check_less_precise = check_less_precise)
+def assert_dataframes_close(df1, df2, check_types=True, rtol=None, atol=None, **kwargs):
+    tolerance = {}
+    if atol: tolerance['atol'] = atol
+    if rtol: tolerance['rtol'] = rtol
+
+    pd.testing.assert_frame_equal(df1.sort_index(axis=1), df2.sort_index(axis=1),
+                                  check_names=True, check_dtype=check_types,
+                                  **tolerance)
 
 
 def assert_SensorResponses_equality(sr0, sr1):
