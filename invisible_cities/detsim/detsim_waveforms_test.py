146073c68fbf0b0759b01ea549e17e4eb01afb58
gondiaz <gonzalodiazlopez10@gmail.com>
2022-08-08 14:33:16 -0500

    Update tests

--- invisible_cities/detsim/detsim_waveforms_test.py
+++ invisible_cities/detsim/detsim_waveforms_test.py
@@ -1,14 +1,31 @@
 import numpy as np
 
-from . detsim_waveforms import histogram_s1_times
+from . detsim_waveforms import generate_s1_waveform
 
-def test_histogram_s1_times():
-    s1_times = np.array([[1, 2, 3, 4, 5]])
-    buffer_length = 5
-    bin_width     = 1
-    start_time    = 1
+from ..core.system_of_units import ns
 
-    expected = np.array([[1, 1, 1, 1, 1]])
-    h = histogram_s1_times(s1_times, buffer_length, bin_width, start_time)
+def test_generate_s1_waveform_pes():
+    "Test that total pes are conserved in s1 waveform"
+    pes_at_pmts = np.array([[1, 2, 3, 4]])
+    time = np.zeros(pes_at_pmts.shape[-1])
+    buffer_length = 1000 * ns
+    bin_width     = 25  * ns
+    start_time    = 0
 
-    np.testing.assert_allclose(h, expected)
+    wfs = generate_s1_waveform(pes_at_pmts, time, buffer_length, bin_width, start_time)
+    np.testing.assert_allclose(np.sum(pes_at_pmts, axis=1), np.sum(wfs, axis=1), rtol=0.001)
+
+
+def test_generate_s1_waveform_multi():
+    "Test waveform creation for multiple s1s"
+    pes_at_pmts = np.array([[1, 2, 3, 4, 1, 2, 3, 4]])
+    time = np.zeros(pes_at_pmts.shape[-1])
+    time[:-4] = 1000 * ns
+
+    buffer_length = 2000 * ns
+    bin_width     = 25  * ns
+    start_time    = 0
+
+    wfs = generate_s1_waveform(pes_at_pmts, time, buffer_length, bin_width, start_time)
+    np.testing.assert_allclose( wfs[:, :int(buffer_length/bin_width/2)]
+                              , wfs[:,  int(buffer_length/bin_width/2):], rtol=0.001)
--- invisible_cities/detsim/simulate_s1_test.py
+++ invisible_cities/detsim/simulate_s1_test.py
@@ -5,8 +5,6 @@ from .. io.dst_io  import load_dst
 
 from . simulate_s1 import compute_scintillation_photons
 from . simulate_s1 import compute_s1_pes_at_pmts
-from . simulate_s1 import generate_s1_time
-from . simulate_s1 import generate_s1_times_from_pes
 
 from . light_tables import create_lighttable_function
 
@@ -42,37 +40,4 @@ def test_compute_s1_pes_at_pmts(lighttable_filenames):
     s1_pes_at_pmts = compute_s1_pes_at_pmts(xs, ys, zs, photons, s1_lt)
 
     assert s1_pes_at_pmts.shape == (12, len(xs))
-
-
-def test_generate_s1_time():
-    np.random.seed(1234)
-    times = generate_s1_time(size=2)
-    expected = np.array([ 57.57690962, 153.87864745])
-
-    np.testing.assert_allclose(times, expected)
-
-
-def test_generate_s1_times_from_pes():
-
-    np.random.seed(1234)
-    s1_pes_at_pmts = np.array([[1, 0, 1],
-                               [0, 2, 1]])  # 2 pmts, 3 hits
-    hit_times = np.array([0, 1, 2])
-
-    expected  = [np.array([ 57.57690962, 155.87864745]),
-                 np.array([ 18.29440868,  13.34541421, 170.82805666])]
-    values = generate_s1_times_from_pes(s1_pes_at_pmts, hit_times)
-
-    assert len(values) == len(expected)
-    for exp, val in zip(expected, values):
-        np.testing.assert_allclose(exp, val)
-
-
-@settings(max_examples=500)
-@given(hit_times = arrays(float, 10, elements=floats(min_value=0)))
-def test_generate_s1_times_from_pes_higher_times(hit_times):
-
-    s1_pes_at_pmts = np.ones((1, 10), dtype=int) # just 1 pmt
-    s1_times = generate_s1_times_from_pes(s1_pes_at_pmts, hit_times)[0]
-
-    assert np.all(s1_times>=hit_times)
+    
