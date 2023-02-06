0ff80cef8cab67fa8a46bfe85ce1a848a055ffdc
soleti <roberto.soleti@gmail.com>
2023-02-06 12:02:54 +0100

    add krmaps_io skeleton

---
+++ invisible_cities/io/krmaps_io.py
@@ -0,0 +1,8 @@
+def write_krmap(*args, **kwargs):
+    raise NotImplementedError("Not yet implemented")
+
+def write_krevol(*args, **kwargs):
+    raise NotImplementedError("Not yet implemented")
+
+def write_mapinfo(*args, **kwargs):
+    raise NotImplementedError("Not yet implemented")
