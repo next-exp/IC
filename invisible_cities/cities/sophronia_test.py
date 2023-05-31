cdfbceba94b1598f019b192ed28eb2028d11708f
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-05-31 16:53:20 +0300

    Add tests for sophronia

--- invisible_cities/cities/sophronia_test.py
+++ invisible_cities/cities/sophronia_test.py
@@ -1,35 +1,89 @@
 import os
-import warnings
-import numpy  as np
 import tables as tb
-import pandas as pd
 
 from pytest import mark
 
-from .. core                 import system_of_units as units
-from .. core.core_functions  import in_range
-from .. core.testing_utils   import assert_dataframes_close
-from .. core.testing_utils   import assert_tables_equality
-from .. core.configure       import configure
-from .. io                   import dst_io as dio
-from .. io.mcinfo_io         import load_mchits_df
-from .. io.mcinfo_io         import load_mcparticles_df
-from .. types.symbols        import all_events
-from .. types.symbols        import RebinMethod
-from .. types.symbols        import SiPMCharge
-
-from .  sophronia            import sophronia
+from .. core.testing_utils import assert_tables_equality
+from .  sophronia          import sophronia
 
 
-def test_sophronia_runs(KrMC_pmaps_filename, config_tmpdir):
-    PATH_IN   = KrMC_pmaps_filename
-    PATH_OUT  = os.path.join(config_tmpdir,'sophronia.h5')
-    conf      = configure('dummy invisible_cities/config/sophronia.conf'.split())
-    nevt_req  = 10
+def test_sophronia_runs(sophronia_config, config_tmpdir):
+    path_out = os.path.join(config_tmpdir, 'sophronia_runs.h5')
+    nevt_req = 1
+    config   = dict(**sophronia_config)
+    config.update(dict(file_out    = path_out,
+                       event_range = nevt_req))
 
-    conf.update(dict(files_in    = PATH_IN,
-                     file_out    = PATH_OUT,
-                     event_range = nevt_req))
+    cnt = sophronia(**config)
+    assert cnt.events_in   == nevt_req
 
-    cnt = sophronia(**conf)
-    assert cnt.events_in  == nevt_req
+
+def test_sophronia_contains_all_tables(sophronia_config, config_tmpdir):
+    path_out = os.path.join(config_tmpdir, "test_sophronia_contains_all_tables.h5")
+    nevt_req = 1
+    config   = dict(**sophronia_config)
+    config.update(dict(file_out    = path_out,
+                       event_range = nevt_req))
+    sophronia(**config)
+
+    with tb.open_file(path_out) as h5out:
+        assert "MC"                   in h5out.root
+        assert "MC/hits"              in h5out.root
+        assert "MC/configuration"     in h5out.root
+        assert "MC/event_mapping"     in h5out.root
+        assert "MC/hits"              in h5out.root
+        assert "MC/particles"         in h5out.root
+        assert "MC/sns_positions"     in h5out.root
+        assert "MC/sns_response"      in h5out.root
+        assert "DST/Events"           in h5out.root
+        assert "RECO/Events"          in h5out.root
+        assert "Run"                  in h5out.root
+        assert "Run/eventMap"         in h5out.root
+        assert "Run/events"           in h5out.root
+        assert "Run/runInfo"          in h5out.root
+        assert "Filters/s12_selector" in h5out.root
+        assert "Filters/valid_hit"    in h5out.root
+
+
+@mark.slow
+def test_sophronia_exact_result(sophronia_config, Th228_hits, config_tmpdir):
+    path_out = os.path.join(config_tmpdir, 'test_sophronia_exact_result.h5')
+    config   = dict(**sophronia_config)
+    config.update(dict(file_out = path_out))
+
+    sophronia(**config)
+
+    tables = ( "MC/hits", "MC/particles"
+             , "DST/Events"
+             , "RECO/Events"
+             , "Run/events", "Run/runInfo"
+             , "Filters/s12_selector", "Filters/valid_hit"
+             )
+
+    with tb.open_file(Th228_hits)   as true_output_file:
+        with tb.open_file(path_out) as      output_file:
+            for table in tables:
+                assert hasattr(output_file.root, table), table
+                got      = getattr(     output_file.root, table)
+                expected = getattr(true_output_file.root, table)
+                assert_tables_equality(got, expected)
+
+
+def test_sophronia_filters_pmaps(sophronia_config, config_tmpdir):
+    path_out = os.path.join(config_tmpdir, 'test_sophronia_filters_pmaps.h5')
+    config   = dict(**sophronia_config)
+    config.update(dict( file_out    = path_out
+                      , event_range = (5, 8)))
+
+    cnt = sophronia(**config)
+
+    assert cnt.events_in          == 3
+    assert cnt.events_out         == 1
+    assert cnt.evtnum_list        == [400076]
+    assert cnt.selection.n_passed == 1
+    assert cnt.selection.n_failed == 2
+
+
+@mark.skip(reason = "need a new test file for this")
+def test_sophronia_filters_events_with_only_nn_hits():
+    pass
