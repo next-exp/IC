d5369c2311b7e6bc5c87e31de41bd704ae1fc77f
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-11-08 00:08:47 +0200

    Make hit correction optional in esmeralda

--- invisible_cities/cities/components.py
+++ invisible_cities/cities/components.py
@@ -15,6 +15,7 @@ from typing          import Dict
 from typing          import Tuple
 from typing          import Union
 from typing          import Optional
+from typing          import Any
 
 import tables as tb
 import numpy  as np
@@ -1475,3 +1476,7 @@ def hits_corrector(map_fname : str, apply_temp : bool) -> Callable:
         return hitc
 
     return correct
+
+
+def identity(x : Any) -> Any:
+    return x
