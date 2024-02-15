4c9c9bf3441ae2f4f98f4fd1101f492d60b62d09
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-02-15 16:33:11 +0100

    Test that all cities accept event_range = 0
    This demonstrates the issue on the bigger picture

--- invisible_cities/cities/cities_test.py
+++ invisible_cities/cities/cities_test.py
@@ -11,9 +11,11 @@ from pytest import raises
 from .. core.configure      import configure
 
 online_cities = "irene dorothea sophronia esmeralda beersheba".split()
-all_cities    = """beersheba berenice buffy detsim diomira dorothea esmeralda
-                   eutropia hypathia irene isaura isidora phyllis sophronia
-                   trude""".split()
+all_cities_with_event_range = online_cities + """
+  berenice buffy detsim diomira hypathia isaura isidora phyllis trude
+  """.split()
+all_cities  = sorted(all_cities_with_event_range + ["eutropia"])
+
 
 @mark.filterwarnings("ignore::UserWarning")
 @mark.parametrize("city", online_cities)
@@ -28,6 +30,23 @@ def test_city_empty_input_file(config_tmpdir, ICDATADIR, city):
     conf = configure(config_file.split())
     conf.update(dict(files_in      = PATH_IN,
                      file_out      = PATH_OUT))
+
+    module_name   = f'invisible_cities.cities.{city}'
+    city_function = getattr(import_module(module_name), city)
+
+    city_function(**conf)
+
+
+@mark.filterwarnings("ignore::UserWarning")
+@mark.parametrize("city", all_cities_with_event_range)
+def test_city_null_event_range(config_tmpdir, ICDATADIR, city):
+    # All cities must run with event_range = 0
+    PATH_OUT = os.path.join(config_tmpdir, 'empty_output.h5')
+
+    config_file = 'dummy invisible_cities/config/{}.conf'.format(city)
+    conf = configure(config_file.split())
+    conf.update(dict( file_out = PATH_OUT
+                     , event_range = 0))
 
     module_name   = f'invisible_cities.cities.{city}'
     city_function = getattr(import_module(module_name), city)
