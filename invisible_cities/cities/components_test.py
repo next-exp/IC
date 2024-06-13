af204927314867f01f648e85683d299940b8bc39
Paola Ferrario <paola.ferrario@gmail.com>
2024-06-13 12:17:55 +0200

    Make the test clearer.

--- invisible_cities/cities/components_test.py
+++ invisible_cities/cities/components_test.py
@@ -425,12 +425,13 @@ def test_check_max_time_units():
 
 def test_read_wrong_pmt_ids(ICDATADIR):
     """
-    Check if an error is raised when wrong PMT IDs are read from nexus file
+    The input file of this test contains sensor IDs that are not present in the database.
+    This should raise an error and this test check that it is actually raised.
     """
     file_in    = os.path.join(ICDATADIR, "nexus_next100_full_wrong_PMT_IDs.h5")
     run_number = 0
     rate       = 0.5
 
+    sns_gen = mcsensors_from_file([file_in], 'next100', run_number, rate)
     with raises(SensorIDMismatch):
-        sns_gen = mcsensors_from_file([file_in], 'next100', run_number, rate)
-        _ = next(sns_gen)
+        next(sns_gen)
