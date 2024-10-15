e6bdf7b63a3ac38f519c6d7f5981471cdadee5d8
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-10-15 16:11:24 +0200

    Rewrite `hits_merger` component to work with DataFrames

--- invisible_cities/cities/components.py
+++ invisible_cities/cities/components.py
@@ -1476,11 +1476,7 @@ def compute_and_write_tracks_info(paolina_params, h5out,
 
 @check_annotations
 def hits_merger(same_peak : bool) -> Callable:
-    def merge_hits(hc : HitCollection) -> HitCollection:
-        merged_hits = hif.merge_NN_hits(hc.hits, same_peak)
-        return HitCollection(hc.event, hc.time, merged_hits)
-
-    return merge_hits
+    return partial(hif.merge_NN_hits, same_peak=same_peak)
 
 
 @check_annotations
