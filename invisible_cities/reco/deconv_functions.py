b0878d7e08718b97e43707167ec5d66f16bca5dc
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-10-18 17:41:48 +0100

    clean up formatting, add protection for background

--- invisible_cities/reco/deconv_functions.py
+++ invisible_cities/reco/deconv_functions.py
@@ -93,14 +93,23 @@ def generate_satellite_mask(im_deconv, satellite_max_size, e_cut, cut_type):
 
     # separate different regions of 0s and 1s
     ccs, component_sizes = collect_component_sizes(im_mask)
+
     # check if no satellites within deposit
     if len(component_sizes) <= 2:
+
         # Return a fully False array, so that no objects get removed
         return np.full(im_deconv.shape, False)
 
     
     # create boolean array for each label of satellite & non-satellite
     too_small = component_sizes < satellite_max_size
+
+    # set the first element to False in too_small, as this is always the 0 components.
+    # This protects against the edge case in which the 0s are less than the satellite_max_size,
+    #  meaning that you'd remove energy values in the deconvolution that are below the threshold.
+    too_small[0] = False 
+    
+
     # apply boolean array to labelled array to hold satellites as True
     too_small_mask = too_small[ccs]
     # return mask to remove satellites
