bea0544c546c0d8fdd81b8c7d0a8144ae814aa9f
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-10-24 09:56:01 +0200

    Replace useless `dbnew` fixture

--- invisible_cities/core/random_sampling_test.py
+++ invisible_cities/core/random_sampling_test.py
@@ -182,12 +182,12 @@ def run_number():
     return 4714
 
 @fixture(scope="module")
-def datasipm(run_number, dbnew):
-    return DataSiPM(dbnew, run_number)
+def datasipm(run_number ):
+    return DataSiPM("new", run_number)
 
 
 @fixture(scope="module", params=[False, True])
-def noise_sampler(request, dbnew, run_number):
+def noise_sampler(request, run_number):
     nsamples = 1000
     smear    = request.param
     thr      = 0.99
@@ -201,7 +201,7 @@ def noise_sampler(request, dbnew, run_number):
     1.45  :   6,
     1.55  :   3,
     np.inf:   9}
-    return (NoiseSampler(dbnew, run_number, nsamples, smear),
+    return (NoiseSampler("new", run_number, nsamples, smear),
             nsamples, smear,
             thr, true_threshold_counts)
 
