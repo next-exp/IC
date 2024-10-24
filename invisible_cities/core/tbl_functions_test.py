1068ac2ef9cea679b91b1db1a9f015b7dbe3e6d8
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-10-24 09:35:11 +0200

    Add tests for `tbl.filters`

--- invisible_cities/core/tbl_functions_test.py
+++ invisible_cities/core/tbl_functions_test.py
@@ -1,7 +1,39 @@
 from pytest import mark
+from pytest import raises
 
 from . import tbl_functions as tbl
 
+
+@mark.parametrize("name", "NOCOMPR ZLIB1 ZLIB4 ZLIB5 ZLIB9 BLOSC5 BLZ4HC5".split())
+def test_filters(name):
+    assert tbl.filters(name) is not None
+
+
+def test_filters_None():
+    assert tbl.filters(None) is None
+
+
+def test_filters_raises():
+    with raises(ValueError):
+        tbl.filters("this_filter_does_not_exist")
+
+
+"""
+    if name is None: return None
+
+    try:
+        level, lib = {"NOCOMPR": (0,  None)        ,
+                      "ZLIB1"  : (1, 'zlib')       ,
+                      "ZLIB4"  : (4, 'zlib')       ,
+                      "ZLIB5"  : (5, 'zlib')       ,
+                      "ZLIB9"  : (9, 'zlib')       ,
+                      "BLOSC5" : (5, 'blosc')      ,
+                      "BLZ4HC5": (5, 'blosc:lz4hc'),
+                      }[name]
+        return tb.Filters(complevel=level, complib=lib)
+    except KeyError:
+        raise ValueError("Compression option {} not found.".format(name))
+"""
 
 @mark.parametrize('filename first_evt'.split(),
                   (('dst_NEXT_v0_08_09_Co56_INTERNALPORTANODE_74_0_7bar_MCRD_10000.root.h5', 740000),
