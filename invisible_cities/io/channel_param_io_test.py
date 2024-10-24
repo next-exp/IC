df63f82e10f76af63384e30f86310e43f0bf3bfb
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-10-24 12:58:02 +0200

    Add more tests

--- invisible_cities/io/channel_param_io_test.py
+++ invisible_cities/io/channel_param_io_test.py
@@ -4,16 +4,20 @@ import numpy  as np
 import tables as tb
 
 from pytest import fixture
+from pytest import raises
+
 from numpy.testing import assert_allclose
 
 from .. core     import tbl_functions as tbl
 
-from . channel_param_io import         generic_params
-from . channel_param_io import       store_fit_values
-from . channel_param_io import   channel_param_writer
-from . channel_param_io import  make_table_dictionary
-from . channel_param_io import     basic_param_reader
-from . channel_param_io import generator_param_reader
+from . channel_param_io import              generic_params
+from . channel_param_io import            store_fit_values
+from . channel_param_io import        channel_param_writer
+from . channel_param_io import       make_table_dictionary
+from . channel_param_io import          basic_param_reader
+from . channel_param_io import      generator_param_reader
+from . channel_param_io import         subset_param_reader
+from . channel_param_io import single_channel_value_reader
 
 
 @fixture(scope="session")
@@ -37,6 +41,15 @@ def fake_channel_data(config_tmpdir):
 
     val_list = np.array(val_list)
     return filename, n_rows, out_dict, val_list
+
+
+def test_basic_param_reader_raises(config_tmpdir):
+    filename = os.path.join(config_tmpdir, "test_basic_param_reader.h5")
+    with tb.open_file(filename, "w"): pass
+
+    with tb.open_file(filename) as file:
+        with raises(tb.NoSuchNodeError):
+            basic_param_reader(file)
 
 
 def test_basic_param_reader(fake_channel_data):
@@ -64,6 +77,46 @@ def test_generator_param_reader(fake_channel_data):
             counter += 1
         assert counter == n_rows
 
+
+def test_generator_param_reader_raises(fake_channel_data):
+    filename, *_ = fake_channel_data
+    with tb.open_file(filename) as file:
+        with raises(ValueError):
+            tuple(generator_param_reader(file, "this_table_does_not_exist")) # consume iterator
+
+
+def test_subset_param_reader(fake_channel_data):
+    filename, n_rows, _, val_list = fake_channel_data
+    with tb.open_file(filename) as file:
+        values_and_errors = tuple(subset_param_reader(file, "FIT_test_generic", ("gain",)))
+        assert len(values_and_errors) == n_rows
+        for got, expected in zip(values_and_errors, val_list[:, 4]):
+            assert np.isclose(got[1][0]["gain"], expected[0])
+            assert np.isclose(got[1][1]["gain"], expected[1])
+
+
+def test_subset_param_reader_raises(fake_channel_data):
+    filename, *_ = fake_channel_data
+    with tb.open_file(filename) as file:
+        with raises(ValueError):
+            tuple(subset_param_reader(file, "this_table_does_not_exist", ())) # consume iterator
+
+
+def test_single_channel_value_reader(fake_channel_data):
+    filename, *_ = fake_channel_data
+    with tb.open_file(filename) as file:
+        params_table   = file.root.FITPARAMS.FIT_test_generic
+        params, errors = single_channel_value_reader(0, params_table, generic_params)
+        assert len(params) == 8
+        assert len(errors) == 8
+
+
+def test_single_channel_value_reader_wrong_id(fake_channel_data):
+    filename, *_ = fake_channel_data
+    with tb.open_file(filename) as file:
+        params_table   = file.root.FITPARAMS.FIT_test_generic
+        with raises(ValueError):
+            single_channel_value_reader(-1, params_table, generic_params)
 
 
 def test_simple_parameters_with_covariance(config_tmpdir):
