5ae41c7598c7604eeaaa9f67833c13732784644d
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-12-17 16:40:18 +0100

    Bump python to 3.13 and unpin packages

--- manage.sh
+++ manage.sh
@@ -2,10 +2,10 @@
 
 COMMAND=$1
 ARGUMENT=$2
-PYTHON_VERSION=3.8
+PYTHON_VERSION=3.13
+
 ## Interpret meaning of command line argument depending on which
 ## function will receive it.
-
 case $COMMAND in
     run_tests_par | compile_and_test_par)     N_PROC=${ARGUMENT:-auto} ;;
 esac
@@ -65,7 +65,7 @@ function install_conda {
     fi
 }
 
-CONDA_ENV_TAG=2024-06-08
+CONDA_ENV_TAG=2024-11-07
 CONDA_ENV_NAME=IC-${PYTHON_VERSION}-${CONDA_ENV_TAG}
 
 function make_environment {
@@ -81,29 +81,29 @@ channels:
 dependencies:
 - python       = ${PYTHON_VERSION}
 # *REMEMBER TO CHANGE CONDA_ENV_TAG WHEN CHANGING VERSION NUMBERS*
-- cython       = 0.29.24
-- jupyter      = 1.0.0
-- jupyterlab   = 3.2.1
-- matplotlib   = 3.4.3
-- networkx     = 2.6.3
-- notebook     = 6.4.5
-- numpy        = 1.23.1
-- pandas       = 1.3.4
-- seaborn      = 0.11.2
-- pymysql      = 1.0.2
-- pytables     = 3.7.0
-- pytest       = 6.2.4
-- scipy        = 1.9.3
-- sphinx       = 4.2.0
-- tornado      = 6.1
-- flaky        = 3.7.0
-- hypothesis   = 6.14.1
-- pytest-xdist = 2.3.0
-- coverage     = 5.5
-- pip          = 21.2.4
-- setuptools   = 58.0.4
+- cython
+- jupyter
+- jupyterlab
+- matplotlib
+- networkx
+- notebook
+- numpy
+- pandas
+- seaborn
+- pymysql
+- pytables
+- pytest
+- scipy
+- sphinx
+- tornado
+- flaky
+- hypothesis
+- pytest-xdist
+- coverage
+- pip
+- setuptools
 - pip:
-  - pytest-instafail==0.4.2
+  - pytest-instafail
 EOF
 
     conda env create -f ${YML_FILENAME}
