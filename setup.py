cf188bc813277d70ab8a5c3bc0b1b62af9441680
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2024-12-18 13:45:24 +0100

    Add version number to GHA
    not sure why this is needed, I forgot.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 from os import path
 from subprocess import check_output
 import numpy
+import re
 
 numpy_include = path.join(path.dirname(numpy.__file__), 'core/include')
 
@@ -12,6 +13,17 @@ version = ( check_output('git describe --tags --always'.split())
           .split("-")[0]
           .removeprefix("v")
           )
+
+# Regular expression to extract the version from a string of the type X.Y.Z
+# where all of X, Y, Z can have any number of digits
+#                    digits dot digits dot digits
+version_pattern = "^ (\d+) (\.) (\d+) (\.) (\d+) $".replace(" ", "")
+
+# HACK:
+# in GHA we only pick up the last commit in the repo history and the the command
+# above returns the commit hash. If this happens, use a dummy version number.
+if not re.match(version, version_pattern):
+    version = "0.0.0"
 
 files      = "invisible_cities/*/*.pyx"
 directives = dict(language_level=3, embedsignature=True)
