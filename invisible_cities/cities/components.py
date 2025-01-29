683f443465d3726d812666f00d20de7190d0786c
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2025-01-29 17:22:36 +0100

    Update `deconv_pmt`
    Now that masked sensors do not reach this point, we have to refactor it.
    Worth mentioning that this was wrong, since `map` did not ensure the
    length of the cwf array matched the length of the coefficient arrays.
    This is now asserted to avoid silent errors.

--- invisible_cities/cities/components.py
+++ invisible_cities/cities/components.py
@@ -344,18 +344,19 @@ def signal_finder(buffer_len   : float,
     return find_signal
 
 
-# TODO: consider caching database
 def deconv_pmt(dbfile, run_number, n_baseline,
                selection=None, pedestal_function=csf.means):
     DataPMT    = load_db.DataPMT(dbfile, run_number = run_number)
     pmt_active = np.nonzero(DataPMT.Active.values)[0].tolist() if selection is None else selection
-    coeff_c    = DataPMT.coeff_c  .values.astype(np.double)
-    coeff_blr  = DataPMT.coeff_blr.values.astype(np.double)
+    coeff_c    = DataPMT.coeff_c  .values.astype(np.double)[pmt_active]
+    coeff_blr  = DataPMT.coeff_blr.values.astype(np.double)[pmt_active]
 
     def deconv_pmt(RWF):
+        assert len(RWF) == len(coeff_c)
         CWF = pedestal_function(RWF[:, :n_baseline]) - RWF
-        return np.array(tuple(map(blr.deconvolve_signal, CWF[pmt_active],
-                                  coeff_c              , coeff_blr      )))
+        CWF = map(blr.deconvolve_signal, CWF, coeff_c, coeff_blr)
+        return np.array(tuple(CWF))
+
     return deconv_pmt
 
 
