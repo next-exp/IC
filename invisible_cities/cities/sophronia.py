4f7fde8276aecaec8bc5be463735a083684c2865
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2025-02-18 13:12:33 +0100

    Copy db info in dorothea and sophronia

--- invisible_cities/cities/sophronia.py
+++ invisible_cities/cities/sophronia.py
@@ -54,6 +54,7 @@ from .. types.ic_types import          NN
 
 from .  components import                  city
 from .  components import          copy_mc_info
+from .  components import          copy_db_info
 from .  components import           print_every
 from .  components import       peak_classifier
 from .  components import   compute_xy_position
@@ -228,4 +229,5 @@ def sophronia( files_in           : OneOrManyFiles
             copy_mc_info(files_in, h5out, result.evtnum_list,
                          detector_db, run_number)
 
+        copy_db_info(files_in[0], h5out)
         return result
