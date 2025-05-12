08efb2b02589cebddd45c599a40e9271df918bdb
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-05-12 15:43:41 +0100

    separate into two tests, implement fixtures

--- invisible_cities/cities/components_test.py
+++ invisible_cities/cities/components_test.py
@@ -489,40 +489,45 @@ def test_read_wrong_pmt_ids(ICDATADIR):
         next(sns_gen)
 
 
-@mark.parametrize("apply_z", (True, False))
-def test_hits_corrector_z_uncorrected_when_flagged( correction_map_filename
-                                                  , apply_z):
+def test_hits_uncorrected_when_flagged( correction_map_filename
+                                      , random_hits_toy_data):
     '''
     Test to ensure that z is uncorrected when `apply_z` is True
     '''
-
-    # create some hits to correct
-    n  = 50
-    xs = np.random.uniform(-10, 10, n)
-    ys = np.random.uniform(-10, 10, n)
-    zs = np.random.uniform( 10, 50, n)
-
-    hits = []
-    for i, x, y, z in zip(range(n), xs, ys, zs):
-        c = Cluster(0, xy(x, y), xy.zero(), 1)
-        h = Hit(i, c, z, 1, xy.zero(), 0)
-        hits.append(h)
-
-    hc = HitCollection(0, 1, hits)
-
+    
+    hc = random_hits_toy_data
+    hz = [h.Z for h in hc.hits]
+    
     correct = hits_corrector(correction_map_filename,
                              apply_temp = False, 
                              norm_strat = NormStrategy.kr,
                              norm_value = None,
-                             apply_z = apply_z)
+                             apply_z = False)
     corrected_z = np.array([h.Z for h in correct(hc).hits])
 
-    # if we expect a change, assert a change and vice versa
-    if apply_z:
-        assert_raises(AssertionError, assert_equal, corrected_z, zs)
-    else:
-        assert_equal(corrected_z, zs)
+    # no change to equal results
+    assert_equal(corrected_z, hz)
 
+
+def test_hits_corrected_when_not_flagged( correction_map_filename
+                                        , random_hits_toy_data):
+    '''
+    Test to ensure that the correction is applied when `apply_z` is False
+    '''
+    
+    hc = random_hits_toy_data
+    hz = [h.Z for h in hc.hits]
+    
+    correct = hits_corrector(correction_map_filename,
+                             apply_temp = False, 
+                             norm_strat = NormStrategy.kr,
+                             norm_value = None,
+                             apply_z = True)
+    corrected_z = np.array([h.Z for h in correct(hc).hits])
+
+    # raise assertion error as expected
+    assert_raises(AssertionError, assert_equal, corrected_z, hz)
+    
 
 @mark.parametrize( "norm_strat norm_value".split(),
                   ( (NormStrategy.kr    , None) # None marks the default value
@@ -534,25 +539,16 @@ def test_hits_corrector_z_uncorrected_when_flagged( correction_map_filename
 def test_hits_corrector_valid_normalization_options( correction_map_filename
                                                    , norm_strat
                                                    , norm_value
-                                                   , apply_temp ):
+                                                   , apply_temp
+                                                   , random_hits_toy_data ):
     """
     Test that all valid normalization options work to some
     extent. Here we just check that the values make some sense: not
     nan and greater than 0. The more exhaustive tests are performed
     directly on the core functions.
     """
-    n  = 50
-    xs = np.random.uniform(-10, 10, n)
-    ys = np.random.uniform(-10, 10, n)
-    zs = np.random.uniform( 10, 50, n)
 
-    hits = []
-    for i, x, y, z in zip(range(n), xs, ys, zs):
-        c = Cluster(0, xy(x, y), xy.zero(), 1)
-        h = Hit(i, c, z, 1, xy.zero(), 0)
-        hits.append(h)
-
-    hc = HitCollection(0, 1, hits)
+    hc = random_hits_toy_data
 
     correct     = hits_corrector(correction_map_filename, apply_temp, norm_strat, norm_value)
     corrected_e = np.array([h.Ec for h in correct(hc).hits])
