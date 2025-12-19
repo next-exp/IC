7e66555b2c4050edf976fc4f4f81c5fbf945e621
mcidlaso <mariacidlaso1@gmail.com>
2025-12-19 14:44:03 +0100

    Add tests functions for the krmap functions

--- invisible_cities/icaros/correction_functions_test.py
+++ invisible_cities/icaros/correction_functions_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from correction_funcitons import normalization
+from correction_functions import normalization
 
 
 def test_method_norm():
