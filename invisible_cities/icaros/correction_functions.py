5f9baeaf9e4732d28613b6748f5fec94db5b590a
mcidlaso <mariacidlaso1@gmail.com>
2026-01-16 14:48:02 +0100

    Drop dropna() from normalization function

--- invisible_cities/icaros/correction_functions.py
+++ invisible_cities/icaros/correction_functions.py
@@ -6,31 +6,31 @@ from scipy.interpolate import griddata
 
 def normalization(krmap, method, x_low, x_high, y_low, y_high):
 
-    mu_values = krmap.mu
+    mu_values = krmap.mu.dropna()
 
     if method == 'max':
-        E_reference_max = mu_values.dropna().max()
+        E_reference_max = mu_values.max()
         return E_reference_max
 
     if method == 'mean chamber':
-        E_reference_chamber = mu_values.dropna().mean()
+        E_reference_chamber = mu_values.mean()
         return E_reference_chamber
 
     if method == 'mean anode':
         mu_values_anode = krmap[krmap.k == 0].mu
-        E_reference_anode = mu_values_anode.dropna().mean()
+        E_reference_anode = mu_values_anode.mean()
         return E_reference_anode
 
     mask_region = (krmap['x'] <= x_high) & (krmap['x'] >= x_low) & (krmap['y'] <= y_high) & (krmap['y'] >= y_low)
 
     if method == 'mean region anode':
         region = krmap[krmap.k == 0][mask_region]
-        E_reference_slice_anode = region.mu.dropna().mean()
+        E_reference_slice_anode = region.mu.mean()
         return E_reference_slice_anode
 
     if method == 'mean region chamber':
         region = krmap[mask_region]
-        E_reference_region = region.mu.dropna().mean()
+        E_reference_region = region.mu.mean()
         return E_reference_region
 
 
