9d210dd2cd88c77cf946121ed2d6049e127d36c0
mcidlaso <mariacidlaso1@gmail.com>
2026-01-20 11:33:14 +0100

    Import functions with the right name

--- invisible_cities/icaros/krmap_functions_test.py
+++ invisible_cities/icaros/krmap_functions_test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from invisible_cities.core.core_functions import in_range, shift_to_bin_centers
 import itertools
-from krmap_functions import create_NaN_map, med_fun, fit_function, map_3D_fits, merge_maps, include coordinates
+from krmap_functions import create_empty_map, get_median, gaussian_fit, fit_map, merge_maps, include_coordinates, gauss_seed, quick_gauss_fit, create_time_slices, get_time_evol, save_map
 from pytest import raises
 from invisible_cities.core.core_functions import fix_random_seed
 
@@ -26,13 +26,13 @@ def test_medfun_empty_input_does_not_raise():
     med_fun(empty_dst)
 
 
-def test_fitfun_does_fit_right():
+def test_gaussianfit_does_fit_right():
     empty_dst = pd.DataFrame(columns = ['DT', 'x', 'y', 'S2e'])
-    result_fit = fit_function(empty_dst, 1)
+    result_fit = gaussian_fit(empty_dst, 1)
     result_fun = med_fun(empty_dst)
 
     assert np.allclose(result_fit.mu.values, result_fun['median'].values, equal_nan=True)
-
+    
 
 
 def test_fitfun_medfun_Nevents():
@@ -73,16 +73,19 @@ def test_medfun_works_with_odd_data():
 
 
 
-def test_fitfun_computes_right_values():
+def test_gaussianfit_computes_right_values(): #If the events follow a gaussian distribution, the median, mean and fit mean values should be the same
 
     with fix_random_seed(42):
         S2e_df = pd.DataFrame({
             'S2e': np.random.normal(loc = 8000, scale = 10.0, size = 10000)
         })
 
-    results = fit_function(S2e_df, bins = 50)
+    results = gaussian_fit(S2e_df, bins = 50)
+    results_median = get_median(S2e_df)
 
     assert np.isclose(results['mu'][0], S2e_df.S2e.mean(), atol = 1)
+    assert np.isclose(results['mu'][0], results_median.median, atol = 1)
+    assert np.isclose(S2e_df.S2e.mean(), results_median.median, atol = 1)
     assert np.isclose(results['sigma'][0], S2e_df.S2e.std(), atol = 0.5)
     assert np.isclose(results['mu_error'][0], S2e_df.S2e.std()/np.sqrt(len(S2e_df)), atol = 0.1)
     #error sigma?
