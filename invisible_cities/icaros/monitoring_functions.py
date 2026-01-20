71a963e83e26ddc767e8e43c80121a36da0860d2
mcidlaso <mariacidlaso1@gmail.com>
2026-01-20 16:48:28 +0100

    Added functions to append multiple time evolutions from different runs for monitoring

--- invisible_cities/icaros/monitoring_functions.py
+++ invisible_cities/icaros/monitoring_functions.py
@@ -1,12 +1,46 @@
 import pandas as pd
 import numpy  as np
+import os
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+from krmap_functions import create_time_slices, get_time_evol
+
+
+def append_t_evols(runs, path, map_names):
+    maps = []
+    for run, map_names in zip(runs, map_names):
+        input_path = path
+        map_files = os.path.join(input_path, map_name)
+        t_evol = pd.read_hdf(map_name, 't_evol/t_evol')
+        maps.append(t_evol)
+        
+        t_evols = pd.concat(maps, ignore_index = True).reset_index(drop=True).sort_values(by = 'ts')
+        
+    return t_evols
+
+
+def create_and_append_t_evols(runs, path, slice_hours, bins_lt, x0, y0, shape, shape_size, p0, dtrange, low_DT, high_DT, bins_Ec):
     
+    dataframes = []
+    for run in runs:
+        input_path = path
+        kdsts = os.path.join(input_path, f'kdst_corrected_{run}.h5')
+        kdst_corrected = pd.read_hdf(kdsts)
+        dfs = create_time_slices(kdst_corrected, run, slice_hours)
+        t_evol = get_time_evol(dfs, run, bins_lt, x0, y0, shape, shape_size, p0, dtrange, low_DT, high_DT, bins_Ec)
+        dataframes.append(t_evol)
+        
+        t_evols = pd.concat(dataframes, ignore_index = True).reset_index(drop = True).sort_values(by = 'ts')
+
+        
+    return t_evols
+
+
     
 def plot_t_evol(df, labels, variables, errors):
                      
     x_dates = pd.to_datetime(df["ts"], unit='s')
+    
                      
     for var, err, label in zip(variables, errors, labels):
         y = df[var].values
