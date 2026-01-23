Fix usage of dropna in normalization function

invisible_cities/icaros/correction_functions.py
@@ -5,53 +5,56 @@ from scipy.interpolate import griddata
 
 def normalization(krmap, method, xy_params = None):
 
-    mu_values = krmap.mu.dropna()
-    
+    krmap = krmap.dropna(subset=['mu'])
+
     anode = krmap[krmap.k == 0]
 
     if method == 'max':
-        E_reference_max = mu_values.max()
+        E_reference_max = krmap.mu.max()
         return E_reference_max
 
     if method == 'mean chamber':
-        E_reference_chamber = mu_values.mean()
+        E_reference_chamber = krmap.mu.mean()
         return E_reference_chamber
-    
-    if method == 'median chamber':
-        E_median_chamber = mu_values.median()
-        return E_median_chamber
-    
-    if method == 'mean anode':
-        mu_values_anode = anode.mu
-        E_reference_anode = mu_values_anode.mean()
-        return E_reference_anode
-    
-    if method == 'median anode':
-        mu_median_anode = anode.mu
-        E_median_anode = mu_median_anode.median()
-        return E_median_anode
-        
-    mask_region = (krmap['x'] <= xy_params['x_high']) & (krmap['x'] >= xy_params['x_low']) & (krmap['y'] <= xy_params['y_high']) & (krmap['y'] >= xy_params['y_low'])
 
-    if method == 'mean region anode':
-        region = anode[mask_region]
-        E_reference_slice_anode = region.mu.mean()
-        return E_reference_slice_anode
+    if method == 'median chamber':
+        E_median_chamber = krmap.mu.median()
+        return E_median_chamber
+
+    if method == 'mean anode':
+        E_reference_anode = anode.mu.mean()
+        return E_reference_anode
+
+    if method == 'median anode':
+        E_median_anode = anode.mu.median()
+        return E_median_anode
+
+    mask_region = (krmap['x'] <= xy_params['x_high']) & (krmap['x'] >= xy_params['x_low']) & (krmap['y'] <= xy_params['y_high']) & (krmap['y'] >= xy_params['y_low'])
+    krmap = krmap[mask_region]
 
     if method == 'mean region chamber':
         region = krmap[mask_region]
         E_reference_region = region.mu.mean()
         return E_reference_region
-    
-    if method == 'median region anode':
-        region = anode[mask_region]
-        E_median_region_anode = region.mu.median()
-        return E_median_region_anode
-    
+
     if method == 'median region':
         region = krmap[mask_region]
         E_median_region = region.mu.median()
         return E_median_region
+
+    mask_region_anode = mask_region & (krmap.k == 0)
+    anode = krmap[mask_region_anode]
+
+    if method == 'mean region anode':
+        E_reference_slice_anode = anode.mu.mean()
+        return E_reference_slice_anode
+
+
+    if method == 'median region anode':
+        E_median_region_anode = anode.mu.median()
+        return E_median_region_anode
+
+
 
 
 def apply_3Dmap(krmap, norm_method, dt, x, y, E, xy_params = None, keV = False):
@@ -69,6 +72,3 @@ def apply_3Dmap(krmap, norm_method, dt, x, y, E, xy_params = None, keV = False):
         Ec = Ec * (41.55 / norm)
 
     return Ec
-        
-        
-    