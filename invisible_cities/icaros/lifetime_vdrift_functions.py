c94afb0389998ae8d02a4176ebfdb8dc3902d337
mcidlaso <mariacidlaso1@gmail.com>
2026-01-30 13:11:09 +0100

    Move sigmoid function from lifetime_vdrift_functions to invisible_cities/core/fit_functions

--- invisible_cities/icaros/lifetime_vdrift_functions.py
+++ invisible_cities/icaros/lifetime_vdrift_functions.py
@@ -33,35 +33,6 @@ def select_lifetime_region(df, x0, y0, shape, shape_size):
 
 
 
-def sigmoid(x          : np.array,
-            scale      : float,
-            inflection : float,
-            slope      : float,
-            offset     : float)->np.array:
-    '''
-    Sigmoid function, it computes the sigmoid of the input array x using the specified
-    parameters for scaling, inflection point, slope, and offset.
-    Parameters
-    ----------
-    x : np.array
-        The input array.
-    scale : float
-        The scaling factor determining the maximum value of the sigmoid function.
-    inflection : float
-        The x-value of the sigmoid's inflection point (where the function value is half of the scale).
-    slope : float
-        The slope parameter that controls the steepness of the sigmoid curve.
-    offset : float
-        The vertical offset added to the sigmoid function.
-    Returns
-    -------
-    np.array
-        Array of computed sigmoid values for x array.
-    '''
-    sigmoid = scale / (1 + np.exp(-slope * (x - inflection))) + offset
-    return sigmoid
-
-
 def compute_drift_v(dtdata   : np.array,
                     dtbins   : np.array,
                     seed     : Tuple[float, float, float, float]):
