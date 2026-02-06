8e613baa4778594b7cf47d1ef60c558d2f96e229
mcidlaso <mariacidlaso1@gmail.com>
2026-02-06 15:50:46 +0100

    Imported functions with correct name and added fixture functions for testing

--- invisible_cities/icaros/correction_functions_test.py
+++ invisible_cities/icaros/correction_functions_test.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from correction_functions import normalization, apply_3Dmap, apply_correctionmap
+from correction_functions import normalization, apply_3Dmap, apply_correctionmap_inplace
 from invisible_cities.core.core_functions import in_range
 from invisible_cities.types.symbols import NormMethod
 from scipy.interpolate import griddata
+from pytest import fixture
 
 
-def test_normalization():
-
+@fixture
+def dummy_map():
     k_vals = np.arange(10)
     i_vals = np.arange(12)
     j_vals = np.arange(12)
@@ -21,7 +22,7 @@ def test_normalization():
     y = j * 25
     dt = k * 45
 
-    mu = (k*i + 800)*j
+    mu = (k*i + 800)*j + 1
 
     map_test = pd.DataFrame({
         'k': k,
@@ -32,6 +33,13 @@ def test_normalization():
         'dt': dt,
         'mu': mu
     })
+
+    return map_test
+
+
+def test_normalization(dummy_map):
+
+    dummy_map['mu'] = dummy_map.mu - 1
 
     region = {'x_low': -100,
               'x_high': 100,
@@ -40,133 +48,64 @@ def test_normalization():
 
 
     region_mask = (
-        in_range(map_test.x, region['x_low'],region['x_high']) &
-        in_range(map_test.y, region['y_low'],region['y_high'])
+        in_range(dummy_map.x, region['x_low'],region['x_high']) &
+        in_range(dummy_map.y, region['y_low'],region['y_high'])
     )
 
 
-    assert normalization(map_test, NormMethod.max) == 9889
-    assert normalization(map_test, NormMethod.mean_chamber) == 4536.125
-    assert normalization(map_test, NormMethod.mean_anode) == 4400
-    assert normalization(map_test, NormMethod.median_chamber) == 4647.5
-    assert normalization(map_test, NormMethod.median_anode) == 4400.0
+    assert normalization(dummy_map, NormMethod.max) == 9889
+    assert normalization(dummy_map, NormMethod.mean_chamber) == 4536.125
+    assert normalization(dummy_map, NormMethod.mean_anode) == 4400
+    assert normalization(dummy_map, NormMethod.median_chamber) == 4647.5
+    assert normalization(dummy_map, NormMethod.median_anode) == 4400.0
 
-    assert normalization(map_test, NormMethod.mean_region_anode, region) == 1200.0
-    assert normalization(map_test, NormMethod.mean_region_chamber, region) == 1210.125
-    assert normalization(map_test, NormMethod.median_region_anode, region) == 1200.0
-    assert normalization(map_test, NormMethod.median_region_chamber, region) == 1213.5
-
-
-
-
-def test_apply_3Dmap():
-
-    k_vals = np.arange(10)+1
-    i_vals = np.arange(12)+1
-    j_vals = np.arange(12)+1
-
-
-    k, i, j = map(np.ravel,
-                  np.meshgrid(k_vals, i_vals, j_vals, indexing= 'ij'))
-
-    x = i * 25
-    y = j * 25
-    dt = k * 45
-
-    mu = (k*i + 800)*j
-
-    map_test = pd.DataFrame({
-        'k': k,
-        'i': i,
-        'j': j,
-        'x': x,
-        'y': y,
-        'dt': dt,
-        'mu': mu
-    })
-
-
-    n = len(map_test)
-    dt = np.linspace(20, 1350, n)
-    x  = np.linspace(-450, 450, n)
-    y  = x
-    E = np.linspace(7e3, 9e3, n)
-    data_points_test = np.stack([dt, x, y], axis = 1)
-
-    Ec = apply_3Dmap(map_test, NormMethod.max, dt, x, y, E, keV = True)
-
-    assert len(Ec) == len(dt)
+    assert normalization(dummy_map, NormMethod.mean_region_anode, region) == 1200.0
+    assert normalization(dummy_map, NormMethod.mean_region_chamber, region) == 1210.125
+    assert normalization(dummy_map, NormMethod.median_region_anode, region) == 1200.0
+    assert normalization(dummy_map, NormMethod.median_region_chamber, region) == 1213.5
 
 
 
-def test_apply_3Dmap_same_values():
-    n = 10*12*12
-    dt = np.linspace(20, 1350, n)
-    x  = np.linspace(-450, 450, n)
-    y  = x
-    E = np.linspace(7e3, 9e3, n)
-    data_points_test = np.stack([dt, x, y], axis = 1)
 
+def test_apply_3Dmap(dummy_map):
 
-    k_vals = np.arange(10)
-    i_vals = np.arange(12)
-    j_vals = np.arange(12)
+    Ec = apply_3Dmap(dummy_map, NormMethod.max, dummy_map.dt, dummy_map.x, dummy_map.y, dummy_map.mu, keV = True)
 
-
-    k, i, j = map(np.ravel,
-                  np.meshgrid(k_vals, i_vals, j_vals, indexing= 'ij'))
-
-    x = i * 25
-    y = j * 25
-    dt = k * 45
-
-    mu = 8000
-
-    map_test = pd.DataFrame({
-        'k': k,
-        'i': i,
-        'j': j,
-        'x': x,
-        'y': y,
-        'dt': dt,
-        'mu': mu
-    })
-
-    Ec = apply_3Dmap(map_test, norm_method = NormMethod.max, dt = dt, x = x, y = y, E = E, keV = False)
-
-    map_test.loc[0, 'mu'] = 2*mu
-
-    Ec2 = apply_3Dmap(map_test, norm_method = NormMethod.max, dt = dt, x = x, y = y, E = E, keV = False)
-    assert (Ec == E).all()
-    assert (Ec2[1:] == 2*E[1:]).all()
+    assert len(Ec) == len(dummy_map)
 
 
 
-def test_apply_correctionmap_shape():
-    x = np.linspace(0, 10, 11)
+def test_apply_3Dmap_same_values(dummy_map):
 
-    kdst = pd.DataFrame({'X':x,
-                         'Y':-x,
-                         'DT': np.linspace(20, 1000, 11),
-                         'S2e': np.linspace(7500, 8500, 11)
+    dummy_map['mu'] = 8000
+
+    Ec = apply_3Dmap(dummy_map, norm_method = NormMethod.max, dt = dummy_map.dt, x = dummy_map.x, y = dummy_map.y, E = dummy_map.mu, keV = False)
+
+    assert (Ec == dummy_map.mu).all()
+
+def test_apply_3Dmap_one_different_value(dummy_map):
+
+    dummy_map['mu'] = 8000
+    dummy_map.loc[0, 'mu'] = 2*8000
+
+    Ec = apply_3Dmap(dummy_map, norm_method = NormMethod.max, dt =dummy_map.dt, x = dummy_map.x, y = dummy_map.y, E = dummy_map.mu, keV = False)
+
+    assert (Ec[1:] == 2*dummy_map.mu[1:]).all()
+
+
+
+def test_apply_correctionmap_shape(dummy_map):
+    dummy = np.linspace(0, 10, 11)
+
+    kdst = pd.DataFrame({'X':dummy,
+                         'Y':-dummy,
+                         'DT': dummy,
+                         'S2e': dummy
                          })
 
     kdst_test = kdst.copy()
 
-    map_3D = pd.DataFrame({'k':np.array([0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2]),
-                           'i':np.array([0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1]),
-                           'j':np.array([0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1]),
-                           'nevents': np.linspace(10000, 12000, 11, dtype=int),
-                           'mu' : np.linspace(7900, 8100, 11),
-                           'sigma' : np.linspace(10, 50, 11),
-                           'mu_error' : np.linspace(0, 10, 11),
-                           'sigma_error' : np.linspace(0, 2, 11),
-                           'dt': np.linspace(20, 1000, 11),
-                           'x': x,
-                           'y': -x
-                           })
-
-    kdst_correct = apply_correctionmap(kdst, map_3D, norm_method = NormMethod.max, xy_params = None, col_name='Ec')
+    kdst_correct = apply_correctionmap_inplace(kdst, dummy_map, norm_method = NormMethod.max, xy_params = None, col_name='Ec')
 
     assert kdst_correct.shape[1] ==  kdst_test.shape[1] + 1
     assert ((kdst_test == kdst_correct.drop(columns = 'Ec')).all()).all()
