a95dcbe51ccba162fda2c42a1681a4a97195eb81
mcidlaso <mariacidlaso1@gmail.com>
2026-02-23 10:30:18 +0100

    removed matplotlib import

--- invisible_cities/icaros/correction_functions_test.py
+++ invisible_cities/icaros/correction_functions_test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from invisible_cities.icaros.correction_functions import normalization, apply_3Dmap, apply_correctionmap_inplace
 from invisible_cities.core.core_functions import in_range
