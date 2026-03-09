ec46b221b1bbe7d82189b2d9d2638bf265545ed5
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-03-09 11:42:59 +0000

    Fix pandas change in groupby behaviour warning

--- invisible_cities/reco/hits_functions.py
+++ invisible_cities/reco/hits_functions.py
@@ -141,7 +141,7 @@ def merge_NN_hits(hits: pd.DataFrame, same_peak: bool = True) -> pd.DataFrame:
 
 
 def empty_hit( event : int  , timestamp: float, peak_no: int
-             , x_peak: float, y_peak   : float, z      : float
+             , x_peak: float, y_peak   : float
              , e     : float, ec       : float):
     """
     Produces an empty hit with NN x and y coordinates and NN charge.
@@ -154,7 +154,6 @@ def empty_hit( event : int  , timestamp: float, peak_no: int
                             , Ypeak    = y_peak
                             , X        = NN
                             , Y        = NN
-                            , Z        = z
                             , Q        = NN
                             , E        = e
                             , Ec       = ec
@@ -201,7 +200,7 @@ def apply_threshold(hits: pd.DataFrame, th: float) -> pd.DataFrame:
         first = hits.iloc[0]
         return empty_hit( first.event, first.time
                         , first.npeak, first.Xpeak, first.Ypeak
-                        , first.Z, raw_e_slice, cor_e_slice)
+                        , raw_e_slice, cor_e_slice)
 
     hits = hits.loc[mask_thresh].copy()
     qsum = np.nansum(hits.Q) + EPSILON
@@ -242,5 +241,7 @@ def threshold_hits(hits: pd.DataFrame, th: float) -> pd.DataFrame:
     - See `apply_threshold` for further details.
     """
     if th <= 0: return hits
-    return (hits.groupby("Z", as_index=False)
-                .apply(apply_threshold, th=th))
+    return (hits.groupby("Z")
+                .apply(apply_threshold, th=th, include_groups=False)
+                .reset_index(level=0, names="Z")
+                )
