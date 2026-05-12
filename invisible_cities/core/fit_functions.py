c6585669550fc86add202e2229233a8d3a7f5ce8
mcidlaso <mariacidlaso1@gmail.com>
2026-05-12 15:19:36 +0200

    Add sigmoid function

--- invisible_cities/core/fit_functions.py
+++ invisible_cities/core/fit_functions.py
@@ -131,6 +131,36 @@ def power(x, const, pow_):
     return const * np.power(x, pow_)
 
 
+def sigmoid(x          : np.array,
+            scale      : float,
+            inflection : float,
+            slope      : float,
+            offset     : float)->np.array:
+    '''
+    Sigmoid function, it computes the sigmoid of the input array x using the specified
+    parameters for scaling, inflection point, slope, and offset.
+    Parameters
+    ----------
+    x : np.array
+        The input array.
+    scale : float
+        The scaling factor determining the maximum value of the sigmoid function.
+    inflection : float
+        The x-value of the sigmoid's inflection point (where the function value is half of the scale).
+    slope : float
+        The slope parameter that controls the steepness of the sigmoid curve.
+    offset : float
+        The vertical offset added to the sigmoid function.
+    Returns
+    -------
+    np.array
+        Array of computed sigmoid values for x array.
+    '''
+    sigmoid = scale / (1 + np.exp(-slope * (x - inflection))) + offset
+    return sigmoid
+
+
+
 # ###########################################################
 # Tools
 def fit(func, x, y, seed=(), fit_range=None, **kwargs):
